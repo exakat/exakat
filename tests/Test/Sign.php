75ed011497e4a1f182e23b00be72bcc99ae81f34
Famille Seguy <famille@MacBookPro-2.local>
2013-06-08 10:59:54 +0800

    More test for Sign. Some are still failing, due to missing support for some operator (namely . and f()).

--- tests/Test/Sign.php
+++ tests/Test/Sign.php
@@ -7,12 +7,16 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Sign extends Tokenizeur {
-    /* 5 methods */
+    /* 10 methods */
     public function testSign01()  { $this->generic_test('Sign.01'); }
     public function testSign02()  { $this->generic_test('Sign.02'); }
     public function testSign03()  { $this->generic_test('Sign.03'); }
     public function testSign04()  { $this->generic_test('Sign.04'); }
     public function testSign05()  { $this->generic_test('Sign.05'); }
     public function testSign06()  { $this->generic_test('Sign.06'); }
+    public function testSign07()  { $this->generic_test('Sign.07'); }
+    public function testSign08()  { $this->generic_test('Sign.08'); }
+    public function testSign09()  { $this->generic_test('Sign.09'); }
+    public function testSign10()  { $this->generic_test('Sign.10'); }
 }
 ?>
