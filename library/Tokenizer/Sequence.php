fed5b56eef423fbf444fa7014c135df2d9640411
Famille Seguy <famille@MacBookPro-2.local>
2013-07-13 10:07:14 +0200

    Added continue to sequence.

--- library/Tokenizer/Sequence.php
+++ library/Tokenizer/Sequence.php
@@ -13,7 +13,7 @@ class Sequence extends TokenAuto {
                           'Var', 'Const', 'Ppp', 'Postplusplus', 'Preplusplus', 'Global', 'Nsname',
                           'Ifthen', 'Include', 'Function', 'Foreach', 'While', 'Arrayappend', 'Cast',
                           'Case', 'Default', 'Break', 'Goto', 'Label', 'Switch', 'Staticmethodcall',
-                          'Static', 
+                          'Static', 'Continue', 
                            );
         
         $yield_operator = array('T_ECHO', 'T_PRINT', 'T_DOT', 'T_AT', 'T_OBJECT_OPERATOR', 'T_BANG',
