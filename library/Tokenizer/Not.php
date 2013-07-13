ef0bf55e5f41e95a931a818e2a8f54ca70316e61
Famille Seguy <famille@MacBookPro-2.local>
2013-07-13 09:29:39 +0200

    Fixed priority between ! and [   ]

--- library/Tokenizer/Not.php
+++ library/Tokenizer/Not.php
@@ -7,7 +7,8 @@ class Not extends TokenAuto {
         $this->conditions = array(0 => array('token' => 'T_BANG',
                                              'atom' => 'none'),
                                   1 => array('atom' => 'yes'),
-                                  2 => array('filterOut' => array('T_OPEN_PARENTHESIS', 'T_OBJECT_OPERATOR', 'T_DOT', 'T_DOUBLE_COLON')),
+                                  2 => array('filterOut' => array('T_OPEN_PARENTHESIS', 'T_OBJECT_OPERATOR', 'T_DOT', 'T_DOUBLE_COLON',
+                                                                  'T_OPEN_BRACKET', 'T_OPEN_CURLY',)),
         );
         
         $this->actions = array('makeEdge'    => array( '1' => 'NOT'),
--- tests/Test/Not.php
+++ tests/Test/Not.php
@@ -7,9 +7,11 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Not extends Tokenizeur {
-    /* 3 methods */
+    /* 5 methods */
     public function testNot01()  { $this->generic_test('Not.01'); }
     public function testNot02()  { $this->generic_test('Not.02'); }
     public function testNot03()  { $this->generic_test('Not.03'); }
+    public function testNot04()  { $this->generic_test('Not.04'); }
+    public function testNot05()  { $this->generic_test('Not.05'); }
 }
 ?>
