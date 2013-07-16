5531ef36fad0b9759dcac8e115cd0ffc802ab5d0
Famille Seguy <famille@MacBookPro-2.local>
2013-07-16 22:50:55 +0200

    Fixed situation in Sequence.

--- tests/Test/Staticmethodcall.php
+++ tests/Test/Staticmethodcall.php
@@ -7,11 +7,12 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Staticmethodcall extends Tokenizeur {
-    /* 4 methods */
+    /* 5 methods */
 
     public function testStaticmethodcall01()  { $this->generic_test('Staticmethodcall.01'); }
     public function testStaticmethodcall02()  { $this->generic_test('Staticmethodcall.02'); }
     public function testStaticmethodcall03()  { $this->generic_test('Staticmethodcall.03'); }
     public function testStaticmethodcall04()  { $this->generic_test('Staticmethodcall.04'); }
+    public function testStaticmethodcall05()  { $this->generic_test('Staticmethodcall.05'); }
 }
 ?>
