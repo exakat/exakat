38d3da9b9b04cfb68d3fe9f05dea3134d11f9550
Famille Seguy <famille@MacBookPro-2.local>
2013-07-16 16:25:29 +0200

    Fixed priorities for Keyvalue with concat, additions, multiplications and object structures. Added related tests.

--- library/Tokenizer/Keyvalue.php
+++ library/Tokenizer/Keyvalue.php
@@ -6,13 +6,14 @@ class Keyvalue extends TokenAuto {
     function _check() {
     
     // @doc if then else
-        $this->conditions = array(-2 => array('filterOut' => array( 'T_NS_SEPARATOR')), 
+        $this->conditions = array(-2 => array('filterOut' => array_merge(array( 'T_NS_SEPARATOR', 'T_DOT', ),
+                                                                         Addition::$operators, Multiplication::$operators)), 
                                   -1 => array('atom' => 'yes'),
                                    0 => array('token' => 'T_DOUBLE_ARROW'),
                                    1 => array('atom' => 'yes'),
                                    2 => array('filterOut' => array_merge( Assignation::$operators, 
                                             array('T_OPEN_BRACKET', 'T_OBJECT_OPERATOR', 'T_INC', 'T_DEC', 'T_NS_SEPARATOR',
-                                                  'T_OPEN_PARENTHESIS', 'T_OPEN_CURLY', ))),
+                                                  'T_OPEN_PARENTHESIS', 'T_OPEN_CURLY', 'T_DOT', 'T_DOUBLE_COLON', ))),
         );
         
         /*
---
+++ tests/Test/Keyvalue.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Test;
+
+include_once(dirname(dirname(__DIR__)).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_test');
+spl_autoload_register('Autoload::autoload_phpunit');
+
+class Keyvalue extends Tokenizeur {
+    /* 2 methods */
+
+    public function testKeyvalue01()  { $this->generic_test('Keyvalue.01'); }
+    public function testKeyvalue02()  { $this->generic_test('Keyvalue.02'); }
+}
+?>
