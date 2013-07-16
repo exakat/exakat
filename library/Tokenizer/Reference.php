4c2947e222ca34b3f5a74b6b849e80d5bf3b05b6
Famille Seguy <famille@MacBookPro-2.local>
2013-07-17 00:12:39 +0200

    Reference have no priority over bracket or curly.

--- library/Tokenizer/Reference.php
+++ library/Tokenizer/Reference.php
@@ -7,7 +7,7 @@ class Reference extends TokenAuto {
         $this->conditions = array(0 => array('token' => 'T_AND',
                                              'atom' => 'none'),
                                   1 => array('atom' => array('Variable', 'Array', 'Property')),
-                                  2 => array('filterOut' => array('T_OPEN_PARENTHESIS')),
+                                  2 => array('filterOut' => array('T_OPEN_PARENTHESIS', 'T_OPEN_BRACKET', 'T_OPEN_CURLY', )),
         );
         
         $this->actions = array('makeEdge'    => array( '1' => 'REFERENCE'),
--- tests/Test/Reference.php
+++ tests/Test/Reference.php
@@ -7,10 +7,12 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Reference extends Tokenizeur {
-    /* 2 methods */
+    /* 4 methods */
     
     public function testReference01()  { $this->generic_test('Reference.01'); }
     public function testReference02()  { $this->generic_test('Reference.02'); }
 
-}
+}    public function testReference03()  { $this->generic_test('Reference.03'); }
+    public function testReference04()  { $this->generic_test('Reference.04'); }
+
 ?>
