b9d5869b0b4904b39104aad9713baf0b3ef33c26
Famille Seguy <famille@MacBookPro-2.local>
2013-07-16 19:31:44 +0200

    Removed parse error.

--- tests/Test/_Throw.php
+++ tests/Test/_Throw.php
@@ -10,7 +10,6 @@ class _Throw extends Tokenizeur {
     /* 4 methods */
 
     public function test_Throw01()  { $this->generic_test('_Throw.01'); }
-    public function test_Throw01()  { $this->generic_test('_Throw.01'); }
     public function test_Throw02()  { $this->generic_test('_Throw.02'); }
     public function test_Throw03()  { $this->generic_test('_Throw.03'); }
     public function test_Throw04()  { $this->generic_test('_Throw.04'); }
