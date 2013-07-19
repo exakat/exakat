5736f4142e5a36c5cd13192f0a39eddf055817b6
Famille Seguy <famille@MacBookPro-2.local>
2013-07-19 17:46:15 +0200

    Added test for empty script.

--- tests/Test/Phpcode.php
+++ tests/Test/Phpcode.php
@@ -7,11 +7,12 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Phpcode extends Tokenizeur {
-    /* 4 methods */
+    /* 5 methods */
 
     public function testPhpcode01()  { $this->generic_test('Phpcode.01'); }
     public function testPhpcode02()  { $this->generic_test('Phpcode.02'); }
     public function testPhpcode03()  { $this->generic_test('Phpcode.03'); }
     public function testPhpcode04()  { $this->generic_test('Phpcode.04'); }
+    public function testPhpcode05()  { $this->generic_test('Phpcode.05'); }
 }
 ?>
