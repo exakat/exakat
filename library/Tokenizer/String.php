5104f7480779ee58b755b999f815ff0c6ecca001
Famille Seguy <famille@MacBookPro-2.local>
2013-07-19 17:46:40 +0200

    Final cleaning.

--- library/Tokenizer/String.php
+++ library/Tokenizer/String.php
@@ -5,15 +5,6 @@ namespace Tokenizer;
 class String extends TokenAuto {
     function _check() {
 
-/*
-        $this->conditions = array( 0 => array('token' => array('T_STRING', 'T_CONSTANT_ENCAPSED_STRING', 'T_CONSTANT_ENCAPSED_STRING', 'T_ENCAPSED_AND_WHITESPACE'),
-                                               'atom' => 'none')
-                                 );
-        
-        $this->actions = array('atom'       => 'String',);
-        
-        $r =  $this->checkAuto();
-*/
 // Case of string with interpolation : "a${b}c";
         $this->conditions = array(  0 => array('token' => array('T_QUOTE'), 'atom' => 'none'),
                                     1 => array('atom'  => 'yes'),
