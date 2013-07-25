7b3b8a1b313bccecb7b722b81ff579d6fd25f579
Famille Seguy <famille@MacBookPro-2.local>
2013-07-26 00:00:49 +0800

    Wider range of argument in References.

--- library/Tokenizer/Reference.php
+++ library/Tokenizer/Reference.php
@@ -6,8 +6,9 @@ class Reference extends TokenAuto {
     function _check() {
         $this->conditions = array(0 => array('token' => 'T_AND',
                                              'atom' => 'none'),
-                                  1 => array('atom' => array('Variable', 'Array', 'Property')),
-                                  2 => array('filterOut' => array('T_OPEN_PARENTHESIS', 'T_OPEN_BRACKET', 'T_OPEN_CURLY', )),
+                                  1 => array('atom' => array('Variable', 'Array', 'Property', 'Functioncall', 'Methodcall', 'Staticmethodcall', 
+                                                             'Staticproperty', 'Staticconstant' )),
+                                  2 => array('filterOut' => array('T_OPEN_PARENTHESIS', 'T_OPEN_BRACKET', 'T_OPEN_CURLY', 'T_OBJECT_OPERATOR', 'T_DOUBLE_COLON', )),
         );
         
         $this->actions = array('makeEdge'    => array( '1' => 'REFERENCE'),
