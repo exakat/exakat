02b41fc6d34461b0708a78b9e0fec50142c5711c
Famille Seguy <famille@MacBookPro-2.local>
2013-07-26 00:01:02 +0800

    Wider range of arguments for reference.

--- tests/Test/Reference.php
+++ tests/Test/Reference.php
@@ -7,12 +7,19 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Reference extends Tokenizeur {
-    /* 4 methods */
+    /* 11 methods */
     
     public function testReference01()  { $this->generic_test('Reference.01'); }
     public function testReference02()  { $this->generic_test('Reference.02'); }
     public function testReference03()  { $this->generic_test('Reference.03'); }
     public function testReference04()  { $this->generic_test('Reference.04'); }
 
-}
+}    public function testReference05()  { $this->generic_test('Reference.05'); }
+    public function testReference06()  { $this->generic_test('Reference.06'); }
+    public function testReference07()  { $this->generic_test('Reference.07'); }
+    public function testReference08()  { $this->generic_test('Reference.08'); }
+    public function testReference09()  { $this->generic_test('Reference.09'); }
+    public function testReference10()  { $this->generic_test('Reference.10'); }
+    public function testReference11()  { $this->generic_test('Reference.11'); }
+
 ?>
