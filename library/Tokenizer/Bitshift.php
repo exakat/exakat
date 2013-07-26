d8338884f53de4b0b3c598cc230944828ddbcd64
Famille Seguy <famille@MacBookPro-2.local>
2013-07-26 22:48:54 +0800

    Added support for Bitshift.

---
+++ library/Tokenizer/Bitshift.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tokenizer;
+
+class Bitshift extends TokenAuto {
+    static public $operators = array('T_SR','T_SL');
+    
+    function _check() {
+        // note : Multiplication:: and Bitshift:: operators are the same! 
+        $this->conditions = array(-2 => array('filterOut' => array('T_OBJECT_OPERATOR', 'T_DOUBLE_COLON', 'T_DOT')),
+                                  -1 => array('atom' => Multiplication::$operands ),
+                                   0 => array('token' => Bitshift::$operators,
+                                             'atom' => 'none'),
+                                   1 => array('atom' => Multiplication::$operands),
+                                   2 => array('filterOut' => array('T_OPEN_PARENTHESIS', 'T_OPEN_CURLY', 'T_OPEN_BRACKET')),
+        );
+        
+        $this->actions = array('makeEdge'    => array('1' => 'RIGHT',
+                                                      '-1' => 'LEFT'
+                                                      ),
+                               'atom'       => 'Bitshift',
+                               );
+        return $this->checkAuto();
+    }
+}
+?>
---
+++ tests/Test/Bitshift.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Test;
+
+include_once(dirname(dirname(__DIR__)).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_test');
+spl_autoload_register('Autoload::autoload_phpunit');
+
+class Bitshift extends Tokenizeur {
+    /* 8 methods */
+
+    public function testBitshift01()  { $this->generic_test('Bitshift.01'); }
+    public function testBitshift02()  { $this->generic_test('Bitshift.02'); }
+    public function testBitshift03()  { $this->generic_test('Bitshift.03'); }
+    public function testBitshift06()  { $this->generic_test('Bitshift.06'); }
+    public function testBitshift07()  { $this->generic_test('Bitshift.07'); }
+    public function testBitshift08()  { $this->generic_test('Bitshift.08'); }
+}
+?>
