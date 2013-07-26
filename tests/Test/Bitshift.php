2af2aa831bc403e7bda7ff06f7145129998c9a46
Famille Seguy <famille@MacBookPro-2.local>
2013-07-27 01:48:09 +0800

    Added missing methods for test.

--- tests/Test/Bitshift.php
+++ tests/Test/Bitshift.php
@@ -12,6 +12,8 @@ class Bitshift extends Tokenizeur {
     public function testBitshift01()  { $this->generic_test('Bitshift.01'); }
     public function testBitshift02()  { $this->generic_test('Bitshift.02'); }
     public function testBitshift03()  { $this->generic_test('Bitshift.03'); }
+    public function testBitshift04()  { $this->generic_test('Bitshift.04'); }
+    public function testBitshift05()  { $this->generic_test('Bitshift.05'); }
     public function testBitshift06()  { $this->generic_test('Bitshift.06'); }
     public function testBitshift07()  { $this->generic_test('Bitshift.07'); }
     public function testBitshift08()  { $this->generic_test('Bitshift.08'); }
