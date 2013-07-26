b2fab912fad5cd8d2391fcf25a9d428acea84580
Famille Seguy <famille@MacBookPro-2.local>
2013-07-26 22:51:44 +0800

    list() is also recognized like a function.

--- library/Tokenizer/Parenthesis.php
+++ library/Tokenizer/Parenthesis.php
@@ -12,7 +12,7 @@ class Parenthesis extends TokenAuto {
                              'Methodcall', 'Staticmethodcall', 
                               );
 
-        $this->conditions = array(-1 => array('filterOut2' => array('T_STRING', 'T_CATCH', 'T_EXIT', 'T_FOR', 'T_SWITCH', 'T_WHILE', 'T_ECHO', 'T_UNSET', 'T_EMPTY', 'T_PRINT', 'T_VARIABLE', 'T_ISSET', 'T_ARRAY', 'T_EVAL', )), 
+        $this->conditions = array(-1 => array('filterOut2' => array('T_STRING', 'T_CATCH', 'T_EXIT', 'T_FOR', 'T_SWITCH', 'T_WHILE', 'T_ECHO', 'T_UNSET', 'T_EMPTY', 'T_PRINT', 'T_VARIABLE', 'T_ISSET', 'T_ARRAY', 'T_EVAL', 'T_LIST',  )), 
                                    0 => array('token' => 'T_OPEN_PARENTHESIS',
                                               'atom' => 'none' ),
                                    1 => array('atom' => $operands),
--- tests/Test/Parenthesis.php
+++ tests/Test/Parenthesis.php
@@ -7,7 +7,7 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Parenthesis extends Tokenizeur {
-    /* 8 methods */
+    /* 9 methods */
     public function testParenthesis01()  { $this->generic_test('Parenthesis.01'); }
     public function testParenthesis02()  { $this->generic_test('Parenthesis.02'); }
     public function testParenthesis03()  { $this->generic_test('Parenthesis.03'); }
@@ -16,5 +16,6 @@ class Parenthesis extends Tokenizeur {
     public function testParenthesis06()  { $this->generic_test('Parenthesis.06'); }
     public function testParenthesis07()  { $this->generic_test('Parenthesis.07'); }
     public function testParenthesis08()  { $this->generic_test('Parenthesis.08'); }
+    public function testParenthesis09()  { $this->generic_test('Parenthesis.09'); }
 }
 ?>
