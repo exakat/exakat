23edaeb989563a0c35692bd9207738dc0be5d066
Famille Seguy <famille@MacBookPro-2.local>
2013-07-27 11:05:08 +0800

    Assignation may be on right of a comparison.

--- library/Tokenizer/Comparison.php
+++ library/Tokenizer/Comparison.php
@@ -11,14 +11,13 @@ class Comparison extends TokenAuto {
                            'Magicconstant', 'Staticconstant', 'String', 'Addition', 'Multiplication',
                           'Nsname', 'Not', 'Reference', 'Parenthesis', 'Noscream', 'Preplusplus', 'Postplusplus',
                           'Bitshift',  );
-        //'Assignation','Assignation', 
         
         $this->conditions = array(-2 => array('filterOut' => array_merge(array('T_OBJECT_OPERATOR', 'T_DOUBLE_COLON'), 
                                                                          Addition::$operators, Bitshift::$operators, Multiplication::$operators)), 
                                   -1 => array('atom' => $operands ),
                                    0 => array('token' => Comparison::$operators,
                                               'atom' => 'none'),
-                                   1 => array('atom' => $operands),
+                                   1 => array('atom' => array_merge($operands, array('Assignation'))),
                                    2 => array('filterOut' => array_merge(array('T_OPEN_PARENTHESIS', 'T_OPEN_BRACKET', 'T_OPEN_CURLY'), 
                                                            Addition::$operators, Multiplication::$operators )),
         );
