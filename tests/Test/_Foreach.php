bd8308584cc0466b25c3c4a1cf7f969a6d47e766
Famille Seguy <famille@MacBookPro-2.local>
2013-07-28 18:16:06 +0800

    Added extra special situations, that compiles but won't run. :(

--- tests/Test/_Foreach.php
+++ tests/Test/_Foreach.php
@@ -7,7 +7,7 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class _Foreach extends Tokenizeur {
-    /* 8 methods */
+    /* 12 methods */
 
     public function test_Foreach01()  { $this->generic_test('_Foreach.01'); }
     public function test_Foreach02()  { $this->generic_test('_Foreach.02'); }
@@ -17,5 +17,9 @@ class _Foreach extends Tokenizeur {
     public function test_Foreach06()  { $this->generic_test('_Foreach.06'); }
     public function test_Foreach07()  { $this->generic_test('_Foreach.07'); }
     public function test_Foreach08()  { $this->generic_test('_Foreach.08'); }
+    public function test_Foreach09()  { $this->generic_test('_Foreach.09'); }
+    public function test_Foreach10()  { $this->generic_test('_Foreach.10'); }
+    public function test_Foreach11()  { $this->generic_test('_Foreach.11'); }
+    public function test_Foreach12()  { $this->generic_test('_Foreach.12'); }
 }
 ?>
---
+++ tests/source/_Foreach.11.php
@@ -0,0 +1,4 @@
+<?php
+    // Compiles but will raise an error !
+    foreach(!(is_array($a) ? $b : explode('d', $d)) as $e) {}
+?>
