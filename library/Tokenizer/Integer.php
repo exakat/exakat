db3a2ae7cefb34338608e2cc5e4a16e05fb28f12
Famille Seguy <famille@MacBookPro-2.local>
2013-07-30 09:09:10 +0800

    Removed the whole code (it was comment). Kept the class for configuration.

--- library/Tokenizer/Integer.php
+++ library/Tokenizer/Integer.php
@@ -4,15 +4,7 @@ namespace Tokenizer;
 
 class Integer extends TokenAuto {
     function _check() {
-/*
-        $this->conditions = array( 0 => array('token' => array('T_LNUMBER', 'T_NUM_STRING'),
-                                               'atom' => 'none')
-                                 );
-        
-        $this->actions = array('atom'       => 'Integer',
-                               );
-        
-        return $this->checkAuto();*/
+        return false;
     }
 }
 
