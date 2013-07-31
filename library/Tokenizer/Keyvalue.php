39a0036fd2f7c7f0b5966bfc1d65ec852c71540c
Famille Seguy <famille@MacBookPro-2.local>
2013-07-31 09:10:52 +0800

    Reverting to atom yes for key values. They may be anything, really.

--- library/Tokenizer/Keyvalue.php
+++ library/Tokenizer/Keyvalue.php
@@ -6,12 +6,11 @@ class Keyvalue extends TokenAuto {
     static public $operators = array('T_DOUBLE_ARROW');
 
     function _check() {
-        $operands = array('Variable', 'Reference', 'Property', 'Staticproperty', 'Array');
         $this->conditions = array(-2 => array('filterOut' => array_merge(array( 'T_NS_SEPARATOR', 'T_DOT', ),
                                                                          Addition::$operators, Multiplication::$operators, Comparison::$operators)), 
-                                  -1 => array('atom' => array('Variable', 'Property', 'Staticproperty', 'Array')),
+                                  -1 => array('atom' => 'yes'),
                                    0 => array('token' => Keyvalue::$operators),
-                                   1 => array('atom' => $operands),
+                                   1 => array('atom' => 'yes'),
                                    2 => array('filterOut' => array_merge( Assignation::$operators, Addition::$operators, Multiplication::$operators, Comparison::$operators, 
                                             array('T_OPEN_BRACKET', 'T_OBJECT_OPERATOR', 'T_INC', 'T_DEC', 'T_NS_SEPARATOR',
                                                   'T_OPEN_PARENTHESIS', 'T_OPEN_CURLY', 'T_DOT', 'T_DOUBLE_COLON', ))));
