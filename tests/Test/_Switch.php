bdfcad80934b487a68cc078e106403e73e3a7628
Famille Seguy <famille@MacBookPro-2.local>
2013-08-05 17:04:30 +0800

    Extra tests for Switch.

--- tests/Test/_Switch.php
+++ tests/Test/_Switch.php
@@ -7,7 +7,7 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class _Switch extends Tokenizeur {
-    /* 16 methods */
+    /* 17 methods */
 
     public function test_Switch01()  { $this->generic_test('_Switch.01'); }
     public function test_Switch02()  { $this->generic_test('_Switch.02'); }
@@ -25,5 +25,6 @@ class _Switch extends Tokenizeur {
     public function test_Switch14()  { $this->generic_test('_Switch.14'); }
     public function test_Switch15()  { $this->generic_test('_Switch.15'); }
     public function test_Switch16()  { $this->generic_test('_Switch.16'); }
+    public function test_Switch17()  { $this->generic_test('_Switch.17'); }
 }
 ?>
