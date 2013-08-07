b929c159b9aeadcc0b51bbe26823a6f4cb0b4567
Famille Seguy <famille@MacBookPro-2.local>
2013-08-07 09:49:36 +0800

    Now also tests for remaining Indexed element.

--- tests/Test/Tokenizeur.php
+++ tests/Test/Tokenizeur.php
@@ -14,12 +14,16 @@ class Tokenizeur extends \PHPUnit_Framework_TestCase {
     function generic_test($file) {
         $shell = 'cd ..; php bin/load -f tests/source/'.$file.'.php; php bin/analyzer; php bin/export -text -f tests/source/'.$file.'';
         $res = shell_exec($shell);
-        // @todo remove this hack : no need to restart neo4j for a test! 
         
         $exp = file_get_contents('exp/'.$file.'.txt');
         $this->assertNotContains("Label : NEXT", $exp);
         
         $this->assertEquals($exp, $res);
+
+        $shell = 'cd ..; php bin/stat -json';
+        $decode = json_decode(shell_exec($shell));
+        
+        $this->assertEquals($decode->INDEXED_count, 0, 'There are '.$decode->INDEXED_count.' INDEXED left');
     }
     
     
