4b0c6cb318ddecda6d128ab2c51f4c5da0f67d62
Famille Seguy <famille@MacBookPro-2.local>
2013-08-07 09:48:58 +0800

    Constant code is no more useful.

--- library/Tokenizer/Constant.php
+++ library/Tokenizer/Constant.php
@@ -6,16 +6,7 @@ class Constant extends TokenAuto {
     static public $operators = array('T_CONSTANT_ENCAPSED_STRING', 'T_STRING');
 
     function _check() {
-
-        $this->conditions = array( 0 => array('token' => Constant::$operators,
-                                               'atom' => 'none'),
-                                   1 => array('filterOut' => array('T_OPEN_PARENTHESIS')),
-                                 );
-        
-        $this->actions = array('atom'       => 'Constant');
-        $this->checkAuto();
-
-        return $this->checkRemaining();
+        return 0;
     }
 }
 
