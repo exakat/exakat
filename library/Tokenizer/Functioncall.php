f451f69c8f73e4932d8a2e543020f5087e9708b2
Famille Seguy <famille@MacBookPro-2.local>
2013-08-07 09:46:22 +0800

    Clean code.

--- library/Tokenizer/Functioncall.php
+++ library/Tokenizer/Functioncall.php
@@ -12,32 +12,30 @@ class Functioncall extends TokenAuto {
         // functioncall(with arguments or void)
         $this->conditions = array(  -2 => array('filterOut' => array('T_FUNCTION')),
                                     -1 => array('filterOut' => array('T_FUNCTION', 'T_NS_SEPARATOR')),
-                                    0 => array('token' => Functioncall::$operators),
-                                    1 => array('atom'  => 'none',
-                                               'token' => 'T_OPEN_PARENTHESIS' ),
-                                    2 => array('atom'  =>  array('Arguments', 'Void')),
-                                    3 => array('atom'  => 'none',
-                                               'token' => 'T_CLOSE_PARENTHESIS' ),
+                                     0 => array('token' => Functioncall::$operators),
+                                     1 => array('atom'  => 'none',
+                                                'token' => 'T_OPEN_PARENTHESIS' ),
+                                     2 => array('atom'  =>  array('Arguments', 'Void')),
+                                     3 => array('atom'  => 'none',
+                                                'token' => 'T_CLOSE_PARENTHESIS'),
         );
         
-        $this->actions = array('makeEdge'    => array('2' => 'ARGUMENTS',),
+        $this->actions = array('makeEdge'   => array(2 => 'ARGUMENTS'),
                                'dropNext'   => array(1),
-                               'atom'       => 'Functioncall',
-                               );
+                               'atom'       => 'Functioncall');
                                
         $this->checkAuto();
 
         // functioncall(with arguments)
         $this->conditions = array( 0 => array('token' => array('T_ECHO', 'T_PRINT', 'T_EXIT'),
-                                              'atom' => 'none'),
-                                   1 => array('atom' => 'Arguments'),
+                                              'atom'  => 'none'),
+                                   1 => array('atom'  => 'Arguments'),
                                    2 => array('filterOut2' => array_merge( array('T_OBJECT_OPERATOR', 'T_DOUBLECOLON', 'T_COMMA', 'T_QUESTION'),
                                                                            Addition::$operators, Multiplication::$operators)),
         );
         
-        $this->actions = array('makeEdge'    => array('1' => 'ARGUMENTS',),
-                               'atom'       => 'Functioncall',
-                               );
+        $this->actions = array('makeEdge'   => array('1' => 'ARGUMENTS'),
+                               'atom'       => 'Functioncall');
         $this->checkAuto();
         
         return $this->checkRemaining();
