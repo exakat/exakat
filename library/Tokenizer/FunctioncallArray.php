22b45dfdb3748967b5838cd0146064a6bea4025f
Famille Seguy <famille@MacBookPro-2.local>
2013-08-08 20:09:32 +0800

    Special regex that process $X[w]()

---
+++ library/Tokenizer/FunctioncallArray.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tokenizer;
+
+class FunctioncallArray extends TokenAuto {
+    static $operators = array();
+
+    function _check() {
+        
+        // $x[3]()
+        $this->conditions = array(   0 => array('atom'  => 'Array'),
+                                     1 => array('atom'  => 'none',
+                                                'token' => 'T_OPEN_PARENTHESIS' ),
+                                     2 => array('atom'  =>  array('Arguments', 'Void')),
+                                     3 => array('atom'  => 'none',
+                                                'token' => 'T_CLOSE_PARENTHESIS'),
+        );
+
+        $this->actions = array('transform'   => array(1 => 'DROP',
+                                                      2 => 'ARGUMENTS',
+                                                      3 => 'DROP'),
+                               'atom'        => 'Functioncall' );
+        $this->checkAuto();
+        
+        return $this->checkRemaining();
+    }
+}
+?>
