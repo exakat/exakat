04f7ddab8f63c8a05c5fed96fe4f2b0187a6041a
Famille Seguy <famille@MacBookPro-2.local>
2013-08-08 14:59:34 +0800

    Parenthesis may accept casted values.

--- library/Tokenizer/Parenthesis.php
+++ library/Tokenizer/Parenthesis.php
@@ -11,7 +11,7 @@ class Parenthesis extends TokenAuto {
                              'Functioncall', 'Boolean', 'Comparison', 'Parenthesis', 'Constant', 'Array',
                              'Instanceof', 'Noscream', 'Magicconstant', 'Logical', 'Ternary',
                              'Assignation', 'Property', 'Staticproperty', 'Staticconstant',
-                             'Methodcall', 'Staticmethodcall', 'Bitshift', 
+                             'Methodcall', 'Staticmethodcall', 'Bitshift', 'Cast', 
                               );
 
         $this->conditions = array(-1 => array('filterOut2' => array('T_STRING', 'T_CATCH', 'T_EXIT', 'T_FOR', 'T_SWITCH', 'T_WHILE', 'T_ECHO', 'T_UNSET', 'T_EMPTY', 'T_PRINT', 'T_VARIABLE', 'T_ISSET', 'T_ARRAY', 'T_EVAL', 'T_LIST', 'T_CLONE', 'T_DECLARE', )), 
