cff71ae5a2dd4f5baa3f0fbb12e5094eec7c42f3
Famille Seguy <famille@MacBookPro-2.local>
2013-08-08 17:59:59 +0800

    Recent version of PHP allows the calling of a method extracted from an array.

--- tests/Test/Functioncall.php
+++ tests/Test/Functioncall.php
@@ -7,50 +7,8 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Functioncall extends Tokenizeur {
-    /* 43 methods */
+    /* 44 methods */
 
-    public function testFunctioncall01()  { $this->generic_test('Functioncall.01'); }
-    public function testFunctioncall02()  { $this->generic_test('Functioncall.02'); }
-    public function testFunctioncall03()  { $this->generic_test('Functioncall.03'); }
-    public function testFunctioncall04()  { $this->generic_test('Functioncall.04'); }
-    public function testFunctioncall05()  { $this->generic_test('Functioncall.05'); }
-    public function testFunctioncall06()  { $this->generic_test('Functioncall.06'); }
-    public function testFunctioncall07()  { $this->generic_test('Functioncall.07'); }
-    public function testFunctioncall08()  { $this->generic_test('Functioncall.08'); }
-    public function testFunctioncall09()  { $this->generic_test('Functioncall.09'); }
-    public function testFunctioncall10()  { $this->generic_test('Functioncall.10'); }
-    public function testFunctioncall11()  { $this->generic_test('Functioncall.11'); }
-    public function testFunctioncall12()  { $this->generic_test('Functioncall.12'); }
-    public function testFunctioncall13()  { $this->generic_test('Functioncall.13'); }
-    public function testFunctioncall14()  { $this->generic_test('Functioncall.14'); }
-    public function testFunctioncall15()  { $this->generic_test('Functioncall.15'); }
-    public function testFunctioncall16()  { $this->generic_test('Functioncall.16'); }
-    public function testFunctioncall17()  { $this->generic_test('Functioncall.17'); }
-    public function testFunctioncall18()  { $this->generic_test('Functioncall.18'); }
-    public function testFunctioncall19()  { $this->generic_test('Functioncall.19'); }
-    public function testFunctioncall20()  { $this->generic_test('Functioncall.20'); }
-    public function testFunctioncall21()  { $this->generic_test('Functioncall.21'); }
-    public function testFunctioncall22()  { $this->generic_test('Functioncall.22'); }
-    public function testFunctioncall23()  { $this->generic_test('Functioncall.23'); }
-    public function testFunctioncall24()  { $this->generic_test('Functioncall.24'); }
-    public function testFunctioncall25()  { $this->generic_test('Functioncall.25'); }
-    public function testFunctioncall26()  { $this->generic_test('Functioncall.26'); }
-    public function testFunctioncall27()  { $this->generic_test('Functioncall.27'); }
-    public function testFunctioncall28()  { $this->generic_test('Functioncall.28'); }
-    public function testFunctioncall29()  { $this->generic_test('Functioncall.29'); }
-    public function testFunctioncall30()  { $this->generic_test('Functioncall.30'); }
-    public function testFunctioncall31()  { $this->generic_test('Functioncall.31'); }
-    public function testFunctioncall32()  { $this->generic_test('Functioncall.32'); }
-    public function testFunctioncall33()  { $this->generic_test('Functioncall.33'); }
-    public function testFunctioncall34()  { $this->generic_test('Functioncall.34'); }
-    public function testFunctioncall35()  { $this->generic_test('Functioncall.35'); }
-    public function testFunctioncall36()  { $this->generic_test('Functioncall.36'); }
-    public function testFunctioncall37()  { $this->generic_test('Functioncall.37'); }
-    public function testFunctioncall38()  { $this->generic_test('Functioncall.38'); }
-    public function testFunctioncall39()  { $this->generic_test('Functioncall.39'); }
-    public function testFunctioncall40()  { $this->generic_test('Functioncall.40'); }
-    public function testFunctioncall41()  { $this->generic_test('Functioncall.41'); }
-    public function testFunctioncall42()  { $this->generic_test('Functioncall.42'); }
-    public function testFunctioncall43()  { $this->generic_test('Functioncall.43'); }
+    public function testFunctioncall44()  { $this->generic_test('Functioncall.44'); }
 }
 ?>
