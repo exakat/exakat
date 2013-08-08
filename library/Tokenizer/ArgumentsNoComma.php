3c127f532920ffe764ec38a805ae5cf397b96a77
Famille Seguy <famille@MacBookPro-2.local>
2013-08-08 20:10:05 +0800

    () is transfered to load.

--- library/Tokenizer/ArgumentsNoComma.php
+++ library/Tokenizer/ArgumentsNoComma.php
@@ -18,20 +18,6 @@ class ArgumentsNoComma extends TokenAuto {
 
         $this->actions = array('insertEdge'   => array(0 => array('Arguments' => 'ARGUMENT')));
         $this->checkAuto();
-        
-        // @note f() : no argument
-        $this->conditions = array(-2 => array('filterOut' => array('T_NS_SEPARATOR')),
-                                  -1 => array('token' => array('T_STRING', 'T_ECHO', 'T_UNSET', 'T_EVAL', 'T_PRINT', 'T_ARRAY', 'T_VARIABLE', 'T_NS_SEPARATOR')),
-                                   0 => array('token' => 'T_OPEN_PARENTHESIS',
-                                              'atom' => 'none'),
-                                   1 => array('token' => 'T_CLOSE_PARENTHESIS',
-                                              'atom'  => 'none'),
-                                   2 => array('filterOut' => array('T_OBJECT_OPERATOR', 'T_DOUBLECOLON')),
-        );
-        
-        $this->actions = array('addEdge'   => array(0 => array('Arguments' => 'ARGUMENT')),
-                               'cleanIndex' => true);
-        $this->checkAuto();
 
         return $this->checkRemaining();
     }
