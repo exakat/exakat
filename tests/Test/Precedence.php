214d60c0859847b954d2b23b1a839f55e884323b
Famille Seguy <famille@MacBookPro-2.local>
2013-08-08 09:46:06 +0800

    Added specific tests for Precedence of operators.

---
+++ tests/Test/Precedence.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Test;
+
+include_once(dirname(dirname(__DIR__)).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_test');
+spl_autoload_register('Autoload::autoload_phpunit');
+
+class Precedence extends Tokenizeur {
+    /* 7 methods */
+
+    public function testPrecedence01()  { $this->generic_test('Precedence.01'); }
+    public function testPrecedence02()  { $this->generic_test('Precedence.02'); }
+    public function testPrecedence03()  { $this->generic_test('Precedence.03'); }
+    public function testPrecedence04()  { $this->generic_test('Precedence.04'); }
+    public function testPrecedence05()  { $this->generic_test('Precedence.05'); }
+    public function testPrecedence06()  { $this->generic_test('Precedence.06'); }
+    public function testPrecedence07()  { $this->generic_test('Precedence.07'); }
+}
+?>
---
+++ tests/source/Precedence.04.php
@@ -0,0 +1,10 @@
+<?php
+
+    $a = 1;
+    $b = 2;
+    print $a++.$b; // ($a++) . $b
+    print "\n";
+    print $a . $b++; // $a . ($b++)
+    print "\n";
+
+?>
