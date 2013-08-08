e055fcd1d225f19bc2b15b57b46baf033c8c6c8a
Famille Seguy <famille@MacBookPro-2.local>
2013-08-08 09:47:33 +0800

    Two situations where References where confused with Logical.

--- library/Tokenizer/Reference.php
+++ library/Tokenizer/Reference.php
@@ -6,11 +6,12 @@ class Reference extends TokenAuto {
     static public $operators = array('T_AND');
 
     function _check() {
-        $this->conditions = array(0 => array('token' => Reference::$operators,
-                                             'atom' => 'none'),
-                                  1 => array('atom' => array('Variable', 'Array', 'Property', 'Functioncall', 'Methodcall', 'Staticmethodcall', 
-                                                             'Staticproperty', 'Staticconstant', 'New',  )),
-                                  2 => array('filterOut' => array('T_OPEN_PARENTHESIS', 'T_OPEN_BRACKET', 'T_OPEN_CURLY', 'T_OBJECT_OPERATOR', 'T_DOUBLE_COLON', )),
+        $this->conditions = array(-1 => array('filterOut2' => array_merge(Logical::$operators, array('T_VARIABLE', 'T_LNUMBER', 'T_DNUMBER', 'T_MINUS', 'T_PLUS', 'T_CLOSE_PARENTHESIS' ))),
+                                   0 => array('token' => Reference::$operators,
+                                              'atom' => 'none'),
+                                   1 => array('atom' => array('Variable', 'Array', 'Property', 'Functioncall', 'Methodcall', 'Staticmethodcall', 
+                                                              'Staticproperty', 'Staticconstant', 'New',  )),
+                                   2 => array('filterOut' => array('T_OPEN_PARENTHESIS', 'T_OPEN_BRACKET', 'T_OPEN_CURLY', 'T_OBJECT_OPERATOR', 'T_DOUBLE_COLON', )),
         );
         
         $this->actions = array('makeEdge'    => array( 1 => 'REFERENCE'),
@@ -24,8 +25,7 @@ class Reference extends TokenAuto {
                                   1 => array('atom' => 'String'),
                                   2 => array('token' => 'T_OPEN_PARENTHESIS'),
                                   3 => array('atom' => 'Arguments'),
-                                  4 => array('token' => 'T_CLOSE_PARENTHESIS'),
-//                                  5 => array('atom' => 'Block'),
+                                  4 => array('token' => 'T_CLOSE_PARENTHESIS')
         );
         
         $this->actions = array('transform'   => array( 1 => 'REFERENCE'),
