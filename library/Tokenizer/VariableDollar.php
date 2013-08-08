50d248ae0c810dd46e80c40afe8bed19ef8ce5ec
Famille Seguy <famille@MacBookPro-2.local>
2013-08-08 10:35:19 +0800

    Fixed naming convention. Clean index.

--- library/Tokenizer/VariableDollar.php
+++ library/Tokenizer/VariableDollar.php
@@ -2,31 +2,33 @@
 
 namespace Tokenizer;
 
-class Variabledollar extends TokenAuto {
+class VariableDollar extends TokenAuto {
     static public $operators = array('T_DOLLAR');
     
     function _check() {
-        $this->conditions = array(0 => array('token' => 'T_DOLLAR',
+        $this->conditions = array(0 => array('token' => VariableDollar::$operators,
                                              'atom' => 'none'),
                                   1 => array('atom' => 'yes'),
                                   2 => array('filterOut' => array('T_OPEN_BRACKET')),
         );
         
-        $this->actions = array( 'transform' => array('1' => 'NAME'),
-                                'atom'      => 'Variable');
+        $this->actions = array( 'transform'  => array(1 => 'NAME'),
+                                'atom'       => 'Variable',
+                                'cleanIndex' => true);
         $this->checkAuto();
 
-        $this->conditions = array(0 => array('token' => 'T_DOLLAR',
+        $this->conditions = array(0 => array('token' => VariableDollar::$operators,
                                              'atom' => 'none'),
                                   1 => array('token' => 'T_OPEN_CURLY'),
                                   2 => array('atom' => 'yes'),
                                   3 => array('token' => 'T_CLOSE_CURLY'),
         );
         
-        $this->actions = array( 'transform' => array(1 => 'DROP',
-                                                     2 => 'NAME',
-                                                     3 => 'DROP'),
-                                'atom'      => 'Variable');
+        $this->actions = array( 'transform'  => array(1 => 'DROP',
+                                                      2 => 'NAME',
+                                                      3 => 'DROP'),
+                                'atom'       => 'Variable',
+                                'cleanIndex' => true);
         $this->checkAuto();
         
         return $this->checkRemaining();
