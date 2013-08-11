8924027838a6f61acee25f5003b08688aa2a670d
Famille Seguy <famille@MacBookPro-2.local>
2013-08-11 22:11:02 +0800

    Test for method call where the object is a function call.

--- tests/Test/Methodcall.php
+++ tests/Test/Methodcall.php
@@ -7,11 +7,12 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Methodcall extends Tokenizeur {
-    /* 4 methods */
+    /* 5 methods */
 
     public function testMethodcall01()  { $this->generic_test('Methodcall.01'); }
     public function testMethodcall02()  { $this->generic_test('Methodcall.02'); }
     public function testMethodcall03()  { $this->generic_test('Methodcall.03'); }
     public function testMethodcall04()  { $this->generic_test('Methodcall.04'); }
+    public function testMethodcall05()  { $this->generic_test('Methodcall.05'); }
 }
 ?>
