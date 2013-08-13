578b88754822d3ca4681ca81a9b2e83f27be91fb
Famille Seguy <famille@MacBookPro-2.local>
2013-08-13 09:17:19 +0800

    Anonymized example.

--- tests/source/Variable.12.php
+++ tests/source/Variable.12.php
@@ -1,11 +1,8 @@
 <?php
 	$result = <<<EOF
+A${$b['c']?'true':'false'}
 
-#Allow smarty {php} tags?  These could be dangerous if you don't trust your users.
-\$config['use_smarty_php_tags'] = ${$config['use_smarty_php_tags']?'true':'false'};
-
-#Automatically assign alias based on page title?
-\$config['auto_alias_content'] = ${$config['auto_alias_content']?'true':'false'};
+D${$e['f']?'true':'false'}
 
 EOF;
 
