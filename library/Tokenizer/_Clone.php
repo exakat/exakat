2bcc039bb105dc0ccf6374502c20f69fcf67192b
Famille Seguy <famille@MacBookPro-2.local>
2013-08-14 14:05:39 +0800

    Clone respects functions.

--- library/Tokenizer/_Clone.php
+++ library/Tokenizer/_Clone.php
@@ -11,7 +11,8 @@ class _Clone extends TokenAuto {
         $this->conditions = array(0 => array('token' => _Clone::$operators,
                                              'atom' => 'none'),
                                   1 => array('atom' =>  $operands),
-                                  2 => array('filterOut' => array('T_OPEN_BRACKET', 'T_OPEN_CURLY', 'T_DOUBLE_COLON', 'T_OBJECT_OPERATOR', 'T_DOT', )),
+                                  2 => array('filterOut' => array('T_OPEN_BRACKET', 'T_OPEN_CURLY', 'T_DOUBLE_COLON', 'T_OBJECT_OPERATOR', 'T_DOT',
+                                                                  'T_OPEN_PARENTHESIS',  )),
         );
         
         $this->actions = array('transform'  => array( 1 => 'CLONE'),
