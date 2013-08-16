55349c1cf7be8decd400108d406d169aeaa2bd58
Famille Seguy <famille@MacBookPro-2.local>
2013-08-16 20:00:29 +0800

    Sequence update collateral damages.

--- library/Tokenizer/Methodcall.php
+++ library/Tokenizer/Methodcall.php
@@ -25,17 +25,21 @@ class Methodcall extends TokenAuto {
         $this->conditions = array( -2 => array('filterOut' => array('T_DOUBLE_COLON')),
                                    -1 => array('atom'      => $operands), 
                                     0 => array('token'     => Methodcall::$operators),
-                                    1 => array('atom'      => 'Block'),
-                                    2 => array('token'     => 'T_OPEN_PARENTHESIS'),
-                                    3 => array('atom'      => array('Arguments', 'Void')),
-                                    4 => array('token'     => 'T_CLOSE_PARENTHESIS')
+                                    1 => array('token'     => 'T_OPEN_CURLY'),
+                                    2 => array('atom'      => 'yes'),
+                                    3 => array('token'     => 'T_CLOSE_CURLY'),
+                                    4 => array('token'     => 'T_OPEN_PARENTHESIS'),
+                                    5 => array('atom'      => array('Arguments', 'Void')),
+                                    6 => array('token'     => 'T_CLOSE_PARENTHESIS')
                                  );
         
         $this->actions = array('transform'   => array( -1 => 'OBJECT',
-                                                        1 => 'METHOD',
-                                                        2 => 'DROP',
-                                                        3 => 'ARGUMENTS',
-                                                        4 => 'DROP'),
+                                                        1 => 'DROP',
+                                                        2 => 'METHOD',
+                                                        3 => 'DROP',
+                                                        4 => 'DROP',
+                                                        5 => 'ARGUMENTS',
+                                                        6 => 'DROP'),
                                'atom'       => 'Methodcall',
                                'cleanIndex' => true);
         $this->checkAuto(); 
--- library/Tokenizer/Property.php
+++ library/Tokenizer/Property.php
@@ -9,12 +9,28 @@ class Property extends TokenAuto {
         $operands = array('Variable', 'Property', 'Array', 'Staticmethodcall', 'Methodcall', 'Functioncall');
         $this->conditions = array( -1 => array('atom' => $operands), 
                                     0 => array('token' => Property::$operators),
-                                    1 => array('atom' => array('String', 'Variable', 'Block')),
+                                    1 => array('atom' => array('String', 'Variable')),
                                     2 => array('filterOut' => array('T_OPEN_PARENTHESIS')),
                                     );
         
         $this->actions = array('makeEdge'   => array( -1 => 'OBJECT',
                                                        1 => 'PROPERTY'),
+                               'atom'       => 'Property',
+                               'cleanIndex' => true);
+        $this->checkAuto(); 
+
+        $this->conditions = array( -1 => array('atom'  => $operands), 
+                                    0 => array('token' => Property::$operators),
+                                    1 => array('token' => 'T_OPEN_CURLY'),
+                                    2 => array('atom'  => 'yes'),
+                                    3 => array('token' => 'T_CLOSE_CURLY'),
+                                    4 => array('filterOut' => array('T_OPEN_PARENTHESIS')),
+                                    );
+        
+        $this->actions = array('transform'   => array( -1 => 'OBJECT',
+                                                        1 => 'DROP',
+                                                        2 => 'PROPERTY',
+                                                        3 => 'DROP'),
                                'atom'       => 'Property',
                                'cleanIndex' => true);
         $this->checkAuto(); 
