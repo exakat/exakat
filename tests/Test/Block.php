2c54b1fb420c338717ae8632c9ccede148ab7053
Famille Seguy <famille@MacBookPro-2.local>
2013-08-16 11:17:40 +0800

    Extra tests for Block and array structure.

--- tests/Test/Block.php
+++ tests/Test/Block.php
@@ -7,12 +7,13 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Block extends Tokenizeur {
-    /* 5 methods */
+    /* 6 methods */
 
     public function testBlock01()  { $this->generic_test('Block.01'); }
     public function testBlock02()  { $this->generic_test('Block.02'); }
     public function testBlock03()  { $this->generic_test('Block.03'); }
     public function testBlock04()  { $this->generic_test('Block.04'); }
     public function testBlock05()  { $this->generic_test('Block.05'); }
+    public function testBlock06()  { $this->generic_test('Block.06'); }
 }
 ?>
