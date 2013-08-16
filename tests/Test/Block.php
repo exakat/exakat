41ecbb53705605c3950a49efa2dbc0d0e8b80995
Famille Seguy <famille@MacBookPro-2.local>
2013-08-16 11:16:30 +0800

    BLock yields to Array-prone structure that are left before.

--- tests/Test/Block.php
+++ tests/Test/Block.php
@@ -7,11 +7,12 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Block extends Tokenizeur {
-    /* 4 methods */
+    /* 5 methods */
 
     public function testBlock01()  { $this->generic_test('Block.01'); }
     public function testBlock02()  { $this->generic_test('Block.02'); }
     public function testBlock03()  { $this->generic_test('Block.03'); }
     public function testBlock04()  { $this->generic_test('Block.04'); }
+    public function testBlock05()  { $this->generic_test('Block.05'); }
 }
 ?>
