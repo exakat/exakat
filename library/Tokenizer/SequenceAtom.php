e213717e15252fd9cd6ba68dbd3c1883a9eb5498
Famille Seguy <famille@MacBookPro-2.local>
2013-08-18 11:39:29 +0800

    Cleaned the code, and fixed a bug with RawString

--- library/Tokenizer/SequenceAtom.php
+++ library/Tokenizer/SequenceAtom.php
@@ -4,21 +4,13 @@ namespace Tokenizer;
 
 class SequenceAtom extends TokenAuto {
     function _check() {
-        $operands = array('Addition', 'Multiplication', 'String', 'Integer', 'Sequence', 
-                          'Float', 'Not', 'Variable','Array','Concatenation', 'Sign',
-                          'Functioncall', 'Constant', 'Parenthesis', 'Comparison', 'Assignation',
-                          'Noscream', 'Staticproperty', 'Property', 'Ternary', 'New', 'Return',
-                          'Instanceof', 'Magicconstant', 'Staticconstant', 'Methodcall', 'Logical',
-                          'Var', 'Const', 'Ppp', 'Postplusplus', 'Preplusplus', 'Global', 'Nsname',
-                          'Ifthen', 'Include', 'Function', 'Foreach', 'While', 'Arrayappend', 'Cast',
-                          'Break', 'Goto', 'Label', 'Switch', 'Staticmethodcall',
-                          'Static', 'Continue', 'Class', 'For', 'Throw', 'Try', 'Abstract', 'Final',
-                          'Bitshift', 'Void', 'Dowhile', 'Halt', 'Interface', 'Block', 'RawString', 
-                          'Phpcode', 'Namespace', 
-                           );
+        $yield_operators =  array_merge(array('T_PROTECTED', 'T_PRIVATE', 'T_PUBLIC', 'T_STATIC', 'T_ABSTRACT', 'T_FINAL', 
+                                              'T_CLOSE_PARENTHESIS', 'T_OPEN_PARENTHESIS', 'T_CLASS', 'T_EXTENDS', 'T_IMPLEMENTS', 
+                                              'T_INTERFACE', 'T_ELSE', 'T_OBJECT_OPERATOR', 'T_QUOTE'),
+                                        Assignation::$operators, Functioncall::$operators);
         
         $next_operator = array_merge(array('T_OPEN_PARENTHESIS', 'T_OBJECT_OPERATOR', 'T_DOUBLE_COLON', 'T_COMMA', 'T_CLOSE_PARENTHESIS', 'T_CATCH',
-                                           'T_OPEN_BRACKET', 'T_OPEN_CURLY', 'T_ELSEIF' ), 
+                                           'T_OPEN_BRACKET', 'T_OPEN_CURLY', 'T_ELSEIF', 'T_QUOTE_CLOSE', ), 
                                      Assignation::$operators, Logical::$operators);
 
         // @note instructions not separated by ; 
@@ -28,14 +20,9 @@ class SequenceAtom extends TokenAuto {
                            'Noscream', 'Property', 'Staticproperty', 'Label', 'Goto', 'Halt', 'Interface', 'Block', 'Break', 'Try', 
                            'Throw', 'Sequence','RawString', 'Phpcode', 
                              );
-        $yield_operators =  array_merge(array('T_PROTECTED', 'T_PRIVATE', 'T_PUBLIC', 'T_STATIC', 'T_ABSTRACT', 'T_FINAL', 
-                                              'T_CLOSE_PARENTHESIS', 'T_OPEN_PARENTHESIS', 'T_CLASS', 'T_EXTENDS', 'T_IMPLEMENTS', 
-                                              'T_INTERFACE', 'T_ELSE', 'T_OBJECT_OPERATOR'),
-                                        Assignation::$operators, Functioncall::$operators);
-        
         $this->conditions = array(-1 => array('filterOut2' => $yield_operators), 
                                    0 => array('atom' => $operands2, 'notToken' => 'T_ELSEIF'),
-                                   1 => array('atom' => $operands2 ),
+                                   1 => array('atom' => $operands2, 'notToken' => 'T_ELSEIF'),
                                    2 => array('filterOut2' => $next_operator),
         );
         $this->actions = array('insertSequence'  => true);
