7010c64e2260990adf32b9b8b1a14c6a8012f01f
Famille Seguy <famille@MacBookPro-2.local>
2013-08-22 21:14:53 +0800

    Extra tests for Throw, with Nsname

--- tests/Test/_Throw.php
+++ tests/Test/_Throw.php
@@ -7,12 +7,13 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class _Throw extends Tokenizeur {
-    /* 5 methods */
+    /* 6 methods */
 
     public function test_Throw01()  { $this->generic_test('_Throw.01'); }
     public function test_Throw02()  { $this->generic_test('_Throw.02'); }
     public function test_Throw03()  { $this->generic_test('_Throw.03'); }
     public function test_Throw04()  { $this->generic_test('_Throw.04'); }
     public function test_Throw05()  { $this->generic_test('_Throw.05'); }
+    public function test_Throw06()  { $this->generic_test('_Throw.06'); }
 }
 ?>
