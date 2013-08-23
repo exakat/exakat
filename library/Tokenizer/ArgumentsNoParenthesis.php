e56dff881e9a185d9bc08f7d9002a11d92c0355d
Famille Seguy <famille@MacBookPro-2.local>
2013-08-23 11:57:00 +0800

    Added extra constraint to ArgumentNoParenthesis. Redo the test results for Precedence.

--- library/Tokenizer/ArgumentsNoParenthesis.php
+++ library/Tokenizer/ArgumentsNoParenthesis.php
@@ -13,7 +13,8 @@ class ArgumentsNoParenthesis extends TokenAuto {
                                     1 => array('atom'  => 'yes', 'notAtom' => array('Sequence', 'Arguments', 'Function', 'Ppp', 'Final', 'Abstract')),
                                     2 => array('filterOut2' => array_merge(array('T_DOT', 'T_DOUBLE_COLON', 'T_OBJECT_OPERATOR', 'T_EQUAL', 'T_QUESTION', 'T_OPEN_PARENTHESIS', 'T_OPEN_BRACKET', 'T_OPEN_CURLY', 'T_COMMA',),
                                                                            Addition::$operators, Multiplication::$operators, 
-                                                                           Bitshift::$operators, Logical::$operators)) 
+                                                                           Bitshift::$operators, Logical::$operators,
+                                                                           Postplusplus::$operators)) 
         );
         
         $this->actions = array('insertEdge'  => array(0 => array('Arguments' => 'ARGUMENT')),
