8c51924fe7feb2021ad436df8eb17bada5c06f11
Damien Seguy <damien.seguy@gmail.com>
2013-11-18 13:16:55 +0800

    No need for dependsOn in Integer.

--- library/Analyzer/Type/Integer.php
+++ library/Analyzer/Type/Integer.php
@@ -5,10 +5,6 @@ namespace Analyzer\Type;
 use Analyzer;
 
 class Integer extends Analyzer\Common\Type {
-    
-    function dependsOn() {
-        return array();
-    }
 
     function analyze() {
         $this->type = 'Integer';
