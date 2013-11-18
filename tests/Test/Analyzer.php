cf7a80e586eeb9069c1eab441314c1dc7a2f793f
Damien Seguy <damien.seguy@gmail.com>
2013-11-18 15:12:53 +0800

    Added test framework for analyzers.

--- tests/Test/Analyzer.php
+++ tests/Test/Analyzer.php
@@ -16,12 +16,44 @@ class Analyzer extends \PHPUnit_Framework_TestCase {
         
         $shell = 'cd ..; php bin/load -f tests/source/'.$file.'.php; php bin/tokenizer; php bin/analyze;';
         $res = shell_exec($shell);
-        $shell = 'cd ..; php bin/export_analyzer '.$analyzer;
-        $res = shell_exec($shell);
+        $shell = 'cd ..; php bin/export_analyzer '.$analyzer.' -o -json';
+        $res = json_decode(shell_exec($shell));
+        $this->assertNotEmpty($res, 'No values were read from the analyzer' );
+
+        $list = array();
+        foreach($res as $r) {
+            $list[] = $r[0];
+        }
+        $this->assertNotEquals(count($list), 0, 'No values were read from the analyzer' );
         
-        $exp = file_get_contents('exp/'.$file.'.txt');
+        include('exp/'.$file.'.php');
         
-        $this->assertEquals($exp, $res);
+        if (isset($expected) && is_array($expected)) {
+            $missing = array();
+            foreach($expected as $e) {
+                if (($id = array_search($e, $list)) !== false) {
+                    unset($list[$id]);
+                } else {
+                    $missing[] = $e;
+                }
+            }
+            $this->assertEquals(count($missing), 0, count($missing)." expected values were not found : ".join(', ', $missing)." in the received values of '".join(', ', $list)."'");
+        }
+        
+        if (isset($expected_not) && is_array($expected)) {
+            $extra = array();
+            foreach($expected_not as $e) {
+                if ($id = array_search($e, $list)) {
+                    $extra[] = $e;
+                    unset($list[$id]);
+                } 
+            }
+            // the not expected
+            $this->assertEquals(count($extra), 0, count($extra)." values were found and shouldn't be : ".join(', ', $extra)."");
+        }
+        
+        // the remainings
+        $this->assertEquals(count($list), 0, count($list)." values were found and are unprocessed : ".join(', ', $list)."");
     }
     
     
