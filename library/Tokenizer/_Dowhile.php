990dc49c776d1352326ee17234930a8aaed23865
Damien Seguy <damien.seguy@gmail.com>
2013-12-13 10:05:10 +0800

    Cleaned code.

--- library/Tokenizer/_Dowhile.php
+++ library/Tokenizer/_Dowhile.php
@@ -26,28 +26,7 @@ class _Dowhile extends TokenAuto {
                                'atom'       => 'Dowhile',
                                'cleanIndex' => true);
         $this->checkAuto();
-/*
-        // do atom ; while()
-        $this->conditions = array( 0 => array('token'  => _Dowhile::$operators),
-                                   1 => array('atom'   => 'yes'),
-                                   2 => array('token'  => 'T_SEMICOLON'),
-                                   3 => array('token'  => 'T_WHILE'),
-                                   4 => array('token'  => 'T_OPEN_PARENTHESIS'),
-                                   5 => array('atom'   => 'yes'),
-                                   6 => array('token'  => 'T_CLOSE_PARENTHESIS')
-        );
-        
-        $this->actions = array('transform'    => array(   1 => 'LOOP',  
-                                                          2 => 'DROP',
-                                                          3 => 'DROP',
-                                                          4 => 'DROP',
-                                                          5 => 'CONDITION',
-                                                          6 => 'DROP'
-                                                        ),
-                               'atom'       => 'Dowhile',
-                               'cleanIndex' => true);
-        $this->checkAuto();
-*/
+
         // do { block } while()
         $this->conditions = array( 0 => array('token' => _Dowhile::$operators),
                                    1 => array('atom'  => 'Block'),
