4762434e0755dc6b80db25cd2dcdc415112f2383
Damien Seguy <damien.seguy@gmail.com>
2013-12-16 13:11:38 +0800

    Better support for markdown, HTML (css) and odt (template).

--- library/Report/HashTable.php
+++ library/Report/HashTable.php
@@ -4,6 +4,13 @@ namespace Report;
 
 class HashTable {
     private $hash = array('Empty' => 'hash');
+    private $sort = TableCounted::SORT_NONE;
+    
+    const SORT_NONE = 1;
+    const SORT_COUNT = 2;
+    const SORT_REV_COUNT = 3;
+    const SORT_KEY = 4;
+    const SORT_REV_KEY = 4;
     
     function setContent($hash = array()) {
         if (!is_null($hash)) {
@@ -11,12 +18,27 @@ class HashTable {
         } 
     }
 
+    function setSort($sort) {
+        if (in_array($sort, range(1, 5))) {
+            $this->sort = $sort; 
+        }
+    }
+
     function toMarkdown() {
         if (count($this->hash) == 0)  {
             $report = "Nothing special to report. ";
         } else {
-            $report = "| Libel        | Value          | 
+            $report = "| Item        | Usage          | 
 | -------:        | -------:          |\n";
+$report .= "|Total number of element|".array_sum($this->hash)."|\n";
+$report .= "|Number of distinct element|".count($this->hash)."|\n";
+$report .= "|Largest element|".max($this->hash)." (".array_search(max($this->hash), $this->hash).")|\n";
+$report .= "|Smaller element|".min($this->hash)." (".array_search(min($this->hash), $this->hash).")|\n";
+$report .= "\n\n";
+
+            $report .= "| Libel        | Value          | 
+| -------:        | -------:          |\n";
+
             foreach($this->hash as $key => $value) {
                 $key = $this->escapeString($key);
                 $report .= "|$key|$value|\n";
--- library/Report/TableCounted.php
+++ library/Report/TableCounted.php
@@ -4,9 +4,13 @@ namespace Report;
 
 class TableCounted {
     private $name = "Unnamed";
+    private $sort = TableCounted::SORT_NONE;
     
-    function __construct() {
-    }
+    const SORT_NONE = 1;
+    const SORT_COUNT = 2;
+    const SORT_REV_COUNT = 3;
+    const SORT_KEY = 4;
+    const SORT_REV_KEY = 4;
     
     function setQuery($query) {
         $this->queryTemplate = $query; 
@@ -15,13 +19,26 @@ class TableCounted {
     function setContent($name) {
         $this->name = $name; 
     }
-    
+
+    function setSort($sort) {
+        if (in_array($sort, range(1, 5))) {
+            $this->sort = $sort; 
+        }
+    }
     
     function toMarkdown() {
         $vertices = query($this->client, $this->queryTemplate)->toArray();
         
         $report = "###{$this->name}\n";
         if (!empty($vertices[0][0])) {
+            // report
+            $report .= "| Item        | Usage          | 
+| -------:        | -------:          |\n";
+$report .= "|Number|".count($vertices)."|\n";
+$report .= "|Total|".array_sum($vertices)."|\n";
+$report .= "\n\n";
+
+            // table
             $report .= "| Item        | Usage          | 
 | -------:        | -------:          |\n";
             
