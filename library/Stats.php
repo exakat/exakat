733776c796a1f0abe86bc31638862f3a7c695e81
Damien Seguy <damien.seguy@gmail.com>
2013-12-16 13:11:08 +0800

    Created a Stat class to collect stats from the server.

---
+++ library/Stats.php
@@ -0,0 +1,59 @@
+<?php
+
+use Everyman\Neo4j\Client,
+	Everyman\Neo4j\Index\NodeIndex,
+	Everyman\Neo4j\Relationship,
+	Everyman\Neo4j\Node,
+	Everyman\Neo4j\Gremlin,
+	Tokenizer\Token;
+
+class Stats {
+    private $client = null;
+    private $stats = array();
+    private $file_filter = '';
+    
+    public function __construct($client = null) {
+        $this->client = $client;
+    }
+
+    public function toArray() {
+        return $this->stats;
+    }
+
+    public function setFileFilter($file) {
+        $this->file_filter = ".has('file', '$file')";
+        
+        return true;
+    }
+    
+    public function __get($name) {
+        if (isset($this->stats[$name])) {
+            return $this->stats[$name];
+        } else {
+            return null;
+        }
+    }
+
+    function collect($type = null) {
+        $this->stats['tokens_count']  = $this->queryOne("g.V.except([g.v(0)]){$this->file_filter}.count()");
+        $this->stats['relations_count']  = $this->queryOne("g.E.except([g.v(0)]){$this->file_filter}.count()");
+        $this->stats['atoms_count']   = $this->queryOne("g.V.except([g.v(0)]).hasNot('atom', 'null'){$this->file_filter}.count()");
+        $this->stats['NEXT_count']    = $this->queryOne("g.E.has('label', 'NEXT').inV{$this->file_filter}.count()");
+        $this->stats['INDEXED_count'] = $this->queryOne("g.E.has('label', 'INDEXED').inV{$this->file_filter}.count()");
+        $this->stats['file_count']    = $this->queryOne("m = [:]; g.V.groupCount(m){it.file}.iterate(); m.size();");
+        //$this->stats['test']        = $this->queryOne("m = []; g.V.fill(m); m.size();");
+    }
+    
+    function queryOne($query) {
+        $r = $this->query($query);
+        return $r[0][0];
+    }
+
+    function query($query) {
+        $params = array('type' => 'IN');
+        $query = new Gremlin\Query($this->client, $query, $params);
+        return $query->getResultSet();
+    }
+}
+
+?>
