92d155baf2c7ae10346ccf8b9c03f0aa60dbd48a
Damien Seguy <damien.seguy@gmail.com>
2013-12-19 22:00:14 +0800

    Fixed the tokenizer, so no need for special  processing.

--- library/Analyzer/Variables/Variablenames.php
+++ library/Analyzer/Variables/Variablenames.php
@@ -5,12 +5,10 @@ namespace Analyzer\Variables;
 use Analyzer;
 
 class Variablenames extends Analyzer\Analyzer {
-    
     function analyze() {
         $this->atomIs("Variable")
              ->hasNoIn('DEFINE')
              ->hasNoIn('PROPERTY') // avoid static properties
-//             ->hasNoIn('NAME') 
              ;
     }
 }
