5d7122d366d629726b0385d6a51fa01ffb9f7351
Damien Seguy <damien.seguy@gmail.com>
2013-12-27 23:32:11 +0800

    Added argv as PHP variable.

--- library/Analyzer/Variables/VariablePhp.php
+++ library/Analyzer/Variables/VariablePhp.php
@@ -13,7 +13,7 @@ class VariablePhp extends Analyzer\Analyzer {
              ->code(array('$_GET','$_POST','$_COOKIE','$_SERVER','_FILES','$_REQUEST','$_SESSION','$_ENV',
 	                        '$PHP_SELF','$HTTP_RAW_POST_DATA',
 	                        '$HTTP_GET_VARS','$HTTP_POST_VARS',
-	                        '$GLOBALS'), true);
+	                        '$GLOBALS', '$argv',), true);
     }
 }
 
