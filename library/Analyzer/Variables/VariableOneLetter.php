9102aec7a892c9fe1b3cfb4e6e3233412431d811
Damien Seguy <damien.seguy@gmail.com>
2013-12-27 23:32:28 +0800

    Created fullcode function.

--- library/Analyzer/Variables/VariableOneLetter.php
+++ library/Analyzer/Variables/VariableOneLetter.php
@@ -5,15 +5,12 @@ namespace Analyzer\Variables;
 use Analyzer;
 
 class VariableOneLetter extends Analyzer\Analyzer {
-    function dependsOn() {
-        return array('Analyzer\\Variables\\Variablenames');
-    }
     
     function analyze() {
         $this->atomIs("Variable")
              ->hasNoIn('DEFINE')
              ->hasNoIn('PROPERTY')
-             ->codeLength(" == 2 ");
+             ->fullcodeLength(" == 2 ");
     }
 }
 
