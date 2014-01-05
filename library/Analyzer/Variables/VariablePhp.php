e59d01d9efe67a25ba2c0d79c6096676deb38b3b
Damien Seguy <damien.seguy@gmail.com>
2014-01-05 09:25:55 +0800

    Exposed the list of PHP variable, so as to be able to call them from other classes.

--- library/Analyzer/Variables/VariablePhp.php
+++ library/Analyzer/Variables/VariablePhp.php
@@ -5,15 +5,17 @@ namespace Analyzer\Variables;
 use Analyzer;
 
 class VariablePhp extends Analyzer\Analyzer {
+    public static $variables = array('$_GET','$_POST','$_COOKIE','$_SERVER','$_FILES',
+                                     '$_REQUEST','$_SESSION','$_ENV',
+	                                 '$PHP_SELF','$HTTP_RAW_POST_DATA',
+      	                             '$HTTP_GET_VARS','$HTTP_POST_VARS',
+	                                 '$GLOBALS', '$argv',);
     
     function analyze() {
         $this->atomIs("Variable")
              ->hasNoIn('DEFINE')
              ->hasNoIn('PROPERTY')
-             ->code(array('$_GET','$_POST','$_COOKIE','$_SERVER','_FILES','$_REQUEST','$_SESSION','$_ENV',
-	                        '$PHP_SELF','$HTTP_RAW_POST_DATA',
-	                        '$HTTP_GET_VARS','$HTTP_POST_VARS',
-	                        '$GLOBALS', '$argv',), true);
+             ->code(VariablePhp::$variables, true);
     }
 }
 
