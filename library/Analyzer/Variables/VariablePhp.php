cc90573fc3caf0af5333835b0197a8f61308e119
Damien Seguy <damien.seguy@gmail.com>
2014-01-07 07:10:56 +0800

    Added missing HTTP_POST_FILES in the list.

--- library/Analyzer/Variables/VariablePhp.php
+++ library/Analyzer/Variables/VariablePhp.php
@@ -8,7 +8,7 @@ class VariablePhp extends Analyzer\Analyzer {
     public static $variables = array('$_GET','$_POST','$_COOKIE','$_FILES','$_SESSION',
                                      '$_REQUEST','$_ENV', '$_SERVER',
                                      '$PHP_SELF','$HTTP_RAW_POST_DATA',
-                                     '$HTTP_GET_VARS','$HTTP_POST_VARS', '$HTTP_ENV_VARS', '$HTTP_SERVER_VARS', '$HTTP_COOKIE_VARS',
+                                     '$HTTP_GET_VARS','$HTTP_POST_VARS', '$HTTP_POST_FILES', '$HTTP_ENV_VARS', '$HTTP_SERVER_VARS', '$HTTP_COOKIE_VARS',
                                      '$GLOBALS', 
                                      '$argv', '$argc');
     
