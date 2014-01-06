97e5a9077fe788c782e856fc0a4cfed17dbd006f
Damien Seguy <damien.seguy@gmail.com>
2014-01-07 06:45:48 +0800

    Added missing PHP variables.

--- library/Analyzer/Variables/VariablePhp.php
+++ library/Analyzer/Variables/VariablePhp.php
@@ -5,11 +5,12 @@ namespace Analyzer\Variables;
 use Analyzer;
 
 class VariablePhp extends Analyzer\Analyzer {
-    public static $variables = array('$_GET','$_POST','$_COOKIE','$_SERVER','$_FILES',
-                                     '$_REQUEST','$_SESSION','$_ENV',
-	                                 '$PHP_SELF','$HTTP_RAW_POST_DATA',
-      	                             '$HTTP_GET_VARS','$HTTP_POST_VARS',
-	                                 '$GLOBALS', '$argv',);
+    public static $variables = array('$_GET','$_POST','$_COOKIE','$_FILES','$_SESSION',
+                                     '$_REQUEST','$_ENV', '$_SERVER',
+                                     '$PHP_SELF','$HTTP_RAW_POST_DATA',
+                                     '$HTTP_GET_VARS','$HTTP_POST_VARS', '$HTTP_ENV_VARS', '$HTTP_SERVER_VARS', '$HTTP_COOKIE_VARS',
+                                     '$GLOBALS', 
+                                     '$argv', '$argc');
     
     function analyze() {
         $this->atomIs("Variable")
