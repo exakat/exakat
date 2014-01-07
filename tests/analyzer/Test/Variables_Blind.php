4982ae9dd32c3910f0ccd62c1e94c478be4fc0a0
Damien Seguy <damien.seguy@gmail.com>
2014-01-07 21:25:52 +0800

    Test with a static property as blind variable (crazy, right? )

--- tests/analyzer/Test/Variables_Blind.php
+++ tests/analyzer/Test/Variables_Blind.php
@@ -7,9 +7,10 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Variables_Blind extends Analyzer {
-    /* 2 methods */
+    /* 3 methods */
 
     public function testVariables_Blind01()  { $this->generic_test('Variables_Blind.01'); }
     public function testVariables_Blind02()  { $this->generic_test('Variables_Blind.02'); }
+    public function testVariables_Blind03()  { $this->generic_test('Variables_Blind.03'); }
 }
 ?>
