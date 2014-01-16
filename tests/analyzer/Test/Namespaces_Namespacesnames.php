85dfd6c2afc48caeb81c09bdb305113403ff42e6
Damien Seguy <damien.seguy@gmail.com>
2014-01-16 11:35:44 +0800

    Lots of tests for namespaces. That was the base for the fix for sequence orders.

--- tests/analyzer/Test/Namespaces_Namespacesnames.php
+++ tests/analyzer/Test/Namespaces_Namespacesnames.php
@@ -7,8 +7,11 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Namespaces_Namespacesnames extends Analyzer {
-    /* 1 methods */
+    /* 4 methods */
 
     public function testNamespaces_Namespacesnames01()  { $this->generic_test('Namespaces_Namespacesnames.01'); }
+    public function testNamespaces_Namespacesnames02()  { $this->generic_test('Namespaces_Namespacesnames.02'); }
+    public function testNamespaces_Namespacesnames03()  { $this->generic_test('Namespaces_Namespacesnames.03'); }
+    public function testNamespaces_Namespacesnames04()  { $this->generic_test('Namespaces_Namespacesnames.04'); }
 }
 ?>
---
+++ tests/analyzer/exp/Namespaces_Namespacesnames.04.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array('A', 'Global', 'B');
+
+$expected_not = array();
+
+?>
