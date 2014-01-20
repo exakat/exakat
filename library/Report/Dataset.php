9ad856d8d846b9661678c1926683acae6a1d11ce
Damien Seguy <damien.seguy@gmail.com>
2014-01-20 22:52:56 +0800

    ANother upgrade of the escaping function.

--- library/Report/Dataset.php
+++ library/Report/Dataset.php
@@ -11,7 +11,9 @@ class Dataset {
             return $value;
         }
         
-        $value = str_replace( "\n", '<BR />', $value );
+        if ($this->string_width($value) > 100) {
+            $value = substr($value, 0, 100).' ...';
+        }
 
 //|` ' == $key \|| in_array($key,$' `|1|
 
@@ -23,17 +25,44 @@ class Dataset {
         $value = preg_replace('/([^\\\\])\*/', '$1\\*', $value);
         $value = preg_replace('/^\*/', '\\*', $value);
 
-        if (strlen($value) > 255) {
-            $value = substr($value, 0, 250).' ...';
-        }
-        
         if (strpos($value, '`') !== false ) {
+            $value = str_replace( "\n", '``<BR />``', $value );
             $value = "`` $value ``";
         } else {
+            $value = str_replace( "\n", '`<BR />`', $value );
             $value = "` $value `";
         }
 
         return $value;
+    }
+
+    protected function string_width($string) {
+        $strings = explode("\n", $string);
+        $max = 0;
+        foreach($strings as $s) {
+            $max = max(strlen($s), $max);
+        }
+        
+        return $max;
+    }
+
+    static function array2tree($array, $delimiter = '\\') {
+        $r = array();
+        
+        foreach ($array as $a) {
+            $parts = explode($delimiter, $a);
+            $current = $r;
+            foreach($parts as $p) {
+                if (isset($current[$p])) {
+                    $current = &$current[$p];
+                } else {
+                    $current[$p] = array();
+                    $current = &$current[$p];
+                }
+            }
+        }
+
+        return $r;
     }
 }
 
