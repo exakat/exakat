e58fe89f3c92a864b5a7648bee24bc6e45ce0fec
Damien Seguy <damien.seguy@gmail.com>
2014-01-23 10:39:15 +0800

    Unit test for For with Function call.

---
+++ tests/analyzer/exp/Structures_ForWithFunctioncall.01.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array('for($j = 0 ; $j < strlen($n) ; $j++) { /**/ } ','for($k = 0 ; $k < $nb ; $k = preg_match(\'\', $l)) { /**/ } ');
+
+$expected_not = array('for($i = 0; $i < $nb; $i++) { /**/ } ', 'for(preg_match(\'\', $m); $m < $nb; $m++) { /**/ } ');
+
+?>
---
+++ tests/analyzer/source/Structures_ForWithFunctioncall.01.php
@@ -0,0 +1,10 @@
+<?php
+for($i = 0; $i < $nb; $i++) { echo $i; }
+
+for($j = 0; $j < strlen($n); $j++) { echo $j; }
+
+for($k = 0; $k < $nb; $k = preg_match('', $l)) { echo $k; }
+
+for(preg_match('', $m); $m < $nb; $m++) { echo $k; }
+
+?>
