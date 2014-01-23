6d962b16d0736c3ccc2efa583a0b333ef0cfb208
Damien Seguy <damien.seguy@gmail.com>
2014-01-23 14:15:22 +0800

    Using the fullcodeTrimmed function.

--- library/Analyzer/Type/Continents.php
+++ library/Analyzer/Type/Continents.php
@@ -13,7 +13,7 @@ class Continents extends Analyzer\Analyzer {
         $ini = parse_ini_file('data/Continents_en.ini');
         
         $this->atomIs('String')
-             ->fullcode(array_values($ini['continents_en']));
+             ->fullcodeTrimmed(array_values($ini['continents_en']));
     }
 }
 
