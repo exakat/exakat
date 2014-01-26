11d2558ade64b92709b6151818c1ac2db79f67b4
Damien Seguy <damien.seguy@gmail.com>
2014-01-26 21:50:17 +0800

    Added an extra test for Octal : it was spotting hexadecimals or decimals.

--- tests/analyzer/Test/Type_Octal.php
+++ tests/analyzer/Test/Type_Octal.php
@@ -7,8 +7,9 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Type_Octal extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testType_Octal01()  { $this->generic_test('Type_Octal.01'); }
+    public function testType_Octal02()  { $this->generic_test('Type_Octal.02'); }
 }
 ?>
---
+++ tests/analyzer/source/Type_Octal.02.php
@@ -0,0 +1,11 @@
+<?php
+
+$hexa = 0xff00ff;
+
+$negative = -12;
+$negative2 = -012;
+
+print $negative2;
+
+
+?>
