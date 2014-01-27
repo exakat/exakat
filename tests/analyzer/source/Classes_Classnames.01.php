03f5b1aa04d1c2030dc0974757306da7a1031c6a
Damien Seguy <damien.seguy@gmail.com>
2014-01-27 10:01:45 +0800

    Fixed code : cannot have a single variable in a class global space.

--- tests/analyzer/source/Classes_Classnames.01.php
+++ tests/analyzer/source/Classes_Classnames.01.php
@@ -2,26 +2,26 @@
 
 namespace a {
     class a {
-        $aa = 1;
+        public $aa = 1;
     }
 }
 
 namespace {
     class b { 
-        $ab = 3;
+        private $ab = 3;
     }
 }
 
 namespace g {
     class h { 
-        $ah = 5;
+        protected $ah = 5;
     }
 }
 
 namespace d {
     class e { 
         function f() {
-            $ad = 7;
+            static $ad = 7;
         }
     }
 }
