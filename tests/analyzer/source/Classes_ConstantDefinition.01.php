4384d8c3548f00e5b616a9bb33562745d076ae34
Damien Seguy <damien.seguy@gmail.com>
2014-01-27 10:01:02 +0800

    Cannot have const inside a function (in a class ? ) for PHP 5.3

--- tests/analyzer/source/Classes_ConstantDefinition.01.php
+++ tests/analyzer/source/Classes_ConstantDefinition.01.php
@@ -7,7 +7,7 @@ class x {
     const REAL_CLASS_CONSTANT2 = 2, REAL_CLASS_CONSTANT3 = 3;
     
     function f() {
-        const IN_METHOD_CONSTANT = 4;
+//        const IN_METHOD_CONSTANT = 4;
     }
 }
 
