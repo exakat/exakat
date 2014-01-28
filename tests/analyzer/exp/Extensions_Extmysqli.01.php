5aff9752b182ce5e3bbd05943c602e24817dc212
Damien Seguy <damien.seguy@gmail.com>
2014-01-28 10:51:35 +0800

    Added support for mysqli

---
+++ tests/analyzer/exp/Extensions_Extmysqli.01.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array('mysqli("localhost", "my_user", "my_password", "world")');
+
+$expected_not = array();
+
+?>
---
+++ tests/analyzer/exp/Extensions_Extmysqli.02.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('mysqli_connect("localhost", "my_user", "my_password", "world")',
+                      'mysqli_connect_errno()',
+                      'mysqli_connect_error()');
+
+$expected_not = array();
+
+?>
