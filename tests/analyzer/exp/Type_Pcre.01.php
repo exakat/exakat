51b207e8632b577b372264f11e3ee302b85fcf2f
Damien Seguy <damien.seguy@gmail.com>
2014-01-28 20:55:19 +0800

    Unit test for Pcre

---
+++ tests/analyzer/exp/Type_Pcre.01.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array("'/[0-9a-z]+/'", "'#[0-9A-Z]+#is'");
+
+$expected_not = array();
+
+?>
---
+++ tests/analyzer/source/Type_Pcre.01.php
@@ -0,0 +1,6 @@
+<?php
+
+preg_match('/[0-9a-z]+/', $x);
+preg_match('#[0-9A-Z]+#is', $y);
+
+?>
