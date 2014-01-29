2d00485411ca7ad564cb0ab22176e695a8d07c23
Damien Seguy <damien.seguy@gmail.com>
2014-01-29 08:43:06 +0800

    Added support for ereg / posix

---
+++ tests/analyzer/source/Extensions_Extexif.01.php
@@ -0,0 +1,13 @@
+<?php
+echo "test1.jpg:<br />\n";
+$exif = exif_read_data('tests/test1.jpg', 'IFD0');
+echo $exif===false ? "No header data found.<br />\n" : "Image contains headers<br />\n";
+
+$exif = exif_read_data('tests/test2.jpg', 0, true);
+echo "test2.jpg:<br />\n";
+foreach ($exif as $key => $section) {
+    foreach ($section as $name => $val) {
+        echo "$key.$name: $val<br />\n";
+    }
+}
+?>
