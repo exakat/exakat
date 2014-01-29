72306110111b6dd6761e993870d180409548be86
Damien Seguy <damien.seguy@gmail.com>
2014-01-29 22:27:58 +0800

    Added a real list of HTTP headers, extracted from the RFC.

--- library/Analyzer/Type/HttpHeader.php
+++ library/Analyzer/Type/HttpHeader.php
@@ -5,13 +5,65 @@ namespace Analyzer\Type;
 use Analyzer;
 
 class HttpHeader extends Analyzer\Analyzer {
+    public $HttpHeadersList = array(
+'Accept',
+'Accept-Charset',
+'Accept-Encoding',
+'Accept-Language',
+'Accept-Ranges',
+'Age',
+'Allow',
+'Authorization',
+'Cache-Control',
+'Connection',
+'Content-Encoding',
+'Content-Language',
+'Content-Length',
+'Content-Location',
+'Content-MD5',
+'Content-Range',
+'Content-Type',
+'Date',
+'ETag',
+'Expect',
+'Expires',
+'From',
+'Host',
+'If-Match',
+'If-Modified-Since',
+'If-None-Match',
+'If-Range',
+'If-Unmodified-Since',
+'Last-Modified',
+'Location',
+'Max-Forwards',
+'Pragma',
+'Proxy-Authenticate',
+'Proxy-Authorization',
+'Range',
+'Byte Ranges',
+'Range Retrieval Requests',
+'Referer',
+'Retry-After',
+'Server',
+'TE',
+'Trailer',
+'Transfer-Encoding',
+'Upgrade',
+'User-Agent',
+'Vary',
+'Via',
+'Warning',
+'WWW-Authenticate    ',
+    );
+
     function dependsOn() {
         return array("Analyzer\\Type\\String");
     }
     
     function analyze() {
         $this->atomIs('String')
-             ->regex('code', '[\'\"](Content-Type|WWW-Authenticate|Cache-Control|Robots-Tag|Content-Transfer-Encoding|Access-Control-Allow-Origin|Location|Retry-After|Content-Range|Content-Encoding|Frame-Options|Content-Disposition|Vary|X-Frame-Options|X-XSS-Protection|Strict-Transport-Security): .*[\'\"]');
+             ->regex('code', '[\'\"]('.join('|', $this->HttpHeadersList).'): .*[\'\"]');
         $this->prepareQuery();
     }
 }
