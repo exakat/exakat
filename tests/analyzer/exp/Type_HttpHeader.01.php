aef9dc717887fb0baf98c49be34bdcf9f23eec1f
Damien Seguy <damien.seguy@gmail.com>
2014-02-01 10:07:31 +0800

    Fixed expected values.

--- tests/analyzer/exp/Type_HttpHeader.01.php
+++ tests/analyzer/exp/Type_HttpHeader.01.php
@@ -1,6 +1,7 @@
 <?php
 
-$expected     = array("Content-Type: application/octet-stream", "Max-Forwards: 34");
+$expected     = array('"Content-Type: application/octet-stream"', 
+                      '"Max-Forwards: 34"');
 
 $expected_not = array("Transfer-Encoding UTF-8", 'normal string');
 
