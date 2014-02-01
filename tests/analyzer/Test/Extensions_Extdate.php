33885127c9835446240579444ea6d950d0208087
Damien Seguy <damien.seguy@gmail.com>
2014-02-01 11:07:41 +0800

    Added tests when the interface is used with absolute notation.

--- tests/analyzer/Test/Extensions_Extdate.php
+++ tests/analyzer/Test/Extensions_Extdate.php
@@ -7,12 +7,13 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Extensions_Extdate extends Analyzer {
-    /* 5 methods */
+    /* 6 methods */
 
     public function testExtensions_Extdate01()  { $this->generic_test('Extensions_Extdate.01'); }
     public function testExtensions_Extdate02()  { $this->generic_test('Extensions_Extdate.02'); }
     public function testExtensions_Extdate03()  { $this->generic_test('Extensions_Extdate.03'); }
     public function testExtensions_Extdate04()  { $this->generic_test('Extensions_Extdate.04'); }
     public function testExtensions_Extdate05()  { $this->generic_test('Extensions_Extdate.05'); }
+    public function testExtensions_Extdate06()  { $this->generic_test('Extensions_Extdate.06'); }
 }
 ?>
