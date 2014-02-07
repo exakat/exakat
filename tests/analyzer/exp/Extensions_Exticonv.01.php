8ba2df36304411d771821e1de7bbc1b7d4641923
Damien Seguy <damien.seguy@gmail.com>
2014-02-07 12:28:12 +0800

    Added unit tests for ext/json

---
+++ tests/analyzer/exp/Extensions_Exticonv.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('iconv("UTF-8", "ISO-8859-1//TRANSLIT", $text)',
+                      'iconv("UTF-8", "ISO-8859-1//IGNORE", $text)',
+                      'iconv("UTF-8", "ISO-8859-1", $text)');
+
+$expected_not = array();
+
+?>
---
+++ tests/analyzer/source/Extensions_Exticonv.01.php
@@ -0,0 +1,9 @@
+<?php
+$text = "This is the Euro symbol '€'.";
+
+echo 'Original : ', $text, PHP_EOL;
+echo 'TRANSLIT : ', iconv("UTF-8", "ISO-8859-1//TRANSLIT", $text), PHP_EOL;
+echo 'IGNORE   : ', iconv("UTF-8", "ISO-8859-1//IGNORE", $text), PHP_EOL;
+echo 'Plain    : ', iconv("UTF-8", "ISO-8859-1", $text), PHP_EOL;
+
+?>
