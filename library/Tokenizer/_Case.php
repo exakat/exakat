5ab8612d91f7218883210ad76095f3634401c4bb
Damien Seguy <damien.seguy@gmail.com>
2014-02-08 16:03:45 +0800

    Also stops on assignation. Probably will stop on other operations that extends the command.

--- library/Tokenizer/_Case.php
+++ library/Tokenizer/_Case.php
@@ -100,7 +100,8 @@ class _Case extends TokenAuto {
                                               'atom'  => 'none', ), 
                                    3 => array('atom'  => 'yes'), //array('Ifthen', 'Sequence', 'Block', 'Switch', 'Return', 'For', 'Foreach',  'String', 'RawString')),
                                    4 => array('atom'  => 'yes'), //array('Ifthen', 'Sequence', 'Break', 'Block', 'Switch', 'Return', 'For', 'Foreach', 'String', 'RawString')),
-                                   5 => array('filterOut2' => array('T_ELSE', 'T_ELSEIF')),
+                                   5 => array('filterOut2' => array_merge(array('T_ELSE', 'T_ELSEIF'),
+                                                                        Assignation::$operators)),
         );
         
         $this->actions = array('createSequenceForCaseWithoutSemicolon' => true,
