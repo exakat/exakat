06eb82574b2f80863575ac9797432b5f0647e590
Damien Seguy <damien.seguy@gmail.com>
2014-02-08 16:01:42 +0800

    Added situation where do is followed by a single instruction and no ;

--- library/Tokenizer/_Dowhile.php
+++ library/Tokenizer/_Dowhile.php
@@ -27,6 +27,25 @@ class _Dowhile extends TokenAuto {
                                'cleanIndex' => true);
         $this->checkAuto();
 
+        // do if() {} while() (no block...)
+        $this->conditions = array( 0 => array('token' => _Dowhile::$operators),
+                                   1 => array('atom'  => 'yes'),
+                                   2 => array('token' => 'T_WHILE'),
+                                   3 => array('token' => 'T_OPEN_PARENTHESIS'),
+                                   4 => array('atom'  => 'yes'),
+                                   5 => array('token' => 'T_CLOSE_PARENTHESIS')
+        );
+        
+        $this->actions = array('transform'    => array(   1 => 'LOOP',  
+                                                          2 => 'DROP',
+                                                          3 => 'DROP',
+                                                          4 => 'CONDITION',
+                                                          5 => 'DROP'
+                                                        ),
+                               'atom'       => 'Dowhile',
+                               'cleanIndex' => true);
+        $this->checkAuto();
+
         // do { block } while()
         $this->conditions = array( 0 => array('token' => _Dowhile::$operators),
                                    1 => array('atom'  => 'Block'),
