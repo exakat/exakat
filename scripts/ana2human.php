00be48e6372db47b1f5f782bf5e19c3c69a115c1
Damien Seguy <damien.seguy@gmail.com>
2014-02-09 10:35:36 +0800

    Script that test the presence of documentation in the code (only english at the moment).

---
+++ scripts/ana2human.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once(dirname(__DIR__).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_library');
+
+$analyzers = glob('library/Analyzer/*/*');
+
+$documented = 0;
+foreach($analyzers as $analyzer) {
+    $analyzer = substr($analyzer, 8, -4);
+    $analyzer = str_replace('/', '\\', $analyzer);
+    if (strpos($analyzer, "\\Common\\" ) !== false) {
+        continue;
+    }
+    $x = new $analyzer(null);
+    if ( $x->getDescription() == '' ) {
+        print "$analyzer has no human version\n";
+    } else {
+        $documented++;
+    }
+}
+
+print "\n\n";
+print count($analyzers)." analyzers\n";
+print "$documented analyzers are documented ( ".number_format($documented / count($analyzers) * 100, 2)." % )\n";
+
+
+?>
