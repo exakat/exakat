728f4309e245125e4938dd01b1a454fa716bbb4e
Damien Seguy <damien.seguy@gmail.com>
2014-02-12 11:31:13 +0800

    Now logging UT results and length.

--- scripts/phpunit.php
+++ scripts/phpunit.php
@@ -1,6 +1,30 @@
 <?php
 
+$row = array('date' => '"'.date('Y-m-d H:i:s').'"', 'id' => 'NULL');
+
+$begin = microtime(true);
 shell_exec('cd tests/analyzer; phpunit alltests.php > phpunit.txt');
+$end = microtime(true);
+
+$row['duration'] = ($end - $begin);
+
+$results = file_get_contents('tests/analyzer/phpunit.txt');
+if (preg_match('/Tests: (\d+), Assertions: (\d+), Failures: (\d+)\./is', $results, $R)) {
+    $row['tests'] = $R[1];
+    $row['assertions'] = $R[2];
+    $row['fails'] = $R[3];
+} elseif (preg_match('/OK \((\d+) tests?, (\d+) assertions?\)/is', $results, $R)) {
+    $row['tests'] = $R[1];
+    $row['assertions'] = $R[2];
+    $row['fails'] = 0;
+} else {
+    var_dump($results);
+}
+
+$mysql = new mysqli('localhost', 'root', '', 'exakat');
+$query = "INSERT INTO unittests (`".join("`, `", array_keys($row))."`) VALUES (".join(", ", array_values($row)).")";
+$mysql->query($query);
+    
 print "OK\n";
 
 ?>
