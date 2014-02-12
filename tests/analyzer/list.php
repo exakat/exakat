1d4ea92ef08df75a0ee4fafd11a88d7946765f0f
Damien Seguy <damien.seguy@gmail.com>
2014-02-12 09:53:40 +0800

    Added option to only list orphaned tests.

--- tests/analyzer/list.php
+++ tests/analyzer/list.php
@@ -1,11 +1,19 @@
 <?php
 
+print "\n";
+if (in_array('-0',$argv) !== false) {
+    define('DISPLAY_0', true);
+} else {
+    define('DISPLAY_0', false);
+}
+
 $tests = glob('../../library/Analyzer/*/*.php');
 $total = 0;
 $missing = 0;
 foreach($tests as $test) {
     $type = basename(dirname($test));
     if ($type == 'Common') { continue; }
+    if ($type == 'Themes')  { continue; }
     $analyzer = basename($test);
     $analyzer = substr($analyzer, 0, -4);
     
@@ -14,9 +22,13 @@ foreach($tests as $test) {
     if (count($files) == 0) {
         $missing ++;
     }
-    print $type."_".$analyzer."   ".count($files)."\n";
+    if (!DISPLAY_0 || count($files) == 0) {
+        print $type."/".$analyzer."   ".count($files)."\n";
+    }
     
 }
+
+print "\n";
 print "Total : $total tests\n";
 print "Missing : $missing tests\n";
 
