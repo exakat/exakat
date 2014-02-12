3385ddfebf30a7a0792251e974458006eff9fe40
Damien Seguy <damien.seguy@gmail.com>
2014-02-12 11:01:28 +0800

    Fixed database for odic (swapped with openssl).

--- tests/analyzer/exp/Extensions_Extodbc.01.php
+++ tests/analyzer/exp/Extensions_Extodbc.01.php
@@ -1,6 +1,6 @@
 <?php
 
-$expected     = array('odbc_connect("Driver={SQL Server Native Client 10.0};Server=$server;Database=$database;", $user, $password)');
+$expected     = array("odbc_connect('Driver={SQL Server Native Client 10.0};Server=\$server;Database=\$database;', \$user, \$password)");
 
 $expected_not = array();
 
