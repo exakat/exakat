a8d7e849ee10fb6a7a9b8d3c47964031c77e50d7
Damien Seguy <damien.seguy@gmail.com>
2014-02-14 07:11:56 +0800

    Second version test.

--- library/Analyzer/Structures/Break0.php
+++ library/Analyzer/Structures/Break0.php
@@ -5,7 +5,8 @@ namespace Analyzer\Structures;
 use Analyzer;
 
 class Break0 extends Analyzer\Analyzer {
-
+    protected $phpversion = '5.3';
+    
     public function analyze() {
         $this->atomIs("Break")
              ->outIs('LEVEL')
