6094bcca015d4528f72cddc6ed3032f2e018b98f
Damien Seguy <damien.seguy@gmail.com>
2014-02-14 07:10:08 +0800

    Removed useless dependance, added first version configuration.

--- library/Analyzer/Type/Binary.php
+++ library/Analyzer/Type/Binary.php
@@ -5,11 +5,13 @@ namespace Analyzer\Type;
 use Analyzer;
 
 class Binary extends Analyzer\Analyzer {
-    function dependsOn() {
+    protected $phpVersion = "5.4+";
+    
+    public function dependsOn() {
         return array("Analyzer\\Type\\Integer");
     }
     
-    function analyze() {
+    public function analyze() {
         $this->atomIs('Integer')
              ->regex('code', '0b[01]+');
     }
--- library/Analyzer/Type/Hexadecimal.php
+++ library/Analyzer/Type/Hexadecimal.php
@@ -5,10 +5,6 @@ namespace Analyzer\Type;
 use Analyzer;
 
 class Hexadecimal extends Analyzer\Analyzer {
-    public function dependsOn() {
-        return array("Integer");
-    }
-    
     public function analyze() {
         $this->atomIs('Integer')
              ->regex('code', '0[xX][0-9a-fA-F]+');
