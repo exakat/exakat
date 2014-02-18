5b72a56956572d518470da0d648f4144b8f2284f
Damien Seguy <damien.seguy@gmail.com>
2014-02-18 18:04:56 +0800

    Spot throw that also assign a variable…

---
+++ library/Analyzer/Structures/ThrowsAndAssign.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Analyzer\Structures;
+
+use Analyzer;
+
+class ThrowsAndAssign extends Analyzer\Analyzer {
+
+    public function analyze() {
+        $this->atomIs("Throw")
+             ->outIs('THROW')
+             ->atomIs('Assignation');
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Structures_ThrowsAndAssign.01.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('$a = new Exception',
+                      '$b = 3');
+
+$expected_not = array('new Exception');
+
+?>
