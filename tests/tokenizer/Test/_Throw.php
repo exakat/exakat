642e585e29d7174b1cf6ca385baa78c9a949e831
Damien Seguy <damien.seguy@gmail.com>
2014-02-18 17:13:09 +0800

    Throw may also be with a assignation. Though, it is useless.

--- tests/tokenizer/Test/_Throw.php
+++ tests/tokenizer/Test/_Throw.php
@@ -7,7 +7,7 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class _Throw extends Tokenizer {
-    /* 7 methods */
+    /* 8 methods */
 
     public function test_Throw01()  { $this->generic_test('_Throw.01'); }
     public function test_Throw02()  { $this->generic_test('_Throw.02'); }
@@ -16,5 +16,6 @@ class _Throw extends Tokenizer {
     public function test_Throw05()  { $this->generic_test('_Throw.05'); }
     public function test_Throw06()  { $this->generic_test('_Throw.06'); }
     public function test_Throw07()  { $this->generic_test('_Throw.07'); }
+    public function test_Throw08()  { $this->generic_test('_Throw.08'); }
 }
 ?>
