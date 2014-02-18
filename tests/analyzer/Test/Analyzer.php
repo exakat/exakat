794597194e15ddd2f7268fa60e367fa32a9dde45
Damien Seguy <damien.seguy@gmail.com>
2014-02-18 17:01:55 +0800

    Added build_root, as it is important to run before the tokenizer.

--- tests/analyzer/Test/Analyzer.php
+++ tests/analyzer/Test/Analyzer.php
@@ -18,7 +18,7 @@ class Analyzer extends \PHPUnit_Framework_TestCase {
         list($analyzer, $number) = explode('.', $file);
         
         $test_config = 'Analyzer\\'.str_replace('_', '\\\\', str_replace('Test', '', get_class($this)));
-        $shell = 'cd ../..; php bin/load -f tests/analyzer/source/'.$file.'.php; php bin/tokenizer -p test;  php bin/fullcode -p test; php bin/analyze -P '.$test_config;
+        $shell = 'cd ../..; php bin/load -f tests/analyzer/source/'.$file.'.php; php bin/build_root; php bin/tokenizer -p test;  php bin/fullcode -p test; php bin/analyze -P '.$test_config;
         $res = shell_exec($shell);
         $shell = 'cd ../..; php bin/export_analyzer '.$analyzer.' -o -json';
         $res = json_decode(shell_exec($shell));
