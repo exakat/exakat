5b13248b75bbd98ecbe807d2b6a73ed1b79ee1b1
Damien Seguy <damien.seguy@gmail.com>
2014-02-18 17:44:14 +0800

    Add support for useless codes spotter, with Multiplication.

---
+++ library/Analyzer/Structures/MultiplyByOne.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Analyzer\Structures;
+
+use Analyzer;
+
+class MultiplyByOne extends Analyzer\Analyzer {
+
+    public function analyze() {
+        $this->atomIs("Assignation")
+             ->code(array('*=', '/='))
+             ->outIs('RIGHT')
+             ->code(1)
+             ->back('first');
+        $this->prepareQuery();
+
+        $this->atomIs("Multiplication")
+             ->code("*")
+             ->outIs('LEFT')
+             ->code('1')
+             ->back('first');
+        $this->prepareQuery();
+
+        $this->atomIs("Multiplication")
+             ->outIs('RIGHT')
+             ->code('1')
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
