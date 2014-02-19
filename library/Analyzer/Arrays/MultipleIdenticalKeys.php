2325aa8d6ac86efce369129f4d257d752451d8ac
Damien Seguy <damien.seguy@gmail.com>
2014-02-19 10:42:54 +0800

    Added tests when an array is defining multiple keys with the same value.

--- library/Analyzer/Arrays/MultipleIdenticalKeys.php
+++ library/Analyzer/Arrays/MultipleIdenticalKeys.php
@@ -9,8 +9,8 @@ class MultipleIdenticalKeys extends Analyzer\Analyzer {
     function analyze() {
         $this->atomIs("Functioncall")
              ->code('array')
-             ->raw("aggregate(x).findAll{ m = [:]; it.out('ARGUMENTS').out('ARGUMENT').has('atom', 'Keyvalue').out('KEY').groupCount(m){it.code}.cap.next().findAll{it.value > 1}.size() > 0}");
-        $this->printQuery();
+             ->raw("aggregate().findAll{ m = [:]; it.out('ARGUMENTS').out('ARGUMENT').has('atom', 'Keyvalue').out('KEY').groupCount(m){it.code}.cap.next().findAll{it.value > 1}.size() > 0}");
+//        $this->printQuery();
 
 //g.V.filter{ it.in("ANALYZED").has("code", 'Analyzer\\Arrays\\MultipleIdenticalKeys').count() == 0}.has("atom", 'Functioncall').as("first").
 //filter{ it.code.toLowerCase() == 'array'}.
---
+++ tests/analyzer/Test/Arrays_MultipleIdenticalKeys.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test;
+
+include_once(dirname(dirname(dirname(__DIR__))).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_test');
+spl_autoload_register('Autoload::autoload_phpunit');
+
+class Arrays_MultipleIdenticalKeys extends Analyzer {
+    /* 1 methods */
+
+    public function testArrays_MultipleIdenticalKeys01()  { $this->generic_test('Arrays_MultipleIdenticalKeys.01'); }
+}
+?>
