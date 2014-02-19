0aba323a6068c9ebe0c8146be8344861a7346caa
Damien Seguy <damien.seguy@gmail.com>
2014-02-19 10:43:18 +0800

    Spot situations where an array has index of integer and string values.

---
+++ tests/analyzer/Test/Arrays_AmbiguousKeys.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test;
+
+include_once(dirname(dirname(dirname(__DIR__))).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_test');
+spl_autoload_register('Autoload::autoload_phpunit');
+
+class Arrays_AmbiguousKeys extends Analyzer {
+    /* 1 methods */
+
+    public function testArrays_AmbiguousKeys01()  { $this->generic_test('Arrays_AmbiguousKeys.01'); }
+}
+?>
