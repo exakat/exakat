75fdcc50d991ade3fae92ba0d7c0d263a872e8c9
Damien Seguy <damien.seguy@gmail.com>
2014-02-22 08:35:53 +0800

    Extra situations for non-pop properties.

--- tests/analyzer/Test/Classes_NonPPP.php
+++ tests/analyzer/Test/Classes_NonPPP.php
@@ -7,8 +7,9 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Classes_NonPPP extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testClasses_NonPPP01()  { $this->generic_test('Classes_NonPPP.01'); }
+    public function testClasses_NonPPP02()  { $this->generic_test('Classes_NonPPP.02'); }
 }
 ?>
