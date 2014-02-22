f2e6bbc4435fd85e7885df1e16cf5ab940b304a2
Damien Seguy <damien.seguy@gmail.com>
2014-02-22 08:35:32 +0800

    Limited Unit test for ext/standard

---
+++ tests/analyzer/exp/Extensions_Extstandard.01.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('file_get_contents(tmpfile())',
+                      'tmpfile()');
+
+$expected_not = array();
+
+?>
---
+++ tests/analyzer/source/Extensions_Extstandard.01.php
@@ -0,0 +1,3 @@
+<?php
+$x = file_get_contents(tmpfile());
+?>
