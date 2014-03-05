d2dcb7b92fc8cbc23713629dc242f61774d6203b
Damien Seguy <damien.seguy@gmail.com>
2014-03-05 11:24:20 +0800

    Actually report the incomparable files.

--- library/Analyzer/Php/Incompilable.php
+++ library/Analyzer/Php/Incompilable.php
@@ -10,6 +10,21 @@ class Incompilable extends Analyzer\Analyzer {
         $this->tokenIs("E_FILE")
              ->is('compile', "'false'");
     }
+    
+    public function toCountedArray($load = "it.fullcode") {
+        $queryTemplate = "m = [:]; g.idx('analyzers')[['analyzer':'Analyzer\\\\Php\\\\Incompilable']].out.hasNot('unicode_block', null).groupCount(m){it.unicode_block}.cap"; 
+        $vertices = $this->query($queryTemplate);
+
+        $report = array();
+        if (count($vertices) > 0) {
+            foreach($vertices[0][0] as $k => $v) {
+                $report[$k] = $v;
+            }   
+        } 
+        
+        return $report;
+    }
+    
 }
 
 ?>
