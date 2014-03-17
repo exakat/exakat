6ac99d8e70d2019731da33657f61ffd2d9f6f687
Damien Seguy <damien.seguy@gmail.com>
2014-03-17 22:28:48 +0800

    Checked reverse.

--- tests/analyzer/list.php
+++ tests/analyzer/list.php
@@ -13,7 +13,7 @@ $missing = 0;
 foreach($tests as $test) {
     $type = basename(dirname($test));
     if ($type == 'Common') { continue; }
-    if ($type == 'Themes')  { continue; }
+    if ($type == 'Themes') { continue; }
     $analyzer = basename($test);
     $analyzer = substr($analyzer, 0, -4);
     
@@ -30,5 +30,31 @@ foreach($tests as $test) {
 print "\n";
 print "Total : $total tests\n";
 print "Missing : $missing tests\n";
+
+$tests = glob('Test/*.php');
+$total = 0;
+$toomuch = 0;
+foreach($tests as $test) {
+    $type = substr($test, 5, -4);
+    if ($type == 'Analyzer') { continue; }
+    if ($type == 'Skeleton') { continue; }
+    if (!file_exists('../../library/Analyzer/'.str_replace('_', '/', $type).'.php')) {
+        print "- $type\n";
+        $toomuch++;
+    }
+    
+    $code = file_get_contents($test);
+    preg_match('#(\d+) methods#is', $code, $r);
+    if ($r[1] == 0) {
+        print "- $type\n";
+    }
+    
+    $total += $r[1];
+}
+
+if ($toomuch) {
+    print "Extra : $toomuch tests\n";
+}
+print "$total methods\n";
 
 ?>
