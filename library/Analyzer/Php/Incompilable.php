286c9f9066f0b865a1d7632d9fae9edd42a44689
Damien Seguy <damien.seguy@gmail.com>
2014-04-02 11:00:57 +0800

    Added function 'toFullArray';

--- library/Analyzer/Php/Incompilable.php
+++ library/Analyzer/Php/Incompilable.php
@@ -5,6 +5,8 @@ namespace Analyzer\Php;
 use Analyzer;
 
 class Incompilable extends Analyzer\Analyzer {
+    protected $severity  = \Analyzer\Analyzer::S_MAJOR;
+    protected $timeToFix = \Analyzer\Analyzer::T_QUICK;
 
     public function analyze() {
         $this->tokenIs("E_FILE")
@@ -18,14 +20,31 @@ class Incompilable extends Analyzer\Analyzer {
 
         $report = array();
         if (count($vertices) > 0) {
-            foreach($vertices[0] as $k => $v) {
-                $report[$k] = $v;
+            foreach($vertices[0] as $v) {
+                $report[] = $v;
             }   
         } 
         
         return $report;
     }
 
+    public function toFullArray() {
+        $queryTemplate = "g.V.has('token', 'E_FILE').has('compile', 'false').fullcode"; 
+        $vertices = $this->query($queryTemplate);
+
+        $report = array();
+        if (count($vertices) > 0) {
+            foreach($vertices[0] as $v) {
+                $report[] = array('code' => 'n/a', 
+                                  'file' => $v, 
+                                  'line' => 'n/a', 
+                                  'desc' => $this->getName());
+            }   
+        } 
+        
+        return $report;
+    }
+    
     public function hasResults() {
         $queryTemplate = "g.V.has('token', 'E_FILE').has('compile', 'false').count()"; 
         $vertices = $this->query($queryTemplate);
