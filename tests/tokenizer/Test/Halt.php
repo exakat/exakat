dc1521cc7570f9a622b37c688ad3ad488a48eefa
Damien Seguy <damien.seguy@gmail.com>
2014-04-03 18:21:39 +0800

    Two more tests for Halt.

--- tests/tokenizer/Test/Halt.php
+++ tests/tokenizer/Test/Halt.php
@@ -7,11 +7,13 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Halt extends Tokenizer {
-    /* 4 methods */
+    /* 6 methods */
 
     public function testHalt01()  { $this->generic_test('Halt.01'); }
     public function testHalt02()  { $this->generic_test('Halt.02'); }
     public function testHalt03()  { $this->generic_test('Halt.03'); }
     public function testHalt04()  { $this->generic_test('Halt.04'); }
+    public function testHalt05()  { $this->generic_test('Halt.05'); }
+    public function testHalt06()  { $this->generic_test('Halt.06'); }
 }
 ?>
