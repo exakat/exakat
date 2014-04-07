7a46e3f378b72e686d4061fa1ad2eee338ae5927
Damien Seguy <damien.seguy@gmail.com>
2014-04-07 20:42:01 +0800

    Added non-wanted values to test.

--- tests/analyzer/exp/Structures_OnceUsage.01.php
+++ tests/analyzer/exp/Structures_OnceUsage.01.php
@@ -3,6 +3,6 @@
 $expected     = array("include_once('include.php')", 
                       "require_once 'require_once.php'");
 
-$expected_not = array();
+$expected_not = array('include $include');
 
 ?>
