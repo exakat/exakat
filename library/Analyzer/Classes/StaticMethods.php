220102a9060c5fa64ab91fadca27979b6f663a2b
Damien Seguy <damien.seguy@gmail.com>
2014-04-07 17:37:47 +0800

    Added analyze spotting the methods.

---
+++ library/Analyzer/Classes/StaticMethods.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Analyzer\Classes;
+
+use Analyzer;
+
+class StaticMethods extends Analyzer\Analyzer {
+
+    public function analyze() {
+        $this->atomIs('Class')
+             ->outIs('BLOCK')
+             ->outIs('CODE')
+             ->outIs('ELEMENT')
+             ->atomIs('Function')
+             ->_as('function')
+             ->outIs('STATIC')
+             ->back('function');
+    }
+}
+
+?>
