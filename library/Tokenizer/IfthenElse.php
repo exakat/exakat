e9a98a41965903f38214a0a9ba2e238fb7f5e03a
Damien Seguy <damien.seguy@gmail.com>
2014-04-09 08:23:13 +0800

    Reverted half the removed code, as it was useful.

--- library/Tokenizer/IfthenElse.php
+++ library/Tokenizer/IfthenElse.php
@@ -6,7 +6,17 @@ class IfthenElse extends TokenAuto {
     public static $operators = array('T_ELSE');
     
     public function _check() {
- 
+        // else { /* block */ }
+        $this->conditions = array(  0 => array('token'     => IfthenElse::$operators,
+                                               'atom'      => 'none'),
+                                    1 => array('notAtom'   => array('Sequence', 'Void'),
+                                               'atom'      => 'yes'),
+                                    2 => array('filterOut' => Token::$instruction_ending),
+        );
+        
+        $this->actions = array( 'to_block_else' => true);
+        $this->checkAuto(); 
+
     // @doc else : endif (empty )
         $this->conditions = array( 0 => array('token' => 'T_ELSE'),
                                    1 => array('token' => 'T_COLON'),
