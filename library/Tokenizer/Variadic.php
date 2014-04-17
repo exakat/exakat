8c9fa288c47f4e6e51a4e69d9e694d284dfb1256
Damien Seguy <damien.seguy@gmail.com>
2014-04-17 09:15:01 +0800

    Initial version for Variadic (PHP 5.6). UT still missing.

---
+++ library/Tokenizer/Variadic.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tokenizer;
+
+class Variadic extends TokenAuto {
+    static public $operators = array('T_ELLIPSIS');
+
+    protected $phpversion = '5.6+';
+
+    public function _check() { 
+        print_r(__METHOD__);
+        $this->conditions = array( 0 => array('token' => Variadic::$operators,
+                                              'atom' => 'none'),
+                                   1 => array('atom' => array('Variable'))
+        );
+        
+        $this->actions = array('transform'    => array( 0 => 'DROP'),
+                               'propertyNext' => array('variadic' => 'true'),
+                               'fullcode'     => true,
+                               );
+        $this->checkAuto();
+
+        return $this->checkRemaining();
+    }
+
+    public function fullcode() {
+        return <<<GREMLIN
+
+fullcode.fullcode = "..." + fullcode.code; 
+
+GREMLIN;
+    }
+}
+
+?>
