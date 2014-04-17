6dda9c91a1b2e72ea1540903d9334a09487eb3d6
Damien Seguy <damien.seguy@gmail.com>
2014-04-18 07:23:21 +0800

    Support AS in traits 'use' de claration'.

--- tests/tokenizer/Test/_Trait.php
+++ tests/tokenizer/Test/_Trait.php
@@ -7,9 +7,11 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class _Trait extends Tokenizer {
-    /* 2 methods */
+    /* 4 methods */
 
     public function test_Trait01()  { $this->generic_test('_Trait.01'); }
     public function test_Trait02()  { $this->generic_test('_Trait.02'); }
+    public function test_Trait03()  { $this->generic_test('_Trait.03'); }
+    public function test_Trait04()  { $this->generic_test('_Trait.04'); }
 }
 ?>
---
+++ tests/tokenizer/source/_Trait.03.php
@@ -0,0 +1,17 @@
+<?php
+trait HelloWorld {
+    public function sayHello() {
+        echo 'Hello World!';
+    }
+}
+
+class TheWorldIsNotEnough {
+    use HelloWorld;
+    public function sayHello() {
+        echo 'Hello Universe!';
+    }
+}
+
+$o = new TheWorldIsNotEnough();
+$o->sayHello();
+?>
