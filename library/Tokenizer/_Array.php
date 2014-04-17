4dcfc5aef4a293bcf41193c803263d2a06f09b6c
Damien Seguy <damien.seguy@gmail.com>
2014-04-18 07:23:39 +0800

    Cleaned code

--- library/Tokenizer/_Array.php
+++ library/Tokenizer/_Array.php
@@ -7,13 +7,12 @@ class _Array extends TokenAuto {
     
     public function _check() {
         // $x[3]
-        $this->conditions = array( -1 => array('atom' => array('Variable', 'Array', 'Property', 'Staticproperty', 'Arrayappend', )),
+        $this->conditions = array( -1 => array('atom'  => array('Variable', 'Array', 'Property', 'Staticproperty', 'Arrayappend')),
                                     0 => array('token' => _Array::$operators),
-                                    1 => array('atom' => 'yes'),
+                                    1 => array('atom'  => 'yes'),
                                     2 => array('token' => array('T_CLOSE_BRACKET', 'T_CLOSE_CURLY')),
                                     3 => array('atom'  => 'none',
                                                'token' => 'T_OPEN_PARENTHESIS' ),
-                                    
                                  );
         
         $this->actions = array('transform'    => array(  -1 => 'VARIABLE', 
@@ -26,17 +25,17 @@ class _Array extends TokenAuto {
         $this->checkAuto(); 
 
         // $x[3]
-        $this->conditions = array( -1 => array('atom' => array('Variable', 'Array', 'Property', 'Staticproperty', 'Arrayappend', )),
+        $this->conditions = array( -1 => array('atom' => array('Variable', 'Array', 'Property', 'Staticproperty', 'Arrayappend')),
                                     0 => array('token' => _Array::$operators),
                                     1 => array('atom' => 'yes'),
                                     2 => array('token' => array('T_CLOSE_BRACKET', 'T_CLOSE_CURLY')),
                                  );
         
-        $this->actions = array('transform'   => array(  -1 => 'VARIABLE', 
-                                                         1 => 'INDEX',
-                                                         2 => 'DROP'),
-                               'atom'        => 'Array',
-                               'cleanIndex'  => true,
+        $this->actions = array('transform'    => array(  -1 => 'VARIABLE', 
+                                                          1 => 'INDEX',
+                                                          2 => 'DROP'),
+                               'atom'         => 'Array',
+                               'cleanIndex'   => true,
                                'add_to_index' => array('S_ARRAY' => 'S_ARRAY'),
                                );
         $this->checkAuto(); 
@@ -45,11 +44,13 @@ class _Array extends TokenAuto {
     }
 
     public function fullcode() {
-        return '
+        return <<<GREMLIN
 x = it;
 it.out("NAME").each { x.fullcode = it.fullcode }
 
-it.filter{ it.out("INDEX").count() == 1}.each{ x.fullcode = it.out("VARIABLE").next().fullcode + "[" + it.out("INDEX").next().fullcode + "]"; }';
+it.filter{ it.out("INDEX").count() == 1}.each{ x.fullcode = it.out("VARIABLE").next().fullcode + "[" + it.out("INDEX").next().fullcode + "]"; }
+
+GREMLIN;
     }
 }
 
