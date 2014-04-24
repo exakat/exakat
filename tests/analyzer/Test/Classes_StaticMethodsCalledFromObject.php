5a88692b1dc1d4f811a5d69838ab419c27d487a7
Damien Seguy <damien.seguy@gmail.com>
2014-04-24 08:47:27 +0800

    Added autoload_library to give access to configuration and version in tokens.

--- tests/analyzer/Test/Classes_StaticMethodsCalledFromObject.php
+++ tests/analyzer/Test/Classes_StaticMethodsCalledFromObject.php
@@ -5,6 +5,7 @@ namespace Test;
 include_once(dirname(dirname(dirname(__DIR__))).'/library/Autoload.php');
 spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
+spl_autoload_register('Autoload::autoload_library');
 
 class Classes_StaticMethodsCalledFromObject extends Analyzer {
     /* 1 methods */
--- tests/analyzer/Test/Type_Binary.php
+++ tests/analyzer/Test/Type_Binary.php
@@ -6,6 +6,7 @@ include_once(dirname(dirname(dirname(__DIR__))).'/library/Autoload.php');
 spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
+spl_autoload_register('Autoload::autoload_library');
 
 class Type_Binary extends Analyzer {
     /* 2 methods */
