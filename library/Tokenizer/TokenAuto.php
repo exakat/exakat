5f6a67ffead1688133a52552c8a6446f02d66cff
Damien Seguy <damien.seguy@gmail.com>
2014-04-25 10:20:20 +0800

    Prevent early merge with namespaces.

--- library/Tokenizer/TokenAuto.php
+++ library/Tokenizer/TokenAuto.php
@@ -1526,7 +1526,7 @@ if (it.out('NEXT').has('atom', 'Sequence').any()) {
 // lone instruction AFTER
 if (it.out('NEXT').filter{ it.atom in ['RawString', 'For', 'Phpcode', 'Function', 'Ifthen', 'Switch', 'Foreach', 
                                        'Dowhile', 'Try', 'Class', 'Interface', 'Trait', 'While', 'Break', 'Assignation', 'Halt',
-                                       'Staticmethodcall' ] && it.token != 'T_ELSEIF' }.any() &&
+                                       'Staticmethodcall', 'Namespace' ] && it.token != 'T_ELSEIF' }.any() &&
     it.out('NEXT').out('NEXT').filter{!(it.token in ['T_CATCH', 'T_OBJECT_OPERATOR', 'T_DOUBLE_COLON' ,
                                                      'T_AND', 'T_LOGICAL_AND', 'T_BOOLEAN_AND', 'T_ANDAND',
                                                      'T_OR' , 'T_LOGICAL_OR' , 'T_BOOLEAN_OR', 'T_OROR',
