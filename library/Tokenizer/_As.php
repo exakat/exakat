27220efbcdb7bb79280804b7c50c8b0e07dc02d3
Damien Seguy <damien.seguy@gmail.com>
2014-04-25 10:19:42 +0800

    Added back AS for PHP 5.3, as it is needed for use a as b, c as d;

--- library/Tokenizer/_As.php
+++ library/Tokenizer/_As.php
@@ -5,8 +5,6 @@ namespace Tokenizer;
 class _As extends TokenAuto {
     static public $operators = array('T_AS');
 
-    protected $phpversion = "5.4+";
-
     public function _check() {
         $this->conditions = array( -1 => array('atom'  => 'Staticconstant'), 
                                     0 => array('token' => _As::$operators,
@@ -14,11 +12,24 @@ class _As extends TokenAuto {
                                     1 => array('token' => 'T_STRING')
         );
         
-        $this->actions = array('makeEdge'     => array(  1 => 'RIGHT',
-                                                        -1 => 'LEFT'),
+        $this->actions = array('makeEdge'     => array( 1 => 'RIGHT',
+                                                       -1 => 'LEFT'),
                                'atom'         => 'As',
                                'cleanIndex'   => true,
                                'makeSequence' => 'it' );
+        $this->checkAuto();
+        
+        $this->conditions = array( -2 => array('notToken' => array('T_NS_SEPARATOR', 'T_USE')),
+                                   -1 => array('atom'  => array('Namespace', 'Identifier')), 
+                                    0 => array('token' => _As::$operators,
+                                               'atom'  => 'none'),
+                                    1 => array('token' => 'T_STRING')
+        );
+        
+        $this->actions = array('makeEdge'     => array( 1 => 'RIGHT',
+                                                       -1 => 'LEFT'),
+                               'atom'         => 'As',
+                               'cleanIndex'   => true);
         $this->checkAuto();
         
         return $this->checkRemaining();
