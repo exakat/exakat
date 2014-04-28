5d7e743b6d171118d4d6ee57452e2ae197a86d04
Damien Seguy <damien.seguy@gmail.com>
2014-04-28 20:37:56 +0800

    Support Halt in php 5.4 and 5.3

--- library/Tokenizer/Halt.php
+++ library/Tokenizer/Halt.php
@@ -9,15 +9,23 @@ class Halt extends TokenAuto {
         $this->conditions = array(0 => array('token' => Halt::$operators,
                                              'atom'  => 'none'),
                                   1 => array('token' => 'T_OPEN_PARENTHESIS'),
-                                  2 => array('atom'  =>  array('Arguments', 'Void')),
-                                  3 => array('token' => 'T_CLOSE_PARENTHESIS'),
+                                  2 => array('token' => 'T_CLOSE_PARENTHESIS'),
                                   );
         
-        $this->actions = array('makeEdge'     => array(2 => 'ARGUMENTS'),
-                               'dropNext'     => array(1),
+        $this->actions = array('transform'    => array(2 => 'DROP',
+                                                       1 => 'DROP'),
                                'atom'         => 'Halt',
                                'makeSequence' => 'it'
                                );
+        $this->checkAuto();
+
+        $this->conditions = array(0 => array('token' => Halt::$operators,
+                                             'atom'  => 'none'),
+                                  1 => array('notToken' => 'T_OPEN_PARENTHESIS')
+                                  );
+        
+        $this->actions = array('atom'         => 'Halt',
+                               'makeSequence' => 'it');
         $this->checkAuto();
         
         return $this->checkRemaining();
@@ -26,7 +34,8 @@ class Halt extends TokenAuto {
     public function fullcode() {
         return <<<GREMLIN
 
-fullcode.setProperty('fullcode', fullcode.code + "()");
+fullcode.setProperty('fullcode', fullcode.code);
+
 GREMLIN;
     }
 }
