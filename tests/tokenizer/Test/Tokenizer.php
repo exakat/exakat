d493cc4970cbfcb04b8cb4113dedb2d5403ef260
Damien Seguy <damien.seguy@gmail.com>
2014-04-30 07:41:44 +0800

    Batch tests by type (All Sign.* are loaded at the same time).

--- tests/tokenizer/Test/Tokenizer.php
+++ tests/tokenizer/Test/Tokenizer.php
@@ -3,12 +3,17 @@
 namespace Test;
 
 class Tokenizer extends \PHPUnit_Framework_TestCase {
-    public function setUp() {
-        shell_exec("cd ../..; php bin/delete -all");
-    }
-
+    public static $loaded = 'none';
+    
     public function generic_test($file) {
-        $shell = 'cd ../..; php bin/load -q -f ./tests/tokenizer/source/'.$file.'.php -p test; php bin/build_root -p test; php bin/tokenizer -p test; php bin/export -text -f ./tests/tokenizer/source/'.$file.'.php';
+        $test = substr($file, 0, -3);
+        if (Tokenizer::$loaded != $test) {
+            $shell = 'cd ../..; sh scripts/clean.sh; php bin/load -q -r -f ./tests/tokenizer/source/'.$test.'*.php -p test; php bin/build_root -p test; php bin/tokenizer -p test;';
+            Tokenizer::$loaded = $test;
+            $res = shell_exec($shell);
+        }
+        
+        $shell = 'cd ../..; php bin/export -text -f ./tests/tokenizer/source/'.$file.'.php';
         $res = shell_exec($shell);
         
         $exp = file_get_contents('exp/'.$file.'.txt');
