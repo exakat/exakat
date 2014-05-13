87606898dfc60747c23f13ec665d83a5ec331ac5
Damien Seguy <damien.seguy@gmail.com>
2014-05-13 11:01:06 +0800

    Added an analyzer to spot abstract classes, with UT

---
+++ library/Analyzer/Classes/Abstractclass.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Analyzer\Classes;
+
+use Analyzer;
+
+class Abstractclass extends Analyzer\Analyzer {
+
+    public function analyze() {
+        $this->atomIs("Class")
+             ->outIs('ABSTRACT')
+             ->back('first');
+    }
+}
+
+?>
