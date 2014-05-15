4f5625e2a5c401163b9240970652a376cd56b23d
Damien Seguy <damien.seguy@gmail.com>
2014-05-15 14:13:37 +0800

    Now calculating the lone tokens (those that are not related to anything)

--- library/Stats.php
+++ library/Stats.php
@@ -35,13 +35,14 @@ class Stats {
     }
 
     function collect($type = null) {
-        $this->stats['tokens_count']  = $this->queryOne("g.V.except([g.v(0)]){$this->file_filter}.count()");
-        $this->stats['relations_count']  = $this->queryOne("g.E.except([g.v(0)]){$this->file_filter}.count()");
-        $this->stats['atoms_count']   = $this->queryOne("g.V.except([g.v(0)]).hasNot('atom', 'null'){$this->file_filter}.count()");
-        $this->stats['NEXT_count']    = $this->queryOne("g.E.has('label', 'NEXT').inV{$this->file_filter}.count()");
-        $this->stats['INDEXED_count'] = $this->queryOne("g.E.has('label', 'INDEXED').inV{$this->file_filter}.count()");
-        $this->stats['file_count']    = $this->queryOne("m = [:]; g.V.inE('FILE').file.groupCount(m).iterate(); m.size();");
-        $this->stats['no_fullcode']    = $this->queryOne("g.V.except([g.v(0)]).has('fullcode', null).hasNot('index', 'true').filter{!(it.token in ['E_FILE', 'E_NAMESPACE', 'E_CLASS', 'E_FUNCTION'])}.count();");
+        $this->stats['tokens_count']    = $this->queryOne("g.V.except([g.v(0)]){$this->file_filter}.count()");
+        $this->stats['relations_count'] = $this->queryOne("g.E.except([g.v(0)]){$this->file_filter}.count()");
+        $this->stats['atoms_count']     = $this->queryOne("g.V.except([g.v(0)]).hasNot('atom', 'null'){$this->file_filter}.count()");
+        $this->stats['NEXT_count']      = $this->queryOne("g.E.has('label', 'NEXT').inV{$this->file_filter}.count()");
+        $this->stats['INDEXED_count']   = $this->queryOne("g.E.has('label', 'INDEXED').inV{$this->file_filter}.count()");
+        $this->stats['file_count']      = $this->queryOne("m = [:]; g.V.inE('FILE').file.groupCount(m).iterate(); m.size();");
+        $this->stats['no_fullcode']     = $this->queryOne("g.V.except([g.v(0)]).has('fullcode', null).hasNot('index', 'true').filter{!(it.token in ['E_FILE', 'E_NAMESPACE', 'E_CLASS', 'E_FUNCTION'])}.count();");
+        $this->stats['lone_token']      = $this->queryOne("g.V.hasNot('atom', null).hasNot('atom', 'File').filter{ it.in.count() == 0}.count()");
     }
     
     function queryOne($query) {
