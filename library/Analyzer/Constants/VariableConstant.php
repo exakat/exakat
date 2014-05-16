5cbad97f146a6723fa0951a636f1c7a632864ce5
Damien Seguy <damien.seguy@gmail.com>
2014-05-16 11:14:29 +0800

    Added UT for inventory of variable constant (constant fetched by constant() function call).

---
+++ library/Analyzer/Constants/VariableConstant.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Analyzer\Constants;
+
+use Analyzer;
+
+class VariableConstant extends Analyzer\Analyzer {
+
+    public function analyze() {
+        $this->atomIs("Functioncall")
+             ->code('constant')
+             ->outIs('ARGUMENTS')
+             ->outIs('ARGUMENT')
+             ;
+    }
+}
+
+?>
---
+++ tests/analyzer/source/Constants_VariableConstant.01.php
@@ -0,0 +1,6 @@
+<?php 
+
+$x = constant('PHP_VERSION');
+print $x;
+
+?>
