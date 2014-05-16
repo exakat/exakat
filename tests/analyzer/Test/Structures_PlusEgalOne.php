7f2ba0982f66e0e9ba06b21b01958a23454f0499
Damien Seguy <damien.seguy@gmail.com>
2014-05-16 16:32:05 +0800

    Added extra check (the operation on the right must be an addition :D).

--- tests/analyzer/Test/Structures_PlusEgalOne.php
+++ tests/analyzer/Test/Structures_PlusEgalOne.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_PlusEgalOne extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testStructures_PlusEgalOne01()  { $this->generic_test('Structures_PlusEgalOne.01'); }
+    public function testStructures_PlusEgalOne02()  { $this->generic_test('Structures_PlusEgalOne.02'); }
 }
 ?>
