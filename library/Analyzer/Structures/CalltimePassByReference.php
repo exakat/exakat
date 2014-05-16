89f7f3b87fc7a6a5749f5ecf9ea30cf108d07732
Damien Seguy <damien.seguy@gmail.com>
2014-05-16 14:19:40 +0800

    Set the php version limitation

--- library/Analyzer/Structures/CalltimePassByReference.php
+++ library/Analyzer/Structures/CalltimePassByReference.php
@@ -7,6 +7,8 @@ use Analyzer;
 class CalltimePassByReference extends Analyzer\Analyzer {
     protected $severity  = \Analyzer\Analyzer::S_MINOR;
     protected $timeToFix = \Analyzer\Analyzer::T_SLOW;
+    
+    public $phpversion = "5.4-";
 
     public function analyze() {
         $this->atomIs("Functioncall")
