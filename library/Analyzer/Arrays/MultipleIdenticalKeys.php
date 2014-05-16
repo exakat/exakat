79f3d3af2c6329b2b988328fffb1d84180ca4236
Damien Seguy <damien.seguy@gmail.com>
2014-05-16 21:41:40 +0800

    Upgraded MultipleIdenticalKeys to has to use full code, not code (good for structured values).

--- library/Analyzer/Arrays/MultipleIdenticalKeys.php
+++ library/Analyzer/Arrays/MultipleIdenticalKeys.php
@@ -9,13 +9,7 @@ class MultipleIdenticalKeys extends Analyzer\Analyzer {
     public function analyze() {
         $this->atomIs("Functioncall")
              ->code('array')
-             ->raw("aggregate().findAll{ m = [:]; it.out('ARGUMENTS').out('ARGUMENT').has('atom', 'Keyvalue').out('KEY').groupCount(m){it.code}.cap.next().findAll{it.value > 1}.size() > 0}");
-//        $this->printQuery();
-
-//g.V.filter{ it.in("ANALYZED").has("code", 'Analyzer\\Arrays\\MultipleIdenticalKeys').count() == 0}.has("atom", 'Functioncall').as("first").
-//filter{ it.code.toLowerCase() == 'array'}.
-//aggregate(x).findAll{ m = [:]; it.out('ARGUMENTS').out('ARGUMENT').has("atom", 'Keyvalue').out('KEY').groupCount(m){it.code}.cap.next().findAll{it.value > 1}.size() > 0}
-
+             ->raw("aggregate().findAll{ m = [:]; it.out('ARGUMENTS').out('ARGUMENT').has('atom', 'Keyvalue').out('KEY').groupCount(m){it.fullcode}.cap.next().findAll{it.value > 1}.size() > 0}");
     }
 }
 
--- tests/analyzer/Test/Arrays_MultipleIdenticalKeys.php
+++ tests/analyzer/Test/Arrays_MultipleIdenticalKeys.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Arrays_MultipleIdenticalKeys extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testArrays_MultipleIdenticalKeys01()  { $this->generic_test('Arrays_MultipleIdenticalKeys.01'); }
+    public function testArrays_MultipleIdenticalKeys02()  { $this->generic_test('Arrays_MultipleIdenticalKeys.02'); }
 }
 ?>
