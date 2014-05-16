4504f26738032e7d79fc878d358d6d57060ad923
Damien Seguy <damien.seguy@gmail.com>
2014-05-16 11:01:10 +0800

    Fixed case name of the test.

--- tests/analyzer/Test/Classes_Finalclass.php
+++ tests/analyzer/Test/Classes_Finalclass.php
@@ -7,9 +7,9 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
-class Classes_FinalClass extends Analyzer {
+class Classes_Finalclass extends Analyzer {
     /* 1 methods */
 
-    public function testClasses_FinalClass01()  { $this->generic_test('Classes_FinalClass.01'); }
+    public function testClasses_Finalclass01()  { $this->generic_test('Classes_Finalclass.01'); }
 }
 ?>
