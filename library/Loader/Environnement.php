9b57cf5885af4e94a02e9a768d91f86a7af8b16a
Damien Seguy <damien.seguy@gmail.com>
2014-05-19 10:11:23 +0800

    Emptied Environnement's class. Will need to be fully removed.

--- library/Loader/Environnement.php
+++ library/Loader/Environnement.php
@@ -13,46 +13,21 @@ class Environnement {
     private $functions = null;
     private $namespaces = null;
     
-    function __construct($client, $file) {
-        $this->client = $client;
-        $this->file = $file;
-        
-        $this->files = new E_file($client, $file);
-        $this->classes = new E_class($client, $file);
-        $this->functions = new E_function($client, $file);
-        $this->namespaces = new E_namespace($client, $file);
-    }
+    function __construct($client, $file) {    }
     
-    public function init($index) {
-        $this->classes->init($index);
-        $this->files->init($index);
-        $this->functions->init($index);
-        $this->namespaces->init($index);
-
+    public function init($index) { 
         return true;
     }
     
     public function relateTo($node) {
-        $this->classes->relateTo($node);
-        $this->files->relateTo($node);
-        $this->functions->relateTo($node);
-        $this->namespaces->relateTo($node);
-
         return true;
     }
     
     function process($token, $token_name) {
-        $this->classes->process($token, $token_name);
-        $this->files->process($token, $token_name);
-        $this->functions->process($token, $token_name);
-        $this->namespaces->process($token, $token_name);
-        
         return true;
     }
 
     public function initFile() {
-        $this->files->init();
-
         return true;
     }
 }
