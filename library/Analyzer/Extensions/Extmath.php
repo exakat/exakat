3bec2efedcdebd802c7735336ff90446acc8d04f
Damien Seguy <damien.seguy@gmail.com>
2014-05-21 10:20:41 +0800

    Some forgotten UT files.

--- library/Analyzer/Extensions/Extmath.php
+++ library/Analyzer/Extensions/Extmath.php
@@ -7,7 +7,7 @@ use Analyzer;
 class Extmath extends Analyzer\Common\Extension {
 
     public function analyze() {
-        $this->source = 'array.ini';
+        $this->source = 'math.ini';
         
         parent::analyze();
     }
