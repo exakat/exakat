9e3cd1634d8f2e810df13d3fa5a038d610654145
Damien Seguy <damien.seguy@gmail.com>
2014-05-22 10:51:02 +0800

    Added filter with Static Methods.

--- library/Analyzer/Classes/StaticMethodsCalledFromObject.php
+++ library/Analyzer/Classes/StaticMethodsCalledFromObject.php
@@ -6,14 +6,20 @@ use Analyzer;
 
 class StaticMethodsCalledFromObject extends Analyzer\Analyzer {
     public function dependsOn() {
-        return array("Analyzer\\Classes\\MethodDefinition");
+        return array("Analyzer\\Classes\\MethodDefinition",
+                     "Analyzer\\Classes\\StaticMethods");
     }
 
     public function analyze() {
         $this->atomIs('Methodcall')
              ->outIs('METHOD')
-             ->raw("filter{ x = it;  g.V.has('atom', 'Function').out('NAME').filter{it.code.toLowerCase() == x.code.toLowerCase()}.filter{ it.in('ANALYZED').has('code', 'Analyzer\\\\Classes\\\\StaticMethods').any()}.any() }")
+             ->raw("filter{ x = it;  g.V.has('atom', 'Function').out('NAME').
+                                         filter{it.code.toLowerCase() == x.code.toLowerCase()}.
+                                         filter{ it.in('ANALYZED').has('code', 'Analyzer\\\\Classes\\\\MethodDefinition').any()}.
+                                         filter{ it.in('ANALYZED').has('code', 'Analyzer\\\\Classes\\\\StaticMethods').any()}.
+                                         any() }")
              ->back('first');
+//        $this->printQuery();
     }
 }
 
