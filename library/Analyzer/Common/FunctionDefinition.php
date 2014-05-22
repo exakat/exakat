7cc32dc24a57d6f5349f8db8eb517696bd5cfbf9
Damien Seguy <damien.seguy@gmail.com>
2014-05-22 11:55:05 +0800

    Fixed 'function definition' spotting.

--- library/Analyzer/Common/FunctionDefinition.php
+++ library/Analyzer/Common/FunctionDefinition.php
@@ -7,8 +7,19 @@ use Analyzer;
 class FunctionDefinition extends Analyzer\Analyzer {
     protected $functions = array();
     
+    public function dependsOn() {
+        return array('Analyzer\\Classes\\MethodDefinition',
+                     'Analyzer\\Interfaces\\InterfaceMethod',
+                     'Analyzer\\Traits\\TraitMethod');
+    }
+    
     public function analyze() {
         $this->atomIs("Function")
+             ->outIs('NAME')
+             ->analyzerIsNot('Analyzer\\Classes\\MethodDefinition')
+             ->analyzerIsNot('Analyzer\\Interfaces\\InterfaceMethod')
+             ->analyzerIsNot('Analyzer\\Traits\\TraitMethod')
+
              ->code($this->functions, false);
     }
 }
