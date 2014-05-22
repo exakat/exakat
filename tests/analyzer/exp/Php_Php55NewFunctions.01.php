7a5e7cc4950db1739c00e26425e0ad46739dc041
Damien Seguy <damien.seguy@gmail.com>
2014-05-22 11:28:14 +0800

    Added support for new PHP 5.5 functions

---
+++ tests/analyzer/exp/Php_Php55NewFunctions.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('cli_GET_process_title', 
+                      'BOOLVAL', 
+                      'array_column');
+
+$expected_not = array('curl_escape',
+                      'curl_share_close');
+
+?>
