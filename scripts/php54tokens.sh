8b39ce0212c8927155e823a0b1a276b08b2e16cc
Damien Seguy <damien.seguy@gmail.com>
2014-05-27 12:15:23 +0800

    Upgraded php..tokens to support spaces.

--- scripts/php54tokens.sh
+++ scripts/php54tokens.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
-php54 -r 'echo count(token_get_all(file_get_contents("'$1'")))."\n";' || true
+php54 -r "echo count(token_get_all(file_get_contents('$1'))).\"\n\";" || true
  
--- scripts/php55tokens.sh
+++ scripts/php55tokens.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
-php55 -r 'echo count(token_get_all(file_get_contents("'$1'")))."\n";' || true
+php55 -r "echo count(token_get_all(file_get_contents('$1'))).\"\n\";" || true
  
