560532df070d10697602ca5218ed46b709c53ab8
Damien Seguy <damien.seguy@gmail.com>
2014-05-29 10:45:20 +0800

    Changed function identifying algorithm.

--- library/Analyzer/Functions/Functionnames.php
+++ library/Analyzer/Functions/Functionnames.php
@@ -9,7 +9,7 @@ class Functionnames extends Analyzer\Analyzer {
     public function analyze() {
         $this->atomIs("Function")
              ->isNot('lambda')
-             ->classIs('Global')
+             ->hasNoParent('Class', array('ELEMENT', 'CODE', 'BLOCK'))
              ->hasNoParent('Interface', array('ELEMENT', 'CODE', 'BLOCK'))
              ->hasNoParent('Trait', array('ELEMENT', 'CODE', 'BLOCK'))
              ->outIs('NAME');
