953aa2096132c62887c25d10d03c7ac089c2110b
Damien Seguy <damien.seguy@gmail.com>
2014-05-29 19:03:24 +0800

    Cleaned comment, g.V => g.idx()

--- library/Analyzer/Arrays/AmbiguousKeys.php
+++ library/Analyzer/Arrays/AmbiguousKeys.php
@@ -10,12 +10,7 @@ class AmbiguousKeys extends Analyzer\Analyzer {
         $this->atomIs("Functioncall")
              ->code('array')
              ->raw("aggregate().findAll{ it.out('ARGUMENTS').out('ARGUMENT').has('atom', 'Keyvalue').out('KEY').groupBy{if (it.hasNot('delimiter', null).count() > 0) { it.noDelimiter; } else { it.code ;}}{it.atom}{it.unique().toList().size()}.cap.next().findAll{it.value > 1}.size() > 0}");
-//        $this->printQuery();
-
-//g.V.filter{ it.in("ANALYZED").has("code", 'Analyzer\\Arrays\\MultipleIdenticalKeys').count() == 0}.has("atom", 'Functioncall').as("first").
-//filter{ it.code.toLowerCase() == 'array'}.
-//aggregate(x).findAll{ m = [:]; it.out('ARGUMENTS').out('ARGUMENT').has("atom", 'Keyvalue').out('KEY').groupCount(m){it.code}.cap.next().findAll{it.value > 1}.size() > 0}
-
+        $this->prepareQuery();
     }
 }
 
