157af94c673683c3d86e723f49f839d38a88c853
Damien Seguy <damien.seguy@gmail.com>
2014-06-01 22:40:52 +0800

    Spot code where several instructions are in the same line.

---
+++ library/Analyzer/Structures/OneLineTwoInstructions.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Analyzer\Structures;
+
+use Analyzer;
+
+class OneLineTwoInstructions extends Analyzer\Analyzer {
+    public function analyze() {
+        $this->atomIs("Sequence")
+             ->outIs('ELEMENT')
+             ->_as('report')
+             ->savePropertyAs('line', 'line_number')
+             ->nextSibling()
+             ->samePropertyAs('line', 'line_number')
+             ->back('report');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Structures_OneLineTwoInstructions.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('$a++',
+                      '$b++',
+                      '$d++',
+                      'h($i)');
+
+$expected_not = array('$c++',
+                      '$e = $f + 1',
+                      );
+
+?>
