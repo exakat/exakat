c4a178293f2bb25c07a9868e64724c7df1c8d7d2
Damien Seguy <damien.seguy@gmail.com>
2014-06-08 20:26:27 +0800

    Removed comments.

--- library/Tokenizer/Keyvalue.php
+++ library/Tokenizer/Keyvalue.php
@@ -7,17 +7,14 @@ class Keyvalue extends TokenAuto {
     static public $atom = 'Keyvalue';
 
     public function _check() {
-        $this->conditions = array(/*-2 => array('filterOut' => array_merge(array( 'T_NS_SEPARATOR', 'T_DOT', 'T_DOUBLE_COLON', 'T_OBJECT_OPERATOR',  ),
-                                                                         Addition::$operators, Multiplication::$operators, Comparison::$operators)), */
-                                  -2 => array('token' => array('T_OPEN_PARENTHESIS', 'T_COMMA', 'T_AS', 'T_OPEN_BRACKET')),
-                                  -1 => array('atom' => 'yes', 'notAtom' => 'Arguments'),
+        $this->conditions = array(-2 => array('token' => array('T_OPEN_PARENTHESIS', 'T_COMMA', 'T_AS', 'T_OPEN_BRACKET')),
+                                  -1 => array('atom' => 'yes', 
+                                              'notAtom' => 'Arguments'),
                                    0 => array('token' => Keyvalue::$operators),
-                                   1 => array('atom' => 'yes', 'notAtom' => 'Arguments'),
+                                   1 => array('atom' => 'yes', 
+                                              'notAtom' => 'Arguments'),
                                    2 => array('token' => array('T_CLOSE_PARENTHESIS', 'T_COMMA', 'T_CLOSE_BRACKET')),
-                                   /*2 => array('filterOut2' => array_merge( Assignation::$operators, Addition::$operators, Multiplication::$operators, Comparison::$operators, 
-                                            array('T_OPEN_BRACKET', 'T_OBJECT_OPERATOR', 'T_INC', 'T_DEC', 'T_NS_SEPARATOR',
-                                                  'T_OPEN_PARENTHESIS', 'T_OPEN_CURLY', 'T_DOT', 'T_DOUBLE_COLON', 'T_QUESTION', )))*/
-                                                  );
+                                  );
         
         $this->actions = array('transform'  => array(-1 => 'KEY',
                                                       1 => 'VALUE'),
