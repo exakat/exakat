2c8681a17f935423ae601d1804571d515d512a04
Damien Seguy <damien.seguy@gmail.com>
2014-06-11 15:45:33 +0800

    Fixed the line counting patch, as it was wrecking havoc in the loading process.

--- tests/analyzer/Test/Structures_OneLineTwoInstructions.php
+++ tests/analyzer/Test/Structures_OneLineTwoInstructions.php
@@ -8,9 +8,10 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_OneLineTwoInstructions extends Analyzer {
-    /* 2 methods */
+    /* 3 methods */
 
     public function testStructures_OneLineTwoInstructions01()  { $this->generic_test('Structures_OneLineTwoInstructions.01'); }
     public function testStructures_OneLineTwoInstructions02()  { $this->generic_test('Structures_OneLineTwoInstructions.02'); }
+    public function testStructures_OneLineTwoInstructions03()  { $this->generic_test('Structures_OneLineTwoInstructions.03'); }
 }
 ?>
