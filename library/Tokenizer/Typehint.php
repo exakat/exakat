0091e7c284f5df7eb72a78a4b4b3bd1a980d732d
Damien Seguy <damien.seguy@gmail.com>
2014-06-12 22:53:29 +0800

    Fixed fullcode.

--- library/Tokenizer/Typehint.php
+++ library/Tokenizer/Typehint.php
@@ -35,7 +35,7 @@ class Typehint extends TokenAuto {
     public function fullcode() {
         return <<<GREMLIN
 
-fullcode.setProperty('fullcode', fullcode.out("CLASS").next().getProperty("code") + " " + fullcode.out("VARIABLE").next().getProperty('fullcode')); 
+fullcode.setProperty('fullcode', fullcode.out("CLASS").next().getProperty("fullcode") + " " + fullcode.out("VARIABLE").next().getProperty('fullcode')); 
 
 GREMLIN;
     }
