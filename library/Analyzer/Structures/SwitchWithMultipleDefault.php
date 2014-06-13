4df6a54ae7a3bcb1bae7ced808181f0bbadbe6ff
Damien Seguy <damien.seguy@gmail.com>
2014-06-13 10:45:41 +0800

    Added a filter on cases' type.

--- library/Analyzer/Structures/SwitchWithMultipleDefault.php
+++ library/Analyzer/Structures/SwitchWithMultipleDefault.php
@@ -7,7 +7,7 @@ use Analyzer;
 class SwitchWithMultipleDefault extends Analyzer\Analyzer {
     public function analyze() {
         $this->atomIs("Switch")
-             ->raw('filter{ it.out("CASES").out("ELEMENT").count() > 1}')
+             ->raw('filter{ it.out("CASES").out("ELEMENT").has("atom", "Default").count() > 1}')
              ->back('first');
         $this->prepareQuery();
     }
