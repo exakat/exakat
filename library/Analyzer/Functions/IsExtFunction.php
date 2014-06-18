e8bc024f83b2440edd8a41aa624783ca27345b2a
Damien Seguy <damien.seguy@gmail.com>
2014-06-18 12:24:46 +0800

    Added a test to spot ext / functions in the code.

---
+++ library/Analyzer/Functions/IsExtFunction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Analyzer\Functions;
+
+use Analyzer;
+
+class IsExtFunction extends Analyzer\Analyzer {
+
+    public function analyze() {
+        $exts = glob('library/Analyzer/Extensions/*.php');
+        $exts[] = 'php_functions.ini';
+        
+        $functions = array();
+        foreach($exts as $ext) {
+            $inifile = str_replace('library/Analyzer/Extensions/Ext', '', str_replace('.php', '.ini', $ext));
+            if ($inifile == 'library/Analyzer/Extensions/Used.ini') { continue; }
+            $ini = $this->loadIni($inifile);
+            
+            if (!isset($ini['functions']) || !is_array($ini['functions'])) {
+                print "No functions defined in $inifile\n";
+            } else {
+                if (!empty($ini['functions'][0])) {
+                    $functions = array_merge($functions, $ini['functions']);
+                }
+            }
+        }
+
+        $functions = array_map(function ($functions) { return '\\'.strtolower($functions); }, $functions);
+        
+        $this->atomIs('Functioncall')
+             ->tokenIsNot(array('T_VARIABLE','T_OPEN_BRACKET'))
+             ->fullnspath($functions);
+    }
+}
+
+?>
