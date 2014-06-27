83a495e8df06345fcf04250eb6602a945d5a33d8
Damien Seguy <damien.seguy@gmail.com>
2014-06-27 17:56:40 +0800

    Fixed UT.

--- tests/analyzer/exp/Classes_MutualExtension.01.php
+++ tests/analyzer/exp/Classes_MutualExtension.01.php
@@ -1,7 +1,8 @@
 <?php
 
 $expected     = array('class b extends a', 
-                      'class g extends e');
+                      'class g extends e',
+                      'class g2 extends \\e2');
 
 $expected_not = array();
 
