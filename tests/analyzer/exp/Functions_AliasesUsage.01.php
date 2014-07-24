0f919b289861e2c2412913e2384b31fb6b1aafe7
Damien Seguy <damien.seguy@gmail.com>
2014-07-24 10:09:14 +0200

    Fixed result.

--- tests/analyzer/exp/Functions_AliasesUsage.01.php
+++ tests/analyzer/exp/Functions_AliasesUsage.01.php
@@ -1,7 +1,7 @@
 <?php
 
 $expected     = array('chop(1)',
-                      'ini_ALTER(1,2)');
+                      'ini_ALTER(1, 2)');
 
 $expected_not = array('pos()',
                       'number_format(1, 2, 3, 4, 5)');
