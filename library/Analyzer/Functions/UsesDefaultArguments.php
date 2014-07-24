d386d697e9ad4fa3fd0983f2a94bb5e41e34e5f1
Damien Seguy <damien.seguy@gmail.com>
2014-07-24 16:50:39 +0200

    Now spots methods that are called with default functions. This should be avoided.

---
+++ library/Analyzer/Functions/UsesDefaultArguments.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Analyzer\Functions;
+
+use Analyzer;
+
+class UsesDefaultArguments extends Analyzer\Analyzer {
+    public function analyze() {
+        $data = new \Data\Methods();
+        $functions = $data->getFunctionsArgsInterval();
+
+        $positions = array(array(), array(), array(), array(), array());
+        foreach($functions as $function) {
+            if ($function['args_min'] == $function['args_max']) { continue; }
+            if ($function['args_max'] == 100) { continue; }
+            for ($i = $function['args_min'] + 1; $i <= $function['args_max']; $i++) {
+                $positions[$i - 1][] = '\\'.$function['name'];
+            }
+        }
+        
+        foreach($positions as $position => $f) {
+            $this->atomIs("Functioncall")
+                 ->hasNoIn('METHOD')
+                 ->fullnspath($f)
+                 ->outIs('ARGUMENTS')
+                 ->noChildWithOrder('ARGUMENT', $position)
+                 ->back('first');
+            $this->prepareQuery();
+        }
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Functions_UsesDefaultArguments.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('\\GETOPT(1)', 
+                      '\\getopt( )', 
+                      'CRYpt(1)', 
+                      'crypt( )');
+
+$expected_not = array('phpinfo( )',
+                      'assert_options( )');
+
+?>
