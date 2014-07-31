22031372478e8174c07ec475ee711b00b9e35761
Damien Seguy <damien.seguy@gmail.com>
2014-07-31 08:37:58 +0200

    Added T_STRING to prevent Block formation (this may be an array, build on top of a property).

--- library/Tokenizer/Block.php
+++ library/Tokenizer/Block.php
@@ -10,7 +10,27 @@ class Block extends TokenAuto {
     // @doc Block
         $this->conditions = array( -1 => array('filterOut2' => array('T_VARIABLE', 'T_DOLLAR', 'T_CLOSE_CURLY', 'T_OPEN_CURLY',
                                                                      'T_OPEN_BRACKET', 'T_CLOSE_BRACKET',  // $x[1]{3}, 
-                                                                     'T_OBJECT_OPERATOR', 'T_DOUBLE_COLON', 'T_AT')), //'T_CLOSE_PARENTHESIS' 
+                                                                     'T_OBJECT_OPERATOR', 'T_DOUBLE_COLON', 'T_AT',
+                                                                     'T_STRING')), 
+                                    0 => array('token'      => Block::$operators),
+                                    1 => array('atom'       => 'yes',
+                                               'notAtom'    => 'SequenceCaseDefault'),
+                                    2 => array('token'      => 'T_CLOSE_CURLY',
+                                               'atom'       => 'none'),
+        );
+        
+        $this->actions = array('to_block'     => true,
+                               'atom'         => 'Sequence',
+                               'cleanIndex'   => true,
+                               'makeSequence' => 'it',
+                               'property'     => array('bracket' => 'true')
+                               );
+        $this->checkAuto(); 
+
+   // @doc interface xxxx { /**/ }
+        $this->conditions = array( -2 => array('token'      => array('T_EXTENDS', 'T_IMPLEMENTS', 'T_INTERFACE', 'T_CLASS',
+                                                                     'T_NAMESPACE', 'T_TRAIT', 'T_USE')), 
+                                   -1 => array('token'      => 'T_STRING'), 
                                     0 => array('token'      => Block::$operators),
                                     1 => array('atom'       => 'yes',
                                                'notAtom'    => 'SequenceCaseDefault'),
