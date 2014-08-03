0e390c4c127caf4915c48932fc468e89f891f64f
Damien Seguy <damien.seguy@gmail.com>
2014-08-03 23:31:49 +0200

    Added export for argument by reference or by value.

--- library/Data/Methods.php
+++ library/Data/Methods.php
@@ -32,6 +32,42 @@ class Methods {
         
         return $return;
     }
+
+    public function getFunctionsReferenceArgs() {
+        $query = "SELECT name AS function, 0 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg0 = 'reference' UNION
+                  SELECT name AS function, 1 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg1 = 'reference' UNION
+                  SELECT name AS function, 2 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg2 = 'reference' UNION
+                  SELECT name AS function, 3 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg3 = 'reference' UNION
+                  SELECT name AS function, 4 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg4 = 'reference' UNION
+                  SELECT name AS function, 5 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg5 = 'reference'
+                  ";
+        $res = $this->sqlite->query($query);
+        $return = array();
+        
+        while($row = $res->fetchArray(SQLITE3_ASSOC)) {
+            $return[] = $row;
+        }
+        
+        return $return;
+    }
+
+    public function getFunctionsValueArgs() {
+        $query = "SELECT name AS function, 0 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg0 = 'value' UNION
+                  SELECT name AS function, 1 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg1 = 'value' UNION
+                  SELECT name AS function, 2 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg2 = 'value' UNION
+                  SELECT name AS function, 3 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg3 = 'value' UNION
+                  SELECT name AS function, 4 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg4 = 'value' UNION
+                  SELECT name AS function, 5 AS position FROM args_is_ref WHERE Class = 'PHP' AND arg5 = 'value'
+                  ";
+        $res = $this->sqlite->query($query);
+        $return = array();
+        
+        while($row = $res->fetchArray(SQLITE3_ASSOC)) {
+            $return[] = $row;
+        }
+        
+        return $return;
+    }
 }
 
 ?>
