5a41033d38adb327600830afb1b91d3dd9aca605
Damien Seguy <damien.seguy@gmail.com>
2014-08-11 08:44:59 +0200

    Doubled all tests to handle Identifier AND namespaces (we only need the last class in a namespace).

--- library/Analyzer/Classes/WrongCase.php
+++ library/Analyzer/Classes/WrongCase.php
@@ -7,9 +7,34 @@ use Analyzer;
 class WrongCase extends Analyzer\Analyzer {
 
     public function analyze() {
+// New
         $this->atomIs("New")
              ->outIs('NEW')
-             ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
+             ->tokenIs('T_STRING')
+             ->codeIsNot(array('static', 'parent', 'self'))
+             ->savePropertyAs('code', 'classe')
+             ->classDefinition()
+             ->outIs('NAME')
+             ->notSamePropertyAs('code', 'classe', true)
+             ->back('first');
+        $this->prepareQuery();
+
+        $this->atomIs("New")
+             ->outIs('NEW')
+             ->tokenIs('T_NS_SEPARATOR')
+             ->orderIs('SUBNAME', 'last')
+             ->savePropertyAs('code', 'classe')
+             ->inIs('SUBNAME')
+             ->classDefinition()
+             ->outIs('NAME')
+             ->notSamePropertyAs('code', 'classe', true)
+             ->back('first');
+        $this->prepareQuery();
+
+// staticMethodcall
+        $this->atomIs("Staticmethodcall")
+             ->outIs('CLASS')
+             ->tokenIs('T_STRING')
              ->codeIsNot(array('static', 'parent', 'self'))
              ->savePropertyAs('code', 'classe')
              ->classDefinition()
@@ -20,7 +45,20 @@ class WrongCase extends Analyzer\Analyzer {
 
         $this->atomIs("Staticmethodcall")
              ->outIs('CLASS')
-             ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
+             ->tokenIs('T_NS_SEPARATOR')
+             ->orderIs('SUBNAME', 'last')
+             ->savePropertyAs('code', 'classe')
+             ->inIs('SUBNAME')
+             ->classDefinition()
+             ->outIs('NAME')
+             ->notSamePropertyAs('code', 'classe', true)
+             ->back('first');
+        $this->prepareQuery();
+
+// Staticproperty
+        $this->atomIs("Staticproperty")
+             ->outIs('CLASS')
+             ->tokenIs('T_STRING')
              ->codeIsNot(array('static', 'parent', 'self'))
              ->savePropertyAs('code', 'classe')
              ->classDefinition()
@@ -31,7 +69,20 @@ class WrongCase extends Analyzer\Analyzer {
 
         $this->atomIs("Staticproperty")
              ->outIs('CLASS')
-             ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
+             ->tokenIs('T_NS_SEPARATOR')
+             ->orderIs('SUBNAME', 'last')
+             ->savePropertyAs('code', 'classe')
+             ->inIs('SUBNAME')
+             ->classDefinition()
+             ->outIs('NAME')
+             ->notSamePropertyAs('code', 'classe', true)
+             ->back('first');
+        $this->prepareQuery();
+
+// Staticconstant
+        $this->atomIs("Staticconstant")
+             ->outIs('CLASS')
+             ->tokenIs('T_STRING')
              ->codeIsNot(array('static', 'parent', 'self'))
              ->savePropertyAs('code', 'classe')
              ->classDefinition()
@@ -42,7 +93,20 @@ class WrongCase extends Analyzer\Analyzer {
 
         $this->atomIs("Staticconstant")
              ->outIs('CLASS')
-             ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
+             ->tokenIs('T_NS_SEPARATOR')
+             ->orderIs('SUBNAME', 'last')
+             ->savePropertyAs('code', 'classe')
+             ->inIs('SUBNAME')
+             ->classDefinition()
+             ->outIs('NAME')
+             ->notSamePropertyAs('code', 'classe', true)
+             ->back('first');
+        $this->prepareQuery();
+
+// Catch
+        $this->atomIs("Catch")
+             ->outIs('CLASS')
+             ->tokenIs('T_STRING')
              ->codeIsNot(array('static', 'parent', 'self'))
              ->savePropertyAs('code', 'classe')
              ->classDefinition()
@@ -53,18 +117,20 @@ class WrongCase extends Analyzer\Analyzer {
 
         $this->atomIs("Catch")
              ->outIs('CLASS')
-             ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
-             ->codeIsNot(array('static', 'parent', 'self'))
+             ->tokenIs('T_NS_SEPARATOR')
+             ->orderIs('SUBNAME', 'last')
              ->savePropertyAs('code', 'classe')
+             ->inIs('SUBNAME')
              ->classDefinition()
              ->outIs('NAME')
              ->notSamePropertyAs('code', 'classe', true)
              ->back('first');
         $this->prepareQuery();
 
+// Typehint
         $this->atomIs("Typehint")
              ->outIs('CLASS')
-             ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
+             ->tokenIs('T_STRING')
              ->codeIsNot(array('static', 'parent', 'self'))
              ->savePropertyAs('code', 'classe')
              ->classDefinition()
@@ -75,11 +141,38 @@ class WrongCase extends Analyzer\Analyzer {
              ->inIs('ARGUMENTS');
         $this->prepareQuery();
 
+        $this->atomIs("Typehint")
+             ->outIs('CLASS')
+             ->tokenIs('T_NS_SEPARATOR')
+             ->orderIs('SUBNAME', 'last')
+             ->savePropertyAs('code', 'classe')
+             ->inIs('SUBNAME')
+             ->classDefinition()
+             ->outIs('NAME')
+             ->notSamePropertyAs('code', 'classe', true)
+             ->back('first')
+             ->inIs('ARGUMENT')
+             ->inIs('ARGUMENTS');
+        $this->prepareQuery();
+
+// instance of
         $this->atomIs("Instanceof")
              ->outIs('RIGHT')
-             ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
+             ->tokenIs('T_STRING')
              ->codeIsNot(array('static', 'parent', 'self'))
              ->savePropertyAs('code', 'classe')
+             ->classDefinition()
+             ->outIs('NAME')
+             ->notSamePropertyAs('code', 'classe', true)
+             ->back('first');
+        $this->prepareQuery();
+
+        $this->atomIs("Instanceof")
+             ->outIs('RIGHT')
+             ->tokenIs('T_NS_SEPARATOR')
+             ->orderIs('SUBNAME', 'last')
+             ->savePropertyAs('code', 'classe')
+             ->inIs('SUBNAME')
              ->classDefinition()
              ->outIs('NAME')
              ->notSamePropertyAs('code', 'classe', true)
