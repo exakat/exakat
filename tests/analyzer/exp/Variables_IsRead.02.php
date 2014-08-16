26ff4fc0ca55ab89c6492927d1ff178f6d6dc393
Damien Seguy <damien.seguy@gmail.com>
2014-08-16 11:21:25 +0200

    Added UT for IsRead.

---
+++ tests/analyzer/exp/Variables_IsRead.02.php
@@ -0,0 +1,14 @@
+<?php
+
+$expected     = array('$read',
+                      '$read2',
+                      '$read3');
+
+$expected_not = array('$written',
+                      '$written2',
+                      '$written3',
+                      '$read_arg',
+                      '$written_arg',
+                      '$ignored3');
+
+?>
