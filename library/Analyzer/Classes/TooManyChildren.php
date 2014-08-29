e584c67f2e75040a4db4f0e93c94f3134ed9c8a4
Damien Seguy <damien.seguy@gmail.com>
2014-08-29 19:36:22 +0200

    Added a check for number of children for a class.

---
+++ library/Analyzer/Classes/TooManyChildren.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Analyzer\Classes;
+
+use Analyzer;
+
+class TooManyChildren extends Analyzer\Analyzer {
+    public function analyze() {
+        // class a with extends
+        $this->atomIs("Class")
+             ->raw('sideEffect{nspath = it.fullnspath;}')
+             ->outIs('NAME')
+             ->raw('filter{ g.idx("Class")[["token":"node"]].out("EXTENDS", "IMPLEMENTS").has("fullnspath", nspath).count() >= 15}')
+             ->back('first');
+        $this->prepareQuery();    
+    }
+}
+
+?>
