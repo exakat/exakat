5cf5c1902055e96f5eecd959b6169cdd37133fef
Damien Seguy <damien.seguy@gmail.com>
2014-09-01 11:32:54 +0200

    Added UT for Used uses, synch with Unused.

---
+++ tests/analyzer/exp/Namespaces_UsedUse.03.php
@@ -0,0 +1,14 @@
+<?php
+
+$expected     = array('SingleWithoutAliasUsed', 
+                      'OriginalUsed', 
+                      'OriginalBothUsedSingleAlias as SingleAliasBothUsed', 
+                      'OriginalUnusedSingleAlias as SingleAliasUsed', 
+                      'OriginalUsedSingleAlias as SingleAliasUnused', 
+                      'OriginalBothUsed as AliasBothUsed', 
+                      'OriginalUnused as AliasUsed',
+                      'OriginalUsed as AliasUnused');
+
+$expected_not = array();
+
+?>
