abb9851dfa08b6f974c03edd9898897417d4c77e
Damien Seguy <damien.seguy@gmail.com>
2014-09-01 09:04:06 +0200

    Added some more useless instructions when they are alone.

--- library/Analyzer/Structures/UselessInstruction.php
+++ library/Analyzer/Structures/UselessInstruction.php
@@ -5,14 +5,13 @@ namespace Analyzer\Structures;
 use Analyzer;
 
 class UselessInstruction extends Analyzer\Analyzer {
-    
     public function analyze() {
         // Structures that should be put somewhere, and never left alone
         $this->atomIs("Sequence")
              ->outIs('ELEMENT')
              ->atomIs(array('Array', 'Addition', 'Multiplication', 'Property', 'Staticproperty', 'Boolean',
                             'Magicconstant', 'Staticconstant', 'Integer', 'Float', 'Sign', 'Nsname',
-                            'Constant', 'String', 'Instanceof'));
+                            'Constant', 'String', 'Instanceof', 'Bitshift', 'Logical', 'Comparison'));
         $this->prepareQuery();
         
         // -$x = 3
