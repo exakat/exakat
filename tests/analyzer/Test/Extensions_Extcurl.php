a64a4f4cb1398e13c8640c8e2966cfd1837a1fa8
Damien Seguy <damien.seguy@gmail.com>
2014-09-03 15:47:07 +0200

    Fixed UT and added another case for namespaces.

--- tests/analyzer/Test/Extensions_Extcurl.php
+++ tests/analyzer/Test/Extensions_Extcurl.php
@@ -8,9 +8,10 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Extensions_Extcurl extends Analyzer {
-    /* 2 methods */
+    /* 3 methods */
 
     public function testExtensions_Extcurl01()  { $this->generic_test('Extensions_Extcurl.01'); }
     public function testExtensions_Extcurl02()  { $this->generic_test('Extensions_Extcurl.02'); }
+    public function testExtensions_Extcurl03()  { $this->generic_test('Extensions_Extcurl.03'); }
 }
 ?>
--- tests/analyzer/exp/Extensions_Extcurl.03.php
+++ tests/analyzer/exp/Extensions_Extcurl.03.php
@@ -2,11 +2,11 @@
 
 $expected     = array('curl_exec($ch)',
                       'curl_close($ch)',
-                      'curl_init("http://www.example.com/")', 
+                      );
+
+$expected_not = array('curl_init("http://www.example.com/")', 
                       'curl_setopt($ch, CURLOPT_FILE, $fp)',
                       'curl_setopt($ch, CURLOPT_HEADER, 0)',
                       );
-
-$expected_not = array();
 
 ?>
