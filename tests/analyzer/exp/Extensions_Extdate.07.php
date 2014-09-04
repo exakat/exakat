19a9f5b5d8b6262280f13e772aeb3db6096a8e4c
Damien Seguy <damien.seguy@gmail.com>
2014-09-04 18:03:54 +0200

    Fixed UT result value.

--- tests/analyzer/exp/Extensions_Extdate.07.php
+++ tests/analyzer/exp/Extensions_Extdate.07.php
@@ -1,8 +1,8 @@
 <?php
 
-$expected     = array('\\date_modify(2)');
+$expected     = array('\\date_modify(2)',
+                      'date_offset_get(1)');
 
-$expected_not = array('date_offset_get(1)',
-                      '\\A\\date_parse_from_format()');
+$expected_not = array('\\A\\date_parse_from_format()');
 
 ?>
