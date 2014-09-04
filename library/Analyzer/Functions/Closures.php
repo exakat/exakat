99bd1d198a8b5952a471ad54ade8b7eedf549f21
Damien Seguy <damien.seguy@gmail.com>
2014-09-04 10:50:01 +0200

    Removed theme property (useless). Use 'true' with is.

--- library/Analyzer/Functions/Closures.php
+++ library/Analyzer/Functions/Closures.php
@@ -5,12 +5,9 @@ namespace Analyzer\Functions;
 use Analyzer;
 
 class Closures extends Analyzer\Analyzer {
-    
-    protected $themes = array('Inventory', 'Appinfo');
-    
     public function analyze() {
         $this->atomIs("Function")
-             ->is('lambda');
+             ->is('lambda', 'true');
     }
 }
 
