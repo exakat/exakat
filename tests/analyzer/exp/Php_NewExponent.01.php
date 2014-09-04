938f8c1d5b1d91519fad905cfd80447ec79e0c51
Damien Seguy <damien.seguy@gmail.com>
2014-09-04 18:46:37 +0200

    Fixed expected results.

--- tests/analyzer/exp/Php_NewExponent.01.php
+++ tests/analyzer/exp/Php_NewExponent.01.php
@@ -4,10 +4,12 @@ $expected     = array('pOW(8, 9)',
                       'pOw(6, 7)',
                       'POW(3, 4)',
                       'pow(1, 2)',
+                      '\\POW(pOw(13, 14), 15)',
+                      '\\POw(pOW(8, 9), 10)',
+                      '\\Pow(POW(3, 4), 5)'
                       );
 
-$expected_not = array('\\POW(pOw(13, 14), 15)',
-                      '\\POw(pOW(8, 9), 10)',
-                      '\\Pow(POW(3, 4), 5)');
+$expected_not = array('pow(13, 14)',
+                      'pow(11, 12)');
 
 ?>
