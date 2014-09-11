609230121ee76e671f91f1bf4b0758801d44e455
Damien Seguy <damien.seguy@gmail.com>
2014-09-11 12:13:19 +0200

    Spot illegal calls to private methods or properties.

---
+++ library/Analyzer/Classes/AccessPrivate.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Analyzer\Classes;
+
+use Analyzer;
+
+class AccessPrivate extends Analyzer\Analyzer {
+    public function analyze() {
+        // class::method()
+        $this->atomIs("Staticmethodcall")
+             ->outIs('METHOD')
+             ->savePropertyAs('code', 'name')
+             ->back('first')
+             ->outIs('CLASS')
+             ->codeIsNot(array('parent', 'static', 'self'))
+             ->raw('filter{ inside = it.fullnspath; it.in.loop(1){it.object.atom != "Class"}{it.object.atom == "Class"}.has("fullnspath", inside).any() == false}')
+             ->classDefinition()
+             ->raw('filter{ it.out("BLOCK").out("ELEMENT").has("atom", "Function").out("NAME").has("code", name).in("NAME").out("PRIVATE").any()}')
+             ->back('first');
+        $this->prepareQuery();
+
+        // parent::$property
+        // static or self ::$property
+
+        // class::$property
+        $this->atomIs("Staticproperty")
+             ->outIs('PROPERTY')
+             ->savePropertyAs('code', 'name')
+             ->back('first')
+             ->outIs('CLASS')
+             ->codeIsNot(array('parent', 'static', 'self'))
+             ->raw('filter{ inside = it.fullnspath; it.in.loop(1){it.object.atom != "Class"}{it.object.atom == "Class"}.has("fullnspath", inside).any() == false}')
+             ->classDefinition()
+             ->raw('filter{ it.out("BLOCK").out("ELEMENT").has("atom", "Ppp").out("DEFINE").has("code", name).in("DEFINE").out("PRIVATE").any()}')
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
