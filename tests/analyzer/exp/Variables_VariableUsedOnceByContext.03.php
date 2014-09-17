83c1ce9577556c1a3bc2bc42ad9c6308233b07eb
Damien Seguy <damien.seguy@gmail.com>
2014-09-17 09:01:23 +0200

    Fixed UT

--- tests/analyzer/exp/Variables_VariableUsedOnceByContext.03.php
+++ tests/analyzer/exp/Variables_VariableUsedOnceByContext.03.php
@@ -1,10 +1,9 @@
 <?php
 
-$expected     = array();
+$expected     = array('$used_once_in_a');
 
 $expected_not = array('A',
                       'B',
-                      '$used_once_in_a',
                       '$used_once_in_a_and_a',
                       );
 
--- tests/analyzer/source/Variables_VariableUsedOnceByContext.03.php
+++ tests/analyzer/source/Variables_VariableUsedOnceByContext.03.php
@@ -2,11 +2,10 @@
 
 namespace A {
     $used_once_in_a++;
-    $used_once_in_a_and_a++;
+    $used_twice_in_a_and_a++;
 }
 
 namespace A {
-    $used_once_a++;
-    $used_once_in_a_and_a++;
+    $used_twice_in_a_and_a++;
 }
 ?>
