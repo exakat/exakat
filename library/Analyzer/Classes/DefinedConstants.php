ff654e1576ec725c61880495e790960711380901
Damien Seguy <damien.seguy@gmail.com>
2014-09-19 14:50:20 +0200

    Upgraded spotting of class constants by identifying Vendor's classes and aliases. More to come.

--- library/Analyzer/Classes/DefinedConstants.php
+++ library/Analyzer/Classes/DefinedConstants.php
@@ -5,6 +5,12 @@ namespace Analyzer\Classes;
 use Analyzer;
 
 class DefinedConstants extends Analyzer\Analyzer {
+    public function dependsOn() {
+        return array('Analyzer\\Classes\\IsExtClass',
+                     'Analyzer\\Classes\\IsVendor',
+                     'Analyzer\\Interfaces\\IsExtInterface');
+    }
+    
     public function analyze() {
         // constants defined at the class level
         $this->atomIs("Staticconstant")
@@ -14,6 +20,30 @@ class DefinedConstants extends Analyzer\Analyzer {
              ->outIs('CLASS')
              ->classDefinition()
              ->raw("filter{ it.out('BLOCK').out('ELEMENT').has('atom', 'Const').out('NAME').filter{ it.code.toLowerCase() == constante.toLowerCase(); }.any(); }")
+             ->back('first');
+        $this->prepareQuery();
+
+        // constants defined at the interface level
+        $this->atomIs("Staticconstant")
+             ->outIs('CONSTANT')
+             ->savePropertyAs('code', 'constante')
+             ->back('first')
+             ->outIs('CLASS')
+             ->interfaceDefinition()
+             ->raw("filter{ it.out('BLOCK').out('ELEMENT').has('atom', 'Const').out('NAME').filter{ it.code.toLowerCase() == constante.toLowerCase(); }.any(); }")
+             ->back('first');
+        $this->prepareQuery();
+
+        // constants defined in a class of an extension
+        $this->atomIs("Staticconstant")
+             ->outIs('CLASS')
+             ->analyzerIs('Analyzer\\Classes\\IsExtClass')
+             ->back('first');
+        $this->prepareQuery();
+
+        // constants defined in a class of an vendor library
+        $this->atomIs("Staticconstant")
+             ->analyzerIs('Analyzer\\Classes\\IsVendor')
              ->back('first');
         $this->prepareQuery();
 
@@ -61,10 +91,7 @@ class DefinedConstants extends Analyzer\Analyzer {
                               .loop(2){ true }
                                       {it.object.out('BLOCK').out('ELEMENT').has('atom', 'Const').out('NAME').filter{ it.code.toLowerCase() == constante.toLowerCase(); }.any();}.any(); }")
              ->back('first');
-//        $this->printQuery();
         $this->prepareQuery();
-        
-        //it.object.out('BLOCK').out('ELEMENT').has('atom', 'Const').out('NAME').filter{ it.code.toLowerCase() == constante.toLowerCase(); }.any();}.any();
     }
 }
 
