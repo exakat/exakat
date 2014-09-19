582463bfbe4dc18e6feae9792ba388b56e40f26f
Damien Seguy <damien.seguy@gmail.com>
2014-09-19 14:53:37 +0200

    Added a rule to spot is a structure is a Trait

---
+++ library/Analyzer/Traits/IsExtTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Analyzer\Traits;
+
+use Analyzer;
+
+class IsExtTrait extends Analyzer\Analyzer {
+
+    public function dependsOn() {
+        return array("Analyzer\\Traits\\TraitUsage");
+    }
+    
+    public function analyze() {
+        $exts = glob('library/Analyzer/Extensions/*.php');
+        $exts[] = 'php_traits.ini';
+        
+        $traits = array();
+        foreach($exts as $ext) {
+            $inifile = str_replace('library/Analyzer/Extensions/Ext', '', str_replace('.php', '.ini', $ext));
+            if ($inifile == 'library/Analyzer/Extensions/Used.ini') { continue; }
+            $ini = $this->loadIni($inifile);
+            
+            if (!isset($ini['traits']) || !is_array($ini['traits'])) {
+//                print "No trait defined in $inifile\n";
+            } else {
+                if (!empty($ini['traits'][0])) {
+                    $traits = array_merge($traits, $ini['traits']);
+                }
+            }
+        }
+
+        $traits = array_map(function ($trait) { return '\\'.strtolower($trait); }, $traits);
+        
+        // no need to process anything!
+        if (empty($traits)) { return true; } 
+        
+        $this->analyzerIs("Analyzer\\Traits\\TraitUsage")
+             ->fullnspath($traits);
+    }
+}
+
+?>
