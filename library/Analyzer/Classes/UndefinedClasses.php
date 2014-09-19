56c33f935f546adf2cf5c5fde8258df5c3d7ea62
Damien Seguy <damien.seguy@gmail.com>
2014-09-19 14:53:13 +0200

    Strengthened the rule with Vendor, trait and interfaces.

--- library/Analyzer/Classes/UndefinedClasses.php
+++ library/Analyzer/Classes/UndefinedClasses.php
@@ -6,12 +6,14 @@ use Analyzer;
 
 class UndefinedClasses extends Analyzer\Analyzer {
     public function dependsOn() {
-        return array('Analyzer\\Classes\\IsExtClass');
+        return array('Analyzer\\Classes\\IsExtClass',
+                     'Analyzer\\Classes\\IsVendor');
     }
     
     public function analyze() {
         $this->atomIs("New")
              ->outIs('NEW')
+             ->analyzerIsNot('Analyzer\\Classes\\IsVendor')
              ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
              ->codeIsNot(array('self', 'parent', 'static'))
              ->analyzerIsNot('Analyzer\\Classes\\IsExtClass')
@@ -20,33 +22,43 @@ class UndefinedClasses extends Analyzer\Analyzer {
         $this->prepareQuery();
 
         $this->atomIs("Staticmethodcall")
+             ->analyzerIsNot('Analyzer\\Classes\\IsVendor')
              ->outIs('CLASS')
              ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
              ->codeIsNot(array('self', 'parent', 'static'))
              ->analyzerIsNot('Analyzer\\Classes\\IsExtClass')
              ->noClassDefinition()
+             ->noInterfaceDefinition()
+             ->noTraitDefinition()
              ->back('first');
         $this->prepareQuery();
 
         $this->atomIs("Staticproperty")
+             ->analyzerIsNot('Analyzer\\Classes\\IsVendor')
              ->outIs('CLASS')
              ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
              ->codeIsNot(array('self', 'parent', 'static'))
              ->analyzerIsNot('Analyzer\\Classes\\IsExtClass')
              ->noClassDefinition()
+             ->noInterfaceDefinition()
+             ->noTraitDefinition()
              ->back('first');
         $this->prepareQuery();
 
         $this->atomIs("Staticconstant")
+             ->analyzerIsNot('Analyzer\\Classes\\IsVendor')
              ->outIs('CLASS')
              ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
              ->codeIsNot(array('self', 'parent', 'static'))
              ->analyzerIsNot('Analyzer\\Classes\\IsExtClass')
              ->noClassDefinition()
+             ->noInterfaceDefinition()
+             ->noTraitDefinition()
              ->back('first');
         $this->prepareQuery();
 
         $this->atomIs("Instanceof")
+             ->analyzerIsNot('Analyzer\\Classes\\IsVendor')
              ->outIs('CLASS')
              ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
              ->codeIsNot(array('self', 'parent', 'static'))
