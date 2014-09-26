f68fd47ed9932a9af4facc3183c6cc5e5ff63f1e
Damien Seguy <damien.seguy@gmail.com>
2014-09-26 12:11:53 +0200

    Added a script to remove most of the report generated except report.

---
+++ scripts/reportclean.sh
@@ -0,0 +1,13 @@
+rm -rf projects/*/Flat-html.html
+rm -rf projects/*/Flat-markdown.md
+rm -rf projects/*/Flat-sqlite.sqlite
+rm -rf projects/*/Flat-text.txt
+
+rm -rf projects/*/Premier-html.html
+rm -rf projects/*/Premier-markdown.md
+rm -rf projects/*/Premier-sqlite.sqlite
+rm -rf projects/*/Premier-text.txt
+rm -rf projects/*/Premier-text.txt
+rm -rf projects/*/Premier-ace.zip
+rm -rf projects/*/Premier-ace
+
