5b35023594041df87646c2c366eb2a845fa89b1c
Damien Seguy <damien.seguy@gmail.com>
2014-09-29 11:09:59 +0200

    Added static constant as a possible part of Multiplication...

--- library/Tokenizer/Multiplication.php
+++ library/Tokenizer/Multiplication.php
@@ -10,7 +10,7 @@ class Multiplication extends TokenAuto {
                                     'Methodcall', 'Staticmethodcall', 'Concatenation', 'Cast',
                                     'Noscream', 'Staticconstant', 'Staticproperty', 'Constant', 
                                     'Boolean', 'Magicconstant', 'Assignation', 'Include', 'Power',
-                                    'Staticconstant', );
+                                    'Staticclass', );
     static public $atom = 'Multiplication';
     
     public function _check() {
