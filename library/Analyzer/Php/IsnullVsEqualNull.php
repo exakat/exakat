c786cbbbec7971051387de2de933f0a8e83444bd
Damien Seguy <damien.seguy@gmail.com>
2014-09-29 11:09:32 +0200

    Added check on Method and variable functions.

--- library/Analyzer/Php/IsnullVsEqualNull.php
+++ library/Analyzer/Php/IsnullVsEqualNull.php
@@ -7,6 +7,8 @@ use Analyzer;
 class IsnullVsEqualNull extends Analyzer\Analyzer {
     public function analyze() {
         $this->atomIs("Functioncall")
+             ->hasNoIn('METHOD')
+             ->tokenIsNot(array('T_VARIABLE', 'T_OPEN_BRACKET'))
              ->fullnspath('\\is_null');
         $this->prepareQuery();
     }
