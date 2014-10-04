640722bb8f979492ad3176dc570930c69291c50c
Damien Seguy <damien.seguy@gmail.com>
2014-10-04 14:49:30 +0200

    Added a suggestions to removed unused variables.

---
+++ library/Analyzer/Structures/ListOmissions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Analyzer\Structures;
+
+use Analyzer;
+
+class ListOmissions extends Analyzer\Analyzer {
+    public function dependsOn() {
+        return array('Analyzer\\Variables\\VariableUsedOnceByContext');
+    }
+    
+    public function analyze() {
+        $this->atomIs("Functioncall")
+             ->fullnspath('\\list')
+             ->outIs('ARGUMENTS')
+             ->outIs('ARGUMENT')
+             ->analyzerIs('Analyzer\\Variables\\VariableUsedOnceByContext');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/source/Structures_ListOmissions.01.php
@@ -0,0 +1,7 @@
+<?php
+
+list($used, $used2, $unused) = array(1,2,3);
+$used = $used2 + 2;
+list($unused2, $unused3, $unused4) = array(1,2,3);
+
+?>
