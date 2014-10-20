47837a11ff854a57f8ff6287e16c63480962b385
Damien Seguy <damien.seguy@gmail.com>
2014-10-20 21:19:59 +0200

    Undefined Constants are not PHP’s own.

--- library/Analyzer/Constants/UndefinedConstants.php
+++ library/Analyzer/Constants/UndefinedConstants.php
@@ -6,18 +6,21 @@ use Analyzer;
 
 class UndefinedConstants extends Analyzer\Analyzer {
     public function dependsOn() {
-        return array('Analyzer\\Constants\\CustomConstantUsage');
+        return array('Analyzer\\Constants\\CustomConstantUsage',
+                     'Analyzer\\Constants\\IsExtConstant');
     }
     
     public function analyze() {
         $this->atomIs("Identifier")
              ->analyzerIs('Analyzer\\Constants\\CustomConstantUsage')
-             ->hasNoConstantDefinition();
+             ->hasNoConstantDefinition()
+             ->analyzerIsNot('Analyzer\\Constants\\IsExtConstant');
         $this->prepareQuery();
 
         $this->atomIs("Nsname")
              ->analyzerIs('Analyzer\\Constants\\CustomConstantUsage')
-             ->hasNoConstantDefinition();
+             ->hasNoConstantDefinition()
+             ->analyzerIsNot('Analyzer\\Constants\\IsExtConstant');
         $this->prepareQuery();
     }
 }
