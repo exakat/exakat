a08e0ceb65604ec97cca6c277cb1d55bdb4482c4
Damien Seguy <damien.seguy@gmail.com>
2014-10-21 10:51:41 +0200

    Spot PHP functions that are not in lower case, as usual.

---
+++ library/Analyzer/Php/UpperCaseFunction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Analyzer\Php;
+
+use Analyzer;
+
+class UpperCaseFunction extends Analyzer\Analyzer {
+    public function analyze() {
+        $this->atomIs('Functioncall')
+             ->analyzerIs('Analyzer\\Functions\\IsExtFunction')
+             ->isNotLowerCase('isNotLowercase');
+        $this->prepareQuery();
+        
+        // some of the keywords are lost anyway : implements, extends, as in foreach(), endforeach/while/for/* are lost in tokenizer (may be keep track of that) 
+
+    }
+}
+
+?>
