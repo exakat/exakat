96a81e203eb6d9d19534d1c2de94cfbca3d6e101
Damien Seguy <damien.seguy@gmail.com>
2014-10-23 11:10:57 +0200

    Added extra test for WrongOptionalParameter

--- tests/analyzer/Test/Functions_WrongOptionalParameter.php
+++ tests/analyzer/Test/Functions_WrongOptionalParameter.php
@@ -8,9 +8,10 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Functions_WrongOptionalParameter extends Analyzer {
-    /* 2 methods */
+    /* 3 methods */
 
     public function testFunctions_WrongOptionalParameter01()  { $this->generic_test('Functions_WrongOptionalParameter.01'); }
     public function testFunctions_WrongOptionalParameter02()  { $this->generic_test('Functions_WrongOptionalParameter.02'); }
+    public function testFunctions_WrongOptionalParameter03()  { $this->generic_test('Functions_WrongOptionalParameter.03'); }
 }
 ?>
