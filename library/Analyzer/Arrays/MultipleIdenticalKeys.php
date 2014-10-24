c4bf94a8ac69c424bcb8b9fa36747791da4d6831
Damien Seguy <damien.seguy@gmail.com>
2014-10-24 14:24:55 +0200

    Added explicit prepareQuery

--- library/Analyzer/Arrays/MultipleIdenticalKeys.php
+++ library/Analyzer/Arrays/MultipleIdenticalKeys.php
@@ -10,6 +10,7 @@ class MultipleIdenticalKeys extends Analyzer\Analyzer {
         $this->atomIs("Functioncall")
              ->code('array')
              ->raw("aggregate().findAll{ m = [:]; it.out('ARGUMENTS').out('ARGUMENT').has('atom', 'Keyvalue').out('KEY').groupCount(m){it.fullcode}.cap.next().findAll{it.value > 1}.size() > 0}");
+        $this->prepareQuery();
     }
 }
 
