7ba7fe47afbcfdbcd86c14ca304a22839d114973
Damien Seguy <damien.seguy@gmail.com>
2014-10-24 14:32:36 +0200

    Removed ‘As’ from potential keywords that have wrong case, as their code is not kept in the tokenizer script.

--- library/Analyzer/Php/UpperCaseKeyword.php
+++ library/Analyzer/Php/UpperCaseKeyword.php
@@ -6,7 +6,7 @@ use Analyzer;
 
 class UpperCaseKeyword extends Analyzer\Analyzer {
     public function analyze() {
-        $this->atomIs(array("Class", "Foreach", 'As', 'Switch', 'For', 'Namespace', 'Use', 'Function',
+        $this->atomIs(array("Class", "Foreach", 'Switch', 'For', 'Namespace', 'Use', 'Function',
                             'Try', 'Catch', 'Case', 'Default', 'Goto', 'Continue', 'Const', 'Break',
                             'Clone', 'DoWhile', 'While', 'Interface', 'Instanceof', 'Insteadof', 'Return',
                             'Throw', 'Trait', 'Interface', 'Var', 'Logical' ))
@@ -15,7 +15,7 @@ class UpperCaseKeyword extends Analyzer\Analyzer {
         $this->prepareQuery();
         
         // some of the keywords are lost anyway : implements, extends, as in foreach(), endforeach/while/for/* are lost in tokenizer (may be keep track of that) 
-
+        // As (in use commands) are not preserved. 
     }
 }
 
