38f55836ef813d709c93b990b0998175178425e0
Damien Seguy <damien.seguy@gmail.com>
2014-10-24 20:27:54 +0200

    Fixed Function detection.

--- library/Analyzer/Constants/UnusedConstants.php
+++ library/Analyzer/Constants/UnusedConstants.php
@@ -12,7 +12,7 @@ class UnusedConstants extends Analyzer\Analyzer {
     public function analyze() {
       $this->atomIs("Functioncall")
              ->hasNoIn('METHOD')
-             ->tokenIsNot('T_VARIABLE')
+             ->tokenIs(array('T_STRING', 'T_NS_SEPARATOR'))
              ->fullnspath("\\define")
              ->outIs('ARGUMENTS')
              ->rankIs('ARGUMENT', 'first')
