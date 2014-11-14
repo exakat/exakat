4f436f16991e87554e55f7305b1eb433e2838164
Damien Seguy <damien.seguy@gmail.com>
2014-11-14 11:12:37 +0100

    Removed debug

--- library/Phpexec.php
+++ library/Phpexec.php
@@ -112,7 +112,7 @@ class Phpexec {
     public function compile($file) {
         $shell = shell_exec($this->phpexec.' -l '.escapeshellarg($file).' 2>&1');
         $shell = preg_replace("/PHP Warning: .*?\n/i", '', $shell);
-        var_dump($shell);
+
         if (trim($shell) == 'No syntax errors detected in '.$file) {
             return true;
         } else {
