d55eb7d73c02b7c2dc714ce9ba246d4b77a2ddc6
Damien Seguy <damien.seguy@gmail.com>
2014-11-15 17:15:19 +0100

    Added fullcode and check_for_string in Shell

--- library/Tokenizer/Shell.php
+++ library/Tokenizer/Shell.php
@@ -8,9 +8,10 @@ class Shell extends TokenAuto {
 
     public function _check() {
 // Case of string with interpolation : `a${b}c`;
-        $this->conditions = array(  0 => array('token' => Shell::$operators, 
-                                               'atom' => 'none'),
-                                    1 => array('atom'  => String::$allowed_classes),
+        $this->conditions = array(  0 => array('token'            => Shell::$operators, 
+                                               'atom'             => 'none'),
+                                    1 => array('atom'             => String::$allowed_classes,
+                                               'check_for_string' => String::$allowed_classes),
                                  );
         
         $this->actions = array( 'make_quoted_string' => 'Shell');
@@ -18,6 +19,14 @@ class Shell extends TokenAuto {
 
         return false;
     }
-}
 
+    public function fullcode() {
+        // fullcode is not meant to reproduce the whole code, but give a quick peek at some smaller code. Just ignoring for the moment.
+        return <<<GREMLIN
+
+fullcode.setProperty("fullcode", it.out('CONTAIN').next().fullcode);
+
+GREMLIN;
+    }
+}
 ?>
