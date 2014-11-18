5fcd212bd8c2dca5aebe4f7cc6c2521ba689aa1a
Damien Seguy <damien.seguy@gmail.com>
2014-11-18 21:43:51 +0100

    Fixed index name

--- library/Tokenizer/Token.php
+++ library/Tokenizer/Token.php
@@ -707,7 +707,7 @@ g.idx('atoms')[['atom':'Class']].each{
 ", "
 // collecting files
 g.idx('atoms')[['atom':'Phpcode']].in.loop(1){true}{it.object.atom == 'File'}.each{ 
-    g.idx('file').put('path', it.filename, it)
+    g.idx('files').put('path', it.filename, it)
 };
 
 ", "
