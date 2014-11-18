1020cd90d78ce18589bc41c88f21a108d835fddc
Damien Seguy <damien.seguy@gmail.com>
2014-11-18 22:41:54 +0100

    Added prepareQuery

--- library/Analyzer/Php/RawPostDataUsage.php
+++ library/Analyzer/Php/RawPostDataUsage.php
@@ -8,6 +8,7 @@ class RawPostDataUsage extends Analyzer\Analyzer {
     public function analyze() {
         $this->atomIs("Variable")
              ->code('$HTTP_RAW_POST_DATA');
+        $this->prepareQuery();
     }
 }
 
