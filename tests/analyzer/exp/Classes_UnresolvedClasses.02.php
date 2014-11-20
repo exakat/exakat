21acffc448d310e2b3cf79b9d307838eedb78c1a
Damien Seguy <damien.seguy@gmail.com>
2014-11-20 21:48:45 +0100

    Fixed UT expected results

--- tests/analyzer/exp/Classes_UnresolvedClasses.02.php
+++ tests/analyzer/exp/Classes_UnresolvedClasses.02.php
@@ -1,7 +1,19 @@
 <?php
 
 $expected     = array('BA( )', 
-                      'F\AB( )');
+                      'F\AB( )',
+                      'G( )', 
+                      'H( )', 
+                      'G2( )', 
+                      'H2( )', 
+                      'A( )', 
+                      'A2( )', 
+                      'E( )', 
+                      'E2( )', 
+                      'E\\AB( )', 
+                      'A\\B\\AB( )', 
+                      'A2\\B2\\AB( )', 
+                      'E2\\AB( )');
 
 $expected_not = array();
 
