7ae3c1e8270e4fdbf2c40668cc166058f12e2a83
Damien Seguy <damien.seguy@gmail.com>
2014-11-21 15:45:57 +0100

    Ignore Extension defined interface.

--- library/Analyzer/Classes/ImplementIsForInterface.php
+++ library/Analyzer/Classes/ImplementIsForInterface.php
@@ -5,11 +5,16 @@ namespace Analyzer\Classes;
 use Analyzer;
 
 class ImplementIsForInterface extends Analyzer\Analyzer {
+    public function dependsOn() {
+        return array('Analyzer\\Interfaces\\IsExtInterface');
+    }
+    
     public function analyze() {
         // class a with implements
         $this->atomIs("Class")
              ->outIs('IMPLEMENTS')
              ->noInterfaceDefinition()
+             ->analyzerIsNot('Analyzer\\Interfaces\\IsExtInterface')
              ->back('first');
         $this->prepareQuery();    
     }
--- tests/analyzer/Test/Classes_ImplementIsForInterface.php
+++ tests/analyzer/Test/Classes_ImplementIsForInterface.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Classes_ImplementIsForInterface extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testClasses_ImplementIsForInterface01()  { $this->generic_test('Classes_ImplementIsForInterface.01'); }
+    public function testClasses_ImplementIsForInterface02()  { $this->generic_test('Classes_ImplementIsForInterface.02'); }
 }
 ?>
