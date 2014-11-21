ccce2e3462df9e8df547888b7ee593114e17e500
Damien Seguy <damien.seguy@gmail.com>
2014-11-21 21:45:18 +0100

    Added UT for check usage of private method and properties

---
+++ tests/analyzer/exp/Classes_UsedPrivateMethod.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('private static $privatepsstatic = 1', 
+'private static $privatepsself = 1', 
+'private $privatep', 
+'private $privatepa = array( )'
+);
+
+$expected_not = array();
+
+?>
