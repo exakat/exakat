1f052bbfa4406f378b4a888414a62835184db94b
Damien Seguy <damien.seguy@gmail.com>
2014-11-21 16:04:09 +0100

    Fixed UT.

--- tests/analyzer/exp/Structures_DanglingArrayReferences.01.php
+++ tests/analyzer/exp/Structures_DanglingArrayReferences.01.php
@@ -1,7 +1,7 @@
 <?php
 
-$expected     = array('foreach($arr as &$value_not_unset){ /**/ } ',
-                      'foreach($arr as &$value_unset_other){ /**/ } ',);
+$expected     = array('foreach($arr as &$value_not_unset) { /**/ } ', 
+                      'foreach($arr as &$value_unset_other) { /**/ } ');
 
 $expected_not = array('foreach ($arr as &$value_unset){ /**/ } ',
                       'foreach ($arr as $value_not_reference){ /**/ } ',
