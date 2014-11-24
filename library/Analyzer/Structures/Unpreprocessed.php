c382bcbb1fa281d58144d048c0bb11827e8c78fe
Damien Seguy <damien.seguy@gmail.com>
2014-11-24 05:56:47 +0100

    Strengthened Functioncall check against methods

--- library/Analyzer/Structures/Unpreprocessed.php
+++ library/Analyzer/Structures/Unpreprocessed.php
@@ -8,6 +8,8 @@ class Unpreprocessed extends Analyzer\Analyzer {
     public function analyze() {
         // $x = explode(',', 'a,b,c,d,e,f') => array('a', 'b', 'c',...)
         $this->atomIs("Functioncall")
+             ->hasNoIn('METHOD')
+             ->tokenIs(array('T_STRING', 'T_NS_SEPARATOR'))
              ->outIs('NAME')
              ->fullnspath(array('\\explode', '\\split'))
              ->inIs('NAME')
