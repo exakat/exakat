e96d80191dec209eabeafe0976fa2fb61d959d45
Damien Seguy <damien.seguy@gmail.com>
2014-11-26 15:16:20 +0100

    Cleaned comments

--- library/Analyzer/Structures/NoDirectAccess.php
+++ library/Analyzer/Structures/NoDirectAccess.php
@@ -9,20 +9,17 @@ class NoDirectAccess extends Analyzer\Analyzer {
         //defined('AJXP_EXEC') or die('Access not allowed'); : Constant used!
         $this->atomIs('Logical')
              ->code('or')
-//             ->outIs('RIGHT')
              // find !defined and defined
              ->atomInside('Functioncall')
              ->hasNoIn('METHOD')
              ->tokenIs('T_STRING')
              ->fullnspath('\\defined')
              ->back('first')
-//             ->outIs('LEFT')
              ->atomInside('Functioncall')
              ->hasNoIn('METHOD')
              ->tokenIs(array('T_EXIT', 'T_DIE'))
              ->fullnspath(array('\\die', '\\exit'))
              ->back('first');
-//        $this->printQuery();
         $this->prepareQuery();
 
         //if(!defined('CMS'))die/exit 
