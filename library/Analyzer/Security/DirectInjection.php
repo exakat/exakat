d87f3813406e0d92fd60ec29de479af59e4b9a6e
Damien Seguy <damien.seguy@gmail.com>
2014-11-28 14:51:29 +0100

    Fixed rule to report functioncall not argument.

--- library/Analyzer/Security/DirectInjection.php
+++ library/Analyzer/Security/DirectInjection.php
@@ -6,37 +6,36 @@ use Analyzer;
 
 class DirectInjection extends Analyzer\Analyzer {
     public function dependsOn() {
-        return array('Analyzer\\Functions\\IsExtFunction');
+        return array('Analyzer\\Security\\SensitiveArgument');
     }
     
     public function analyze() {
         // $_GET/_POST ... directly as argument of PHP functions
         $this->atomIs("Variable")
              ->code(array('$_POST', '$_GET', '$_ENV', '$_REQUEST', '$_FILES', '$_COOKIE', '$PHP_SELF'))
+             ->analyzerIs('Analyzer\\Security\\SensitiveArgument')
              ->inIs('ARGUMENT')
-             ->inIs('ARGUMENTS')
-             ->analyzerIs('Analyzer\\Functions\\IsExtFunction');
+             ->inIs('ARGUMENTS');
         $this->prepareQuery();
 
         // $_GET/_POST ['index']... directly as argument of PHP functions
         $this->atomIs("Variable")
              ->code(array('$_POST', '$_GET', '$_ENV', '$_SERVER', '$_REQUEST', '$_FILES', '$_COOKIE', '$PHP_SELF'))
-             ->inIs('VARIABLE')
+             ->analyzerIs('Analyzer\\Security\\SensitiveArgument')
              ->inIs('ARGUMENT')
-             ->inIs('ARGUMENTS')
-             ->analyzerIs('Analyzer\\Functions\\IsExtFunction');
+             ->inIs('ARGUMENTS');
         $this->prepareQuery();
 
         // $_GET/_POST ['index']['index2']... directly as argument of PHP functions
         $this->atomIs("Variable")
              ->code(array('$_POST', '$_GET', '$_ENV', '$_SERVER', '$_REQUEST', '$_FILES', '$_COOKIE', '$PHP_SELF'))
              ->raw('in("VARIABLE").loop(1){true}{ it.object.atom == "Array"}')
+             ->analyzerIs('Analyzer\\Security\\SensitiveArgument')
              ->inIs('ARGUMENT')
-             ->inIs('ARGUMENTS')
-             ->analyzerIs('Analyzer\\Functions\\IsExtFunction');
+             ->inIs('ARGUMENTS');
         $this->prepareQuery();
-
-        // $_GET/_POST... inside a string is useless
+        
+        // $_GET/_POST array... inside a string is useless
         // "$_GET/_POST ['index']"... inside a string or a concatenation
         $this->atomIs("Variable")
              ->code(array('$_POST', '$_GET', '$_ENV', '$_SERVER', '$_REQUEST', '$_FILES', '$_COOKIE', '$PHP_SELF'))
@@ -44,9 +43,9 @@ class DirectInjection extends Analyzer\Analyzer {
              ->inIs('CONCAT');
         $this->prepareQuery();
 
-        // "$_GET/_POST ['index']"... inside an operation is probably OK
+        // "$_GET/_POST ['index']"... inside an operation is probably OK if not concatenation! 
 
-        // foreach 
+        // foreach (looping on incoming variables)
         $this->atomIs("Variable")
              ->code(array('$_POST', '$_GET', '$_ENV', '$_SERVER', '$_REQUEST', '$_FILES', '$_COOKIE', '$PHP_SELF'))
              ->raw('in("VARIABLE").loop(1){true}{ it.object.atom == "Array"}')
--- tests/analyzer/exp/Security_DirectInjection.01.php
+++ tests/analyzer/exp/Security_DirectInjection.01.php
@@ -5,10 +5,8 @@ $expected     = array('foreach($_FILES["pictures"]["error"] as $key => $error) {
                       '"A" . $_SERVER[\'incoming\'] . "B"',
                       '$_COOKIE[\'incoming\']',
                       'move_uploaded_file($_FILES[\'name\'][\'filename\'], $_FILES[\'name\'][\'filename\'])',
-                      'echo $_GET[\'incoming\']',
                       'move_uploaded_file($_FILES[\'name\'][\'filename\'], $_FILES[\'name\'][\'filename\'])',
                       'echo $_GET[\'incoming\']',
-                      'array_sum($_POST)',
 );
 
 $expected_not = array();
