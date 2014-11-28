89ec3d999640a973f70d9f58ae45ba437fc312d4
Damien Seguy <damien.seguy@gmail.com>
2014-11-28 17:49:28 +0100

    Using php_incoming.ini

--- library/Analyzer/Security/DirectInjection.php
+++ library/Analyzer/Security/DirectInjection.php
@@ -10,9 +10,12 @@ class DirectInjection extends Analyzer\Analyzer {
     }
     
     public function analyze() {
+        $vars = $this->loadIni('php_incoming.ini');
+        $vars = $vars['incoming'];
+
         // $_GET/_POST ... directly as argument of PHP functions
         $this->atomIs("Variable")
-             ->code(array('$_POST', '$_GET', '$_ENV', '$_REQUEST', '$_FILES', '$_COOKIE', '$PHP_SELF'))
+             ->code($vars)
              ->analyzerIs('Analyzer\\Security\\SensitiveArgument')
              ->inIs('ARGUMENT')
              ->inIs('ARGUMENTS');
@@ -20,7 +23,7 @@ class DirectInjection extends Analyzer\Analyzer {
 
         // $_GET/_POST ['index']... directly as argument of PHP functions
         $this->atomIs("Variable")
-             ->code(array('$_POST', '$_GET', '$_ENV', '$_SERVER', '$_REQUEST', '$_FILES', '$_COOKIE', '$PHP_SELF'))
+             ->code($vars)
              ->analyzerIs('Analyzer\\Security\\SensitiveArgument')
              ->inIs('ARGUMENT')
              ->inIs('ARGUMENTS');
@@ -28,7 +31,7 @@ class DirectInjection extends Analyzer\Analyzer {
 
         // $_GET/_POST ['index']['index2']... directly as argument of PHP functions
         $this->atomIs("Variable")
-             ->code(array('$_POST', '$_GET', '$_ENV', '$_SERVER', '$_REQUEST', '$_FILES', '$_COOKIE', '$PHP_SELF'))
+             ->code($vars)
              ->raw('in("VARIABLE").loop(1){true}{ it.object.atom == "Array"}')
              ->analyzerIs('Analyzer\\Security\\SensitiveArgument')
              ->inIs('ARGUMENT')
@@ -38,7 +41,7 @@ class DirectInjection extends Analyzer\Analyzer {
         // $_GET/_POST array... inside a string is useless
         // "$_GET/_POST ['index']"... inside a string or a concatenation
         $this->atomIs("Variable")
-             ->code(array('$_POST', '$_GET', '$_ENV', '$_SERVER', '$_REQUEST', '$_FILES', '$_COOKIE', '$PHP_SELF'))
+             ->code($vars)
              ->raw('in("VARIABLE").loop(1){true}{ it.object.atom == "Array"}')
              ->inIs('CONCAT');
         $this->prepareQuery();
@@ -47,7 +50,7 @@ class DirectInjection extends Analyzer\Analyzer {
 
         // foreach (looping on incoming variables)
         $this->atomIs("Variable")
-             ->code(array('$_POST', '$_GET', '$_ENV', '$_SERVER', '$_REQUEST', '$_FILES', '$_COOKIE', '$PHP_SELF'))
+             ->code($vars)
              ->raw('in("VARIABLE").loop(1){true}{ it.object.atom == "Array"}')
              ->inIs('SOURCE');
         $this->prepareQuery();
