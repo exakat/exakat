41e4730e19a14e6c71fbb991c9fea5bb5cbf4f9f
Damien Seguy <damien.seguy@gmail.com>
2014-11-30 22:02:12 +0100

    Spot undefined properties (internal call)

---
+++ library/Analyzer/Classes/UndefinedProperty.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Analyzer\Classes;
+
+use Analyzer;
+
+class UndefinedProperty extends Analyzer\Analyzer {
+    public function dependsOn() {
+        return array('Analyzer\\Classes\\DefinedProperty',
+                     'Analyzer\\Classes\\HasMagicProperty');
+    }
+    
+    public function analyze() {
+        // only for internal calls. External calls still needs some work
+        $this->atomIs('Property')
+             ->analyzerIsNot('Analyzer\\Classes\\DefinedProperty')
+             ->outIs('OBJECT')
+             ->code('$this')
+             ->goToClass()
+             ->analyzerIsNot('Analyzer\\Classes\\HasMagicProperty')
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Classes_UndefinedProperty.01.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('$this->undefined');
+
+$expected_not = array('$this->undefinedButMagic',
+                      '$y->undefinedButNotInternal');
+
+?>
