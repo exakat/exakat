e646ca9f782339a009ccd1f49d611233c500a568
Damien Seguy <damien.seguy@gmail.com>
2014-12-03 17:50:40 +0100

    Exclude dynamic properties

--- tests/analyzer/Test/Classes_UndefinedProperty.php
+++ tests/analyzer/Test/Classes_UndefinedProperty.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Classes_UndefinedProperty extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testClasses_UndefinedProperty01()  { $this->generic_test('Classes_UndefinedProperty.01'); }
+    public function testClasses_UndefinedProperty02()  { $this->generic_test('Classes_UndefinedProperty.02'); }
 }
 ?>
