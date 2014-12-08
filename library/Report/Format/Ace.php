7267eb561caf5d98278ed7a98f6fb00b415f6b92
Damien Seguy <damien.seguy@gmail.com>
2014-12-08 22:54:33 +0100

    Added google tags for exakat.io

--- library/Report/Format/Ace.php
+++ library/Report/Format/Ace.php
@@ -171,6 +171,16 @@ if (empty($project_code_source)) {
 		<!--ace settings handler-->
 
 		<script src="assets/js/ace-extra.min.js"></script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-51182320-1', 'exakat.io');
+          ga('send', 'pageview');
+
+        </script>
 	</head>
 
 	<body>
