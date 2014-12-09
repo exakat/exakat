fb1072a259c8cc535c7869eab42245dcf4dc7342
Damien Seguy <damien.seguy@gmail.com>
2014-12-09 22:27:21 +0100

    Added Curly Arrays rule

---
+++ library/Analyzer/Arrays/CurlyArrays.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Analyzer\Arrays;
+
+use Analyzer;
+
+class CurlyArrays extends Analyzer\Analyzer {
+    public function analyze() {
+        // $x[3]{3}
+        $this->atomIs("Array")
+             ->code('{');
+        $this->prepareQuery();
+    }
+}
+
+?>
