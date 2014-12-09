7b0ee87f3c04645a085d5ecf6c340cf15716573f
Damien Seguy <damien.seguy@gmail.com>
2014-12-09 14:25:26 +0100

    Cleaned code (removed full class mention in static call, cleaned comment)

--- library/Tokenizer/Nsname.php
+++ library/Tokenizer/Nsname.php
@@ -8,9 +8,9 @@ class Nsname extends TokenAuto {
 
     public function _check() {
         // @note \a\b\c (\ initial)
-        $this->conditions = array( -2 => array('filterOut'  => array('T_NS_SEPARATOR')), 
-                                   -1 => array('filterOut2' => 'T_NS_SEPARATOR'), // 'T_STRING', 'T_NAMESPACE'
-                                    0 => array('token'      => Nsname::$operators,
+        $this->conditions = array( -2 => array('filterOut'  => self::$operators), 
+                                   -1 => array('filterOut2' => 'T_NS_SEPARATOR'), 
+                                    0 => array('token'      => self::$operators,
                                                'atom'       => 'none'),
                                     1 => array('atom'       => array('Identifier', 'Boolean', 'Null')),
         );
@@ -23,8 +23,8 @@ class Nsname extends TokenAuto {
         $this->checkAuto();
 
         // @note a\b\c as F
-        $this->conditions = array( 0 => array('token' => Nsname::$operators),
-                                   1 => array('token' => 'T_AS'),
+        $this->conditions = array( 0 => array('token' => self::$operators),
+                                   1 => array('token' => _As::$operators),
                                    2 => array('atom'  => 'Identifier'),
         );
         
