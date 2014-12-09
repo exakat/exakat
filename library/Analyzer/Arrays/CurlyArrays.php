c6a71adccf8f28f5ac1733f8ae1457929ece3932
Damien Seguy <damien.seguy@gmail.com>
2014-12-09 22:30:46 +0100

    Ignore internal arrays.

--- library/Analyzer/Arrays/CurlyArrays.php
+++ library/Analyzer/Arrays/CurlyArrays.php
@@ -8,7 +8,8 @@ class CurlyArrays extends Analyzer\Analyzer {
     public function analyze() {
         // $x[3]{3}
         $this->atomIs("Array")
-             ->code('{');
+             ->code('{')
+             ->hasNoIn('VARIABLE');
         $this->prepareQuery();
     }
 }
