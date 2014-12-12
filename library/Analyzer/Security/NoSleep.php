204ace03f5bfb6562ea190525bc6ce31b6f3e3f4
Damien Seguy <damien.seguy@gmail.com>
2014-12-12 19:14:33 +0100

    Added rule to spot usage of sleep

---
+++ library/Analyzer/Security/NoSleep.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Analyzer\Security;
+
+use Analyzer;
+
+class NoSleep extends Analyzer\Analyzer {
+    public function analyze() {
+        // simple call to usleep 
+        $this->atomIs("Functioncall")
+             ->hasNoIn('METHOD')
+             ->tokenIs(array('T_STRING', 'T_NS_SEPARATOR'))
+             ->fullnspath(array('\\sleep', '\\usleep'));
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/source/Security_NoSleep.01.php
@@ -0,0 +1,8 @@
+<?php
+
+sleep(1);
+
+usleep(2000);
+
+$o->sleep(4000); // not a sleep! 
+?>
