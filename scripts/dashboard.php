deda6733944171fcabfda76aace25bde14dd055f
Damien Seguy <damien.seguy@gmail.com>
2014-12-14 22:21:23 +0100

    Fixed variables names

--- scripts/dashboard.php
+++ scripts/dashboard.php
@@ -18,8 +18,8 @@ print "\n";
 
 $res = shell_exec('cd tests/analyzer/; php list.php -0');
 preg_match('/Total : (\d+) tests/is', $res, $R);
-$total_UT = $R[1];
-print $total_UT." total analyzer tests\n";
+$totalUnitTests = $R[1];
+print $totalUnitTests." total analyzer tests\n";
 
 preg_match_all('/\s(\w*)\s*(\d+)/is', $res, $R);
 
@@ -42,14 +42,14 @@ if (!file_exists('tests/analyzer/phpunit.txt')) {
         print "There were {$R[1]} failures in ".count(array_unique($R2[1]))." tests! Check the tests! \n";
         print "  + ".join("\n  + ", array_unique($R2[1]))."\n\n";
         
-        if ($R[1] != $total_UT) {
-            print "Not all the tests were run! Only {$R[1]} out of $total_UT. Please, run php scripts/phpunit.php\n";
+        if ($R[1] != $totalUnitTests) {
+            print "Not all the tests were run! Only {$R[1]} out of $totalUnitTests. Please, run php scripts/phpunit.php\n";
         } else {
             print "All tests where recently run, some are KO\n";
         }
     } elseif (preg_match('/OK \((\d+) test, (\d+) assertions\)/is', $results, $R)) {
-        if ($R[1] != $total_UT) {
-            print "Not all the tests were run! Only {$R[1]} out of $total_UT. Please, run php scripts/phpunit.php\n";
+        if ($R[1] != $totalUnitTests) {
+            print "Not all the tests were run! Only {$R[1]} out of $totalUnitTests. Please, run php scripts/phpunit.php\n";
         } else {
             print "All tests where recently run and OK\n";
         }
@@ -63,7 +63,7 @@ $tokens = 0;
 $indexed = array();
 $next = array();
 $fullcode = array();
-$lone_token = array();
+$loneToken = array();
 $files = glob('projects/*/log/stat.log');
 foreach($files as $file) {
     $log = file_get_contents($file);
@@ -83,8 +83,8 @@ foreach($files as $file) {
         $fullcode[] = $file." ({$R[0]})";
     }
 
-    if (preg_match('/lone_token : (\d+)/', $log, $R) && $R[1] != 0) {
-        $lone_token[] = $file." ({$R[0]})";
+    if (preg_match('/loneToken : (\d+)/', $log, $R) && $R[1] != 0) {
+        $loneToken[] = $file." ({$R[0]})";
     }
 }
 
@@ -109,56 +109,40 @@ if ($fullcode) {
     print "All ".count($files)." stat.log are free of no_fullcode\n";
 }
 
-if ($lone_token) {
+if ($loneToken) {
     print count($fullcode)." stat.log have reported lone tokens\n";
-    print "  + ".join("\n  + ", $lone_token)."\n\n";
+    print "  + ".join("\n  + ", $loneToken)."\n\n";
 } else {
     print "All ".count($files)." stat.log are free lone tokens\n";
 }
 
 print "\n".count($files)." projects collecting ".number_format($tokens, 0)." tokens\n\n";
 
-$files = glob('projects/*/');
-$sqlite_md = array();
-$report_md = array();
-foreach($files as $file) {
-    if ($file == 'projects/default/') { continue; }
-    if ($file == 'projects/tests/') { continue; }
-    
-    if (!file_exists($file.'Premier-markdown.md')) {
-        $report_md[] = $file;
-    }
-
-    if (!file_exists($file.'Premier-sqlite.sqlite')) {
-        $sqlite_md[] = $file;
-    }
-}
-
 $files = glob('human/en/*/*');
-$extra_docs = array();
+$extraDocs = array();
 foreach($files as $k => $v) {
-    $extra_docs[substr($v, 9, -4)] = 1;
+    $extraDocs[substr($v, 9, -4)] = 1;
 }
 
 $analyzers = Analyzer\Analyzer::listAnalyzers();
-$missing_doc = array();
+$missingDoc = array();
 foreach($analyzers as $a) {
-    unset($extra_docs[$a]);
+    unset($extraDocs[$a]);
     $o = Analyzer\Analyzer::getInstance($a, null);
     if (is_null($o)) { 
         print "Couldn't get an instance for '$a'\n\n";
         continue 1;
     }
     if ($o->getDescription() === '') {
-        $missing_doc[] = $a.' (human/en/'.$a.'.ini)';
+        $missingDoc[] = $a.' (human/en/'.$a.'.ini)';
     }
 }
 
-if ($missing_doc) {
-    print count($missing_doc)." analyzer are missing their documentation\n";
-    print "  + ".join("\n  + ", $missing_doc)."\n\n";
+if ($missingDoc) {
+    print count($missingDoc)." analyzer are missing their documentation\n";
+    print "  + ".join("\n  + ", $missingDoc)."\n\n";
     
-    foreach($missing_doc as $document) {
+    foreach($missingDoc as $document) {
         list($documentName, ) = explode(' ', $document);
         if (!file_exists('human/en/'.$documentName.'.ini')) {
             file_put_contents('human/en/'.$documentName.'.ini', "name=\"\";\ndescription=\"\";\n");
@@ -168,9 +152,9 @@ if ($missing_doc) {
     print "All ".count($analyzers)." analyzers have their documentation\n\n";
 }
 
-if ($extra_docs) {
-    print count($extra_docs)." docs are available without analyzer\n";
-    print "  + ".join("\n  + ", array_keys($extra_docs))."\n\n";
+if ($extraDocs) {
+    print count($extraDocs)." docs are available without analyzer\n";
+    print "  + ".join("\n  + ", array_keys($extraDocs))."\n\n";
 } else {
     print "All ".count($analyzers)." docs have analyzers\n\n";
 }
@@ -178,40 +162,40 @@ if ($extra_docs) {
 $sqlite = new Sqlite3('data/analyzers.sqlite');
 $res = $sqlite->query("select folder, name from analyzers");
 
-$in_sqlite = array();
+$inSqlite = array();
 while($row = $res->fetchArray()) {
-    $in_sqlite[] = $row['folder'].'/'.$row['name'];
+    $inSqlite[] = $row['folder'].'/'.$row['name'];
 }
 
-$missing_in_sqlite = array_diff($analyzers, $in_sqlite);
+$missingInSqlite = array_diff($analyzers, $inSqlite);
 
 
-if (count($missing_in_sqlite)) {
-    print count($missing_in_sqlite)." analysers missing in Sqlite. Inserting them\n";
+if (count($missingInSqlite)) {
+    print count($missingInSqlite)." analysers missing in Sqlite. Inserting them\n";
 
-    $id_unassigned = $sqlite->query("SELECT id FROM categories WHERE name='Unassigned'")->fetchArray();
-    $id_unassigned = $id_unassigned[0];
+    $idUnassigned = $sqlite->query("SELECT id FROM categories WHERE name='Unassigned'")->fetchArray();
+    $idUnassigned = $idUnassigned[0];
     
-    foreach($missing_in_sqlite as $analyser) {
+    foreach($missingInSqlite as $analyser) {
         list($folder, $name) = explode('/', $analyser);
         
         $sqlite->query("INSERT INTO analyzers ('folder', 'name') VALUES ('$folder', '$name')");
         $id = $sqlite->lastInsertRowID();
 
-        $sqlite->query("INSERT INTO analyzers_categories VALUES ('$id', '$id_unassigned')"); 
+        $sqlite->query("INSERT INTO analyzers_categories VALUES ('$id', '$idUnassigned')"); 
     }
 }
 
-$missing_severity = $sqlite->query("SELECT count(*) FROM categories c
+$missingSeverity = $sqlite->query("SELECT count(*) FROM categories c
 JOIN analyzers_categories ac ON c.id = ac.id_categories
 JOIN analyzers a ON ac.id_analyzer = a.id
 WHERE (c.name in ('Analyze', 'Coding Conventions', 'Dead code')) AND a.severity IS NULL")->fetchArray();
-$missing_severity = $missing_severity[0];
-if ($missing_severity == 0) {
+$missingSeverity = $missingSeverity[0];
+if ($missingSeverity == 0) {
     print "All analysers have their severity in Sqlite.\n";
 
 } else {
-    print $missing_severity." analysers are missing severity in Sqlite.\n";
+    print $missingSeverity." analysers are missing severity in Sqlite.\n";
 
     $res = $sqlite->query("SELECT a.folder, a.name FROM categories c
 JOIN analyzers_categories ac ON c.id = ac.id_categories
@@ -221,8 +205,6 @@ WHERE (c.name in ('Analyze', 'Coding Conventions', 'Dead code', 'Security')) AND
     while ($row = $res->fetchArray()) {
         print '+ '.$row['folder'].'/'.$row['name']."\n";
     }
-
-//    print_r($missing_severity);
 }
 
 
@@ -236,11 +218,11 @@ if ($unassigned[0] > 0) {
     print "All ".$total." analyzers are assigned. \n";
 }
 
-$unassigned_res = $sqlite->query("select * from analyzers as a left join analyzers_categories as ac on ac.id_analyzer = a.id where ac.id_categories IS NULL"); 
-$unassigned2 = $unassigned_res->fetchArray();
+$unassignedRes = $sqlite->query("select * from analyzers as a left join analyzers_categories as ac on ac.id_analyzer = a.id where ac.id_categories IS NULL"); 
+$unassigned2 = $unassignedRes->fetchArray();
 if (!empty($unassigned2)) { 
     $all = array( $unassigned2['folder'].'/'.$unassigned2['name'].'('.$unassigned2['id'].')');
-    while($unassigned2 = $unassigned_res->fetchArray()) {
+    while($unassigned2 = $unassignedRes->fetchArray()) {
         $all[] = $unassigned2['folder'].'/'.$unassigned2['name'].'('.$unassigned2['id'].')';
     }
 
@@ -249,8 +231,8 @@ if (!empty($unassigned2)) {
     print "All ".$total." analyzers are linked. \n\n";
 }
 
-$analyzers_count = $sqlite->query("select count(*) from categories as c join analyzers_categories as ac on c.id = ac.id_categories where c.name='Analyze'")->fetchArray(); 
-print $analyzers_count[0]." analyzers \n";
+$analyzersCount = $sqlite->query("select count(*) from categories as c join analyzers_categories as ac on c.id = ac.id_categories where c.name='Analyze'")->fetchArray(); 
+print $analyzersCount[0]." analyzers \n";
 
 // check for analyzer log 
 $res = shell_exec('grep -r javax.script.ScriptException projects/*/log/analyze.*.final.log');
