a23bce769f44f23e078063655a23197fbceed12c
Damien Seguy <damien.seguy@gmail.com>
2014-12-15 22:08:48 +0100

    Spot implicit globals

---
+++ library/Analyzer/Structures/ImplicitGlobal.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Analyzer\Structures;
+
+use Analyzer;
+
+class ImplicitGlobal extends Analyzer\Analyzer {
+    public function analyze() {
+        $this->atomIs("Global")
+             ->hasFunction()
+             ->outIs('GLOBAL')
+             ->_as('result')
+             ->savePropertyAs('code', 'theGlobal')
+             ->raw('filter{ g.idx("atoms")[["atom":"Global"]].out("GLOBAL").filter{ it.in.loop(1){it.object.atom != "Function"}{it.object.atom == "Function"}.any() == false}.filter{theGlobal == it.code}.any() == false }')
+             ->back('result');
+        $this->prepareQuery();
+    }
+}
+
+?>
