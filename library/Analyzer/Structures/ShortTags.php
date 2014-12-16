0e148dc0a4579f97e3c4518ceec780fbb55c6af5
Damien Seguy <damien.seguy@gmail.com>
2014-12-16 15:59:14 +0100

    Fixed variable name

--- library/Analyzer/Structures/ShortTags.php
+++ library/Analyzer/Structures/ShortTags.php
@@ -5,7 +5,7 @@ namespace Analyzer\Structures;
 use Analyzer;
 
 class ShortTags extends Analyzer\Analyzer {
-    protected $phpconfiguration = array("short_open_tags" => true);
+    protected $phpConfiguration = array("short_open_tags" => true);
 
     public function analyze() {
         $this->atomIs("Phpcode")
