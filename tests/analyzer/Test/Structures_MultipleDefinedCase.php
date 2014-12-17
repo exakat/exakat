7b0edbc7d2d6f4f74951154614d237f957b501ea
Damien Seguy <damien.seguy@gmail.com>
2014-12-17 14:58:51 +0100

    More tests for Switch.

--- tests/analyzer/Test/Structures_MultipleDefinedCase.php
+++ tests/analyzer/Test/Structures_MultipleDefinedCase.php
@@ -8,8 +8,10 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_MultipleDefinedCase extends Analyzer {
-    /* 1 methods */
+    /* 3 methods */
 
     public function testStructures_MultipleDefinedCase01()  { $this->generic_test('Structures_MultipleDefinedCase.01'); }
+    public function testStructures_MultipleDefinedCase02()  { $this->generic_test('Structures_MultipleDefinedCase.02'); }
+    public function testStructures_MultipleDefinedCase03()  { $this->generic_test('Structures_MultipleDefinedCase.03'); }
 }
 ?>
