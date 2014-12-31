7a47639bc89140a5c306e87b4246a1913d2680c7
Damien Seguy <damien.seguy@gmail.com>
2014-12-31 16:09:31 +0100

    added visiblity; Moved call to query() to method

--- library/Analyzer/Namespaces/Vendor.php
+++ library/Analyzer/Namespaces/Vendor.php
@@ -17,7 +17,7 @@ class Vendor extends Analyzer\Analyzer {
     public function toArray() {
         $analyzer = str_replace('\\', '\\\\', get_class($this));
         $queryTemplate = "g.idx('analyzers')[['analyzer':'".$analyzer."']].out.fullcode.tokenize(\" \")[1]"; 
-        $vertices = query($this->client, $queryTemplate);
+        $vertices = $this->query($this->client, $queryTemplate);
 
         $report = array();
         if (count($vertices) > 0) {
@@ -29,21 +29,8 @@ class Vendor extends Analyzer\Analyzer {
         return $report;
     }
 
-    function toCountedArray($load = "it.fullcode") {
+    public function toCountedArray($load = "it.fullcode") {
         return parent::toCountedArray("it.fullcode.tokenize(\" \\\\\")[1]");
-/*        
-        $analyzer = str_replace('\\', '\\\\', get_class($this));
-        $queryTemplate = "m = [:]; g.idx('analyzers')[['analyzer':'".$analyzer."']].out.groupCount(m){}.cap"; 
-        $vertices = query($this->client, $queryTemplate);
-
-        $report = array();
-        if (count($vertices) > 0) {
-            foreach($vertices[0][0] as $k => $v) {
-                $report[$k] = $v;
-            }   
-        } 
-        
-        return $report;*/
     }
 
 }
