b56e6a25ab27711f99e6ae2fd99e9ed60e754ba0
Damien Seguy <damien.seguy@gmail.com>
2015-01-08 13:58:01 +0100

    Added special cases when variable is used as array

--- library/Analyzer/Structures/ForeachReferenceIsNotModified.php
+++ library/Analyzer/Structures/ForeachReferenceIsNotModified.php
@@ -10,6 +10,18 @@ class ForeachReferenceIsNotModified extends Analyzer\Analyzer {
     }
     
     public function analyze() {
+        // case of a variable
+        $this->atomIs("Foreach")
+             ->outIs('VALUE')
+             ->is('reference', 'true')
+             ->savePropertyAs('code', 'value')
+             ->inIs('VALUE')
+             ->outIs('BLOCK')
+             ->raw('filter{ it.out.loop(1){true}{it.object.atom == "Variable"}.has("code",value).filter{ it.in("ANALYZED").has("code", "Analyzer\\\\Variables\\\\IsModified").any()}.any() == false}')
+             ->back('first');
+        $this->prepareQuery();
+
+        // case of a variable
         $this->atomIs("Foreach")
              ->outIs('VALUE')
              ->is('reference', 'true')
