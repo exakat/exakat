101292c764ffd88f4dd34f3e9ff7466cedb0b2a9
Damien Seguy <damien.seguy@gmail.com>
2015-01-08 11:07:46 +0100

    Added support for **

--- library/Analyzer/Structures/MultiplyByOne.php
+++ library/Analyzer/Structures/MultiplyByOne.php
@@ -8,7 +8,7 @@ class MultiplyByOne extends Analyzer\Analyzer {
     public function analyze() {
         // $x *= 1;
         $this->atomIs('Assignation')
-             ->code(array('*=', '/=', '%='))
+             ->code(array('*=', '/=', '%=', '**='))
              ->outIs('RIGHT')
              ->code(1)
              ->back('first');
@@ -25,6 +25,20 @@ class MultiplyByOne extends Analyzer\Analyzer {
         $this->atomIs('Multiplication')
              ->outIs('RIGHT')
              ->code('1')
+             ->back('first');
+        $this->prepareQuery();
+
+        // $x = $y ** 1 
+        $this->atomIs('Power')
+             ->outIs('RIGHT')
+             ->code('1')
+             ->back('first');
+        $this->prepareQuery();
+
+        // 1 ** $a;
+        $this->atomIs('Power')
+             ->outIs('LEFT')
+             ->code(array('1', '0'))
              ->back('first');
         $this->prepareQuery();
         
--- tests/analyzer/Test/Structures_MultiplyByOne.php
+++ tests/analyzer/Test/Structures_MultiplyByOne.php
@@ -8,10 +8,11 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_MultiplyByOne extends Analyzer {
-    /* 3 methods */
+    /* 4 methods */
 
     public function testStructures_MultiplyByOne01()  { $this->generic_test('Structures_MultiplyByOne.01'); }
     public function testStructures_MultiplyByOne02()  { $this->generic_test('Structures_MultiplyByOne.02'); }
     public function testStructures_MultiplyByOne03()  { $this->generic_test('Structures_MultiplyByOne.03'); }
+    public function testStructures_MultiplyByOne04()  { $this->generic_test('Structures_MultiplyByOne.04'); }
 }
 ?>
