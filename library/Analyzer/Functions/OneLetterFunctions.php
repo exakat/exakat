807407ac726b50142aede4c908545da9f2c8ffda
Damien Seguy <damien.seguy@gmail.com>
2015-01-19 21:51:46 +0100

    Fixed case for methodcall

--- library/Analyzer/Functions/OneLetterFunctions.php
+++ library/Analyzer/Functions/OneLetterFunctions.php
@@ -7,14 +7,14 @@ use Analyzer;
 class OneLetterFunctions extends Analyzer\Analyzer {
     public function dependsOn() {
         return array('Analyzer\\Functions\\Functionnames',
-                     'Analyzer\\Classes\\Methoddefinition');
+                     'Analyzer\\Classes\\MethodDefinition');
     }
     
     public function analyze() {
         $this->atomIs('Function')
              ->outIs('NAME')
              ->analyzerIs('Analyzer\\Functions\\Functionnames')
-             ->analyzerIsNot('Analyzer\\Classes\\Methoddefinition')
+             ->analyzerIsNot('Analyzer\\Classes\\MethodDefinition')
              ->fullcodeLength(' == 1 ');
         $this->prepareQuery();
     }
