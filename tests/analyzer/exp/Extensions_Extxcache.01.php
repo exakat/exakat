2f240432752e34268bb35d855a90714701fb05f7
Damien Seguy <damien.seguy@gmail.com>
2015-01-20 13:15:18 +0100

    Added support for xcache

---
+++ tests/analyzer/exp/Extensions_Extxcache.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('xcache_isset("count")', 
+                      'xcache_inc("count")', 
+                      'xcache_set("count", load_count_from_mysql( ))');
+
+$expected_not = array();
+
+?>
