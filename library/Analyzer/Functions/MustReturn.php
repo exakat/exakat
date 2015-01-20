1143549188c8b852eb1e7ac2aba5db0cc3c697cb
Damien Seguy <damien.seguy@gmail.com>
2015-01-20 12:45:33 +0100

    Must return check for functions and interfaces.

--- library/Analyzer/Functions/MustReturn.php
+++ library/Analyzer/Functions/MustReturn.php
@@ -5,12 +5,18 @@ namespace Analyzer\Functions;
 use Analyzer;
 
 class MustReturn extends Analyzer\Analyzer {
+    public function dependsOn() {
+        return array('Analyzer\\Classes\\MethodDefinition');
+    }
+    
     public function analyze() {
         $this->atomIs('Function')
              ->hasNoOut('ABSTRACT')
+             ->raw('filter{ it.in("ELEMENT").in("BLOCK").has("atom", "Interface").any() == false}')
              ->outIs('NAME')
              ->code(array('__call', '__callStatic', '__get', '__isset', '__sleep', '__toString', '__set_state', 
                           '__invoke', '__debugInfo'))
+             ->analyzerIs('Analyzer\\Classes\\MethodDefinition')
              ->back('first')
              ->noAtomInside('Return');
         $this->prepareQuery();
--- tests/analyzer/Test/Functions_MustReturn.php
+++ tests/analyzer/Test/Functions_MustReturn.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Functions_MustReturn extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testFunctions_MustReturn01()  { $this->generic_test('Functions_MustReturn.01'); }
+    public function testFunctions_MustReturn02()  { $this->generic_test('Functions_MustReturn.02'); }
 }
 ?>
