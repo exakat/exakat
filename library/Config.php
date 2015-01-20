1eb1c64c153dac7f72d3ff9390070a86a366223f
Damien Seguy <damien.seguy@gmail.com>
2015-01-20 23:03:00 +0100

    Moved to camelCase

--- library/Config.php
+++ library/Config.php
@@ -2,28 +2,28 @@
 
 class Config {
     static private $singleton = null;
-           private $config_file = array();
+           private $configFile = array();
            private $commandline = array();
            private $argv = array();
-           private $project_config = array();
+           private $projectConfig = array();
         
            private $options = array();
      
     private function __construct($argv) {
         $this->argv = $argv;
         
-        $this->config_file = parse_ini_file('./config/config.ini');
+        $this->configFile = parse_ini_file('./config/config.ini');
 
         // then read the config from the commandline (if any)
-        $this->read_commandline();
+        $this->readCommandline();
         
         // then read the config for the project in its folder
         if (isset($this->commandline['project'])) {
-            $this->read_project_config($this->commandline['project']);
+            $this->readProjectConfig($this->commandline['project']);
         } 
         
         // build the actual config. Project overwrite commandline overwrites config, if any.
-        $this->options = array_merge($this->config_file, $this->commandline, $this->project_config);
+        $this->options = array_merge($this->configFile, $this->commandline, $this->projectConfig);
     }
     
     static public function factory($argv = array()) {
@@ -54,14 +54,14 @@ class Config {
         print "It is not possible to modify configuration $name with value '$value'\n";
     }
 
-    private function read_project_config($project) {
+    private function readProjectConfig($project) {
         if (!file_exists('./projects/'.$project.'/config.ini')) {
             return null;
         }
         
-        $this->project_config = parse_ini_file('./projects/'.$project.'/config.ini');
+        $this->projectConfig = parse_ini_file('./projects/'.$project.'/config.ini');
         
-        foreach($this->project_config as &$value) {
+        foreach($this->projectConfig as &$value) {
             if (is_array($value) && empty($value[0])) {
                 unset($value[0]);
             }
@@ -73,42 +73,42 @@ class Config {
                            'other_php_versions' => array('53', '54', '55', '56'));
         
         foreach($defaults as $name => $value) {
-            if (!isset($this->project_config[$name])) {
-                $this->project_config[$name] = $value;
+            if (!isset($this->projectConfig[$name])) {
+                $this->projectConfig[$name] = $value;
             }
         }
         
         return null;
     }
 
-    private function read_commandline() {
+    private function readCommandline() {
         $args = $this->argv;
 
         if (empty($args)) {
             return null;
         }
         
-        $options_boolean = array(
-                                 '-v'     => array('verbose',    false),
-                                 '-h'     => array('help',       false),
-                                 '-r'     => array('recursive',  false),
-                                 '-u'     => array('update',     false),
-                                 '-D'     => array('delete',     false),
-                                 '-l'     => array('lint',       false),
-                                 '-json'  => array('json',       false),
-                                 '-ss'    => array('ss',         false),
-                                 '-sm'    => array('sm',         false),
-                                 '-sl'    => array('sl',         false),
-                                 '-nodep' => array('noDependencies', false),
+        $optionsBoolean = array(
+                                 '-v'      => array('verbose',    false),
+                                 '-h'      => array('help',       false),
+                                 '-r'      => array('recursive',  false),
+                                 '-u'      => array('update',     false),
+                                 '-D'      => array('delete',     false),
+                                 '-l'      => array('lint',       false),
+                                 '-json'   => array('json',       false),
+                                 '-ss'     => array('ss',         false),
+                                 '-sm'     => array('sm',         false),
+                                 '-sl'     => array('sl',         false),
+                                 '-nodep'  => array('noDependencies', false),
                                  '-norefresh' => array('noRefresh', false),
-                                 '-today' => array('today',      false),
-                                 '-none'  => array('none',       false),
-                                 '-table' => array('table',      false),
-                                 '-text'  => array('text',       false),
-                                 '-o'     => array('output',     false),
+                                 '-today'  => array('today',      false),
+                                 '-none'   => array('none',       false),
+                                 '-table'  => array('table',      false),
+                                 '-text'   => array('text',       false),
+                                 '-o'      => array('output',     false),
                                  );
 
-        foreach($options_boolean as $key => $config) {
+        foreach($optionsBoolean as $key => $config) {
             if (($id = array_search($key, $args)) !== false) {
                 $this->commandline[$config[0]] = true;
 
@@ -118,16 +118,19 @@ class Config {
             }
         }
                                  
-        $options_value   = array('-f' => array('filename',    null),
+        $optionsValue   = array('-f' => array('filename',    null),
                                  '-d' => array('dirname',     null),
                                  '-p' => array('project',     'default'),
                                  '-P' => array('program',     null),
                                  '-R' => array('repository',  false),
                                  '-T' => array('thema',       null),
+                                 '-report' => array('report',     'Premier'),
+                                 '-format' => array('format',     'Text'),
+                                 '-file' => array('file',     'report'),
 //                                 '-q' => array('loader',   'Load\Csv'),
                                  );
 
-        foreach($options_value    as $key => $config) {
+        foreach($optionsValue as $key => $config) {
             if ( ($id = array_search($key, $args)) !== false) {
                 $this->commandline[$config[0]] = $args[$id + 1];
 
