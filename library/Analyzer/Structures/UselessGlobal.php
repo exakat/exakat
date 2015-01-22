10628c92912b43ecd1ff3f6dcf3f8add5f1f0740
Damien Seguy <damien.seguy@gmail.com>
2015-01-22 22:45:31 +0100

    Spot useless Global commands (only used once)

---
+++ library/Analyzer/Structures/UselessGlobal.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Analyzer\Structures;
+
+use Analyzer;
+
+class UselessGlobal extends Analyzer\Analyzer {
+    public function dependsOn() {
+        return array('Analyzer\\Variables\\VariableUsedOnceByContext',
+                     'Analyzer\\Structures\\UnusedGlobal');
+    }
+    
+    public function analyze() {
+        // Global are unused if used only once
+        $this->atomIs('Global')
+             ->outIs('GLOBAL')
+             ->analyzerIsNot('Analyzer\\Structures\\UnusedGlobal')
+             ->savePropertyAs('code', 'variable')
+            // search in $GLOBALS
+             ->raw('filter{ g.idx("atoms")[["atom":"Variable"]].has("code", "\$GLOBALS").in("VARIABLE").has("atom", "Array").out("INDEX").has("atom", "String").has("noDelimiter", variable.substring(1, variable.size())).any() == false }')
+             ->eachCounted('it.fullcode', 1);
+        $this->prepareQuery();
+
+        $this->atomIs('Array')
+             ->outIs('VARIABLE')
+             ->code('$GLOBALS')
+             ->inIs('VARIABLE')
+             ->outIs('INDEX')
+             ->atomIs('String')
+             ->raw('filter{ index = it.noDelimiter; g.idx("atoms")[["atom":"Global"]].out("GLOBAL").has("atom", "Variable").filter{ it.code ==  "\\$" + index}.any() == false}')
+             ->inIs('INDEX')
+             ->eachCounted('it.fullcode', 1);
+        $this->prepareQuery();
+        
+        // used only once
+        
+        // written only
+    }
+}
+
+?>
