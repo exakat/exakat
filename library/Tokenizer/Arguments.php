93145458f07b5d02c9d9299f418471f31e109f0e
Damien Seguy <damien.seguy@gmail.com>
2015-01-25 23:21:28 +0100

    Added missing valid atoms

--- library/Tokenizer/Arguments.php
+++ library/Tokenizer/Arguments.php
@@ -8,7 +8,7 @@ class Arguments extends TokenAuto {
 
     public function _check() {
         // @note arguments separated by ,
-        $this->conditions = array(-2 => array('token'   => array('T_OPEN_PARENTHESIS', 'T_ECHO', 'T_GLOBAL', 'T_EXTENDS')),
+        $this->conditions = array(-2 => array('token'   => array('T_OPEN_PARENTHESIS', 'T_ECHO', 'T_VAR', 'T_GLOBAL', 'T_EXTENDS')),
                                   -1 => array('atom'    => 'yes'),
                                    0 => array('token'   => Arguments::$operators,
                                               'atom'    => 'none'),
@@ -18,7 +18,7 @@ class Arguments extends TokenAuto {
                                                                              'Staticmethodcall', 'Staticconstant', 'Staticproperty',
                                                                              'New', 'Functioncall', 'Nsname', 'Identifier', 'Void',
                                                                              'Variable', 'Array', 'Assignation', 'Typehint', 'Keyvalue',
-                                                                             'Float', 'Concatenation', 'Parenthesis')),
+                                                                             'Float', 'Concatenation', 'Parenthesis', 'Cast', 'Sign')),
                                    2 => array('token'   => array('T_CLOSE_PARENTHESIS', 'T_COMMA', 'T_SEMICOLON', 'T_CLOSE_TAG', 
                                                                  'T_OPEN_CURLY'))
                                  );
