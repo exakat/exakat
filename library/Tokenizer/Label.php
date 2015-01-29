b744e2ecb951affba9fe311dbad002c5c8872024
Damien Seguy <damien.seguy@gmail.com>
2015-01-29 09:15:37 +0100

    Removed colon as label prohibiting.

--- library/Tokenizer/Label.php
+++ library/Tokenizer/Label.php
@@ -8,7 +8,7 @@ class Label extends TokenAuto {
     
     public function _check() {
         $this->conditions = array(-2 => array('filterOut' => array_merge(array('T_QUESTION','T_CASE', 'T_DOT', 'T_NS_SEPARATOR',
-                                                                               'T_OBJECT_OPERATOR', 'T_DOUBLE_COLON', 'T_NEW', 'T_COLON',
+                                                                               'T_OBJECT_OPERATOR', 'T_DOUBLE_COLON', 'T_NEW',
                                                                                'T_INSTANCEOF'),
                                                                          Assignation::$operators, Addition::$operators,
                                                                          Multiplication::$operators, Comparison::$operators,
