8135d05e9296742823decf864a480e5f2bafd717
Damien Seguy <damien.seguy@gmail.com>
2015-01-30 11:00:50 +0100

    Added support for elseif

--- library/Tokenizer/ArgumentsNoParenthesis.php
+++ library/Tokenizer/ArgumentsNoParenthesis.php
@@ -16,7 +16,8 @@ class ArgumentsNoParenthesis extends Arguments {
                                     1 => array('atom'       => 'yes',
                                                'notAtom'    => 'Arguments'),
                                     2 => array('token'      => array('T_SEMICOLON', 'T_CLOSE_TAG', 'T_ENDIF', 'T_ENDSWITCH', 'T_ENDFOR',
-                                                                     'T_ENDFOREACH', 'T_COMMA', 'T_CLOSE_PARENTHESIS', 'T_QUESTION', 'T_COLON'))
+                                                                     'T_ENDFOREACH', 'T_COMMA', 'T_CLOSE_PARENTHESIS', 'T_QUESTION', 'T_COLON',
+                                                                     'T_ELSEIF'))
         );
         
         $this->actions = array('insertEdge'  => array(0 => array('Arguments' => 'ARGUMENT')),
