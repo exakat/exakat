65c9b6f2f2985538e995ea5caf1c0ba725aafef5
Damien Seguy <damien.seguy@gmail.com>
2015-01-30 11:00:20 +0100

    Noscream doesn't support Label

--- library/Tokenizer/Noscream.php
+++ library/Tokenizer/Noscream.php
@@ -10,7 +10,7 @@ class Noscream extends TokenAuto {
         $this->conditions = array(0 => array('token' => Noscream::$operators,
                                              'atom'  => 'none'),
                                   1 => array('atom'  => 'yes',
-                                             'notAtom' => 'Assignation'),
+                                             'notAtom' => array('Assignation', 'Label')),
                                   2 => array('filterOut' => array('T_OPEN_PARENTHESIS', 'T_OBJECT_OPERATOR',
                                                                   'T_DOUBLE_COLON', 'T_OPEN_BRACKET', 'T_OPEN_PARENTHESIS',
                                                                   'T_OPEN_CURLY'))
