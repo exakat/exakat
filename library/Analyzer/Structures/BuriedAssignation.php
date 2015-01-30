20bc297b7beb50fc6c09eaa91cfb9a82a9f27c44
Damien Seguy <damien.seguy@gmail.com>
2015-01-30 14:16:49 +0100

    Added case where the assignation is in comparison/parenthesis

--- library/Analyzer/Structures/BuriedAssignation.php
+++ library/Analyzer/Structures/BuriedAssignation.php
@@ -10,11 +10,16 @@ class BuriedAssignation extends Analyzer\Analyzer {
              ->hasNoIn('ELEMENT')
              // in a While
              ->raw('filter{ it.in("CONDITION", "INIT").any() == false}')
+
              // in a IF
              ->raw('filter{ it.in("CODE").in("CONDITION").any() == false}')
+             // in a if( ($a =2) !== 3) {}
+             ->raw('filter{ it.in("CODE").in.has("atom", "Comparison").in("CODE").in("CONDITION").any() == false}')
+
              // in a chained assignation
              ->raw('filter{ it.in("RIGHT").has("atom", "Assignation").any() == false}')
-             // in a chained assignation
+
+             // in an argument (with or without typehint)
              ->raw('filter{ it.in("ARGUMENT").in("ARGUMENTS").has("atom", "Function").any() == false}')
              ->raw('filter{ it.in("VARIABLE").in("ARGUMENT").in("ARGUMENTS").has("atom", "Function").any() == false}');
         $this->prepareQuery();
