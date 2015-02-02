3cb66b8fe050da70fbb8868cdfa33e387a4c9952
Damien Seguy <damien.seguy@gmail.com>
2015-02-02 14:36:55 +0100

    Added more situations where to find file objects

--- library/Analyzer/Structures/FileUsage.php
+++ library/Analyzer/Structures/FileUsage.php
@@ -8,10 +8,27 @@ class FileUsage extends Analyzer\Analyzer {
     public function analyze() {
         $this->atomFunctionIs(array('fopen', 'file_get_contents', 'file_put_contents'));
         $this->prepareQuery();
-        
+
+        $fileClasses = array('\\SplFileObject', '\\SplTempFileObject', '\\SplFileInfo');
+
         $this->atomIs('New')
              ->outis('NEW')
-             ->fullnspath(array('\\SplFileObject', '\\SplTempFileObject', '\\SplFileInfo'));
+             ->fullnspath($fileClasses);
+        $this->prepareQuery();
+
+        $this->atomIs('Staticmethodcall')
+             ->outis('CLASS')
+             ->fullnspath($fileClasses);
+        $this->prepareQuery();
+
+        $this->atomIs('Staticproperty')
+             ->outis('CLASS')
+             ->fullnspath($fileClasses);
+        $this->prepareQuery();
+
+        $this->atomIs('Staticconstant')
+             ->outis('CLASS')
+             ->fullnspath($fileClasses);
         $this->prepareQuery();
     }
 }
