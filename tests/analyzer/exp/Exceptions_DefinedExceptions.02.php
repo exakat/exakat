b4039c06b4c700b04023830916c44dd3870dfa04
Damien Seguy <damien.seguy@gmail.com>
2015-02-03 13:20:02 +0100

    Reversed UT

--- tests/analyzer/exp/Exceptions_DefinedExceptions.02.php
+++ tests/analyzer/exp/Exceptions_DefinedExceptions.02.php
@@ -1,9 +1,9 @@
 <?php
 
-$expected     = array('class d extends e' );
-
-$expected_not = array('class a extends Exception',
+$expected     = array('class a extends Exception',
                       'class b extends RuntimeException',
-                      'class c extends a');
+                      'class c extends a' );
+
+$expected_not = array('class d extends e');
 
 ?>
