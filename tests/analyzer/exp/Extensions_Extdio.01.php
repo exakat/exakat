5124577ed3d1296d40a6e53cb5cc1ee78aaceef2
Damien Seguy <damien.seguy@gmail.com>
2015-02-10 11:57:32 +0100

    Fixed UT expected output

--- tests/analyzer/exp/Extensions_Extdio.01.php
+++ tests/analyzer/exp/Extensions_Extdio.01.php
@@ -3,7 +3,7 @@
 $expected     = array("dio_open('/dev/ttyS0', O_RDWR | O_NOCTTY | O_NONBLOCK)", 
                       "dio_fcntl($fd, F_SETFL, O_SYNC)", 
                       "dio_read($fd, 256)", 
-                      "dio_tcsetattr($fd, array('baud' => 9600, 'bits' => 8, 'stop' => 1, 'parity' => 0))");
+                      "dio_tcsetattr(\$fd, array('baud' => 9600, 'bits' => 8, 'stop' => 1, 'parity' => 0))");
 
 $expected_not = array();
 
