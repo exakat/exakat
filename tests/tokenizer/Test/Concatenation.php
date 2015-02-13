5b4e129eb4e082bea1a25ae7d5f8215217da8111
Damien Seguy <damien.seguy@gmail.com>
2015-02-13 14:13:43 +0100

    Added test for concatenation with special case

--- tests/tokenizer/Test/Concatenation.php
+++ tests/tokenizer/Test/Concatenation.php
@@ -7,7 +7,7 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Concatenation extends Tokenizer {
-    /* 61 methods */
+    /* 62 methods */
     public function testConcatenation01()  { $this->generic_test('Concatenation.01'); }
     public function testConcatenation02()  { $this->generic_test('Concatenation.02'); }
     public function testConcatenation03()  { $this->generic_test('Concatenation.03'); }
@@ -69,5 +69,6 @@ class Concatenation extends Tokenizer {
     public function testConcatenation59()  { $this->generic_test('Concatenation.59'); }
     public function testConcatenation60()  { $this->generic_test('Concatenation.60'); }
     public function testConcatenation61()  { $this->generic_test('Concatenation.61'); }
+    public function testConcatenation62()  { $this->generic_test('Concatenation.62'); }
 }
 ?>
