1b001d4f6c851d01f25e9666cc7be80fc366fa7e
Damien Seguy <damien.seguy@gmail.com>
2015-02-16 21:18:17 +0100

    Fixed tests (they relied on * to load all tests at once)

--- tests/tokenizer/Test/Tokenizer.php
+++ tests/tokenizer/Test/Tokenizer.php
@@ -3,15 +3,9 @@
 namespace Test;
 
 class Tokenizer extends \PHPUnit_Framework_TestCase {
-    public static $loaded = 'none';
-    
     public function generic_test($file) {
-        $test = substr($file, 0, -3);
-        if (Tokenizer::$loaded != $test) {
-            $shell = 'cd ../..; sh scripts/clean.sh; php bin/load -r -f ./tests/tokenizer/source/'.$test.'*.php -p test; php bin/build_root -p test; php bin/tokenizer -p test;';
-            Tokenizer::$loaded = $test;
-            $res = shell_exec($shell);
-        }
+        $shell = 'cd ../..; sh scripts/clean.sh; php bin/load -r -f ./tests/tokenizer/source/'.$file.'.php -p test; php bin/build_root -p test; php bin/tokenizer -p test;';
+        $res = shell_exec($shell);
         
         $shell = 'cd ../..; php bin/export -text -f ./tests/tokenizer/source/'.$file.'.php';
         $res = shell_exec($shell);
