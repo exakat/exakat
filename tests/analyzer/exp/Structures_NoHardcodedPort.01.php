6540abb5639e6d272ff6231387b7751f66fb0483
Damien Seguy <damien.seguy@gmail.com>
2015-02-19 11:05:24 +0100

    Removed extra \

--- tests/analyzer/exp/Structures_NoHardcodedPort.01.php
+++ tests/analyzer/exp/Structures_NoHardcodedPort.01.php
@@ -1,7 +1,7 @@
 <?php
 
 $expected     = array("ftp_connect('my.ftphost.com', 21)",
-                      'maxdb_real_connect(\$link, \$hostname, \$username, \$passwd, \$dbname, \'8080\')');
+                      'maxdb_real_connect($link, $hostname, $username, $passwd, $dbname, \'8080\')');
 
 $expected_not = array();
 
