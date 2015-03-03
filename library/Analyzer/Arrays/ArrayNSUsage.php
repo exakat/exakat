e7b99f20f210cf36c0871b4dbe3a20a381dc1034
Damien Seguy <damien.seguy@gmail.com>
2015-03-03 22:09:55 +0100

    Lifted version limit

--- library/Analyzer/Arrays/ArrayNSUsage.php
+++ library/Analyzer/Arrays/ArrayNSUsage.php
@@ -26,8 +26,6 @@ namespace Analyzer\Arrays;
 use Analyzer;
 
 class ArrayNSUsage extends Analyzer\Analyzer {
-    public $phpVersion = '5.4+';
-
     public function analyze() {
         $this->atomIs('Array')
              ->is('short_syntax', 'true');
