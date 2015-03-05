ab4adc1edb65580c70f948efb9fd690852239f45
Damien Seguy <damien.seguy@gmail.com>
2015-03-05 22:21:31 +0100

    Finalized build with access to neo4j_project

--- library/Exakat.php
+++ library/Exakat.php
@@ -22,7 +22,7 @@
 
 class Exakat {
     const VERSION = '0.1.0';
-    const BUILD = '120';
+    const BUILD = '135';
     
     public function execute(Config $config) {
         switch ($config->command) {
