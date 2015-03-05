1b968ae464a647cfbab7c4e29de570c73a4e07d2
Damien Seguy <damien.seguy@gmail.com>
2015-03-05 12:45:32 +0100

    Removed useless variable; and code comments

--- library/Loader/ColonType.php
+++ library/Loader/ColonType.php
@@ -56,27 +56,19 @@ class ColonType {
             
             if (!is_string($token) || $token != ':') {
                 // dropping non-alternative if.
-                $a = array_pop($this->relatedAtom);
-                if (count($this->relatedAtom) == 0) {
-//                    print "relatedAtom is Empty\n";
-                }
-//                print "Check for Next ($a)\n";
-//                  print_r($this->relatedAtom);
+                array_pop($this->relatedAtom);
             }
         }
 
         if (is_array($token)) {
             if (isset($this->waitFor['array'][$token[3]])) {
                 $this->relatedAtom[] = $this->waitFor['array'][$token[3]];
-//                print "[] = ".$this->waitFor['array'][$token[3]]."\n";
                 
                 if (isset($this->parenthesedToken[$token[3]])) {
                     $this->parenthesisStack[$this->parenthesisLevel] = 1;
                 }
                 
                 if ($token[3] == 'T_ELSE') {
-//                    print_r($this->relatedAtom);
-//                    print "T_ELSE ++\n";
                     $this->checkNext = true;
                 }
             }
@@ -90,8 +82,6 @@ class ColonType {
             } elseif ($token == ')') {
                 $this->parenthesisLevel--;
                 if (isset($this->parenthesisStack[$this->parenthesisLevel])) {
-//                    print_r($this->parenthesisStack);
-//                    print_r($this->parenthesisLevel);
                     $this->checkNext = true;
                     unset($this->parenthesisStack[$this->parenthesisLevel]);
                 }
@@ -106,14 +96,6 @@ class ColonType {
             $this->relatedAtom = array('Label');
         }
         
-//        print "related atom Is $r[1]\n";
-        
         return $r;
     }
-    
-    public function dump() {
-//        print_r($this->relatedAtom);
-//        print_r($this->parenthesisStack);
-    }
-
 }
