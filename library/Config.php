a4091028d4a8f4a76b5b85d449efd5961f94a1fa
Damien Seguy <damien.seguy@gmail.com>
2015-03-05 10:46:51 +0100

    Added check if -default.ini doesn't exists

--- library/Config.php
+++ library/Config.php
@@ -53,7 +53,11 @@ class Config {
             $this->configFile = parse_ini_file($configFile);
         } else {
             $configFile = $this->dir_root.'/config/config-default.ini'; 
-            $this->configFile = parse_ini_file($configFile);
+            if (file_exists($configFile)) {
+                $this->configFile = parse_ini_file($configFile);
+            } else {
+                $this->configFile = array();
+            }
         }
 
         // then read the config from the commandline (if any)
@@ -140,24 +144,24 @@ class Config {
         }
         
         $optionsBoolean = array(
-                                 '-v'      => array('verbose',    false),
-                                 '-h'      => array('help',       false),
-                                 '-r'      => array('recursive',  false),
-                                 '-u'      => array('update',     false),
-                                 '-D'      => array('delete',     false),
-                                 '-l'      => array('lint',       false),
-                                 '-json'   => array('json',       false),
-                                 '-dot'    => array('dot',     false),
-                                 '-ss'     => array('ss',         false),
-                                 '-sm'     => array('sm',         false),
-                                 '-sl'     => array('sl',         false),
-                                 '-nodep'  => array('noDependencies', false),
-                                 '-norefresh' => array('noRefresh', false),
-                                 '-today'  => array('today',      false),
-                                 '-none'   => array('none',       false),
-                                 '-table'  => array('table',      false),
-                                 '-text'   => array('text',       false),
-                                 '-o'      => array('output',     false),
+                                 '-v'         => array('verbose',        false),
+                                 '-h'         => array('help',           false),
+                                 '-r'         => array('recursive',      false),
+                                 '-u'         => array('update',         false),
+                                 '-D'         => array('delete',         false),
+                                 '-l'         => array('lint',           false),
+                                 '-json'      => array('json',           false),
+                                 '-dot'       => array('dot',            false),
+                                 '-ss'        => array('ss',             false),
+                                 '-sm'        => array('sm',             false),
+                                 '-sl'        => array('sl',             false),
+                                 '-nodep'     => array('noDependencies', false),
+                                 '-norefresh' => array('noRefresh',      false),
+                                 '-today'     => array('today',          false),
+                                 '-none'      => array('none',           false),
+                                 '-table'     => array('table',          false),
+                                 '-text'      => array('text',           false),
+                                 '-o'         => array('output',         false),
                                  );
 
         foreach($optionsBoolean as $key => $config) {
@@ -170,17 +174,18 @@ class Config {
             }
         }
                                  
-        $optionsValue   = array('-f' => array('filename',    null),
-                                '-d' => array('dirname',     null),
-                                '-p' => array('project',     'default'),
-                                '-P' => array('program',     null),
-                                '-R' => array('repository',  false),
-                                '-T' => array('thema',       null),
-                                '-report' => array('report',     'Premier'),
-                                '-format' => array('format',     'Text'),
-                                '-file' => array('file',     'report'),
-                                '-style' => array('style',   'ALL'), //
-//                                '-q' => array('loader',   'Load\Csv'),
+        $optionsValue   = array('-f'           => array('filename',    null),
+                                '-d'           => array('dirname',     null),
+                                '-p'           => array('project',     'default'),
+                                '-P'           => array('program',     null),
+                                '-R'           => array('repository',  false),
+                                '-T'           => array('thema',       null),
+                                '-report'      => array('report',      'Premier'),
+                                '-format'      => array('format',      'Text'),
+                                '-file'        => array('file',        'report'),
+                                '-style'       => array('style',       'ALL'), 
+                                '-neo4j_host'  => array('neo4j_host',  '127.0.0.1'), 
+                                '-neo4j_port'  => array('neo4j_port',  '7474'), 
                                  );
 
         foreach($optionsValue as $key => $config) {
