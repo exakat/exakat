260a9d7fb1b5c1694caeae36f729b60679d0890f
Damien Seguy <damien.seguy@gmail.com>
2015-03-09 23:13:50 +0100

    Spot visibility problems early

---
+++ tests/analyzer/exp/Interfaces_ConcreteVisibility.01.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('private function interfaceMethod1 ( ) { /**/ } ',
+                      'protected function interfaceMethod2 ( ) { /**/ } ',);
+
+$expected_not = array('private function interfaceMethod3 ( ) { /**/ } ',
+                      'function interfaceMethod4 ( ) { /**/ } ',
+                      'function classMethod() {}',
+                      'function notMethod() {}',
+                      'private function traitMethod1() {}',
+                      );
+
+?>
