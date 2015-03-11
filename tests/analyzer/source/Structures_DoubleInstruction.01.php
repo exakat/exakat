c763bf28f3e0f0416d4473d4d26a9eefb11407e2
Damien Seguy <damien.seguy@gmail.com>
2015-03-11 13:32:52 +0100

    Spot instructions that are copy/paste in a row

---
+++ tests/analyzer/source/Structures_DoubleInstruction.01.php
@@ -0,0 +1,14 @@
+<?php
+
+    $string[3] = 2;
+    $array["b"] = 3;
+    $array["b"] = 3;
+    $string[3] = 2;
+    
+    $c++;
+    $c++;
+    
+    if ($debug) { $d++; }
+    if ($debug) { $d++; }
+
+?>
