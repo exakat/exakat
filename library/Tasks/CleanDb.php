241c8d1cb56ad0c1cd0bfa98559d54448eb00ed2
Damien Seguy <damien.seguy@gmail.com>
2015-03-16 19:00:42 +0100

    Clean the neo4j database faster

---
+++ library/Tasks/CleanDb.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright 2012-2015 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+
+namespace Tasks;
+
+use Everyman\Neo4j\Client,
+	Everyman\Neo4j\Index\NodeIndex,
+	Everyman\Neo4j\Relationship,
+	Everyman\Neo4j\Node,
+	Everyman\Neo4j\Cypher\Query;
+
+class CleanDb implements Tasks {
+    private $client = null;
+    
+    public function run(\Config $config) {
+        if ($config->quick) {
+            shell_exec('cd '.$config->dir_root.'/neo4j/;./bin/neo4j stop; rm -rf data; mkdir data; ./bin/neo4j start');
+            print "Database cleaned with restart\n";
+        } else {
+            $client = new Client();
+        
+            $queryTemplate = 'MATCH (n) 
+OPTIONAL MATCH (n)-[r]-() 
+DELETE n,r';
+        	$query = new Query($client, $queryTemplate, array());
+	        $result = $query->getResultSet();
+            print "Database cleaned\n";
+        }
+    }
+}
+
+?>
