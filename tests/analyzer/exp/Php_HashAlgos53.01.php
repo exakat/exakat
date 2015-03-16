da4a7a72f4317433d9fa9082b82a41a28f760778
Damien Seguy <damien.seguy@gmail.com>
2015-03-16 12:08:51 +0100

    Spot old algorithms

---
+++ tests/analyzer/exp/Php_HashAlgos53.01.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array("'fnv132'");
+
+$expected_not = array("'ripemd160'");
+
+?>
---
+++ tests/analyzer/source/Php_HashAlgos53.01.php
@@ -0,0 +1,6 @@
+<?php
+
+echo hash('fnv132', 'The quick brown fox jumped over the lazy dog.');
+
+echo hash('ripemd160', 'The quick brown fox jumped over the lazy dog.');
+?>
