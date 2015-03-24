411046e946278546a547c32f54b0a2ace2a39d71
Damien Seguy <damien.seguy@gmail.com>
2015-03-24 15:50:18 +0100

    Removed direct call to __DIR__

--- library/Description.php
+++ library/Description.php
@@ -23,20 +23,20 @@
 
 class Description {
     private $language = 'en';
-    private $ini = array('human' => '',
-                         'name' => '',
+    private $ini = array('human'    => '',
+                         'name'     => '',
                          'clearphp' => '');
     
     public function __construct($analyzer) {
-        $filename = dirname(__DIR__).'/human/'.$this->language.'/'.str_replace('\\', '/', str_replace("Analyzer\\", "", $analyzer)).'.ini';
+        $config = \Config::factory();
+
+        $filename = $config->dir_root.'/human/'.$this->language.'/'.str_replace('\\', '/', str_replace("Analyzer\\", "", $analyzer)).'.ini';
         
-        if (!file_exists($filename)) {
-            $this->ini = array('description' => '',
-                               'name' => '',
-                               'clearphp' => '');
-        } else {
+        if (file_exists($filename)) {
             $this->ini = parse_ini_file($filename) + $this->ini;
         }
+        
+        // else is the default values already defined above
     }
 
     public function setLanguage($language) {
