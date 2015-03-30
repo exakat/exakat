5fb28256a834ff9547e943483b50093eb7f84c50
Damien Seguy <damien.seguy@gmail.com>
2015-03-30 21:31:56 +0200

    Spot unresolved Catch

---
+++ tests/analyzer/exp/Classes_UnresolvedCatch.01.php
@@ -0,0 +1,16 @@
+<?php
+
+$expected     = array('catch (LocalNonException $e) { /**/ } ',
+                      'catch (OutOfBoundsException $e) { /**/ } ',
+                      'catch (UndefinedClass $e) { /**/ } ',
+                      'catch (Exception $e) { /**/ } ',
+);
+
+$expected_not = array('catch (LocalException $e) { /**/ } ',
+                      'catch (LocalSubException $e) { /**/ } ',
+                      'catch (LocalSubSubException $e) { /**/ } ',
+                      'catch (\\Exception $e) { /**/ } ',
+                      'catch (\\OutOfBoundsException $e) { /**/ } ',
+                        );
+
+?>
