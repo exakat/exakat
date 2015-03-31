7b2ee2c64fdb4ef6ee945b40c38c88829e310c8d
Damien Seguy <damien.seguy@gmail.com>
2015-03-31 22:06:02 +0200

    Preprocessed value

--- library/Analyzer/Constants/InconsistantCase.php
+++ library/Analyzer/Constants/InconsistantCase.php
@@ -29,11 +29,11 @@ class InconsistantCase extends Analyzer\Analyzer {
 
     public function analyze() {
         $this->atomIs('Null')
-             ->groupFilter("if (it.code == it.code.toLowerCase()) { x2 = 'lower'; } else if (it.code == it.code.toUpperCase()) { x2 = 'upper'; } else {x2 = 'mixed'; }", 10 / 100);
+             ->groupFilter("if (it.code == it.code.toLowerCase()) { x2 = 'lower'; } else if (it.code == it.code.toUpperCase()) { x2 = 'upper'; } else {x2 = 'mixed'; }", 0.1);
         $this->prepareQuery();
 
         $this->atomIs('Null')
-             ->groupFilter("if (it.code == it.code.toLowerCase()) { x2 = 'lower'; } else if (it.code == it.code.toUpperCase()) { x2 = 'upper'; } else {x2 = 'mixed'; }", 10 / 100);
+             ->groupFilter("if (it.code == it.code.toLowerCase()) { x2 = 'lower'; } else if (it.code == it.code.toUpperCase()) { x2 = 'upper'; } else {x2 = 'mixed'; }", 0.1);
         $this->prepareQuery();
     }
 }
