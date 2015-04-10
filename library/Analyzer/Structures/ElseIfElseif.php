efbdfbe528be61a48248a59abc8f90b802253315
Damien Seguy <damien.seguy@gmail.com>
2015-04-10 18:40:18 +0200

    Avoid "else if"

---
+++ library/Analyzer/Structures/ElseIfElseif.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Analyzer\Structures;
+
+use Analyzer;
+
+class ElseIfElseif extends Analyzer\Analyzer {
+    // if () {} else  if {} 
+    // but not if () {} elseif {} 
+    public function analyze() {
+        $this->atomIs('Ifthen')
+             ->outIs('ELSE')
+             ->raw('filter{ it.out("ELEMENT").count() == 1}')
+             ->outIs('ELEMENT')
+             ->atomIs('Ifthen')
+             ->tokenIsNot('T_ELSEIF');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Structures_ElseIfElseif.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('if ( 20) ;',
+                      'if ( 21) ;',
+);
+
+$expected_not = array();
+
+?>
