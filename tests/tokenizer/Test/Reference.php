6b4b23366a3468b3b1894914d6e1654968bfb7db
Damien Seguy <damien.seguy@gmail.com>
2015-04-12 14:15:07 +0200

    Added extra check

--- tests/tokenizer/Test/Reference.php
+++ tests/tokenizer/Test/Reference.php
@@ -7,7 +7,7 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Reference extends Tokenizer {
-    /* 39 methods */
+    /* 40 methods */
     
     public function testReference01()  { $this->generic_test('Reference.01'); }
     public function testReference02()  { $this->generic_test('Reference.02'); }
@@ -49,5 +49,6 @@ class Reference extends Tokenizer {
     public function testReference37()  { $this->generic_test('Reference.37'); }
     public function testReference38()  { $this->generic_test('Reference.38'); }
     public function testReference39()  { $this->generic_test('Reference.39'); }
+    public function testReference40()  { $this->generic_test('Reference.40'); }
 
 ?>
---
+++ tests/tokenizer/source/Reference.40.php
@@ -0,0 +1,18 @@
+<?php
+        if (~B() & $a) {
+            return C;
+        } elseif (!B() & $a) {
+            return C;
+        } else
+        
+        for (@B() & $a; $i < 10; $i++) {
+            return C;
+        }
+
+        switch (B() & $a) {
+            case 1 : 
+            return C;
+        }
+
+
+?>
