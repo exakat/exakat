b62ded571abca14994d342ef9929b9826829b976
Damien Seguy <damien.seguy@gmail.com>
2015-04-24 12:26:01 +0200

    Updated Clone with special cases for Parenthesis

--- library/Tokenizer/_Clone.php
+++ library/Tokenizer/_Clone.php
@@ -29,12 +29,12 @@ class _Clone extends TokenAuto {
 
     public function _check() {
         $operands = array('Variable', 'Property', 'Array', 'Staticproperty', 'Staticmethodcall', 'Staticconstant', 'Staticclass',
-                          'Functioncall', 'Methodcall', 'New', 'Noscream', 'Concatenation', 'Ternary' );
+                          'Functioncall', 'Methodcall', 'New', 'Noscream', 'Concatenation', 'Ternary', 'Parenthesis' );
         $this->conditions = array(0 => array('token' => _Clone::$operators,
                                              'atom' => 'none'),
                                   1 => array('atom' =>  $operands),
-                                  2 => array('filterOut' => array('T_OPEN_BRACKET', 'T_OPEN_CURLY', 'T_DOUBLE_COLON', 'T_OBJECT_OPERATOR', 'T_DOT',
-                                                                  'T_OPEN_PARENTHESIS',  )),
+                                  2 => array('filterOut' => array('T_OPEN_BRACKET', 'T_OPEN_CURLY', 'T_DOUBLE_COLON',
+                                                                  'T_OBJECT_OPERATOR', 'T_DOT', 'T_OPEN_PARENTHESIS',)),
         );
         
         $this->actions = array('transform'    => array( 1 => 'CLONE'),
@@ -43,21 +43,6 @@ class _Clone extends TokenAuto {
                                'makeSequence' => 'it');
         $this->checkAuto();
 
-        $this->conditions = array(0 => array('token' => _Clone::$operators,
-                                             'atom'  => 'none'),
-                                  1 => array('token' => 'T_OPEN_PARENTHESIS'),
-                                  2 => array('atom'  =>  $operands),
-                                  3 => array('token' => 'T_CLOSE_PARENTHESIS'),
-        );
-        
-        $this->actions = array('transform'    => array( 1 => 'DROP',
-                                                        2 => 'CLONE',
-                                                        3 => 'DROP',),
-                               'atom'         => 'Clone',
-                               'cleanIndex'   => true,
-                               'makeSequence' => 'it');
-        $this->checkAuto();
-        
         return false;
     }
 
