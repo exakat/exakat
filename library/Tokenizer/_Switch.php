81fff0a009124020b814441e3053719ee6737a9d
Damien Seguy <damien.seguy@gmail.com>
2015-05-01 08:21:04 +0200

    Removed one rule, and cleaned 'alternative'

--- library/Tokenizer/_Switch.php
+++ library/Tokenizer/_Switch.php
@@ -52,36 +52,17 @@ class _Switch extends TokenAuto {
                                'makeSequence' => 'it');
         $this->checkAuto();
 
-        // switch ( $cdt ) Block
-        $this->conditions = array(0 => array('token' => _Switch::$operators,
-                                             'atom'  => 'none'),
-                                  1 => array('token' => 'T_OPEN_PARENTHESIS',
-                                             'property' => array('association' => 'Switch')),
-                                  2 => array('atom'  => 'yes'),
-                                  3 => array('token' => 'T_CLOSE_PARENTHESIS'),
-                                  4 => array('atom'  => 'Sequence')
-        );
-        
-        $this->actions = array('transform'    => array( 1 => 'DROP',
-                                                        2 => 'NAME',
-                                                        3 => 'DROP',
-                                                        4 => 'CASES'),
-                               'atom'         => 'Switch',
-                               'cleanIndex'   => true,
-                               'makeSequence' => 'it');
-        $this->checkAuto();
-
         // alternative syntax
-        $this->conditions = array(0 => array('token' => _Switch::$operators,
-                                             'atom'  => 'none'),
-                                  1 => array('token' => 'T_OPEN_PARENTHESIS',
+        $this->conditions = array(0 => array('token'    => _Switch::$operators,
+                                             'atom'     => 'none'),
+                                  1 => array('token'    => 'T_OPEN_PARENTHESIS',
                                              'property' => array('association' => 'Switch')),
-                                  2 => array('atom'  => 'yes'),
-                                  3 => array('token' => 'T_CLOSE_PARENTHESIS'),
-                                  4 => array('token' => 'T_COLON',
+                                  2 => array('atom'     => 'yes'),
+                                  3 => array('token'    => 'T_CLOSE_PARENTHESIS'),
+                                  4 => array('token'    => 'T_COLON',
                                              'property' => array('relatedAtom' => 'Switch')),
-                                  5 => array('atom'  => 'SequenceCaseDefault'),
-                                  6 => array('token' => 'T_ENDSWITCH'),
+                                  5 => array('atom'     => array('SequenceCaseDefault', 'Void')),
+                                  6 => array('token'    => 'T_ENDSWITCH'),
         );
         
         $this->actions = array('transform'    => array( 1 => 'DROP',
