80333993c0e34b1f61908c1b9994fc6145335ab0
Damien Seguy <damien.seguy@gmail.com>
2015-05-05 21:07:52 +0200

    Fixed exp UT

--- tests/analyzer/exp/Files_GlobalCodeOnly.01.php
+++ tests/analyzer/exp/Files_GlobalCodeOnly.01.php
@@ -1,6 +1,6 @@
 <?php
 
-$expected     = array('tests/analyzer/source/Files_GlobalCodeOnly.01.php');
+$expected     = array('./tests/analyzer/source/Files_GlobalCodeOnly.01.php');
 
 $expected_not = array();
 
