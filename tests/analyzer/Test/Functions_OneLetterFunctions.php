da3635de45dc4653b660b0554edc7cbf2d41eaec
Damien Seguy <damien.seguy@gmail.com>
2015-05-05 17:46:33 +0200

    Extra test for Traits and interfaces.

--- tests/analyzer/Test/Functions_OneLetterFunctions.php
+++ tests/analyzer/Test/Functions_OneLetterFunctions.php
@@ -8,9 +8,10 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Functions_OneLetterFunctions extends Analyzer {
-    /* 2 methods */
+    /* 3 methods */
 
     public function testFunctions_OneLetterFunctions01()  { $this->generic_test('Functions_OneLetterFunctions.01'); }
     public function testFunctions_OneLetterFunctions02()  { $this->generic_test('Functions_OneLetterFunctions.02'); }
+    public function testFunctions_OneLetterFunctions03()  { $this->generic_test('Functions_OneLetterFunctions.03'); }
 }
 ?>
---
+++ tests/analyzer/source/Functions_OneLetterFunctions.03.php
@@ -0,0 +1,18 @@
+<?php
+
+function a() {}
+function a2() {}
+
+trait t {
+    function c() {}
+    function c2() {}
+}
+
+interface i {
+    function f();
+    function f23();
+}
+
+$closure = function() {}
+
+?>
