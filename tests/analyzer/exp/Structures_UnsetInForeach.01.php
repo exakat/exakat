3c131899519b92145c28b18fa7dc7e3879d3e830
Damien Seguy <damien.seguy@gmail.com>
2015-05-06 22:50:04 +0200

    Fixed exp UT

--- tests/analyzer/exp/Structures_UnsetInForeach.01.php
+++ tests/analyzer/exp/Structures_UnsetInForeach.01.php
@@ -1,14 +1,13 @@
 <?php
 
-$expected     = array('foreach($a as $b => &$unsetArrayC) ;',
-                      'foreach($a as $unsetArrayB => $c) ;',
-                      'foreach($a as &$unsetArrayC) ;',
-                      'foreach($a as $b => $unsetC) ;',
-                      'foreach($a as $b => &$unsetRefC) ;',
-                      'foreach($a as $unsetB => $c) ;',
-                      'foreach($a as $unsetC) ;',
-                      'foreach($a as &$unsetRefC) ;',
-);
+$expected     = array('foreach($a as $unsetC) { /**/ } ', 
+                      'foreach($a as $unsetB => $c) { /**/ } ', 
+                      'foreach($a as &$unsetRefC) { /**/ } ', 
+                      'foreach($a as $b => $unsetC) { /**/ } ', 
+                      'foreach($a as $b => &$unsetRefC) { /**/ } ', 
+                      'foreach($a as &$unsetArrayC) { /**/ } ', 
+                      'foreach($a as $unsetArrayB => $c) { /**/ } ', 
+                      'foreach($a as $b => &$unsetArrayC) { /**/ } ');
 
 $expected_not = array();
 
