c0ec8afff20329f3d73e8167b78fc14c2866bd52
Damien Seguy <damien.seguy@gmail.com>
2015-05-08 17:50:37 +0200

    Fixed UT

--- tests/analyzer/Test/Classes_IsInterfaceMethod.php
+++ tests/analyzer/Test/Classes_IsInterfaceMethod.php
@@ -8,8 +8,10 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Classes_IsInterfaceMethod extends Analyzer {
-    /* 1 methods */
+    /* 3 methods */
 
     public function testClasses_IsInterfaceMethod01()  { $this->generic_test('Classes_IsInterfaceMethod.01'); }
+    public function testClasses_IsInterfaceMethod02()  { $this->generic_test('Classes_IsInterfaceMethod.02'); }
+    public function testClasses_IsInterfaceMethod03()  { $this->generic_test('Classes_IsInterfaceMethod.03'); }
 }
 ?>
