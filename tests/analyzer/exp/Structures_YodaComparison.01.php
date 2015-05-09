170804c5a00cf2886c6d7046e9c10a1fba5a9f2d
Damien Seguy <damien.seguy@gmail.com>
2015-05-09 15:54:48 +0200

    Added missing test with Float

--- tests/analyzer/exp/Structures_YodaComparison.01.php
+++ tests/analyzer/exp/Structures_YodaComparison.01.php
@@ -4,7 +4,8 @@ $expected     = array('$a == null',
                       '$a == true',
                       '$a == 2',
                       '$a == 1',
-                      '$d[3] === \A\SOME_YODA_CONSTANT'
+                      '$d[3] === \A\SOME_YODA_CONSTANT',
+                      '$a == 1.2'
 );
 
 $expected_not = array('$d[3] != YODA_SOME_CONSTANT');
--- tests/analyzer/exp/Structures_YodaComparison.02.php
+++ tests/analyzer/exp/Structures_YodaComparison.02.php
@@ -2,7 +2,8 @@
 
 $expected     = array('$a == 2', 
                       '$a == true', 
-                      '$a == null');
+                      '$a == null',
+                      '$a == 1.2');
 
 $expected_not = array();
 
