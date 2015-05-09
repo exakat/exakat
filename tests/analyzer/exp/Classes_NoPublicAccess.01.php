f1fdae3604f5cd88145e9308f7a0605dc54acf25
Damien Seguy <damien.seguy@gmail.com>
2015-05-09 16:09:36 +0200

    Fixed UT

--- tests/analyzer/exp/Classes_NoPublicAccess.01.php
+++ tests/analyzer/exp/Classes_NoPublicAccess.01.php
@@ -1,11 +1,11 @@
 <?php
 
-$expected     = array('public $unused = 2');
+$expected     = array('public $unused = 2',
+                      'public static $usedButStatic = 4');
 
 $expected_not = array('$used',
                       '$usedInside',
-                      '$usedButStatic',
-                      'public static $usedButStatic = 4'
+                      '$usedButStatic'
 );
 
 ?>
