b3de1a1042dea948fe10129484061f28cfb8d619
Damien Seguy <damien.seguy@gmail.com>
2015-05-12 15:14:33 +0200

    Added clearPHP link

--- human/en/Structures/EvalUsage.php
+++
@@ -1,9 +0,0 @@
-name = "eval()";
-description = "eval() allows the compilation and execution of PHP code at runtime. It takes a string, and then run the code.
-
-The number of problems linked to this function is important : 
-+ Security problems, when some external variables are included in the executed PHP code
-+ Caching problems, as this code won't be caught by APC or similar code cache
-+ Performances, as it is much more efficient to have the code compiled by PHP before running, that at runtime.
-
-It is usually possible to replace that kind of code by some permanant one, given the dynamic nature of PHP. ";
--- library/Analyzer/Exceptions/Unthrown.php
+++ library/Analyzer/Exceptions/Unthrown.php
@@ -34,9 +34,7 @@ class Unthrown extends Analyzer\Analyzer {
         $this->atomIs('Class')
              ->analyzerIs('Analyzer\\Exceptions\\DefinedExceptions')
              ->savePropertyAs('fullnspath', 'path')
-             ->raw('filter{ g.idx("atoms")[["atom":"Throw"]].out("THROW").out("NEW").has("fullnspath", path).any() == false}')
-//             ->back('first')
-             ;
+             ->raw('filter{ g.idx("atoms")[["atom":"Throw"]].out("THROW").out("NEW").has("fullnspath", path).any() == false}');
         $this->prepareQuery();
     }
 }
