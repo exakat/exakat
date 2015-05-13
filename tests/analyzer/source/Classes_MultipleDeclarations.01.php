e878baad672781429d9aeaeee575bc3ab309b1c4
Damien Seguy <damien.seguy@gmail.com>
2015-05-13 15:59:14 +0200

    Spotting Mutliple declarations of classes (based on the same FQN)

---
+++ tests/analyzer/source/Classes_MultipleDeclarations.01.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace a {
+    class b1 {}
+
+    class b2 {}
+
+    class b3 {}
+
+    class b4 {}
+}
+
+// those may be in separated files...
+namespace a {
+    class b2 {}
+
+    class b3 {}
+
+    class b4 {}
+}
+
+namespace a {
+    class b3 {}
+
+    class b4 {}
+}
+
+namespace a {
+    class b4 {}
+}
+
+?>
