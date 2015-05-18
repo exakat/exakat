56e5be1478d0857e5412459143c14ebabd619482
Damien Seguy <damien.seguy@gmail.com>
2015-05-18 18:19:30 +0200

    Constants may be arrays

--- library/Tokenizer/_Array.php
+++ library/Tokenizer/_Array.php
@@ -28,27 +28,12 @@ class _Array extends TokenAuto {
     static public $atom = 'Array';
     static public $allowedObject = array('Variable', 'Array', 'Property', 'Staticproperty', 'Arrayappend',
                                          'Functioncall', 'Methodcall', 'Staticmethodcall', 'String',
-                                         'Staticconstant');
+                                         'Staticconstant', 'Identifier');
     
     public function _check() {
         // $x[3] and mutlidimensional too
         $this->conditions = array( -2 => array('notToken'      => array_merge(Staticproperty::$operators, Property::$operators)),
                                    -1 => array('atom'          => _Array::$allowedObject),
-                                    0 => array('token'         => _Array::$operators,
-                                               'checkForArray' => true),
-                                    1 => array('atom'          => 'yes',
-                                               'notAtom'       => 'Sequence'),
-                                    2 => array('token'         => array('T_CLOSE_BRACKET', 'T_CLOSE_CURLY')),
-                                 );
-        
-        $this->actions = array('toArray'      => true,
-                               'makeSequence' => 'b1',
-                               'cleanIndex'   => true);
-        $this->checkAuto();
-
-        // Class::Constante[INDEX]
-        $this->conditions = array( -2 => array('token'         => 'T_DOUBLE_COLON'),
-                                   -1 => array('token'         => 'T_STRING'),
                                     0 => array('token'         => _Array::$operators,
                                                'checkForArray' => true),
                                     1 => array('atom'          => 'yes',
