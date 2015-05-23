c8af4d169107ccab4e520855b7cd1c15b8981f77
Damien Seguy <damien.seguy@gmail.com>
2015-05-23 07:06:39 +0200

    Also spotting void after definitions (class, interface, traits...)

--- tests/analyzer/Test/Structures_EmptyLines.php
+++ tests/analyzer/Test/Structures_EmptyLines.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_EmptyLines extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testStructures_EmptyLines01()  { $this->generic_test('Structures_EmptyLines.01'); }
+    public function testStructures_EmptyLines02()  { $this->generic_test('Structures_EmptyLines.02'); }
 }
 ?>
---
+++ tests/analyzer/source/Structures_EmptyLines.02.php
@@ -0,0 +1,14 @@
+<?php   
+
+class x { private $b; } ;
+
+function y ($y) { $y++;};
+
+interface i { function i();} ;
+
+trait t {};
+
+{};
+
+$a++;
+?>
