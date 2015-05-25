9dcb923c993559f357acb5f478ba8e1b60a0b857
Damien Seguy <damien.seguy@gmail.com>
2015-05-25 20:42:50 +0200

    Added 'T_DO' as non-blocking

--- library/Tokenizer/Block.php
+++ library/Tokenizer/Block.php
@@ -34,7 +34,6 @@ class Block extends TokenAuto {
                                     1 => array('atom'    => 'yes'),
                                     2 => array('token'   => 'T_CLOSE_CURLY',
                                                'atom'    => 'none'),
-                                    //3 => array('token'   => array('T_CLOSE_CURLY', 'T_SEMICOLON', 'T_OPEN_CURLY')),
         );
         
         $this->actions = array('toBlock'      => true);
@@ -46,7 +45,7 @@ class Block extends TokenAuto {
                                                                      'T_CLOSE_PARENTHESIS', 'T_OPEN_PARENTHESIS',// x(1){3},
                                                                      'T_OPEN_BRACKET', 'T_CLOSE_BRACKET',  // $x[1]{3},
                                                                      'T_OBJECT_OPERATOR', 'T_DOUBLE_COLON', 'T_AT',
-                                                                     'T_STRING', 'T_COMMA', 'T_ELSE')),
+                                                                     'T_STRING', 'T_COMMA', 'T_ELSE', 'T_DO')),
                                     0 => array('token'      => self::$operators,
                                                'property'   => array('association' => 'none')),
                                     1 => array('atom'       => 'yes'),
