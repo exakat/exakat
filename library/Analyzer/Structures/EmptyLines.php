61a3715743cb8746f526bee1c7050b58546bb189
Damien Seguy <damien.seguy@gmail.com>
2015-05-25 20:43:40 +0200

    Fixed UT

--- library/Analyzer/Structures/EmptyLines.php
+++ library/Analyzer/Structures/EmptyLines.php
@@ -30,12 +30,12 @@ class EmptyLines extends Analyzer\Analyzer {
         // one void in the sequence
         $this->atomIs('Void')
              ->hasIn('ELEMENT')
-             ->isNot('rank', '0');
+             ->isNot('rank', 0);
         $this->prepareQuery();
 
         // if the void is only one, we must check if this is a condition
         $this->atomIs('Void')
-             ->is('rank', '0')
+             ->is('rank', 0)
              ->inIs('ELEMENT')
              ->hasNoIn('BLOCK')
              ->back('first');
