078ac3d449797bcd93b9558c6badc55e66b968c9
Damien Seguy <damien.seguy@gmail.com>
2015-06-03 10:47:49 +0200

    Spot empty namespaces.

---
+++ tests/analyzer/exp/Namespaces_EmptyNamespace.03.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('namespace R', 
+                      'namespace B\\D');
+
+$expected_not = array('namespace Global', );
+
+?>
---
+++ tests/analyzer/source/Namespaces_EmptyNamespace.01.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace B;
+
+namespace B\D;
+
+namespace R;
+
+namespace B\E;
+
+class H { }
