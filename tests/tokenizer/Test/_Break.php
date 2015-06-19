be87f7ce24c74f6b28e45a655399986945f60beb
Damien Seguy <damien.seguy@gmail.com>
2015-06-19 11:05:24 +0200

    New tests for _Break

--- tests/tokenizer/Test/_Break.php
+++ tests/tokenizer/Test/_Break.php
@@ -7,11 +7,13 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class _Break extends Tokenizer {
-    /* 4 methods */
+    /* 6 methods */
 
     public function test_Break01()  { $this->generic_test('_Break.01'); }
     public function test_Break02()  { $this->generic_test('_Break.02'); }
     public function test_Break03()  { $this->generic_test('_Break.03'); }
     public function test_Break04()  { $this->generic_test('_Break.04'); }
+    public function test_Break05()  { $this->generic_test('_Break.05'); }
+    public function test_Break06()  { $this->generic_test('_Break.06'); }
 }
 ?>
