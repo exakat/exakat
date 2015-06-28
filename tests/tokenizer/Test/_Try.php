602d2b2f3b0c6b3a9faef01f0c614f4481c0605c
Damien Seguy <damien.seguy@gmail.com>
2015-06-28 21:11:31 +0200

    Fixed Sequence that ate Catch blocks (Catch goes to Try)

--- tests/tokenizer/Test/_Try.php
+++ tests/tokenizer/Test/_Try.php
@@ -7,7 +7,7 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class _Try extends Tokenizer {
-    /* 14 methods */
+    /* 15 methods */
 
     public function test_Try01()  { $this->generic_test('_Try.01'); }
     public function test_Try02()  { $this->generic_test('_Try.02'); }
@@ -23,5 +23,6 @@ class _Try extends Tokenizer {
     public function test_Try12()  { $this->generic_test('_Try.12'); }
     public function test_Try13()  { $this->generic_test('_Try.13'); }
     public function test_Try14()  { $this->generic_test('_Try.14'); }
+    public function test_Try15()  { $this->generic_test('_Try.15'); }
 }
 ?>
