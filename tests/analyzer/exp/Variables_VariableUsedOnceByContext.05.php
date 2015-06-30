81e6168c96a51f06f2441f43fcc933c7c5a952cb
Damien Seguy <damien.seguy@gmail.com>
2015-06-30 18:28:29 +0200

    Exclude abstract from usedOnce.

---
+++ tests/analyzer/exp/Variables_VariableUsedOnceByContext.05.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('$bt2',
+                      '$bc2');
+
+$expected_not = array('$bt',
+                      '$bc',
+                      '$ct',
+                      '$cc');
+
+?>
