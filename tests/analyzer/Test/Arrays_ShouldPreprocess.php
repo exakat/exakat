10bf2bfb9183d95390c8ae14c0cc4739d27295ae
Damien Seguy <damien.seguy@gmail.com>
2015-06-30 12:25:24 +0200

    Fixed rule when using proprerties (static or not)

--- tests/analyzer/Test/Arrays_ShouldPreprocess.php
+++ tests/analyzer/Test/Arrays_ShouldPreprocess.php
@@ -8,12 +8,13 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Arrays_ShouldPreprocess extends Analyzer {
-    /* 5 methods */
+    /* 6 methods */
 
     public function testArrays_ShouldPreprocess01()  { $this->generic_test('Arrays_ShouldPreprocess.01'); }
     public function testArrays_ShouldPreprocess02()  { $this->generic_test('Arrays_ShouldPreprocess.02'); }
     public function testArrays_ShouldPreprocess03()  { $this->generic_test('Arrays_ShouldPreprocess.03'); }
     public function testArrays_ShouldPreprocess04()  { $this->generic_test('Arrays_ShouldPreprocess.04'); }
     public function testArrays_ShouldPreprocess05()  { $this->generic_test('Arrays_ShouldPreprocess.05'); }
+    public function testArrays_ShouldPreprocess06()  { $this->generic_test('Arrays_ShouldPreprocess.06'); }
 }
 ?>
