85c0276dcc8b171b537137f4d4d3b3bb77410665
Damien Seguy <damien.seguy@gmail.com>
2015-07-05 16:16:43 +0200

    Added support for ibase

---
+++ tests/analyzer/source/Extensions_Extibase.01.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost:/path/to/your.gdb';
+
+$dbh = ibase_connect($host, $username, $password);
+$stmt = 'SELECT * FROM tblname';
+$sth = ibase_query($dbh, $stmt);
+while ($row = ibase_fetch_object($sth)) {
+    echo $row->email, "\n";
+}
+ibase_free_result($sth);
+ibase_close($dbh);
+?>
