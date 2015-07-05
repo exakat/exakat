a83c9eba0293bb6d0513d96d8ac8c7a69c80e60d
Damien Seguy <damien.seguy@gmail.com>
2015-07-05 16:23:55 +0200

    Those extensions have Directives

--- library/Report/Content/Directives/Amqp.php
+++ library/Report/Content/Directives/Amqp.php
@@ -25,7 +25,7 @@ namespace Report\Content\Directives;
 
 use Everyman\Neo4j\Client;
 
-class Date extends Directives {
+class Amqp extends Directives {
     public function __construct(Client $neo4j) {
         parent::__construct($neo4j);
         $this->name         = 'Amqp';
