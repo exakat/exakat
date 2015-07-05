be7c4fc83160f85822c89a6e346a7a758a52ac25
Damien Seguy <damien.seguy@gmail.com>
2015-07-05 16:38:45 +0200

    Added support ev

---
+++ tests/analyzer/exp/Extensions_Extev.01.php
@@ -0,0 +1,20 @@
+<?php
+
+$expected     = array('EvTimer(10, 0, function ( ) { /**/ } )', 
+                      'EvTimer(2, 0, function ( ) { /**/ } )', 
+                      'EvTimer(2, 1, function ($w) { /**/ } )', 
+                      'Ev', 
+                      'Ev', 
+                      'Ev', 
+                      'Ev', 
+                      'Ev', 
+                      'Ev', 
+                      'Ev', 
+                      'Ev', 
+                      'Ev', 
+                      'Ev', 
+                      'EvTimer');
+
+$expected_not = array();
+
+?>
