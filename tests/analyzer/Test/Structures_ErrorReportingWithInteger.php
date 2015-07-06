320d1de1df3622569974ffbbdde3bfd6323ae0e0
Damien Seguy <damien.seguy@gmail.com>
2015-07-06 12:48:36 +0200

    Report with Integer is OK with -1.

--- tests/analyzer/Test/Structures_ErrorReportingWithInteger.php
+++ tests/analyzer/Test/Structures_ErrorReportingWithInteger.php
@@ -8,10 +8,11 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_ErrorReportingWithInteger extends Analyzer {
-    /* 3 methods */
+    /* 4 methods */
 
     public function testStructures_ErrorReportingWithInteger01()  { $this->generic_test('Structures_ErrorReportingWithInteger.01'); }
     public function testStructures_ErrorReportingWithInteger02()  { $this->generic_test('Structures_ErrorReportingWithInteger.02'); }
     public function testStructures_ErrorReportingWithInteger03()  { $this->generic_test('Structures_ErrorReportingWithInteger.03'); }
+    public function testStructures_ErrorReportingWithInteger04()  { $this->generic_test('Structures_ErrorReportingWithInteger.04'); }
 }
 ?>
---
+++ tests/analyzer/exp/Structures_ErrorReportingWithInteger.04.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('error_reporting(-10)');
+
+$expected_not = array('error_reporting(-1)',
+                      'error_reporting(E_ALL - 1)');
+
+?>
