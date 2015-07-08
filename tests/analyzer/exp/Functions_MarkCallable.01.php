6fbe50d84aeb092bcaeb5aa741aa7221bd400901
Damien Seguy <damien.seguy@gmail.com>
2015-07-08 19:00:13 +0200

    Fille UT

--- tests/analyzer/exp/Functions_MarkCallable.01.php
+++ tests/analyzer/exp/Functions_MarkCallable.01.php
@@ -1,6 +1,8 @@
 <?php
 
-$expected     = array();
+$expected     = array("'callableString'", 
+                     "'MyClass::myCallbackMethod'", 
+                     "'string', 'string'");
 
 $expected_not = array();
 
--- tests/analyzer/source/Functions_MarkCallable.01.php
+++ tests/analyzer/source/Functions_MarkCallable.01.php
@@ -1,3 +1,15 @@
 <?php
 
+array_map('callableString', $array);
+call_user_func($array, 'nonCallableString');
+//array_map($array, $nonCallableVar);
+
+array_filter($array, array('string', 'string'));
+array_reduce(array('string', 'string'), $array);
+
+preg_replace_callback('a', 'b', 'MyClass::myCallbackMethod');
+sqlite_create_function('MyClass::myNonCallbackMethod', $b, $c);
+sqlite_create_aggregate($a, 'MyClass2::myNonCallbackMethod', $c);
+
+
 ?>
