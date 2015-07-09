a4fb19fffa6327815232caf4b7c8015aaa8ae0f8
Damien Seguy <damien.seguy@gmail.com>
2015-07-09 10:33:52 +0200

    Upgraded UT exp

--- tests/analyzer/exp/Security_CompareHash.01.php
+++ tests/analyzer/exp/Security_CompareHash.01.php
@@ -2,8 +2,8 @@
 
 $expected     = array("'0' != hash('md5', '240610708', false)", 
                       "hash('md5', '240610708', false) == '0'", 
-                      "elseif (hash('crc32', '2332', false)) { /**/ } ", 
-                      "if (hash('alder32', '00e00099', false)) { /**/ }  else elseif (hash('crc32', '2332', false)) { /**/ } ");
+                      'elseif (hash(\'crc32\', \'2332\', false)) {   }', 
+                      'if (hash(\'alder32\', \'00e00099\', false)) {   } elseif (hash(\'crc32\', \'2332\', false)) {   }');
 
 $expected_not = array();
 
