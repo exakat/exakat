0b2d5a38c879a7c4075ab473441e6a81e4bab688
Damien Seguy <damien.seguy@gmail.com>
2015-07-09 11:02:51 +0200

    Also included -1 in the rule

--- tests/analyzer/exp/Structures_PlusEgalOne.02.php
+++ tests/analyzer/exp/Structures_PlusEgalOne.02.php
@@ -1,7 +1,11 @@
 <?php
 
-$expected     = array();
+$expected     = array('$c -= 1', 
+                      '$a = $a - 1',
+                      '$a4 = -1 + $a4');
 
-$expected_not = array();
+$expected_not = array('$e = $f * 1',
+                      '$a2 = $a2 - 2',
+                      '$a3 = $b3 - 1');
 
 ?>
