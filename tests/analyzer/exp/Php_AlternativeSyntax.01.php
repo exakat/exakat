7904a464e5a344a00cbec9c3d14bf7fc3dba65a0
Damien Seguy <damien.seguy@gmail.com>
2015-07-09 13:41:17 +0200

    Fixed UT exp

--- tests/analyzer/exp/Php_AlternativeSyntax.01.php
+++ tests/analyzer/exp/Php_AlternativeSyntax.01.php
@@ -1,8 +1,8 @@
 <?php
 
 $expected     = array('if ($alternative) : ; endif', 
-                      'for($i = 0 ; $i < 10 ; $i++) : ; endfor', 
-                      'foreach($a as $b) : ; endforeach', 
+                      'for($i = 0 ; $i < 10 ; $i++) : $y++ endfor', 
+                      'foreach($a as $b) : $y++ endforeach',
                       'while ($alternative) : ; endwhile',
                       'switch ($alternative) :  /**/  endswitch');
 
