37faa4b4ce63f5eaefc1c8306c8914c3d2bf8a5a
Damien Seguy <damien.seguy@gmail.com>
2015-07-21 10:41:04 -0400

    Check on edge case for Ifthen

---
+++ tests/tokenizer/source/Ifthen2.01.php
@@ -0,0 +1,21 @@
+<?php
+
+$a=<<<B
+
+B;
+$a.='C';
+
+
+
+if((isset($b['D'])))
+{
+	if(E($b['D']))
+	$c='G'.H($b['D']).'J';
+	else $c='K'.H($b['D']).'N'.H($b['P']).'Q'.H($b['S']).'T';
+	$d="U$this->VW".H($b[Y])."Z$cAA";
+	$e=AB($d);
+	if(AC())AD(AC());
+
+
+}
+
