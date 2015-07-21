0d7154c25f4da0c7ee8dd0acb3a050b85e7784b1
Damien Seguy <damien.seguy@gmail.com>
2015-07-21 10:37:05 -0400

    Checked precedence for Multiplications

--- tests/tokenizer/Test/Sign.php
+++ tests/tokenizer/Test/Sign.php
@@ -7,7 +7,7 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Sign extends Tokenizer {
-    /* 53 methods */
+    /* 54 methods */
     public function testSign01()  { $this->generic_test('Sign.01'); }
     public function testSign02()  { $this->generic_test('Sign.02'); }
     public function testSign03()  { $this->generic_test('Sign.03'); }
@@ -61,5 +61,6 @@ class Sign extends Tokenizer {
     public function testSign51()  { $this->generic_test('Sign.51'); }
     public function testSign52()  { $this->generic_test('Sign.52'); }
     public function testSign53()  { $this->generic_test('Sign.53'); }
+    public function testSign54()  { $this->generic_test('Sign.54'); }
 }
 ?>
