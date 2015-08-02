37df9072acbbe9effca801595544d04677dfdea5
Damien Seguy <damien.seguy@gmail.com>
2015-08-02 09:44:38 +0200

    Fixed noToken to notToken

--- library/Tokenizer/Nsname.php
+++ library/Tokenizer/Nsname.php
@@ -30,7 +30,7 @@ class Nsname extends TokenAuto {
     public function _check() {
         // @note \a\b\c (\ initial)
         $this->conditions = array( -2 => array('filterOut' => self::$operators),
-                                   -1 => array('noToken'   => 'T_NS_SEPARATOR'),
+                                   -1 => array('notToken'  => 'T_NS_SEPARATOR'),
                                     0 => array('token'     => self::$operators,
                                                'atom'      => 'none'),
                                     1 => array('atom'      => array('Identifier', 'Boolean', 'Null')),
