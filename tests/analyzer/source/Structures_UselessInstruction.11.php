b56dd34cc9f54ac3b9f3c926530e227c9f7c8683
Damien Seguy <damien.seguy@gmail.com>
2015-08-10 11:12:39 +0200

    Backport variables differentiation

--- tests/analyzer/source/Structures_UselessInstruction.11.php
+++ tests/analyzer/source/Structures_UselessInstruction.11.php
@@ -1,11 +1,11 @@
 <?php
 
 
-new $x instanceof X;
-(new $x) instanceof X;
+new $x1 instanceof X;
+(new $x2) instanceof X;
 
-($a = new $x) instanceof X;
-($a = (new $x)) instanceof X;
+($a = new $x3) instanceof X;
+($a = (new $x4)) instanceof X;
 
 $y = new $x5;
 
