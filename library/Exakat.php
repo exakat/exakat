87872baf2da3b59901bb165c8886cf57031f01cb
Damien Seguy <damien.seguy@gmail.com>
2015-08-17 17:50:12 +0200

    Bump version

--- library/Exakat.php
+++ library/Exakat.php
@@ -22,7 +22,7 @@
 
 class Exakat {
     const VERSION = '0.2.5';
-    const BUILD = '184';
+    const BUILD = '186';
     
     public function execute(Config $config) {
         switch ($config->command) {
