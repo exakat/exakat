9030a7c73858602dc89f3ccb325a7ab6fc478017
Damien Seguy <damien.seguy@gmail.com>
2015-09-11 11:02:31 +0200

    Added extra test to validate = and (and / && ) priorities

--- tests/tokenizer/Test/Logical.php
+++ tests/tokenizer/Test/Logical.php
@@ -7,7 +7,7 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Logical extends Tokenizer {
-    /* 43 methods */
+    /* 44 methods */
 
     public function testLogical01()  { $this->generic_test('Logical.01'); }
     public function testLogical02()  { $this->generic_test('Logical.02'); }
@@ -52,5 +52,6 @@ class Logical extends Tokenizer {
     public function testLogical41()  { $this->generic_test('Logical.41'); }
     public function testLogical42()  { $this->generic_test('Logical.42'); }
     public function testLogical43()  { $this->generic_test('Logical.43'); }
+    public function testLogical44()  { $this->generic_test('Logical.44'); }
 }
 ?>
