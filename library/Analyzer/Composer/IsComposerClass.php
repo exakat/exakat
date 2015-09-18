a04fd185ef67308d7d8af9bb975ea94d75bd204c
Damien Seguy <damien.seguy@gmail.com>
2015-09-18 14:36:21 +0200

    Mark a class as Composer's

---
+++ library/Analyzer/Composer/IsComposerClass.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Analyzer\Composer;
+
+use Analyzer;
+
+class IsComposerClass extends Analyzer\Analyzer {
+    public function analyze() {
+        $data = new \Data\Composer();
+
+        $classes = $data->getComposerClasses();
+        $classesFullNP = $this->makeFullNsPath($classes);
+        
+        $this->atomIs('Class')
+             ->outIs('IMPLEMENTS', 'EXTENDS')
+             ->fullnspath($classesFullNP);
+        $this->prepareQuery();
+
+        $this->atomIs('Instanceof')
+             ->outIs('CLASS')
+             ->fullnspath($classesFullNP);
+        $this->prepareQuery();
+
+        $this->atomIs('Typehint')
+             ->outIs('CLASS')
+             ->fullnspath($classesFullNP);
+        $this->prepareQuery();
+    }
+}
+
+?>
