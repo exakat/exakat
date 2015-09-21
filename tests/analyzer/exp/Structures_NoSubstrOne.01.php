8022459f71bcd195a6fa5ed995d87309d0191090
Damien Seguy <damien.seguy@gmail.com>
2015-09-21 19:58:34 +0200

    Added NoSubstr One analyzer

---
+++ tests/analyzer/exp/Structures_NoSubstrOne.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('substr($b, 0, -1)', 
+                      'substr($b, 0, 1)', 
+                      'substr($b, 0, 1)');
+
+$expected_not = array('mb_substr($mb_, 0, 1)');
+
+?>
---
+++ tests/analyzer/source/Structures_NoSubstrOne.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = substr($b, 0, 1);
+$a = substr($b, 0, -1);
+$a = substr($b, 0, -2);
+$a = substr($b, 0, +1);
+
+$a = mb_substr($mb_, 0, 1);
+
+$a = $mb->substr($mbm, 0, 1);
+
+?>
