257b4b750b92fb5baa421c91986c335f87c4573b
Damien Seguy <damien.seguy@gmail.com>
2015-09-23 20:37:52 +0200

    Fixed UT exp

--- tests/analyzer/exp/Constants_InvalidName.04.php
+++ tests/analyzer/exp/Constants_InvalidName.04.php
@@ -1,9 +1,9 @@
 <?php
 
-$expected     = array('\a\constant\in\unset\space',
-                      '\a\co$nstant\in\unset\space',
-                      'cons$tant',
-                      '\a\co$nstant\in\unset\space',);
+$expected     = array('\'\a\constant\in\unset\space\'',
+                      '\'\a\co$nstant\in\unset\space\'',
+                      '\'cons$tant\'',
+                      '\'\a\co$nstant\in\unset\space\'',);
 
 $expected_not = array('a\constant\in\another\space',
                       '\a\constant\in\another\space');
