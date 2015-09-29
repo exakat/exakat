59f10f78abe4c8ed8f8d61715f40409d50efa712
Damien Seguy <damien.seguy@gmail.com>
2015-09-29 15:45:09 +0200

    Spot new PHP 70 functions

---
+++ tests/analyzer/source/Php_Php70NewFunctions.01.php
@@ -0,0 +1,18 @@
+<?php
+
+function get_resources() {}
+function get_resource() {}
+
+class x {
+    function gc_mem_caches() {}
+}
+
+interface i {
+    function random_bytes();
+}
+
+trait t {
+    function random_int() {}
+}
+
+?>
