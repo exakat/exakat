3c80a4fdecf86fff75034cddc1c17efd92edf2f6
Damien Seguy <damien.seguy@gmail.com>
2015-09-30 15:58:10 +0200

    Spot ambiguous syntax in PHP 5

---
+++ library/Analyzer/Variables/Php5IndirectExpression.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Analyzer\Variables;
+
+use Analyzer;
+
+class Php5IndirectExpression extends Analyzer\Analyzer {
+    protected $phpVersion = '7.0-';
+    
+    public function analyze() {
+//$$foo['bar']['baz']	${$foo['bar']['baz']}	($$foo)['bar']['baz']
+        $this->atomIs('Variable')
+             ->tokenIs('T_DOLLAR')
+             ->outIs('NAME')
+             ->atomIs('Array')
+             ->back('first');
+        $this->prepareQuery();
+
+//$foo->$bar['baz']	$foo->{$bar['baz']}	($foo->$bar)['baz']
+        $this->atomIs('Property')
+             ->outIs('PROPERTY')
+             ->atomIs('Array')
+             ->back('first');
+        $this->prepareQuery();
+
+
+//$foo->$bar['baz']()	$foo->{$bar['baz']}()	($foo->$bar)['baz']()
+        $this->atomIs('Methodcall')
+             ->outIs('METHOD')
+             ->outIs('NAME')
+             ->atomIs('Array')
+             ->back('first');
+        $this->prepareQuery();
+
+//Foo::$bar['baz']()
+        $this->atomIs('Staticmethodcall')
+             ->outIs('METHOD')
+             ->outIs('NAME')
+             ->atomIs('Array')
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
