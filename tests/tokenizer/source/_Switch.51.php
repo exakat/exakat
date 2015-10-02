831802ca60bec0ca17fb36faa4522971fb6f6d67
Damien Seguy <damien.seguy@gmail.com>
2015-10-02 20:30:47 +0200

    Added extra result

---
+++ tests/tokenizer/source/_Switch.51.php
@@ -0,0 +1,35 @@
+<?php
+
+					if ( B($b) )
+					{
+						switch(C($b))
+						{
+							case 1:
+								$b = NULL;
+								break;
+							case 2:
+								$b = D($b);
+								break;
+							default:
+								$c = 'E';
+								break;
+						}
+					}
+					elseif ( $b instanceOf F )
+					{
+						foreach($d as $e) {
+						    $e++;
+						}
+					}
+					elseif ( $b instanceOf F )
+					{
+						for($d =1; $d < 3; $d++) {
+						    $e++;
+						}
+					}
+					elseif ( $b instanceOf F )
+					{
+						while($d) {
+						    $e++;
+						};
+					}
