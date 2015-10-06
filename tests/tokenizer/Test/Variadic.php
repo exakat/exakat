c60e3b91bcac8661d600938a23aa2bd743f7d62e
Damien Seguy <damien.seguy@gmail.com>
2015-10-06 09:35:47 +0200

    Added two more tests for Variadic

--- tests/tokenizer/Test/Variadic.php
+++ tests/tokenizer/Test/Variadic.php
@@ -7,8 +7,10 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Variadic extends Tokenizer {
-    /* 1 methods */
+    /* 3 methods */
 
     public function testVariadic01()  { $this->generic_test('Variadic.01'); }
+    public function testVariadic02()  { $this->generic_test('Variadic.02'); }
+    public function testVariadic03()  { $this->generic_test('Variadic.03'); }
 }
 ?>
