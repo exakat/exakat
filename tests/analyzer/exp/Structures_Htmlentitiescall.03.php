213c187520e22498b0377c27636de6de6f2b702d
Damien Seguy <damien.seguy@gmail.com>
2015-10-16 12:14:45 +0200

    Fixed UT for HTMLentities calls

--- tests/analyzer/exp/Structures_Htmlentitiescall.03.php
+++ tests/analyzer/exp/Structures_Htmlentitiescall.03.php
@@ -1,13 +1,17 @@
 <?php
 
-$expected     = array('htmlentities($miss_12, \ENT_QUOTES, , \'UTF-8\')',
-                      'htmlentities($miss_14, \ENT_QUOTES | \ENT_COMPAT)', 
-                      'htmlentities($miss_15, ENT_QUOTES | \ENT_COMPAT)', 
+$expected     = array('htmlentities($miss_2)',
                       'htmlentities($miss_11, ENT_QUOTES)', 
-                      'htmlentities($miss_13, ENT_QUOTES | ENT_COMPAT)', 
-                      'htmlentities($miss_16, ENT_QUOTES | \ENT_COMPAT)', 
-                      'htmlentities($miss_2)');
+                      'htmlentities($miss_16, ENT_QUOTES | \ENT_COMPAT | \E_ALL, \'UTF-8\')', 
+                      'htmlentities($miss_15, ENT_QUOTES | \ENT_COMPAT | E_ALL, \'UTF-8\')'
 
-$expected_not = array();
+);
+
+$expected_not = array('htmlentities($miss_12, \ENT_QUOTES, \'UTF-8\')',
+                      'htmlentities($miss_14, \ENT_QUOTES | \ENT_COMPAT, \'UTF-8\')', 
+                      'htmlentities($miss_15, ENT_QUOTES | \ENT_COMPAT, \'UTF-8\')', 
+                      'htmlentities($miss_13, ENT_QUOTES | ENT_COMPAT, \'UTF-8\')', 
+                      'htmlentities($miss_16, ENT_QUOTES | \ENT_COMPAT, \'UTF-8\')', 
+                      );
 
 ?>
--- tests/analyzer/source/Structures_Htmlentitiescall.03.php
+++ tests/analyzer/source/Structures_Htmlentitiescall.03.php
@@ -1,7 +1,11 @@
 <?php
 
+// Using default (should be caught)
 htmlentities($miss_2);
+htmlentities($miss_11, ENT_QUOTES);
 
+
+// All OK
 htmlentities($miss_11, ENT_QUOTES, 'UTF-8');
 htmlentities($miss_12, \ENT_QUOTES, 'UTF-8');
 
@@ -10,5 +14,9 @@ htmlentities($miss_14, \ENT_QUOTES | \ENT_COMPAT, 'UTF-8');
 
 htmlentities($miss_15, ENT_QUOTES | \ENT_COMPAT, 'UTF-8');
 htmlentities($miss_16, ENT_QUOTES | \ENT_COMPAT, 'UTF-8');
+
+// Wrong Constants
+htmlentities($miss_15, ENT_QUOTES | \ENT_COMPAT | E_ALL, 'UTF-8');
+htmlentities($miss_16, ENT_QUOTES | \ENT_COMPAT | \E_ALL, 'UTF-8');
 
 ?>
