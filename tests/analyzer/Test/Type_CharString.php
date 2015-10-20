c420429ce41144a6bcc2c927ad315fef86355b24
Damien Seguy <damien.seguy@gmail.com>
2015-10-20 14:23:50 +0200

    Finalize UT upgrade

--- tests/analyzer/Test/Type_CharString.php
+++ tests/analyzer/Test/Type_CharString.php
@@ -7,9 +7,9 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
-class Type_String extends Analyzer {
+class Type_CharString extends Analyzer {
     /* 1 methods */
 
-    public function testType_String01()  { $this->generic_test('Type_String.01'); }
+    public function testType_String01()  { $this->generic_test('Type_CharString.01'); }
 }
 ?>
