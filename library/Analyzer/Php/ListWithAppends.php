b0059cf8f786c389c1c5089bcee276394937090d
Damien Seguy <damien.seguy@gmail.com>
2015-10-21 14:01:01 +0200

    Cleaned code

--- library/Analyzer/Php/ListWithAppends.php
+++ library/Analyzer/Php/ListWithAppends.php
@@ -28,12 +28,15 @@ use Analyzer;
 class ListWithAppends extends Analyzer\Analyzer {
     public function analyze() {
         $this->atomIs('Functioncall')
+             ->hasNoIn('METHOD')
              ->tokenIs('T_LIST')
-//             ->outIs('ARGUMENTS')
+
              // more than one Arrayappend, for initial filtering
              ->filter('it.out("ARGUMENTS").out("ARGUMENT").has("atom", "Arrayappend").count() > 1')
-             // more than one Arrayappend, for initial filtering
+
+             // several appends to the same array
              ->filter('it.out("ARGUMENTS").out("ARGUMENT").has("atom", "Arrayappend").groupCount{it.out("VARIABLE").next().code}{it.b + 1}.cap.next().findAll{it.value > 1}.any()')
+
              ->back('first');
         $this->prepareQuery();
     }
