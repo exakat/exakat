6609bd2ccb2a119224bb2ac102ed76b1aebb3baa
Damien Seguy <damien.seguy@gmail.com>
2015-10-23 09:47:10 +0200

    UT exp values

---
+++ tests/analyzer/exp/Structures_GlobalInGlobal.01.php
@@ -0,0 +1,16 @@
+<?php
+
+$expected     = array('$global',
+                      '$global2',
+                      '$global');
+
+$expected_not = array('$functionVar',
+                      '$closureVar',
+                      '$classVar',
+                      '$methodVar',
+                      '$interfaceVar',
+                      '$traitVar',
+                      '$traitMethodVar',
+                      );
+
+?>
