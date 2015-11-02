b9b45096da49d4e0da72996cedfd23164dd5f8af
Damien Seguy <damien.seguy@gmail.com>
2015-11-02 10:37:46 +0100

    Added test for inotify

---
+++ tests/analyzer/exp/Extensions_Extinotify.01.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('inotify_read($fd)', 
+                      'inotify_init( )', 
+                      'inotify_queue_len($fd)', 
+                      'inotify_rm_watch($fd, $watch_descriptor)', 
+                      'inotify_add_watch($fd, __FILE__, IN_ATTRIB)', 
+                      'inotify_read($fd)'
+);
+
+$expected_not = array();
+
+?>
