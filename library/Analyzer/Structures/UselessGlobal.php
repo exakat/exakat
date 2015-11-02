3853ee0798ed47ca947798645015892a4810d282
Damien Seguy <damien.seguy@gmail.com>
2015-11-02 21:56:09 +0100

    Spedup Analyzer

--- library/Analyzer/Structures/UselessGlobal.php
+++ library/Analyzer/Structures/UselessGlobal.php
@@ -27,28 +27,37 @@ use Analyzer;
 
 class UselessGlobal extends Analyzer\Analyzer {
     public function dependsOn() {
-        return array('Analyzer\\Variables\\VariableUsedOnceByContext',
-                     'Analyzer\\Structures\\UnusedGlobal');
+        return array('Variables/VariableUsedOnceByContext',
+                     'Structures/UnusedGlobal');
     }
     
     public function analyze() {
         // Global are unused if used only once
+        $inGlobals = $this->query(<<<GREMLIN
+g.idx("atoms")[["atom":"Variable"]].has("code", "\\\$GLOBALS").in("VARIABLE").has("atom", "Array").out("INDEX").has("atom", "String").transform{ '\$' + it.noDelimiter}.unique()
+GREMLIN
+);
+        print_r($inGlobals);
         $this->atomIs('Global')
              ->outIs('GLOBAL')
-             ->analyzerIsNot('Analyzer\\Structures\\UnusedGlobal')
-             ->savePropertyAs('code', 'variable')
+             ->analyzerIsNot('Structures/UnusedGlobal')
             // search in $GLOBALS
-             ->raw('filter{ g.idx("atoms")[["atom":"Variable"]].has("code", "\$GLOBALS").in("VARIABLE").has("atom", "Array").out("INDEX").has("atom", "String").has("noDelimiter", variable.substring(1, variable.size())).any() == false }')
+             ->codeIsNot($inGlobals)
              ->eachCounted('it.fullcode', 1);
         $this->prepareQuery();
 
+        $globals = $this->query(<<<GREMLIN
+g.idx("atoms")[["atom":"Global"]].out("GLOBAL").has("atom", "Variable").has("token", "T_VARIABLE").transform{ it.code.substring(1, it.code.size())}.unique()
+GREMLIN
+);
+        print_r($globals);
         $this->atomIs('Array')
              ->outIs('VARIABLE')
              ->code('$GLOBALS')
              ->inIs('VARIABLE')
              ->outIs('INDEX')
              ->atomIs('String')
-             ->raw('filter{ index = it.noDelimiter; g.idx("atoms")[["atom":"Global"]].out("GLOBAL").has("atom", "Variable").filter{ it.code ==  "\\$" + index}.any() == false}')
+             ->noDelimiterIsNot($globals)
              ->inIs('INDEX')
              ->eachCounted('it.fullcode', 1);
         $this->prepareQuery();
