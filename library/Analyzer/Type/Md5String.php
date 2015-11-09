500aa741216d5c1a31abe03d8a3e41b11751dd9a
Damien Seguy <damien.seguy@gmail.com>
2015-11-09 21:13:38 +0100

    Fixed analyzer (0 has to be the first in the string)

--- library/Analyzer/Type/Md5String.php
+++ library/Analyzer/Type/Md5String.php
@@ -26,13 +26,9 @@ namespace Analyzer\Type;
 use Analyzer;
 
 class Md5String extends Analyzer\Analyzer {
-    public function dependsOn() {
-        return array('String');
-    }
-    
     public function analyze() {
         $this->atomIs('String')
-             ->regex('code', '^[\'\\"]?[0-9A-Fa-f]{32}[\'\\"]?\\$');
+             ->regex('code', '^[\'\\"]?0[0-9A-Fa-f]{31}[\'\\"]?\\$');
         $this->prepareQuery();
     }
 }
--- tests/analyzer/exp/Type/Md5String.01.php
+++ tests/analyzer/exp/Type/Md5String.01.php
@@ -7,6 +7,6 @@ $expected     = array("'0cc175b9c0f1b6a831c399e269771111'",
                       '0cc175b9c0f1b6a831c399e269774444
 ');
 
-$expected_not = array();
+$expected_not = array("'1cc175b9c0f1b6a831c399e269773333'");
 
 ?>
