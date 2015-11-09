31217f8637cd38d4bd109198db009c97b2b8df8b
Damien Seguy <damien.seguy@gmail.com>
2015-11-09 10:01:19 +0100

    Fixed expected values

--- tests/analyzer/exp/Php/ScalarTypehintUsage.04.php
+++ tests/analyzer/exp/Php/ScalarTypehintUsage.04.php
@@ -1,9 +1,9 @@
 <?php
 
-$expected     = array('float $float = 2.0', 
-                      'int $int = 1', 
-                      'bool $bool = false', 
-                      'string $string = "3"'
+$expected     = array('float &$float = 2.0', 
+                      'int &$int = 1', 
+                      'bool &$bool = false', 
+                      'string &$string = "3"'
 );
 
 $expected_not = array('stdclass &$stdclass = null');
