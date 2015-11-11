34ba7c8067900412196fab8ec18670741817bcb0
Damien Seguy <damien.seguy@gmail.com>
2015-11-11 11:53:01 +0100

    Added Analyze for Security (Register globals)

---
+++ tests/analyzer/exp/Security/RegisterGlobals.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('import_request_variables( )', 
+                      'extract($_REQUEST, EXTR_SKIP | EXTR_REFS)', 
+                      'extract($_POST, EXTR_IF_EXISTS | EXTR_REFS)', 
+                      'extract($_GET, EXTR_IF_EXISTS)',
+                      'foreach($_REQUEST as $k5 => $v5) { /**/ } ');
+
+$expected_not = array();
+
+?>
---
+++ tests/analyzer/source/Security/RegisterGlobals.01.php
@@ -0,0 +1,41 @@
+<?php
+
+// Some loop
+foreach($source1 as $k1 => $v1) {
+    $$k1 = $v1;
+}
+
+// Some loop on GPC without Keys
+foreach($_GET as $v2) {
+    $$a2 = $v2;
+}
+
+// Some loop on GPC with Keys without $$k
+foreach($_GET as $k3 => $v3) {
+    $$v3 = 1;
+}
+
+// Some loop on GPC with $$k not set
+foreach($_REQUEST as $k4 => $v4) {
+    $x4 = $$k4;
+}
+
+// Some loop on GPC with $$k 
+foreach($_REQUEST as $k5 => $v5) {
+    $$k5 = $v5;
+}
+
+// With extract
+extract($_FILES);
+extract($_GET, EXTR_IF_EXISTS);
+extract($_POST, EXTR_IF_EXISTS | EXTR_REFS);
+
+extract($_REQUEST, EXTR_SKIP); // This is OK
+extract($_REQUEST, EXTR_SKIP | EXTR_REFS); // This is OK
+extract($args, EXTR_SKIP); // This is OK
+
+
+// Old style
+import_request_variables();
+
+?>
