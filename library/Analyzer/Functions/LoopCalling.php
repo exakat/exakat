ffdd17acab4e1e9cda741f6e15c68da43f628142
Damien Seguy <damien.seguy@gmail.com>
2015-11-12 10:59:42 +0100

    Added UT; Added loop 4.

--- library/Analyzer/Functions/LoopCalling.php
+++ library/Analyzer/Functions/LoopCalling.php
@@ -69,8 +69,41 @@ class LoopCalling extends Analyzer\Analyzer {
              ->hasNoIn('METHOD')
              ->samePropertyAs('code', 'name')
              ->back('first')
+             ->outIs('NAME');
+        $this->prepareQuery();
+
+        // loop of 4
+        $this->atomIs('Function')
              ->outIs('NAME')
-             ;
+             ->savePropertyAs('code', 'name')
+             ->back('first')
+             ->outIs('BLOCK')
+             ->atomInside('Functioncall')
+             ->hasNoIn('METHOD')
+             ->notSamePropertyAs('code', 'name')
+
+             ->functionDefinition()
+             ->inIs('NAME')
+             ->outIs('BLOCK')
+             ->atomInside('Functioncall')
+             ->hasNoIn('METHOD')
+             ->notSamePropertyAs('code', 'name')
+
+             ->functionDefinition()
+             ->inIs('NAME')
+             ->outIs('BLOCK')
+             ->atomInside('Functioncall')
+             ->hasNoIn('METHOD')
+             ->notSamePropertyAs('code', 'name')
+
+             ->functionDefinition()
+             ->inIs('NAME')
+             ->outIs('BLOCK')
+             ->atomInside('Functioncall')
+             ->hasNoIn('METHOD')
+             ->samePropertyAs('code', 'name')
+             ->back('first')
+             ->outIs('NAME');
         $this->prepareQuery();
     }
 }
