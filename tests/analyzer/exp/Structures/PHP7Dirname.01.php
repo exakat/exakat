5d6358be137ff2c9c33ef14c958051f954d90729
Damien Seguy <damien.seguy@gmail.com>
2015-11-15 21:33:13 +0100

    Added a check on dirname

---
+++ tests/analyzer/exp/Structures/PHP7Dirname.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('dirname(dirname(dirname(dirname(dirname(dirname(dirname(dirname($x))))))))',
+                      'dirname(dirname(dirname(dirname($x))))',
+                      'dirname(dirname($x))',
+);
+
+$expected_not = array('dirname($x, 2)',
+                      'dirname(dirname($y), 2)',
+                      'dirname(dirname(dirname($x)))');
+
+?>
---
+++ tests/analyzer/source/Structures/PHP7Dirname.01.php
@@ -0,0 +1,10 @@
+<?php
+
+dirname($x);
+dirname(dirname($x));
+dirname(dirname(dirname(dirname($x))));
+dirname(dirname(dirname(dirname(dirname(dirname(dirname(dirname($x))))))));
+
+dirname($x, 2);
+dirname(dirname($y), 2);
+
