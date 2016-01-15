d48bab8b2452eab3074fe9a5d4b70374100cc417
Damien Seguy <damien.seguy@gmail.com>
2016-01-15 18:45:21 +0100

    Added docs to the report

--- library/Reports/Faceted.php
+++ library/Reports/Faceted.php
@@ -46,6 +46,9 @@ class Faceted extends FacetedJson {
         $js = str_replace('DUMP_JSON', $json, $js);
         file_put_contents($dirName.'/'.$fileName.'/app.js', $js);        
 
+        $css = file_get_contents($config->dir_root.'/media/faceted/faceted.css');
+        file_put_contents($dirName.'/'.$fileName.'/faceted.css', $css);        
+
         $html = file_get_contents($config->dir_root.'/media/faceted/index.html');
 
         $html = str_replace('PROJECT_NAME', $this->config->project_name, $html);
@@ -55,6 +58,32 @@ class Faceted extends FacetedJson {
 //        $html = str_replace('PROJECT_FAVICON', $faviconHtml, $html);
 
         file_put_contents($dirName.'/'.$fileName.'/index.html', $html);        
+
+        $sqlite      = new \sqlite3($dirName.'/dump.sqlite');
+
+        $errors = json_decode($json);
+        $docsList = array();
+        foreach($errors as $error) {
+            $docsList[$error->analyzer] = $error->error;
+        }
+        asort($docsList);
+
+        $docsHtml = '<dl>';
+        foreach($docsList as $id => $dl) {
+            $ini = parse_ini_file($config->dir_root.'/human/en/'.$id.'.ini');
+            $description = htmlentities($ini['description'], ENT_COMPAT | ENT_HTML401, 'UTF-8');
+            $description = preg_replace_callback('/\s*(&lt;\?php.*?\?&gt;)\s*/si', function ($r) { return '<br />'.highlight_string(html_entity_decode($r[1]), true);}, $description);
+            $description = nl2br($description);
+
+            $docsHtml .= "<dt id=\"$id\">$dl</dt>
+        <dd>$description</dd>\n";
+        }
+        $docsHtml .= '</dl>';
+
+        $docs = file_get_contents($config->dir_root.'/media/faceted/docs.html');
+        $docs = str_replace('DOCS_LIST', $docsHtml, $docs);
+        $docs = str_replace('PROJECT_NAME', $this->config->project_name, $docs);
+        file_put_contents($dirName.'/'.$fileName.'/docs.html', $docs);        
         
     }
 }
