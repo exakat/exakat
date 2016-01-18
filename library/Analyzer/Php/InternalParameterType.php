32a7b0a927ad48b1ca687ae947be60213329fb56
Damien Seguy <damien.seguy@gmail.com>
2016-01-18 11:45:55 +0100

    Added licence

--- library/Analyzer/Php/InternalParameterType.php
+++ library/Analyzer/Php/InternalParameterType.php
@@ -1,5 +1,24 @@
 <?php
-
+/*
+ * Copyright 2012-2016 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
 namespace Analyzer\Php;
 
 use Analyzer;
@@ -23,10 +42,10 @@ class InternalParameterType extends Analyzer\Analyzer {
                                 'void'     => 'Void');
         foreach($args as $position => $types) {
 //            if ($position != 1) { continue; }
-            
+
             foreach($types as $type => $functions) {
 //                if ($type != 'string') { continue; }
-                
+
                 if (strpos($type, ',') !== false) {
                     continue; // No support for multiple type yet
                 }
@@ -45,7 +64,7 @@ class InternalParameterType extends Analyzer\Analyzer {
                      // only include literals
                      ->isLiteral()
                     // Closure ? Array ? 
-                    
+
                     // Constant (Identifier), logical, concatenation, addition ? 
                     // Those will have to be replaced after more research
 //                     ->atomIsNot(array('Constant', 'Logical', 'Concatenation', 'Addition', 'Power', 'Multiplication'))
