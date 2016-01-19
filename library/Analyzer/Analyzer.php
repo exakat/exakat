87ff32b861ad87cc112095ffb936360ce44eddd8
Damien Seguy <damien.seguy@gmail.com>
2016-01-19 13:38:03 +0100

    Added comments

--- library/Analyzer/Analyzer.php
+++ library/Analyzer/Analyzer.php
@@ -1609,8 +1609,9 @@ GREMLIN
     }
 
     public function isLiteral() {
+        // Closures are literal if not using a variable from the context
         $this->addMethod(<<<GREMLIN
-filter{ it.atom in ["Integer", "Boolean", "Magicconstant", "Float", "String", "Heredoc", "Function"] ||
+filter{ (it.atom in ["Integer", "Boolean", "Magicconstant", "Float", "String", "Heredoc", "Function"]) ||
         (it.atom == 'Functioncall' && it.constante == true && it.token in ['T_ARRAY', 'T_OPEN_BRACKET'])
 }
 
