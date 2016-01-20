2bcd4292d20b30df8050d43113eda5c1b3aaaada
Damien Seguy <damien.seguy@gmail.com>
2016-01-20 23:15:38 +0100

    Fix 'broken pipes' on debian

--- library/Tasks/Files.php
+++ library/Tasks/Files.php
@@ -141,7 +141,7 @@ class Files extends Tasks {
             display('Check compilation for '.$version);
             $stats['notCompilable'.$version] = -1;
             
-            $shell = 'cat '.$tmpFileName.'  | tr "\n" "\0" |  xargs -n1 -P5 -0I {} sh -c "'.$config->{'php'.$version}.' -l {} 2>&1" || true';
+            $shell = 'cat '.$tmpFileName.'  | tr "\n" "\0" | tail -n +1 |  xargs -n1 -P5 -0I {} sh -c "'.$config->{'php'.$version}.' -l {} 2>&1" || true';
             $res = trim(shell_exec($shell));
 
             $resFiles = explode("\n", $res);
