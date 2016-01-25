250d21b949c5814d8093cd7f5f73a350f2a4745c
Damien Seguy <damien.seguy@gmail.com>
2016-01-25 14:24:42 +0100

    Added tests for ext/event

---
+++ tests/analyzer/exp/Extensions/Extevent.01.php
@@ -0,0 +1,18 @@
+<?php
+
+$expected     = array('Event', 
+                      'EventBufferEvent', 
+                      'EventUtil', 
+                      'EventBufferEvent', 
+                      'EventBufferEvent', 
+                      'EventBufferEvent', 
+                      'Event', 
+                      'EventBufferEvent', 
+                      'EventBufferEvent', 
+                      'EventBufferEvent($base, NULL, EventBufferEvent::OPT_CLOSE_ON_FREE | EventBufferEvent::OPT_DEFER_CALLBACKS, "readcb", NULL, "eventcb")', 
+                      'EventDnsBase($base, TRUE)', 
+                      'EventBase( )');
+
+$expected_not = array();
+
+?>
