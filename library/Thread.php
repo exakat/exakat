18210ea9ce2ef294bc4be003b42323a4b20e8abb
Damien Seguy <damien.seguy@gmail.com>
2016-01-25 14:58:56 +0100

    Always provides a file descriptor for stderr (even /dev/null)

--- library/Thread.php
+++ library/Thread.php
@@ -30,7 +30,7 @@ class Thread {
         ++$this->commandId;
         $descriptors = array( 0 => array('pipe', 'r'),  // stdin is a pipe that the child will read from
                               1 => array('pipe', 'w'),  // stdout is a pipe that the child will write to
-                              2 => array('file', '/tmp/error-output.txt', 'a') // stderr is a file to write to
+                              2 => array('file', '/dev/null', 'a') // stderr is a file to write to
                             );
         $this->process[$this->commandId] = proc_open($command.' &', $descriptors, $pipes);
         // only keeping the read pipe
