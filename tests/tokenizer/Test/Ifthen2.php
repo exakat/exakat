5e16de55829ffb0a718b860414ea956b23a3d1f6
Damien Seguy <damien.seguy@gmail.com>
2016-01-26 21:16:30 +0100

    Special case for ifthen with parenthesis.

--- tests/tokenizer/Test/Ifthen2.php
+++ tests/tokenizer/Test/Ifthen2.php
@@ -7,10 +7,11 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Ifthen2 extends Tokenizer {
-    /* 3 methods */
+    /* 4 methods */
 
     public function testIfthen201()  { $this->generic_test('Ifthen2.01'); }
     public function testIfthen202()  { $this->generic_test('Ifthen2.02'); }
     public function testIfthen203()  { $this->generic_test('Ifthen2.03'); }
+    public function testIfthen204()  { $this->generic_test('Ifthen2.04'); }
 }
 ?>
