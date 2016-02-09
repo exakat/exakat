ea73a3be3fa82fcc0610fbbfa996e6824a8381f0
Damien Seguy <damien.seguy@gmail.com>
2016-02-09 11:05:39 +0100

    Fixed parse error

--- tests/analyzer/exp/Classes/DynamicPropertyCall.03.php
+++ tests/analyzer/exp/Classes/DynamicPropertyCall.03.php
@@ -3,6 +3,6 @@
 $expected     = array('Classe::$$propertyname2',
                       '$object->{$propertyname1}');
 
-$expected_not = array('$object->{$propertyname1 . '4'}');
+$expected_not = array('$object->{$propertyname1 . \'4\'}');
 
 ?>
