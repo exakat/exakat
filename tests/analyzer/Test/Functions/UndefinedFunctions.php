25472ac762165e36e1209091b7ea4e09173b0228
Damien Seguy <damien.seguy@gmail.com>
2016-02-16 13:55:15 +0100

    isset may have arbitrary number of arguments

--- tests/analyzer/Test/Functions/UndefinedFunctions.php
+++ tests/analyzer/Test/Functions/UndefinedFunctions.php
@@ -8,9 +8,10 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Functions_UndefinedFunctions extends Analyzer {
-    /* 2 methods */
+    /* 3 methods */
 
     public function testFunctions_UndefinedFunctions01()  { $this->generic_test('Functions_UndefinedFunctions.01'); }
     public function testFunctions_UndefinedFunctions02()  { $this->generic_test('Functions/UndefinedFunctions.02'); }
+    public function testFunctions_UndefinedFunctions03()  { $this->generic_test('Functions/UndefinedFunctions.03'); }
 }
 ?>
---
+++ tests/analyzer/exp/Functions/UndefinedFunctions.03.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('isnotset($b)');
+
+$expected_not = array('empty($a)',
+                      'isset($b)');
+
+?>
