20eaf6641744b3d3c308700f848ed9e0c0ffa833
Damien Seguy <damien.seguy@gmail.com>
2016-02-19 13:37:10 +0100

    Added UT

---
+++ tests/analyzer/exp/Extensions/Extibase.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('ibase_fetch_object($sth)', 
+                      'ibase_free_result($sth)', 
+                      'ibase_connect($host, $username, $password)', 
+                      'ibase_query($dbh, $stmt), ibase_close($dbh)');
+
+$expected_not = array();
+
+?>
