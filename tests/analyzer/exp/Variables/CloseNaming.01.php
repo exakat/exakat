bbfdf6f4774f9364da6a9a1c29f28fab04136f87
Damien Seguy <damien.seguy@gmail.com>
2016-02-29 11:35:05 +0100

    Added UT

--- tests/analyzer/exp/Variables/CloseNaming.01.php
+++ tests/analyzer/exp/Variables/CloseNaming.01.php
@@ -1,7 +1,17 @@
 <?php
 
-$expected     = array();
+$expected     = array('$context',
+                      '$content',
+                      '$cose', 
+                      '$close', 
+                      '$abc', 
+                      '$abe', 
+                      '$abc', 
+                      '$abd');
 
-$expected_not = array();
+$expected_not = array('$index',
+                      '$indexs',
+                      '$array',
+                      '$arrays');
 
 ?>
--- tests/analyzer/source/Variables/CloseNaming.01.php
+++ tests/analyzer/source/Variables/CloseNaming.01.php
@@ -1,3 +1,18 @@
 <?php
 
+function a() {
+    // Close names
+    $cose = $close;
+    $abc = $abd;
+    $abc = $abe; 
+    
+    $context = $content;
+    
+    // Not close names
+    $a = $b; // to short
+    $cd = $ce; // too short
+    
+    $array = $arrays; // build with s
+    $index = $indexs;
+}
 ?>
