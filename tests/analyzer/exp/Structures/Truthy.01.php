a9445199482bc4080e3d37dd156b0e5a8d3b281b
Damien Seguy <damien.seguy@gmail.com>
2016-03-01 15:33:55 +0100

    Mark literals as Truthy

---
+++ tests/analyzer/exp/Structures/Truthy.01.php
@@ -0,0 +1,23 @@
+<?php
+
+$expected     = array('array(12)', 
+                      '[ 12 ]', 
+                      '[ [   ] ]', 
+                      'array(array( ))', 
+                      '<<<\'PHP\'
+dd
+PHP', 
+                      '"false"', 
+                      '"foo"', 
+                      '"true"', 
+                      '-2', 
+                      '2.3e5', 
+                      '1', 
+                      '12', 
+                      '12', 
+                      'TRUE', 
+                      'true');
+
+$expected_not = array();
+
+?>
---
+++ tests/analyzer/source/Structures/Truthy.01.php
@@ -0,0 +1,49 @@
+<?php
+
+$a1  = "";        // bool(false)
+$a8  = "false";   // bool(true)
+$a80 = "true";    // bool(true)
+$a4  = "foo";     // bool(true)
+$a42  = "f$oo";   // bool(true)
+$a41 = <<<PHP
+PHP;
+$a43 = <<<'PHP'
+PHP;
+$a44 = <<<PHP
+sd$f
+PHP;
+$a45 = <<<'PHP'
+dd
+PHP;
+
+$a10 = 0;         // bool(false)
+$a11 = -0;        // bool(false)
+$a2  = 1;         // bool(true)
+$a3  = -2;        // bool(true)
+
+$a5  = 2.3e5;     // bool(true)
+$a6  = 0.0e5;     // bool(false)
+$a61 = -0.0e15;   // bool(false)
+$a62 = +0.0e15;   // bool(false)
+$a63 = 0.01e3;    // bool(false)
+$a64 = 0.01e5;    // bool(true)
+$a65 = +0.01e3;   // bool(true)
+
+$a6  = array(12); // bool(true)
+$a7  = array();   // bool(false)
+$a70 = array(array());   // bool(true), then bool(false)
+$a62 = [12];      // bool(true)
+$a72 = [];        // bool(false)
+$a72 = [[]];      // bool(true), then bool(false)
+
+$b1  = true;
+$b2  = false;
+$b3  = FALSE;
+$b4  = False;
+$b5  = TRUE;
+
+$b6  = NULL;
+
+// resources are always true
+
+?>
