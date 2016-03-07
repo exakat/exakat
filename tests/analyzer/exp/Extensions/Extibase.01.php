ca85d9d907a6e1607ad19efd2996533c453c0839
Damien Seguy <damien.seguy@gmail.com>
2016-03-07 11:20:26 +0100

    Fixed exp UT

--- tests/analyzer/exp/Extensions/Extibase.01.php
+++ tests/analyzer/exp/Extensions/Extibase.01.php
@@ -3,7 +3,8 @@
 $expected     = array('ibase_fetch_object($sth)', 
                       'ibase_free_result($sth)', 
                       'ibase_connect($host, $username, $password)', 
-                      'ibase_query($dbh, $stmt), ibase_close($dbh)');
+                      'ibase_query($dbh, $stmt)', 
+                      'ibase_close($dbh)');
 
 $expected_not = array();
 
