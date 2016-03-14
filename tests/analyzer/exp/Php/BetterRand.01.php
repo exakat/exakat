eeb0ae559720d5ccac1e9cc1672d4303b0c74e5c
Damien Seguy <damien.seguy@gmail.com>
2016-03-14 07:56:06 +0100

    Spot rand/mt_rand usage, suggest upgrading to random_int

---
+++ tests/analyzer/exp/Php/BetterRand.01.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('rand(1, 2)', 
+                      '\mt_rand(3, 5)');
+
+$expected_not = array('openssl_random_pseudo_bytes( )');
+
+?>
---
+++ tests/analyzer/source/Php/BetterRand.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = rand(1,2);
+$b = \mt_rand(3,5);
+
+$d = $x->openssl_random_pseudo_bytes();
+
+
+?>
