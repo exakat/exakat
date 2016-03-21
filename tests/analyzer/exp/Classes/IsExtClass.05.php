9758f4470f8b082ca6b6c29e292e40d70ec5b7bb
Damien Seguy <damien.seguy@gmail.com>
2016-03-21 11:18:57 +0100

    Filled UT

--- tests/analyzer/exp/Classes/IsExtClass.05.php
+++ tests/analyzer/exp/Classes/IsExtClass.05.php
@@ -1,7 +1,11 @@
 <?php
 
-$expected     = array();
+$expected     = array('mysqli', 
+                      'Mongo(1)', 
+                      'mysqli( )');
 
-$expected_not = array();
+$expected_not = array('$a->b',
+                      '$a->b( )',
+                      '$a( )');
 
 ?>
--- tests/analyzer/source/Classes/IsExtClass.05.php
+++ tests/analyzer/source/Classes/IsExtClass.05.php
@@ -1,8 +1,14 @@
 <?php
 
 new $a->b;
-//new $a::b;
 new $a->b();
 new $a();
+
+    $mysqli = new mysqli;
+    $mysql = new mysqli();
+    $pgsql = new Mongo(1);
+
+    $db = new customclass;
+
 
 ?>
