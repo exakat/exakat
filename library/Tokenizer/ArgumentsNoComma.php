d47d1718483388a9dce05bca5633128f3d1a86a3
Damien Seguy <damien.seguy@gmail.com>
2016-03-28 10:48:10 +0200

    Linked Arguments* to Arguments

--- library/Tokenizer/ArgumentsNoComma.php
+++ library/Tokenizer/ArgumentsNoComma.php
@@ -35,8 +35,7 @@ class ArgumentsNoComma extends Arguments {
                                                                          array('T_OPEN_CURLY', 'T_OPEN_PARENTHESIS'))),
                                    0 => array('token'     => ArgumentsNoComma::$operators,
                                               'atom'      => 'none'),
-                                   1 => array('atom'      => 'yes',
-                                              'notAtom'   => 'Arguments'),
+                                   1 => array('atom'      => Arguments::$arguments),
                                    2 => array('token'     => 'T_CLOSE_PARENTHESIS',
                                               'atom'      => 'none')
         );
@@ -54,8 +53,7 @@ class ArgumentsNoComma extends Arguments {
                                                                        'T_CATCH', 'T_DECLARE', 'T_ELSEIF'))),
                                    0 => array('token'     => ArgumentsNoComma::$operators,
                                               'atom'      => 'none'),
-                                   1 => array('atom'      => 'yes',
-                                              'notAtom'   => 'Arguments'),
+                                   1 => array('atom'      => Arguments::$arguments),
                                    2 => array('token'     => 'T_CLOSE_PARENTHESIS',
                                               'atom'      => 'none')
         );
@@ -69,8 +67,7 @@ class ArgumentsNoComma extends Arguments {
         $this->conditions = array(-1 => array('atom'      => 'Parenthesis'),
                                    0 => array('token'     => ArgumentsNoComma::$operators,
                                               'atom'      => 'none'),
-                                   1 => array('atom'      => 'yes',
-                                              'notAtom'   => 'Arguments'),
+                                   1 => array('atom'      => Arguments::$arguments),
                                    2 => array('token'     => 'T_CLOSE_PARENTHESIS',
                                               'atom'      => 'none')
         );
@@ -85,8 +82,7 @@ class ArgumentsNoComma extends Arguments {
                                   -1 => array('token'     => 'T_AND'),
                                    0 => array('token'     => ArgumentsNoComma::$operators,
                                               'atom'      => 'none'),
-                                   1 => array('atom'      => 'yes',
-                                              'notAtom'   => 'Arguments'),
+                                   1 => array('atom'      => Arguments::$arguments),
                                    2 => array('token'     => 'T_CLOSE_PARENTHESIS',
                                               'atom'      => 'none'),
                                    3 => array('filterOut' => 'T_DOUBLE_COLON'),
@@ -98,11 +94,10 @@ class ArgumentsNoComma extends Arguments {
         $this->checkAuto();
 
        // @note f[1](2) : no comma
-        $this->conditions = array(-1 => array('atom'     => 'Array'),
+        $this->conditions = array(-1 => array('atom'      => 'Array'),
                                    0 => array('token'     => ArgumentsNoComma::$operators,
                                               'atom'      => 'none'),
-                                   1 => array('atom'      => 'yes',
-                                              'notAtom'   => 'Arguments'),
+                                   1 => array('atom'      => Arguments::$arguments),
                                    2 => array('token'     => 'T_CLOSE_PARENTHESIS',
                                               'atom'      => 'none'),
                                    3 => array('filterOut' => array('T_DOUBLE_COLON', 'T_OPEN_PARENTHESIS')),
@@ -114,16 +109,15 @@ class ArgumentsNoComma extends Arguments {
         $this->checkAuto();
 
         // @note a->{f}(1) : no comma
-        $this->conditions = array(-4 => array('token' => array('T_OBJECT_OPERATOR', 'T_DOUBLE_COLON')),
-                                  -3 => array('token' => 'T_OPEN_CURLY'),
-                                  -2 => array('atom' => 'yes'),
-                                  -1 => array('token' => 'T_CLOSE_CURLY'),
-                                   0 => array('token' => ArgumentsNoComma::$operators,
-                                              'atom'  => 'none'),
-                                   1 => array('atom'  => 'yes',
-                                              'notAtom' => array('Arguments', 'Void')),
-                                   2 => array('token' => 'T_CLOSE_PARENTHESIS',
-                                              'atom'  => 'none'),
+        $this->conditions = array(-4 => array('token'     => array('T_OBJECT_OPERATOR', 'T_DOUBLE_COLON')),
+                                  -3 => array('token'     => 'T_OPEN_CURLY'),
+                                  -2 => array('atom'      => 'yes'),
+                                  -1 => array('token'     => 'T_CLOSE_CURLY'),
+                                   0 => array('token'     => ArgumentsNoComma::$operators,
+                                              'atom'      => 'none'),
+                                   1 => array('atom'      => Arguments::$arguments),
+                                   2 => array('token'     => 'T_CLOSE_PARENTHESIS',
+                                              'atom'      => 'none'),
                                    3 => array('filterOut' => array('T_DOUBLE_COLON', 'T_OPEN_PARENTHESIS')),
         );
 
