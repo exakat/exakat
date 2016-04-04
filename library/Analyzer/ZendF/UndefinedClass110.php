1fd364a7675572267149f1bf93f68a3d7ffc8005
Damien Seguy <damien.seguy@gmail.com>
2016-04-04 18:45:27 +0200

    Spot undefined classes in ZF 1.10

---
+++ library/Analyzer/ZendF/UndefinedClass110.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Analyzer\ZendF;
+
+use Analyzer;
+
+class UndefinedClass110 extends UndefinedClasses {
+    public function analyze() {
+        $this->release = '1.10';
+        parent::analyze();
+    }
+}
+
+?>
