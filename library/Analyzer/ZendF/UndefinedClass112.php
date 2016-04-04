1780929d45a8e47c7479b7a6faeafe2cc510941a
Damien Seguy <damien.seguy@gmail.com>
2016-04-04 18:47:35 +0200

    Spot undefined classes in Zend Framework 12

---
+++ library/Analyzer/ZendF/UndefinedClass112.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Analyzer\ZendF;
+
+use Analyzer;
+
+class UndefinedClass112 extends UndefinedClasses {
+    public function analyze() {
+        $this->release = '1.12';
+        parent::analyze();
+    }
+}
+
+?>
