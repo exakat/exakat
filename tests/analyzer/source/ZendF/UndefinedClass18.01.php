63d7388e0696482853406249256af4d8f369d986
Damien Seguy <damien.seguy@gmail.com>
2016-04-04 18:48:01 +0200

    Updated UT for ZF 1.8

--- tests/analyzer/source/ZendF/UndefinedClass18.01.php
+++ tests/analyzer/source/ZendF/UndefinedClass18.01.php
@@ -1,9 +1,11 @@
 <?php
-$a = new Zend_View_Helper_BaseUrl(); // Not in 1.8
 
-$a instanceof Zend_Ldap_Collection_Iterator_Interface; // Only in 9
+$a = new Zend_Acl_Exception(); // In all versions
 
-new Zend_Service_Simpy_WatchlistFilterSet(); // Not in 12
+$a = new Zend_Math_BigInteger_Exception(); // Only in 1.8
 
-new Zend_Acl_Role_Interface(); //in all versions
+$a = new Zend_Http_UserAgent_Features_Adapter_WurflApi(); // only in 1.11
+
+$a = new Zend_Cloud_Infrastructure_Adapter_Ec2(); //only in 1.12
+
 ?>
