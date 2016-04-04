f7a5e83291657004df182515b3ad679654fd6b4e
Damien Seguy <damien.seguy@gmail.com>
2016-04-04 18:43:51 +0200

    Upgraded tests for ZF 1.8

--- tests/analyzer/exp/ZendF/UndefinedClass18.01.php
+++ tests/analyzer/exp/ZendF/UndefinedClass18.01.php
@@ -1,9 +1,9 @@
 <?php
 
-$expected     = array('Zend_View_Helper_BaseUrl( )',
-                      'Zend_Ldap_Collection_Iterator_Interface',);
+$expected     = array('Zend_Http_UserAgent_Features_Adapter_WurflApi( )',
+                      'Zend_Cloud_Infrastructure_Adapter_Ec2( )');
 
-$expected_not = array('Zend_Acl_Role_Interface',
-                      'Zend_Service_Simpy_WatchlistFilterSet( )');
+$expected_not = array('Zend_Math_BigInteger_Exception( )',
+                      'Zend_Acl_Exception( )',);
 
 ?>
