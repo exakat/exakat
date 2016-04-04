96f39ac5bcd98871563be8c5c3b03f856c1e7782
Damien Seguy <damien.seguy@gmail.com>
2016-04-04 18:43:39 +0200

    Spot undefined functions in ZF 1.9

---
+++ library/Analyzer/ZendF/UndefinedClass19.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Analyzer\ZendF;
+
+use Analyzer;
+
+class UndefinedClass19 extends UndefinedClasses {
+    public function analyze() {
+        $this->release = '1.9';
+        parent::analyze();
+    }
+}
+
+?>
