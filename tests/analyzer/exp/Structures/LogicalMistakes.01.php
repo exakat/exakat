d459df1f45e5f72247996e939a86d733a89d07cb
Damien Seguy <damien.seguy@gmail.com>
2016-04-11 13:42:48 +0200

    New analyzer : Logical Mistakes

---
+++ tests/analyzer/exp/Structures/LogicalMistakes.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('$a != 1 || $a != 3', 
+                      '$a != 1 OR $a != 4', 
+                      '$a != 1 or $a != 2');
+
+$expected_not = array('$a != 1 OR $b != 5',
+                      '$a != 1 xor $a != 2');
+
+?>
