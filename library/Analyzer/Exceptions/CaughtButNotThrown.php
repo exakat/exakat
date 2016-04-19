ce6111f2e267bea589baa08a25a831d1d6eaeb70
Damien Seguy <damien.seguy@gmail.com>
2016-04-19 19:06:37 +0200

    Automatically ignore PHP exceptions

--- library/Analyzer/Exceptions/CaughtButNotThrown.php
+++ library/Analyzer/Exceptions/CaughtButNotThrown.php
@@ -7,9 +7,12 @@ use Analyzer;
 class CaughtButNotThrown extends Analyzer\Analyzer {
     public function analyze() {
         // There is a catch() but its class is not defined
+        
+        $phpExceptions = $this->loadIni('php_exception.ini', 'classes');
 
         $this->atomIs('Catch')
              ->outIs('CLASS')
+             ->fullnspathIsNot($this->makeFullNsPath($phpExceptions))
              ->savePropertyAs('fullnspath', 'fnp')
              ->filter('g.idx("atoms")[["atom":"Throw"]].out("THROW").out("NEW").hasNot("fullnspath", null)
                          .hasNot("fullnspath", null)
