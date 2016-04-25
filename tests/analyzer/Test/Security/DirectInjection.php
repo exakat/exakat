d0852f76e37a084c2c93a458f0188c57c8346071
Damien Seguy <damien.seguy@gmail.com>
2016-04-25 12:02:24 +0200

    Update UT

--- tests/analyzer/Test/Security/DirectInjection.php
+++ tests/analyzer/Test/Security/DirectInjection.php
@@ -8,11 +8,12 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Security_DirectInjection extends Analyzer {
-    /* 4 methods */
+    /* 5 methods */
 
     public function testSecurity_DirectInjection01()  { $this->generic_test('Security_DirectInjection.01'); }
     public function testSecurity_DirectInjection02()  { $this->generic_test('Security/DirectInjection.02'); }
     public function testSecurity_DirectInjection03()  { $this->generic_test('Security/DirectInjection.03'); }
     public function testSecurity_DirectInjection04()  { $this->generic_test('Security/DirectInjection.04'); }
+    public function testSecurity_DirectInjection05()  { $this->generic_test('Security/DirectInjection.05'); }
 }
 ?>
