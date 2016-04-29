72cf663e1095b7fdea2781054862f441af9f58db
Damien Seguy <damien.seguy@gmail.com>
2016-04-29 16:18:44 +0200

    Spotting overwritten variables (by themselves)

---
+++ tests/analyzer/source/Variables/Overwriting.01.php
@@ -0,0 +1,17 @@
+<?php
+
+$b = array_slice($b, $c, $d, C);
+$e = $f . $g . $h . $e . $i . $j;
+$k = apply_filters('E', $k, $l);
+$b = mySort($b, 'G', H);
+$b = B($b, 'J', K);
+$m = self::M($m, $n);
+$m = "$nN" . O('P', 'Q') . "R$m";
+$m = static::M($m, $n);
+$o = implode('V', $o);
+$p = str_replace('X', 'Y', $p);
+$m = L::M($m, $n);
+
+$o = (array) $o;
+
+?>
