7a0d0cf92d22179f1465cde28fd37712bcbc79dc
Damien Seguy <damien.seguy@gmail.com>
2016-04-29 21:51:57 +0200

    Spot variables that have the value of $_GET, etc.

---
+++ library/Analyzer/Security/GPRAliases.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Copyright 2012-2016 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+namespace Analyzer\Security;
+
+use Analyzer;
+
+class GPRAliases extends Analyzer\Analyzer {
+    public function analyze() {
+        // Web variables
+        $webVariables = array('$_GET', '$_POST', '$_REQUEST', '$_COOKIE');
+
+        // $a = $_POST
+        $this->atomIs('Assignation')
+             ->code('=')
+             ->outIs('RIGHT')
+             ->atomIs('Variable')
+             ->code($webVariables, true)
+             ->back('first')
+             ->outIs('LEFT');
+        $this->prepareQuery();
+
+        // $a = $_POST['a']
+        $this->atomIs('Assignation')
+             ->code('=')
+             ->outIs('RIGHT')
+             ->atomIs('Array')
+             ->outIsIE('VARIABLE')
+             ->atomIs('Variable')
+             ->code($webVariables, true)
+             ->back('first')
+             ->outIs('LEFT');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/source/Security/GPRAliases.01.php
@@ -0,0 +1,8 @@
+<?php
+
+$a = $_POST;
+$b = C::$_POST;
+$c = $_GET['c']['y'];
+$c2 = $_COOKIE['d']['y'];
+$c3 = $_post['d']['y'];
+?>
