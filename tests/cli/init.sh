91ab9ff27fb20500be482c9d5a6648562ae819b3
Damien Seguy <damien.seguy@gmail.com>
2016-05-02 09:20:21 +0200

    Added tests for SVN

--- tests/cli/init.sh
+++ tests/cli/init.sh
@@ -10,6 +10,16 @@ php exakat init -p ut2 -R https://github.com/sculpin/sculpin.git -v
 php exakat status -p ut2 
 php exakat remove -p ut2
 
+#svn project
+php exakat init -p ut3 -R svn://svn.code.sf.net/p/proc-php/code/trunk -svn -v
+php exakat status -p ut3 
+php exakat remove -p ut3
+
+#bazaar project
+#hg project
+#tar.gz project
+#zip project
+
 
 cd -
 
