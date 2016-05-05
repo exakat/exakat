e32644f39630329c8aa94c00b369b90074359b89
Damien Seguy <damien.seguy@gmail.com>
2016-05-05 16:30:53 +0200

    Spot dirname(__FILE__) and suggest __DIR__

---
+++ tests/analyzer/exp/Structures/CouldUseDir.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('\dirname(__FILE__)',
+                      'DIRname(__FILE__)');
+
+$expected_not = array('diRName(__FILE__)',
+                      'basename(__FILE__)');
+
+?>
