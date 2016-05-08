7688ac469c2446e81f1a654c8a42fdeeb6d626d1
Damien Seguy <damien.seguy@gmail.com>
2016-05-08 22:34:51 +0200

    Added str_rot13 as 'crypto'

--- library/Analyzer/Security/AvoidThoseCrypto.php
+++ library/Analyzer/Security/AvoidThoseCrypto.php
@@ -27,7 +27,7 @@ use Analyzer;
 
 class AvoidThoseCrypto extends Analyzer\Analyzer {
     public function analyze() {
-        // in hashing functions
+        // in ext/hash() functions
         $this->atomFunctionIs(Analyzer\Php\HashAlgos::$functions)
              ->outIs('ARGUMENTS')
              ->outIs('ARGUMENT')
@@ -35,8 +35,8 @@ class AvoidThoseCrypto extends Analyzer\Analyzer {
              ->noDelimiter(array('md2', 'md4', 'md5', 'crc32', 'crc32b', 'sha0', 'sha1'));
         $this->prepareQuery();
 
-        // in hashing functions
-        $this->atomFunctionIs(array('crypt', 'md5', 'md5_file', 'sha1_file', 'sha1', 'crc32'));
+        // as core functions
+        $this->atomFunctionIs(array('crypt', 'md5', 'md5_file', 'sha1_file', 'sha1', 'crc32', 'str_rot13'));
         $this->prepareQuery();
     }
 }
