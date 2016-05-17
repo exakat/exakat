b1b117de9431cff10883992a0d28710a6d14ccfb
Damien Seguy <damien.seguy@gmail.com>
2016-05-17 09:50:02 +0200

    Also allow instanceof in + * **

--- library/Tokenizer/Multiplication.php
+++ library/Tokenizer/Multiplication.php
@@ -31,7 +31,8 @@ class Multiplication extends TokenAuto {
                                     'Methodcall', 'Staticmethodcall', 'Concatenation', 'Cast',
                                     'Noscream', 'Staticconstant', 'Staticproperty', 'Constant',
                                     'Boolean', 'Magicconstant', 'Assignation', 'Include', 'Power',
-                                    'Staticclass', 'Null', 'Shell', 'Function', 'Bitshift', 'New', 'Yield' );
+                                    'Staticclass', 'Null', 'Shell', 'Function', 'Bitshift', 'New', 'Yield',
+                                    'Instanceof' );
     static public $atom = 'Multiplication';
     
     public function _check() {
