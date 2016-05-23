e5273686b85c56ba0ab83b365fa5d6110cef91c0
Damien Seguy <damien.seguy@gmail.com>
2016-05-23 13:18:37 -0500

    Fixed quote enclosing

--- library/Analyzer/Docs.php
+++ library/Analyzer/Docs.php
@@ -49,11 +49,11 @@ class Docs {
             $theme = array_map(function ($x) { return trim($x, '"'); }, $theme);
             $where = 'c.name in ("'.join('", "', $theme).'")';
         } else {
-            $where = 'c.name = "'.$theme.'"';
+            $where = 'c.name = "'.trim($theme, '"').'"';
         }
 
         $query = <<<SQL
-        SELECT a.folder, a.name FROM analyzers AS a
+        SELECT DISTINCT a.folder, a.name FROM analyzers AS a
     JOIN analyzers_categories AS ac
         ON ac.id_analyzer = a.id
     JOIN categories AS c
