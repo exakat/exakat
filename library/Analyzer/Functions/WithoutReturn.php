5fb71de3b4a531f796bece2da12d85a5387349ee
Damien Seguy <damien.seguy@gmail.com>
2016-06-16 16:34:20 +0200

    Upgraded analyzer (removed raw => hasNoInterface)

--- library/Analyzer/Functions/WithoutReturn.php
+++ library/Analyzer/Functions/WithoutReturn.php
@@ -29,7 +29,7 @@ class WithoutReturn extends Analyzer\Analyzer {
     public function analyze() {
         $this->atomIs('Function')
              ->hasNoOut('ABSTRACT')
-             ->raw('filter{ it.in("ELEMENT").in("BLOCK").has("atom", "Interface").any() == false}')
+             ->hasNoInterface()
              ->outIs('NAME')
              ->codeIsNot(array('__construct', '__destruct', '__wakeup', '__autoload'))
              ->back('first')
