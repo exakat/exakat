ed754b2f541ede05fcad3e501d50be87a5839c7f
Damien Seguy <damien.seguy@gmail.com>
2016-06-23 12:15:09 +0200

    Upgraded to G3

--- library/Analyzer/Arrays/MultipleIdenticalKeys.php
+++ library/Analyzer/Arrays/MultipleIdenticalKeys.php
@@ -29,8 +29,15 @@ class MultipleIdenticalKeys extends Analyzer\Analyzer {
 
     public function analyze() {
         $this->atomIs('Functioncall')
-             ->code('array')
-             ->raw("aggregate().findAll{ m = [:]; it.out('ARGUMENTS').out('ARGUMENT').has('atom', 'Keyvalue').out('KEY').groupCount(m){it.fullcode}.cap.next().findAll{it.value > 1}.size() > 0}");
+             ->fullnspathIs('\\array')
+             ->raw(<<<GREMLIN
+out("ARGUMENTS").out("ARGUMENT").hasLabel("Keyvalue").out("KEY").as("a").values("fullcode").as("b").select("a")
+.in("KEY").in("ARGUMENT").out("ARGUMENT").hasLabel("Keyvalue").out("KEY")
+.where(neq("a")).values("fullcode").where(eq("b"))
+GREMLIN
+)
+             ->back('first')
+             ->analyzerIsNot('self');
         $this->prepareQuery();
     }
 }
