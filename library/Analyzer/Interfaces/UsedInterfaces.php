d49f105c9063fbab47f6ec71c4853c4176409e5d
Damien Seguy <damien.seguy@gmail.com>
2016-06-27 18:17:50 +0200

    Upgraded to G3; Reduced amount of queries.

--- library/Analyzer/Interfaces/UsedInterfaces.php
+++ library/Analyzer/Interfaces/UsedInterfaces.php
@@ -28,30 +28,14 @@ use Analyzer;
 class UsedInterfaces extends Analyzer\Analyzer {
     public function analyze() {
         // interface used in a class definition
-        $this->atomIs('Interface')
-             ->savePropertyAs('fullnspath', 'interfacedns')
-             ->raw('filter{ g.idx("atoms")[["atom":"Class"]].out("IMPLEMENTS").has("fullnspath", interfacedns).any()}');
-        $this->prepareQuery();
+        $classes = $this->query('g.V().hasLabel("Class", "Interface").out("IMPLEMENTS", "EXTENDS").values("fullnspath").unique()');
+        $instanceof = $this->query('g.V().hasLabel("Instanceof").out("CLASS").values("fullnspath").unique()');
+        $typehints = $this->query('g.V().hasLabel("Function").out("ARGUMENTS").out("ARGUMENT").out("TYPEHINT").values("fullnspath").unique()');
 
-        // interface used in a class definition
-        $this->atomIs('Interface')
-             ->analyzerIsNot('self')
-             ->savePropertyAs('fullnspath', 'interfacedns')
-             ->raw('filter{ g.idx("atoms")[["atom":"Interface"]].out("EXTENDS").has("fullnspath", interfacedns).any()}');
-        $this->prepareQuery();
+        $all = array_merge($classes, $instanceof, $typehints);
 
-        // interface used in a instanceof
         $this->atomIs('Interface')
-             ->analyzerIsNot('self')
-             ->savePropertyAs('fullnspath', 'interfacedns')
-             ->raw('filter{ g.idx("atoms")[["atom":"Instanceof"]].out("CLASS").has("fullnspath", interfacedns).any()}');
-        $this->prepareQuery();
-        
-        // interface used in a instanceof
-        $this->atomIs('Interface')
-             ->analyzerIsNot('self')
-             ->savePropertyAs('fullnspath', 'interfacedns')
-             ->raw('filter{ g.idx("atoms")[["atom":"Typehint"]].out("CLASS").has("fullnspath", interfacedns).any()}');
+             ->fullnspathIs($all);
         $this->prepareQuery();
     }
 }
