16ef0608e9f4fc83449a0ea8771b3c35f3caa37a
Damien Seguy <damien.seguy@gmail.com>
2016-07-01 11:02:35 +0200

    Filled UT with non-expected; Added extra text in the analzyer

--- tests/analyzer/exp/Php/DirectiveName.01.php
+++ tests/analyzer/exp/Php/DirectiveName.01.php
@@ -3,6 +3,8 @@
 $expected     = array("ini_set('NotADirective', 0)",
                       "\ini_get('AlsoNotADirective')",);
 
-$expected_not = array();
+$expected_not = array('ini_get($a, $b)',
+                      'ini_set(CONSTANTE, 3)',
+                      'ini_get("$a", $b)');
 
 ?>
