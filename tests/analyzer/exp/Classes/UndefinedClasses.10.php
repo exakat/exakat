4386034a8784035b69f808c95d3bb7c548df9c88
Damien Seguy <damien.seguy@gmail.com>
2016-07-01 10:52:42 +0200

    Fixed UT

--- tests/analyzer/exp/Classes/UndefinedClasses.10.php
+++ tests/analyzer/exp/Classes/UndefinedClasses.10.php
@@ -1,9 +1,13 @@
 <?php
 
-$expected     = array();
+$expected     = array('new b\e',
+                      'new b\e( )');
 
 $expected_not = array('new b\d',
                       'new b\d( )',
+                      '$c instanceof b\d',
+                      '$d instanceof b',
+
                       'b\d',
                       'b');
 
