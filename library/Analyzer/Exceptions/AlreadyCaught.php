e8442993cb01f2abaace71e50bb881aeda1e9af4
Damien Seguy <damien.seguy@gmail.com>
2016-07-04 22:33:00 +0200

    ported AlreadyCaught from G2

---
+++ library/Analyzer/Exceptions/AlreadyCaught.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * Copyright 2012-2016 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+namespace Analyzer\Exceptions;
+
+use Analyzer;
+
+class AlreadyCaught extends Analyzer\Analyzer {
+    public function analyze() {
+        // Check that the class of on catch is not a parent of of the next catch
+        // class A, class B extends A
+        // catch(A $a) {} catch (B $b) <= then Catch A is wrong 
+        $this->atomIs('Try')
+             ->outIs('CATCH')
+             ->savePropertyAs('rank', 'rank')
+             ->outIs('CLASS')
+             ->savePropertyAs('fullnspath', 'fnp')
+             ->inIs('CLASS')
+             ->inIs('CATCH')
+             ->outIs('CATCH')
+             ->isMore('rank', 'rank')
+             ->outIs('CLASS')
+             ->classDefinition()
+             ->goToAllParents()
+             ->samePropertyAs('fullnspath', 'fnp')
+             ->back('first')
+             ->analyzerIsNot('self');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/source/Exceptions/AlreadyCaught.04.php
@@ -0,0 +1,50 @@
+<?php
+
+class A implements Throwable {}
+class B implements Throwable {}
+class C extends B {}
+
+try {
+    something();
+} 
+catch(A $a1) { }
+catch(B $b2 ) { }
+catch(C $c3 ) { }
+
+try {
+    something();
+} 
+catch( A $a1) { }
+catch( C $c2) { }
+catch( B $b3) { }
+
+try {
+    something();
+} 
+catch(B $b1) { }
+catch(C $c2) { }
+catch(A $a3) { }
+
+try {
+    something();
+} 
+catch(B $b1) { }
+catch(A $a2) { }
+catch(C $c3) { }
+
+try {
+    something();
+} 
+catch(C $c1) { }
+catch(B $b2) { }
+catch(A $a3) { }
+
+try {
+    something();
+} 
+catch(C $c1) { }
+catch(A $a2) { }
+catch(B $b3) { }
+
+
+?>
