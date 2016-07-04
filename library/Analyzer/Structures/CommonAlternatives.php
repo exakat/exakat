ee3195dac7fe01bd1c07f15e757cdfce7baf5b56
Damien Seguy <damien.seguy@gmail.com>
2016-07-04 23:34:06 +0200

    Ported Common Alternative from G2

---
+++ library/Analyzer/Structures/CommonAlternatives.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Copyright 2012-2016 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+namespace Analyzer\Structures;
+
+use Analyzer;
+
+class CommonAlternatives extends Analyzer\Analyzer {
+        // some expressions are common between two then / else block
+        public function analyze() {
+        $this->atomIs('Ifthen')
+             ->outIs('THEN')
+             ->atomIs('Sequence')
+             ->outIs('ELEMENT')
+             ->_as('results')
+             ->savePropertyAs('fullcode', 'expression')
+             ->inIs('ELEMENT')
+             ->inIs('THEN')
+             ->outIs('ELSE')
+             ->atomIs('Sequence')
+             ->outIs('ELEMENT')
+             ->samePropertyAs('fullcode', 'expression')
+             ->back('results');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Structures/CommonAlternatives.01.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('$b = strtolower($b[2])');
+
+$expected_not = array('$a++',
+                      '$b++');
+
+?>
