141ecc485ab98abdea48e93995fffcc900e922b4
Damien Seguy <damien.seguy@gmail.com>
2016-07-04 23:35:55 +0200

    Upgraded LogicalMistakes from G2

---
+++ tests/analyzer/Test/Structures/LogicalMistakes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Test;
+
+include_once(dirname(dirname(dirname(dirname(__DIR__)))).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_test');
+spl_autoload_register('Autoload::autoload_phpunit');
+spl_autoload_register('Autoload::autoload_library');
+
+class Structures_LogicalMistakes extends Analyzer {
+    /* 5 methods */
+
+    public function testStructures_LogicalMistakes01()  { $this->generic_test('Structures/LogicalMistakes.01'); }
+    public function testStructures_LogicalMistakes02()  { $this->generic_test('Structures/LogicalMistakes.02'); }
+    public function testStructures_LogicalMistakes03()  { $this->generic_test('Structures/LogicalMistakes.03'); }
+    public function testStructures_LogicalMistakes04()  { $this->generic_test('Structures/LogicalMistakes.04'); }
+    public function testStructures_LogicalMistakes05()  { $this->generic_test('Structures/LogicalMistakes.05'); }
+}
+?>
