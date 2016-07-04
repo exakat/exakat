74bbe7dd535bb9b174a2718759e8fd73fb59409b
Damien Seguy <damien.seguy@gmail.com>
2016-07-04 19:51:55 +0200

    Added extra check on fullnspath for functions

--- library/Tasks/LoadFinal.php
+++ library/Tasks/LoadFinal.php
@@ -71,7 +71,7 @@ GREMLIN;
         // update fullnspath with fallback for functions 
         $query = <<<GREMLIN
 g.V().hasLabel("Functioncall").has("fullnspath").has('token', 'T_STRING')
-
+.where( __.in("NEW", "METHOD").count().is(eq(0)))
 .sideEffect{ f = it.get().value("fullnspath");}
 .where( g.V().hasLabel("Function").out("NAME").filter{ it.get().value("fullnspath") == f }.count().is(eq(0)) )
 
