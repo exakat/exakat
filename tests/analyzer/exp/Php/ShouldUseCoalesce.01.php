c89ac46daf597cdbe9177549effaacc2c2160a21
Damien Seguy <damien.seguy@gmail.com>
2016-07-05 00:10:08 +0200

    Ported Should Use Coalesce from G2

---
+++ tests/analyzer/exp/Php/ShouldUseCoalesce.01.php
@@ -0,0 +1,17 @@
+<?php
+
+$expected     = array('isset($a) ? $a : \'b\'',
+                      '!isset($a) ? \'b\' : $a',
+                      '$a->f === NULL ? $a->f : \'b\'',
+                      'nuLL === $a->f ? $a->f : \'b\'',
+                      'nuLL !== $a->f ? \'b\' : $a->f',
+                      'if (($model = Model::get($id)) === NULL) { /**/ } ', 
+                      'if (NULL === ($model = Model::get($id))) { /**/ } '
+ );
+
+$expected_not = array('isset($a) ? $b : \'b\'',
+                      'isset($a) ? \'b\' : $a',
+                      '$a->f == NULL ? $a->f : \'b\'',
+);
+
+?>
