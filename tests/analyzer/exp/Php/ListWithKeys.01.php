17401f048f5680b37f9ddd077afe3db37268a0ed
Damien Seguy <damien.seguy@gmail.com>
2016-07-04 21:58:02 +0200

    Get Php/ListWithKeys  from G2

---
+++ tests/analyzer/exp/Php/ListWithKeys.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('list(\'a\' => $oneBit, \'b\' => $twoBit, \'c\' . \'d\' => $threeBit)',
+                      'list(1 => $oneBit, 2 => $twoBit, 3 => $threeBit)',
+                      );
+
+$expected_not = array('list($a, $b, $c)');
+
+?>
