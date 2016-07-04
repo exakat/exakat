49aee9293036e4809c1b624f9f7b48d42184fd20
Damien Seguy <damien.seguy@gmail.com>
2016-07-04 21:37:27 +0200

    Moved Indirect Injection from G2 (moved to Beta group)

---
+++ tests/analyzer/exp/Security/IndirectInjection.01.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('$a1',
+                      '\'a\' . $a3',
+                      '"c" . $a6',
+                      '\'a\' . $a2',
+                      '"c$a5"',
+                      '\'a\' . $a4',
+                      'foreach($a7 as $b => $c) { /**/ } ',);
+
+$expected_not = array();
+
+?>
---
+++ tests/analyzer/source/Security/IndirectInjection.01.php
@@ -0,0 +1,27 @@
+<?php 
+
+$a1 = $_GET['a1']; 
+f($a1); 
+function f($a1a) { 
+    exec($a1a);
+}
+
+$a2 = $_POST;
+shell_exec('a'.$a2);
+
+$a3 = $_COOKIE['a'];
+shell_exec('a'.$a3);
+
+$a4 = $_REQUEST['a']['b'];
+shell_exec('a'.$a4);
+
+$a5 = $_GET['a']['b'];
+$b = "c$a5";
+
+$a6 = $_GET['a']['b'];
+$b = "c".$a6;
+
+$a7 = $_GET;
+foreach($a7 as $b => $c) {}
+
+?>
