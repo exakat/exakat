aac494faf7d593bcf078535b1f42e92c99b5248d
Damien Seguy <damien.seguy@gmail.com>
2016-07-05 11:32:44 +0200

    Upgraded API; Upgraded UT

--- tests/analyzer/exp/Structures/NoSubstrOne.01.php
+++ tests/analyzer/exp/Structures/NoSubstrOne.01.php
@@ -1,7 +1,6 @@
 <?php
 
 $expected     = array('substr($b, 0, -1)', 
-                      'substr($b, 0, 1)', 
                       'substr($b, 0, 1)');
 
 $expected_not = array('mb_substr($mb_, 0, 1)');
