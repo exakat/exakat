41b3921cb61a2161979cd890579a8cf1651a1084
Damien Seguy <damien.seguy@gmail.com>
2016-07-05 23:33:02 +0200

    Upgraded API

--- library/Analyzer/Arrays/CurlyArrays.php
+++ library/Analyzer/Arrays/CurlyArrays.php
@@ -29,7 +29,7 @@ class CurlyArrays extends Analyzer\Analyzer {
     public function analyze() {
         // $x[3]{3}
         $this->atomIs('Array')
-             ->code('{')
+             ->codeIs('{')
              ->hasNoIn('VARIABLE');
         $this->prepareQuery();
     }
