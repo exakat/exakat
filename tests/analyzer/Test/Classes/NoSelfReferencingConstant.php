f17f9ef54911a16fbd7f3a3a73b85a9e5c823f10
Damien Seguy <damien.seguy@gmail.com>
2016-07-05 10:40:32 +0200

    Added new UT

--- tests/analyzer/Test/Classes/NoSelfReferencingConstant.php
+++ tests/analyzer/Test/Classes/NoSelfReferencingConstant.php
@@ -8,9 +8,10 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Classes_NoSelfReferencingConstant extends Analyzer {
-    /* 2 methods */
+    /* 3 methods */
 
     public function testClasses_NoSelfReferencingConstant01()  { $this->generic_test('Classes_NoSelfReferencingConstant.01'); }
     public function testClasses_NoSelfReferencingConstant02()  { $this->generic_test('Classes_NoSelfReferencingConstant.02'); }
+    public function testClasses_NoSelfReferencingConstant03()  { $this->generic_test('Classes/NoSelfReferencingConstant.03'); }
 }
 ?>
