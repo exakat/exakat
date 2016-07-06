136a833b434848f0c55530bfeea2dd80856b3d2d
Damien Seguy <damien.seguy@gmail.com>
2016-07-06 11:28:38 +0200

    Added extra protection when using \n and saving a string to scripts

--- library/Graph/Gremlin3.php
+++ library/Graph/Gremlin3.php
@@ -80,7 +80,8 @@ class Gremlin3 extends Graph {
                         unset($params[$name]);
                     }
                 } elseif (is_array($value)) {
-                    $value = array_map(function ($x) { return str_replace('$', '\\$', addslashes($x)); }, $value);
+
+                    $value = array_map(function ($x) { return str_replace(array('$', "\n"), array('\\$', "\\\n"), addslashes($x)); }, $value);
                     $gremlin = "{ ['".join("','", $value)."'] }";
                     $defName = 'a'.crc32($gremlin);
                     $defFileName = $this->scriptDir.$defName.'.gremlin';
