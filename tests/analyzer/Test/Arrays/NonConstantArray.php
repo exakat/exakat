e28b03a738b83a9f5176a315979448bffdc84d64
Damien Seguy <damien.seguy@gmail.com>
2016-07-11 16:05:21 +0200

    Upgraded UT

--- tests/analyzer/Test/Arrays/NonConstantArray.php
+++ tests/analyzer/Test/Arrays/NonConstantArray.php
@@ -8,10 +8,11 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Arrays_NonConstantArray extends Analyzer {
-    /* 3 methods */
+    /* 4 methods */
 
     public function testArrays_NonConstantArray01()  { $this->generic_test('Arrays_NonConstantArray.01'); }
     public function testArrays_NonConstantArray02()  { $this->generic_test('Arrays_NonConstantArray.02'); }
     public function testArrays_NonConstantArray03()  { $this->generic_test('Arrays/NonConstantArray.03'); }
+    public function testArrays_NonConstantArray04()  { $this->generic_test('Arrays/NonConstantArray.04'); }
 }
 ?>
