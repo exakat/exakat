03e3754ad7020d53df539ddff456c50f799f2290
Damien Seguy <damien.seguy@gmail.com>
2016-07-12 18:01:15 +0200

    Added support for anonymization, for file/directory/project

--- library/Tasks/Anonymize.php
+++ library/Tasks/Anonymize.php
@@ -34,9 +34,74 @@ class Anonymize extends Tasks {
     private $strings = 'A';
 
     public function run(\Config $config) {
-        $file = $config->file;
-        if (empty($file)) {
-            die("Usage : php exakat anonymize -f <filename>\n");
+        
+        var_dump($config->file);
+        var_dump($config->project);
+        var_dump($config->dirname);
+        if (($file = $config->file) !== 'stdout') {
+            display("Anonymizing file $file\n");
+
+            if (!file_exists($file)) {
+                die('Can\'t anonymize '.$file.' as it doesn\'t exist'."\n");
+            }
+            
+            if (!$this->checkCompilation($file)) {
+                die('Can\'t anonymize '.$file.' as it doesn\'t compile with PHP '.PHP_VERSION."\n");
+            }
+            $this->processFile($file);
+        } elseif (!empty($dir = $config->dirname)) {
+            if (substr($dir, -1) === '/') {
+                $dir = substr($dir, 0, -1);
+            }
+
+            if (!file_exists($dir)) {
+                die('Can\'t anonymize '.$dir.' as it doesn\'t exist'."\n");
+            }
+
+            display("Anonymizing directory $dir\n");
+
+            $files = rglob($dir);
+            $total = 0;
+            if (file_exists($dir.'.anon')) {
+                rmdirRecursive($dir.'.anon');
+            }
+            mkdir($dir.'.anon', 0755);
+            foreach($files as $file) {
+                if ($this->checkCompilation($file)) {
+                    ++$this->strings;
+                    $total += (int) $this->processFile($file, $dir.'.anon/'.$this->strings.'.php');
+                }
+            }
+            display("Anonymized $total files\n");
+        } elseif (($project = $config->project) !== 'default') {
+            display("Anonymizing project $project\n");
+            $dir = $config->projects_root.'/projects/'.$project.'/code';
+
+            if (!file_exists($config->projects_root.'/projects/'.$project)) {
+                die('Can\'t anonymize project '.$project.' as it doesn\'t exist'."\n");
+            }
+
+            if (!file_exists($config->projects_root.'/projects/'.$project.'/code')) {
+                die('Can\'t anonymize project '.$project.' as it doesn\'t have code'."\n");
+            }
+
+            $files = rglob($dir);
+            $total = 0;
+            if (file_exists($dir.'.anon')) {
+                rmdirRecursive($dir.'.anon');
+            }
+            mkdir($dir.'.anon', 0755);
+            foreach($files as $file) {
+                if ($this->checkCompilation($file)) {
+                    ++$this->strings;
+                    $total += (int) $this->processFile($file, $dir.'.anon/'.$this->strings.'.php');
+                }
+            }
+            display("Anonymized $total files\n");        
+        } else {
+            die("Usage : php exakat anonymize -f <filename>
+                             -d <dirname>
+                             -p <project>\n");
         }
 
         if (!file_exists($file)) {
@@ -44,19 +109,21 @@ class Anonymize extends Tasks {
         }
         display( 'Processing file ', $file,' into ', $file, ".anon\n");
 
-        $this->checkCompilation($file);
-        $this->processFile($file);
     }
     
-    private function processFile($file) {
+    private function processFile($file, $anonFile = null) {
         $php = file_get_contents($file);
         $tokens = token_get_all($php);
+        if (count($tokens) < 3) {
+            display( "Ignoring $file, no PHP-code.\n");
+            return false;
+        }
 
         $checks = array('T_TRAIT', 'T_FINALLY', 'T_YIELD', 'T_YIELDFROM', 'T_COALESCE', 'T_CHARACTER', 'T_BAD_CHARACTER');
         foreach($checks as $check) {
-            if (!defined($check)) { 
-                define($check, 1); 
-            } 
+            if (!defined($check)) {
+                define($check, 1);
+            }
         }
 
         $php = '';
@@ -75,11 +142,11 @@ class Anonymize extends Tasks {
                         if (isset($this->dnumberValues[$t[1]])) {
                             $t[1] = floor(rand(0, 100000) ) / 100;
                         } else {
-                            $this->dnumberValues[$t[1]] = ++$dnumber;
+                            $this->dnumberValues[$t[1]] = ++$this->dnumber;
                             $t[1] = floor(rand(0, 100000) ) / 100;
                         }
                         break;
-                    case T_VARIABLE: 
+                    case T_VARIABLE:
                         if ($t[1] != '$this') {
                             if (isset($this->variableNames[$t[1]])) {
                                 $t[1] = $this->variableNames[$t[1]];
@@ -91,8 +158,8 @@ class Anonymize extends Tasks {
                         break;
                     case T_CONSTANT_ENCAPSED_STRING:
                         ++$this->strings;
-                        if (in_array($this->strings, array('IF', 'AS', 'DO', 'OR'))) { 
-                            ++$this->strings; 
+                        if (in_array($this->strings, array('IF', 'AS', 'DO', 'OR'))) {
+                            ++$this->strings;
                         }
                         if (isset($this->stringsNames[$t[1]])) {
                             $t[1] = $this->stringsNames[$t[1]];
@@ -107,8 +174,8 @@ class Anonymize extends Tasks {
                         // otherwise, fall through!
                     case T_ENCAPSED_AND_WHITESPACE :
                         ++$this->strings;
-                        if (in_array($this->strings, array('IF', 'AS', 'DO', 'OR'))) { 
-                            ++$this->strings; 
+                        if (in_array($this->strings, array('IF', 'AS', 'DO', 'OR'))) {
+                            ++$this->strings;
                         }
                         if (isset($this->stringsNames[$t[1]])) {
                             $t[1] = $this->stringsNames[$t[1]];
@@ -122,7 +189,7 @@ class Anonymize extends Tasks {
                         $t[1] = "\n";
                         break;
 
-                    case T_INLINE_HTML : 
+                    case T_INLINE_HTML :
                         ++$this->strings;
                         if (isset($this->stringsNames[$t[1]])) {
                             $t[1] = $this->stringsNames[$t[1]];
@@ -150,93 +217,95 @@ class Anonymize extends Tasks {
                 
                         break;
                 
-                    case T_END_HEREDOC: 
+                    case T_END_HEREDOC:
                         $t[1] = $heredoc;
                         (unset) $heredoc;
 
                         break;
 
-                    case T_ISSET : 
-                    case T_EXIT : 
+                    case T_ISSET :
+                    case T_EXIT :
 
-                    case T_ARRAY_CAST : 
-                    case T_BOOL_CAST : 
-                    case T_DOUBLE_CAST : 
-                    case T_OBJECT_CAST : 
-                    case T_UNSET_CAST : 
-                    case T_INT_CAST : 
-                    case T_STRING_CAST : 
+                    case T_ARRAY_CAST :
+                    case T_BOOL_CAST :
+                    case T_DOUBLE_CAST :
+                    case T_OBJECT_CAST :
+                    case T_UNSET_CAST :
+                    case T_INT_CAST :
+                    case T_STRING_CAST :
 
                     case T_CONST :
                     case T_LIST :
             
-                    case T_NAMESPACE : 
-                    case T_IMPLEMENTS : 
+                    case T_NAMESPACE :
+                    case T_IMPLEMENTS :
 
                     case T_MUL_EQUAL :
                     case T_DIV_EQUAL :
 
                     case T_RETURN :
-                    case T_SWITCH : 
-                    case T_CASE : 
-                    case T_DEFAULT : 
-                    case T_ENDSWITCH : 
-                    case T_ECHO : 
-                    case T_PRINT : 
-                    case T_EMPTY : 
+                    case T_SWITCH :
+                    case T_CASE :
+                    case T_DEFAULT :
+                    case T_ENDSWITCH :
+                    case T_ECHO :
+                    case T_PRINT :
+                    case T_EMPTY :
                     case T_ARRAY :
-                    case T_GLOBAL : 
-                    case T_TRY : 
-                    case T_CATCH : 
-                    case T_DOUBLE_ARROW : 
+                    case T_GLOBAL :
+                    case T_TRY :
+                    case T_CATCH :
+                    case T_DOUBLE_ARROW :
                     case T_CURLY_OPEN:
-                    case T_ELSE : 
-                    case T_PUBLIC : 
-                    case T_PROTECTED : 
-                    case T_PRIVATE : 
-                    case T_SL : 
-                    case T_SR : 
+                    case T_ELSE :
+                    case T_PUBLIC :
+                    case T_PROTECTED :
+                    case T_PRIVATE :
+                    case T_FINAL :
+
+                    case T_SL :
+                    case T_SR :
                     case T_IS_EQUAL :
                     case T_IS_SMALLER_OR_EQUAL :
                     case T_MINUS_EQUAL :
-                    case T_WHILE : 
-                    case T_ENDWHILE : 
-                    case T_IS_GREATER_OR_EQUAL : 
-                    case T_PLUS_EQUAL : 
-                    case T_POW : 
-                    case T_CLASS : 
-                    case T_INTERFACE : 
+                    case T_WHILE :
+                    case T_ENDWHILE :
+                    case T_IS_GREATER_OR_EQUAL :
+                    case T_PLUS_EQUAL :
+                    case T_POW :
+                    case T_CLASS :
+                    case T_INTERFACE :
                     case T_CONTINUE :
-                    case T_WHITESPACE : 
-                    case T_AS : 
+                    case T_WHITESPACE :
+                    case T_AS :
                     case T_BOOLEAN_OR :
                     case T_BOOLEAN_AND :
-                    case T_BREAK : 
+                    case T_BREAK :
                     case T_DEC :
                     case T_DO :
-                    case T_IS_NOT_IDENTICAL : 
-                    case T_SR_EQUAL : 
+                    case T_IS_NOT_IDENTICAL :
+                    case T_SR_EQUAL :
                     case T_XOR_EQUAL :
-                    case T_OR_EQUAL : 
-                    case T_IS_NOT_EQUAL : 
-                    case T_COALESCE : 
+                    case T_OR_EQUAL :
+                    case T_IS_NOT_EQUAL :
+                    case T_COALESCE :
             
-                    case T_OPEN_TAG_WITH_ECHO : 
-                    case T_CALLABLE : 
-                    case T_UNSET : 
-                    case T_EVAL : 
+                    case T_OPEN_TAG_WITH_ECHO :
+                    case T_CALLABLE :
+                    case T_UNSET :
+                    case T_EVAL :
 
-                    case T_DOLLAR_OPEN_CURLY_BRACES : 
+                    case T_DOLLAR_OPEN_CURLY_BRACES :
 
-                    case T_FINALLY : 
-                    case T_YIELD : 
+                    case T_FINALLY :
+                    case T_YIELD :
 
                     case T_FOR :
                     case T_ENDFOR :
-                    case T_FOREACH : 
-                    case T_ENDFOREACH : 
+                    case T_FOREACH :
+                    case T_ENDFOREACH :
 
-                    case T_FUNCTION : 
+                    case T_FUNCTION :
                     case T_INC:
                     case T_DOUBLE_COLON:
                     case T_THROW:
@@ -247,35 +316,35 @@ class Anonymize extends Tasks {
                     case T_OBJECT_OPERATOR:
                     case T_DIR:
                     case T_STATIC:
-                    case T_VAR : 
+                    case T_VAR :
                     case T_OPEN_TAG:
                     case T_CLOSE_TAG:
                     case T_INSTANCEOF:
 
-                    case T_INCLUDE : 
-                    case T_INCLUDE_ONCE : 
-                    case T_REQUIRE : 
-                    case T_REQUIRE_ONCE : 
+                    case T_INCLUDE :
+                    case T_INCLUDE_ONCE :
+                    case T_REQUIRE :
+                    case T_REQUIRE_ONCE :
 
-                    case T_TRAIT : 
-                    case T_EXTENDS : 
-                    case T_USE : 
-                    case T_INSTEADOF : 
+                    case T_TRAIT :
+                    case T_EXTENDS :
+                    case T_USE :
+                    case T_INSTEADOF :
 
-                    case T_LOGICAL_AND : 
-                    case T_LOGICAL_OR : 
-                    case T_LOGICAL_XOR : 
+                    case T_LOGICAL_AND :
+                    case T_LOGICAL_OR :
+                    case T_LOGICAL_XOR :
 
                     case T_IF:
                     case T_ELSEIF:
-                    case T_ENDIF : 
+                    case T_ENDIF :
 
-                    case T_FILE : 
-                    case T_CLASS_C : 
-                    case T_FUNC_C : 
-                    case T_LINE : 
-                    case T_METHOD_C : 
-                    case T_NS_C : 
+                    case T_FILE :
+                    case T_CLASS_C :
+                    case T_FUNC_C :
+                    case T_LINE :
+                    case T_METHOD_C :
+                    case T_NS_C :
 
                     case T_POW_EQUAL :
                     case T_SL_EQUAL :
@@ -299,7 +368,7 @@ class Anonymize extends Tasks {
                         // simply ignore
                         break;
 
-                    default: 
+                    default:
                         echo token_name($t[0]), " is unknown token\n", print_r($t, true);
                 }
 
@@ -308,15 +377,19 @@ class Anonymize extends Tasks {
                 $php .= $t;
             }
         }
-        file_put_contents($file.'.anon', $php);
+        if ($anonFile === null) {
+            $anonFile = $file.'.anon';
+        }
+        
+        file_put_contents($anonFile, $php);
+        
+        return true;
     }
     
     private function checkCompilation($file) {
-        $res = shell_exec($_SERVER['_'].' -l '.$file);
+        $res = shell_exec($_SERVER['_'].' -l '.$file.' 2>&1');
         //@todo : differentiate fatal error and non-fatal ones.
-        if (substr($res, 0, 28) != 'No syntax errors detected in') {
-            die( "Can't compile '".$file."' script with PHP version ".phpversion().".\n");
-        }
+        return substr($res, 0, 28) == 'No syntax errors detected in';
     }
 }
 
