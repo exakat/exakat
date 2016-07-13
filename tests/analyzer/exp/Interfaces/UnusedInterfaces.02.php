c23d6888d7887432c4d12a531153acd7ad412e8b
Damien Seguy <damien.seguy@gmail.com>
2016-07-13 12:08:22 +0200

    Upgraded UT

--- tests/analyzer/exp/Interfaces/UnusedInterfaces.02.php
+++ tests/analyzer/exp/Interfaces/UnusedInterfaces.02.php
@@ -1,15 +1,15 @@
 <?php
 
-$expected     = array('interface unusedInterface',
-                      'interface d extends usedInterfaceImplements2, \usedInterfaceFPImplements2');
+$expected     = array('interface unusedInterface { /**/ } ',
+                      'interface d extends usedInterfaceImplements2, \usedInterfaceFPImplements2 { /**/ } ');
 
-$expected_not = array('interface usedInterfaceFPImplements',
-                      'interface usedInterfaceImplements',
-                      'interface usedInterfaceFPImplements2',
-                      'interface usedInterfaceImplements2',
-                      'interface usedInterfaceInstanceof',
-                      'interface usedInterfaceFPInstanceof',
-                      'interface usedInterfaceTypehint',
-                      'interface usedInterfaceFPTypehint');
+$expected_not = array('interface usedInterfaceFPImplements { /**/ } ',
+                      'interface usedInterfaceImplements { /**/ } ',
+                      'interface usedInterfaceFPImplements2 { /**/ } ',
+                      'interface usedInterfaceImplements2 { /**/ } ',
+                      'interface usedInterfaceInstanceof { /**/ } ',
+                      'interface usedInterfaceFPInstanceof { /**/ } ',
+                      'interface usedInterfaceTypehint { /**/ } ',
+                      'interface usedInterfaceFPTypehint { /**/ } ');
 
 ?>
