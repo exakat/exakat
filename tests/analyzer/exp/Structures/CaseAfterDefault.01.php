a0bac844504f727933402f9ce609f4d22bd389ad
Damien Seguy <damien.seguy@gmail.com>
2016-07-13 15:15:53 +0200

    Removing Case After Default

--- tests/analyzer/exp/Structures/CaseAfterDefault.01.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$expected     = array('switch ($expr1) { /**/ } ');
-
-$expected_not = array('switch ($expr2) { /**/ } ',
-                      'switch ($expr3) { /**/ } ',
-                      'switch ($nestedExpr) { /**/ } ');
-
-?>
