d00479f6041449d2eb02147c13e494bee45aaf8e
Damien Seguy <damien.seguy@gmail.com>
2016-07-14 10:28:01 +0200

    Checked all tests, added some for ::

--- tests/analyzer/Test/Arrays/ShouldPreprocess.php
+++ tests/analyzer/Test/Arrays/ShouldPreprocess.php
@@ -8,7 +8,7 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Arrays_ShouldPreprocess extends Analyzer {
-    /* 6 methods */
+    /* 7 methods */
 
     public function testArrays_ShouldPreprocess01()  { $this->generic_test('Arrays_ShouldPreprocess.01'); }
     public function testArrays_ShouldPreprocess02()  { $this->generic_test('Arrays_ShouldPreprocess.02'); }
@@ -16,5 +16,6 @@ class Arrays_ShouldPreprocess extends Analyzer {
     public function testArrays_ShouldPreprocess04()  { $this->generic_test('Arrays_ShouldPreprocess.04'); }
     public function testArrays_ShouldPreprocess05()  { $this->generic_test('Arrays_ShouldPreprocess.05'); }
     public function testArrays_ShouldPreprocess06()  { $this->generic_test('Arrays/ShouldPreprocess.06'); }
+    public function testArrays_ShouldPreprocess07()  { $this->generic_test('Arrays/ShouldPreprocess.07'); }
 }
 ?>
--- tests/analyzer/exp/Arrays/ShouldPreprocess.06.php
+++ tests/analyzer/exp/Arrays/ShouldPreprocess.06.php
@@ -1,10 +1,10 @@
 <?php
 
-$expected     = array('$d = [   ]', 
-                      '$a = [   ]', 
-                      '$e = [   ]', 
-                      '$x->y = [   ]', 
-                      '$b = [   ]'
+$expected     = array('$d = [ ]', 
+                      '$a = [ ]', 
+                      '$e = [ ]', 
+                      '$x->y = [ ]', 
+                      '$b = [ ]'
 );
 
 $expected_not = array('$e[45]',
@@ -13,6 +13,7 @@ $expected_not = array('$e[45]',
                       '$b[2]',
                       '$d[2]',
                       '$x->y[33]',
+                      '$x->y3 = [ ]',
                       '$x->y[333]'
                       );
 
