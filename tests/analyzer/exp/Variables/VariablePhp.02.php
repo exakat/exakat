476f3c2297bd9cc425954793471df6b9a8745c27
Damien Seguy <damien.seguy@gmail.com>
2016-07-14 11:18:37 +0200

    Upgraded API, added more UT

---
+++ tests/analyzer/exp/Variables/VariablePhp.02.php
@@ -0,0 +1,15 @@
+<?php
+
+$expected     = array( '$argc', 
+                       '$_POST', 
+                       '$_GET', 
+                       '$PHP_SELF', 
+                       '$GLOBALS', 
+                       '$argv',
+                       '$_REQUEST'
+);
+
+$expected_not = array('$COOKIE',
+                      '$globals');
+
+?>
