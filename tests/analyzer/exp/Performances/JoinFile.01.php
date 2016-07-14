4615ed9838e8af37ac961ec50a4824e269da0a63
Damien Seguy <damien.seguy@gmail.com>
2016-07-14 08:53:56 +0200

    Upgraded API

--- tests/analyzer/exp/Performances/JoinFile.01.php
+++ tests/analyzer/exp/Performances/JoinFile.01.php
@@ -1,10 +1,10 @@
 <?php
 
 $expected     = array('file($file5)', 
-                      'file($file6)', 
+                      '\\file($file7)', 
                       'join(\'sb\', \file($file2))', 
                       'implode(\'\', file($file1))');
 
-$expected_not = array();
+$expected_not = array('file($file6)', );
 
 ?>
