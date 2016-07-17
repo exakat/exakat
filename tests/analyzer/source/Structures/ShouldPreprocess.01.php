d9bed3b8bd23b78097ccdbc7682f44bb4a18e827
Damien Seguy <damien.seguy@gmail.com>
2016-07-17 18:25:43 +0200

    Upgraded with functions calls

--- tests/analyzer/source/Structures/ShouldPreprocess.01.php
+++ tests/analyzer/source/Structures/ShouldPreprocess.01.php
@@ -3,18 +3,20 @@
     $a = 'a' . "b";
     $b = true * "b";
     $c = null +  2.2;
-//    $d = 'a' . strtolower("b");
+    $d = 'a' . strtolower("b");
+    $d = 'a' . rand("b");
 //  could do but needs 'determinist' functions
     $e = 2 << 3;
     $f = !(2 + 4 - 4);
     $g = (6 and 7);
     $h = 8 ^ 9;
+    $h = 8 ** CONSTANTE;
+    $i = 'a' . Stdclass::c;
     
 // KOK
     $A = 'a' . $a;
     $B = __DIR__ + 1;
     $C = 'a' . $a->b;
-    $D = 'a' . Stdclass::c;
     $E = 'a' . Stdclass::$D;
     $E = 'a' % Stdclass::D();
     $E = 'a' >> $c->d();
