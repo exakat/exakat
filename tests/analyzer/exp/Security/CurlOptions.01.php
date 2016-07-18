8e628f8d58240d8ac4ce3b695feaf881a629a64f
Damien Seguy <damien.seguy@gmail.com>
2016-07-18 17:56:36 +0200

    Upgraded UT

--- tests/analyzer/exp/Security/CurlOptions.01.php
+++ tests/analyzer/exp/Security/CurlOptions.01.php
@@ -2,10 +2,10 @@
 
 $expected     = array('curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false)',
                       'curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0)',
-                      '[ \CURLOPT_SSL_VERIFYPEER => 0 ]',
+                      '[\CURLOPT_SSL_VERIFYPEER => 0]',
                       'array(CURLOPT_SSL_VERIFYPEER => 0)',
                       'array(\CURLOPT_SSL_VERIFYPEER => 0)',
-                      '[ CURLOPT_SSL_VERIFYPEER => 0 ]',
+                      '[CURLOPT_SSL_VERIFYPEER => 0]',
                       );
 
 $expected_not = array('array(\CURLOPT_URL => \'a\')',
--- tests/analyzer/exp/Structures/Bracketless.01.php
+++ tests/analyzer/exp/Structures/Bracketless.01.php
@@ -1,12 +1,12 @@
 <?php
 
-$expected     = array('for(7 ;   ;  )  /**/ ', 
-                      'foreach($a2 as $b2)  /**/ ',
-                      'while (7)  /**/ ',
-                      'elseif (5)  /**/  elseif (6)  /**/  elseif (8)  /**/  else  /**/ ', 
-                      'elseif (6)  /**/  elseif (8)  /**/  else  /**/ ', 
-                      'if (4)  /**/  elseif (5)  /**/  elseif (6)  /**/  elseif (8)  /**/  else  /**/ ',
-                       'elseif (8)  /**/  else  /**/ '
+$expected     = array('for(7 ;   ;  ) /**/ ', 
+                      'foreach($a2 as $b2) /**/ ',
+                      'while (7) /**/ ',
+                      'elseif(8)  /**/  else  /**/  ', 
+                      'elseif(6)  /**/  elseif(8)  /**/  else  /**/  ', 
+                      'elseif(5)  /**/  elseif(6)  /**/  elseif(8)  /**/  else  /**/  ', 
+                      'if(4)  /**/  elseif(5)  /**/  elseif(6)  /**/  elseif(8)  /**/  else  /**/  ',
                       );
 
 $expected_not = array('if ( 14) { /**/ }  else if ( 15) { /**/ } ', 
