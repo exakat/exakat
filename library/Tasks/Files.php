31bc89b56a7ac60cad430d0da9be4d44ace813d2
Damien Seguy <damien.seguy@gmail.com>
2016-07-18 16:51:51 +0200

    Backported portability fix for file names with ( and )

--- library/Tasks/Files.php
+++ library/Tasks/Files.php
@@ -136,7 +136,7 @@ class Files extends Tasks {
         display('Counted files');
         
         $tmpFileName = tempnam(sys_get_temp_dir(), 'exakatFile');
-        file_put_contents($tmpFileName, $config->projects_root.'/projects/'.$dir.'/code'.join("\n$config->projects_root/projects/$dir/code", $files));
+        file_put_contents($tmpFileName, '"'.$config->projects_root.'/projects/'.$dir.'/code'.implode("\"\n\"$config->projects_root/projects/$dir/code", $files).'"');
 
         $versions = $config->other_php_versions;
 
@@ -219,8 +219,7 @@ class Files extends Tasks {
         $tokens = (int) array_sum(explode("\n", $resultNosot));
 
         display('Check short tag (with directive activated)');
-        $shell = 'cat '.$tmpFileName.' | sort |  tr "\n" "\0" |  xargs -n1 -P5 -0I '.$config->php.' -d short_open_tag=1 -r "echo count(token_get_all(file_get_contents(\$argv[1]))).\" \$argv[1]\n\";" 2>>/dev/null || true ';
-        
+        $shell = 'cat '.$tmpFileName.' | sort |  tr "\n" "\0" |  xargs -n1 -P5 -0I '.$config->php.' -d short_open_tag=1 -r "echo count(@token_get_all(file_get_contents(\$argv[1]))).\" \$argv[1]\n\";" 2>>/dev/null || true ';        
         $resultSot = shell_exec($shell);
         $tokenssot = (int) array_sum(explode("\n", $resultSot));
 
