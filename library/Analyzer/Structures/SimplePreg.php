2a6c6c03b316d08cfc0da3462e7f8dfc3ec553ce
Damien Seguy <damien.seguy@gmail.com>
2016-07-19 10:16:06 +0200

    Added licence

--- library/Analyzer/Structures/SimplePreg.php
+++ library/Analyzer/Structures/SimplePreg.php
@@ -1,5 +1,24 @@
 <?php
-
+/*
+ * Copyright 2012-2016 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
 namespace Analyzer\Structures;
 
 use Analyzer;
@@ -7,7 +26,7 @@ use Analyzer;
 class SimplePreg extends Analyzer\Analyzer {
     public function analyze() {
         // almost data/pcre.ini but not preg_last_error
-        $functions = array('\preg_match', '\preg_match_all', '\preg_replace', '\preg_replace_callback', 
+        $functions = array('\preg_match', '\preg_match_all', '\preg_replace', '\preg_replace_callback',
                            '\preg_filter', '\preg_split', '\preg_quote', '\preg_grep');
 
         $this->atomFunctionIs($functions)
