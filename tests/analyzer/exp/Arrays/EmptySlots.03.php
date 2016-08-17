e2155dfd0b2f59d0b45febb78efa53b59b805d3b
Damien Seguy <damien.seguy@gmail.com>
2016-08-17 10:20:20 +0200

    Fixed exp

--- tests/analyzer/exp/Arrays/EmptySlots.03.php
+++ tests/analyzer/exp/Arrays/EmptySlots.03.php
@@ -1,7 +1,7 @@
 <?php
 
-$expected     = array( '[ 7,  ]', 
-                       '[ 1, 2, 3,  ]');
+$expected     = array( '[7,  ]', 
+                       '[1, 2, 3,  ]');
 
 $expected_not = array('[4, 5, 6]');
 
