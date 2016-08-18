a17c4bf1b97ed0920f554ad5870d9d2152b81bd9
Damien Seguy <damien.seguy@gmail.com>
2016-08-18 11:29:59 +0200

    Mark properties as being used by children classes

---
+++ library/Analyzer/Classes/PropertyUsedBelow.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * Copyright 2012-2016 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Analyzer\Classes;
+
+use Analyzer;
+
+class PropertyUsedBelow extends Analyzer\Analyzer {
+    public function analyze() {
+        //////////////////////////////////////////////////////////////////
+        // property + $this->property
+        //////////////////////////////////////////////////////////////////
+        $this->atomIs('Ppp')
+             ->hasNoOut('STATIC')
+             ->outIs('PPP')
+             ->_as('ppp')
+             ->savePropertyAs('propertyname', 'propertyname')
+             ->analyzerIsNot('self')
+             ->goToClass()
+             ->raw('where( __.repeat( out("DEFINITION").in("EXTENDS") ).emit().times('.self::MAX_LOOPING.')
+                             .where( __.out("BLOCK").repeat( __.out()).emit(hasLabel("Property")).times('.self::MAX_LOOPING.')
+                                       .out("OBJECT").has("code", "\\$this").in("OBJECT")
+                                       .out("PROPERTY").has("token", "T_STRING").filter{ it.get().value("code").toLowerCase() == propertyname.toLowerCase()}
+                              )
+                             .count().is(neq(0)) )')
+             ->back('ppp');
+        $this->prepareQuery();
+
+        //////////////////////////////////////////////////////////////////
+        // static property : inside the self class
+        //////////////////////////////////////////////////////////////////
+        $this->atomIs('Ppp')
+             ->hasOut('STATIC')
+             ->outIs('PPP')
+             ->_as('ppp')
+             ->outIsIE('LEFT')
+             ->analyzerIsNot('self')
+             ->savePropertyAs('code', 'property')
+             ->goToClass()
+             ->raw('where( __.repeat( out("DEFINITION").in("EXTENDS") ).emit().times('.self::MAX_LOOPING.')
+                             .where( __.out("BLOCK").repeat( __.out()).emit(hasLabel("Staticproperty")).times('.self::MAX_LOOPING.')
+                                       .out("PROPERTY").has("token", "T_VARIABLE").filter{ it.get().value("code").toLowerCase() == property.toLowerCase()}
+                              )
+                             .count().is(neq(0)) )')
+             ->back('ppp');
+        $this->prepareQuery();
+        
+        // This could be also checking for fnp : it needs to be a 'family' class check.
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Classes/PropertyUsedBelow.01.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('$usedProtectedByBelowF',
+                      '$usedProtectedByBelowE',
+                      '$usedProtectedByBelowC',
+                      );
+
+$expected_not = array('$usedProtectedByAbove',
+                      '$unusedProtected',
+                      '$unusedProtectedByBelowD'
+);
+
+?>
