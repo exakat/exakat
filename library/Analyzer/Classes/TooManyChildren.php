fca1f2fd10860904810cfdbea78aa57e75117fbe
Damien Seguy <damien.seguy@gmail.com>
2016-08-23 10:18:44 +0800

    Upgraded API

--- library/Analyzer/Classes/TooManyChildren.php
+++ library/Analyzer/Classes/TooManyChildren.php
@@ -29,9 +29,7 @@ class TooManyChildren extends Analyzer\Analyzer {
     public function analyze() {
         // class a with extends
         $this->atomIs('Class')
-             ->raw('sideEffect{nspath = it.fullnspath;}')
-             ->outIs('NAME')
-             ->raw('filter{ g.idx("atoms")[["atom":"Class"]].out("EXTENDS", "IMPLEMENTS").has("fullnspath", nspath).count() >= 15}')
+             ->raw('where( __.out("DEFINITION").in("EXTENDS", "IMPLEMENTS").count().is(gt(15)) )')
              ->back('first');
         $this->prepareQuery();
     }
