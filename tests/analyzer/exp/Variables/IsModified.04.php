4fea69c9d518753d462cbbc8fead89e1e11d8abb
Damien Seguy <damien.seguy@gmail.com>
2016-08-24 14:09:31 +0800

    Upgraded UT

--- tests/analyzer/exp/Variables/IsModified.04.php
+++ tests/analyzer/exp/Variables/IsModified.04.php
@@ -1,11 +1,11 @@
 <?php
 
 $expected     = array('$a',
-                      '$b',
-                      '$c',
-                      '$d',
-                      '$e',
-                      '$f');
+                      'NS2\TH2 $f = null', 
+                      'NS1\TH1 $e', 
+                      'TH2 $d = null', 
+                      'TH1 $c', 
+                      '$b = 1');
 
 $expected_not = array('null',
                       '1');
