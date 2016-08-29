94481131c83a9859bf5b78077ef8fd3572a8198e
Damien Seguy <damien.seguy@gmail.com>
2016-08-29 11:02:18 +0200

    Spotting comparison that are always true with count() etc.

---
+++ tests/analyzer/exp/Structures/NeverNegative.02.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('abs($a) < 0', 
+                      'strlen($a) < 0', 
+                      'sizeof($a) < 0', 
+                      'count($a) < 0');
+
+$expected_not = array('sin($a) < 0');
+
+?>
---
+++ tests/analyzer/source/Structures/NeverNegative.02.php
@@ -0,0 +1,9 @@
+<?php
+
+if (count($a) < 0) { }
+if (sizeof($a) < 0) { }
+if (strlen($a) < 0) { }
+if (abs($a) < 0) { }
+if (sin($a) < 0) { }
+
+?>
