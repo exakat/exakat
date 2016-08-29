f46e8b5be681a4b11d7e9102f578f21721055d2d
Damien Seguy <damien.seguy@gmail.com>
2016-08-29 11:31:34 +0200

    List of new function names in PHP 7.1

---
+++ tests/analyzer/exp/Php/Php71NewFunctions.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('mb_scrub()',
+                      'is_iterable()');
+
+$expected_not = array('$x->mb_ord()',
+                      'Y::mb_chr()'
+);
+
+?>
---
+++ tests/analyzer/source/Php/Php71NewFunctions.01.php
@@ -0,0 +1,9 @@
+<?php
+
+mb_scrub();
+is_iterable();
+
+$x->mb_ord();
+Y::mb_chr();
+
+?>
