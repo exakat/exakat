1d3b60b31c5530e9f757d3a19d5324e9e9821a7d
Damien Seguy <damien.seguy@gmail.com>
2016-08-30 09:16:23 +0200

    Fixed UT

--- tests/analyzer/exp/Php/UpperCaseKeyword.01.php
+++ tests/analyzer/exp/Php/UpperCaseKeyword.01.php
@@ -1,12 +1,12 @@
 <?php
 
-$expected     = array( 'WHILE ($c++)  { /**/ } ', 
+$expected     = array( 'WHILE ($c++) { /**/ } ', 
+                       'DO { /**/ } WHILE($a++ AND $b++)',
                        'CLASS x { /**/ } ', 
                        'CLASS x IMPLEMENTS a { /**/ } ',
 //                       'class x4 EXTENDS b { /**/ } ', // Extends and implements are not preserved ATM
                        'FOREACH($a AS $b) { /**/ } ',
-                       '$a++ AND $b++',
-                       'DO { /**/ } WHILE while ($a++ AND $b++)');
+                       '$a++ AND $b++');
 
 $expected_not = array('class x2 { /**/ } ', 
                       'foreach($a2 as $b2) { /**/ }');
