fbafe87e3a070dc1a4ffcba1a653fe29c7efa1e8
Damien Seguy <damien.seguy@gmail.com>
2016-08-30 10:43:02 +0200

    Fixed UT

--- tests/analyzer/exp/Classes/UnresolvedClasses.02.php
+++ tests/analyzer/exp/Classes/UnresolvedClasses.02.php
@@ -2,12 +2,13 @@
 
 $expected     = array( 'BAX( )',
                        'F\ABX( )',
-                      'E2\ABX( )', 
+
+);
+
+$expected_not = array('E2\ABX( )', 
                       'A2\B2\ABX( )', 
                       'A\B\ABX( )',
                       'E\ABX( )'
 );
-
-$expected_not = array( );
 
 ?>
