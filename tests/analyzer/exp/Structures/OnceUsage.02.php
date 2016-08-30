3ee8aba9e5a61a3db7ef5d12b7023dc9e2ab6883
Damien Seguy <damien.seguy@gmail.com>
2016-08-30 09:49:23 +0200

    Fixed UT

--- tests/analyzer/exp/Structures/OnceUsage.02.php
+++ tests/analyzer/exp/Structures/OnceUsage.02.php
@@ -1,7 +1,7 @@
 <?php
 
 $expected     = array('require_once CONSTANTE', 
-                      'include_once \'include.php\'');
+                      'include_once \'include.php\' or die  ');
 
 $expected_not = array('include $include;');
 
