a2420ccd241a5c1f1dfdcc769034e6e45b013143
Damien Seguy <damien.seguy@gmail.com>
2016-09-02 16:42:59 +0200

    Added one UT

--- tests/analyzer/Test/Arrays/IsModified.php
+++ tests/analyzer/Test/Arrays/IsModified.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Arrays_IsModified extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testArrays_IsModified01()  { $this->generic_test('Arrays_IsModified.01'); }
+    public function testArrays_IsModified02()  { $this->generic_test('Arrays/IsModified.02'); }
 }
 ?>
