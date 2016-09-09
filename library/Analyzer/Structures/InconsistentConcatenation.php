72c13ddb455780a9ae7de67a616b7c03f7b8f6af
Damien Seguy <damien.seguy@gmail.com>
2016-09-09 10:53:21 +0200

    Removed comment

--- library/Analyzer/Structures/InconsistentConcatenation.php
+++ library/Analyzer/Structures/InconsistentConcatenation.php
@@ -28,7 +28,6 @@ use Analyzer;
 class InconsistentConcatenation extends Analyzer\Analyzer {
     public function analyze() {
         $this->atomIs('Concatenation')
-//             ->filter('it.out("CONCAT").filter{it.atom in ["Variable", "Array", "Functioncall", "Property", "Methodcall", "Staticmethodcall", "Staticconsant", "Staticproperty"]}.any()')
              ->raw('where( __.out("CONCAT").hasLabel("Variable", "Array", "Functioncall", "Property", "Methodcall", "Staticmethodcall", "Staticconsant", "Staticproperty").count().is(neq(0)) )')
              ->outIs('CONCAT')
              ->atomIs('String')
