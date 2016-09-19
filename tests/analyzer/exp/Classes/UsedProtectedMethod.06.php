6e7fbb3a8a75ede3ba00a19b5cffd5d9efd1198b
Damien Seguy <damien.seguy@gmail.com>
2016-09-19 21:39:33 +0200

    Check that Protected Methods are being used.

---
+++ tests/analyzer/exp/Classes/UsedProtectedMethod.06.php
@@ -0,0 +1,14 @@
+<?php
+
+$expected     = array('protected function ma31( ) { /**/ } ',
+                      'protected function ma2( ) { /**/ } ',
+                      'protected function ma32( ) { /**/ } ',
+);
+
+$expected_not = array('protected function unused( ) { /**/ } ',
+                      'protected function ma321( ) { /**/ } ',
+                      'private function pma1( ) { /**/ } ',
+                      'public function puma1( ) { /**/ } ',
+                      );
+
+?>
