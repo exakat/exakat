4fd62d7a39dd1950e0148d3e222f00025b2073e4
Damien Seguy <damien.seguy@gmail.com>
2016-09-20 16:04:00 +0200

    Added edge cases in for Try

---
+++ tests/tokenizer/source/_Try.17.php
@@ -0,0 +1,10 @@
+<?php
+
+
+     function B() {
+        foreach ($b as $c)
+            try {
+                $d->C->D();
+                $d->C->F();
+            } catch(G $e){ }
+    }
---
+++ tests/tokenizer/source/_Try.18.php
@@ -0,0 +1,10 @@
+<?php
+
+
+     function B() {
+        for ($b = 0; $c < 10; $c++)
+            try {
+                $d->C->D();
+                $d->C->F();
+            } catch(G $e){ }
+    }
