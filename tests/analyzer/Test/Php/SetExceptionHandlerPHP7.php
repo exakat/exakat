0c1e4075210f86b27960e6b9227103d93c55b3b6
Damien Seguy <damien.seguy@gmail.com>
2016-09-28 17:06:12 +0200

    Upgraded analyser to handle non-literal strings

--- tests/analyzer/Test/Php/SetExceptionHandlerPHP7.php
+++ tests/analyzer/Test/Php/SetExceptionHandlerPHP7.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Php_SetExceptionHandlerPHP7 extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testPhp_SetExceptionHandlerPHP701()  { $this->generic_test('Php/SetExceptionHandlerPHP7.01'); }
+    public function testPhp_SetExceptionHandlerPHP702()  { $this->generic_test('Php/SetExceptionHandlerPHP7.02'); }
 }
 ?>
