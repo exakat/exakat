f767e6743790066f3fe34022a175747743c11a4b
Damien Seguy <damien.seguy@gmail.com>
2016-09-30 08:02:56 +0300

    Added support for throwable

--- tests/analyzer/Test/Classes/UnresolvedCatch.php
+++ tests/analyzer/Test/Classes/UnresolvedCatch.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Classes_UnresolvedCatch extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testClasses_UnresolvedCatch01()  { $this->generic_test('Classes_UnresolvedCatch.01'); }
+    public function testClasses_UnresolvedCatch02()  { $this->generic_test('Classes/UnresolvedCatch.02'); }
 }
 ?>
