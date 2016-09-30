49dad341c0c11c4b3b5f805c0b942234af93f978
Damien Seguy <damien.seguy@gmail.com>
2016-09-30 09:57:28 +0300

    Added special case for () and ?  :; Added support for +/- 0

---
+++ tests/analyzer/exp/Structures/AddZero.03.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('$a->b += 0',
+                      '$a->b += -0',
+                      '$a->b += +0',
+);
+
+$expected_not = array('$a->b += (isset($r[0]->total->{1}) ? $r[0]->total->{1} : 0)',
+                      '$a->b += isset($r[0]->total->{1}) ? $r[0]->total->{1} : 0'
+);
+
+?>
