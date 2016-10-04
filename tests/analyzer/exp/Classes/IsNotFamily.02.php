3f821b38887b8c29b1d77b640a0da582273bbe13
Damien Seguy <damien.seguy@gmail.com>
2016-10-04 11:35:20 +0200

    Removed non-limited repeat. Handle cases where 2 classes are defined the same way.

--- tests/analyzer/exp/Classes/IsNotFamily.02.php
+++ tests/analyzer/exp/Classes/IsNotFamily.02.php
@@ -1,13 +1,14 @@
 <?php
 
-$expected     = array( '\A2::f( )', 
-                       'A1::f( )', 
-                       'C::f( )',
+$expected     = array( 'A1::f( )', 
+                       '\A2::f( )', 
+                       '\C::f( )',
                        'B::f( )',
-                       'A1::f( )', 
-                       '\A1::f( )'
 );
 
-$expected_not = array();
+$expected_not = array( 'A1::f( )', 
+                       '\A1::f( )',
+
+);
 
 ?>
---
+++ tests/analyzer/exp/Classes/IsNotFamily.06.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('c::notAClass( )');
+
+$expected_not = array('a::inFamilyb( )',
+                      'a::inFamilya( )',
+                      'a::notDefined( )');
+
+?>
