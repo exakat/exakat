661732e60f8c17123524eca6f58a7a9e4f6b7b2d
Damien Seguy <damien.seguy@gmail.com>
2016-10-05 11:45:10 +0200

    Excluded include from 'undefined functions

--- tests/analyzer/Test/Functions/UndefinedFunctions.php
+++ tests/analyzer/Test/Functions/UndefinedFunctions.php
@@ -8,10 +8,11 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Functions_UndefinedFunctions extends Analyzer {
-    /* 3 methods */
+    /* 4 methods */
 
     public function testFunctions_UndefinedFunctions01()  { $this->generic_test('Functions_UndefinedFunctions.01'); }
     public function testFunctions_UndefinedFunctions02()  { $this->generic_test('Functions/UndefinedFunctions.02'); }
     public function testFunctions_UndefinedFunctions03()  { $this->generic_test('Functions/UndefinedFunctions.03'); }
+    public function testFunctions_UndefinedFunctions04()  { $this->generic_test('Functions/UndefinedFunctions.04'); }
 }
 ?>
