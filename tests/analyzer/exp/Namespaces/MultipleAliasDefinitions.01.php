546c8ce2d38079c9139d2b792ff774f21ae83b97
Damien Seguy <damien.seguy@gmail.com>
2016-10-07 17:21:27 +0200

    Fixed UT

--- tests/analyzer/exp/Namespaces/MultipleAliasDefinitions.01.php
+++ tests/analyzer/exp/Namespaces/MultipleAliasDefinitions.01.php
@@ -1,7 +1,9 @@
 <?php
 
-$expected     = array();
+$expected     = array('use a\d as D', 
+                      'use d\d', 
+                      'use a\e as D');
 
-$expected_not = array();
+$expected_not = array('use a\b as C');
 
 ?>
