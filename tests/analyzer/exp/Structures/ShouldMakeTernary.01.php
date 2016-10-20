9509bea9ea7f6dbaaf39d77f7b13d181fb03fa5b
Damien Seguy <damien.seguy@gmail.com>
2016-10-20 22:12:28 +0200

    New suggestion to use a ternary, instead of a if/then

---
+++ tests/analyzer/exp/Structures/ShouldMakeTernary.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('if($a >= 8)  /**/  else  /**/  ', 
+                      'if($a > 7) { /**/ } else { /**/ } ', 
+                      'if($a == 3) { /**/ } else { /**/ } ', 
+                      'if($a > 5) { /**/ } else { /**/ } ', 
+                      'if($a > 6) { /**/ } else { /**/ } '
+);
+
+$expected_not = array('if($a != 4) { /**/ } else { /**/ } ', );
+
+?>
