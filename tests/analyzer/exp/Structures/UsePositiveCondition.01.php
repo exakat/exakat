e4f272c958148e34cbff02b45f32e285bdc2a25a
Damien Seguy <damien.seguy@gmail.com>
2016-10-31 11:51:55 +0100

    New analysis : use positive conditions

---
+++ tests/analyzer/exp/Structures/UsePositiveCondition.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('if($a4 !== 3) { /**/ } else { /**/ } ');
+
+$expected_not = array('if($a) { /**/ } ',
+                      'if($a2) { /**/ } else { /**/ } ',
+                      'if($a3 !== 3) { /**/ } else { /**/ } ');
+
+?>
---
+++ tests/analyzer/exp/Structures/UsePositiveCondition.02.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('if(!customCall($a3)) { /**/ } else { /**/ } ');
+
+$expected_not = array('if(!empty($a)) { /**/ } ',
+                      'if(empty($a2)) { /**/ } else { /**/ } ');
+
+?>
