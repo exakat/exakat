ef11477a46005c4d7da2e6d8c3c70f502ed15262
Damien Seguy <damien.seguy@gmail.com>
2016-10-31 10:48:04 +0100

    Fixed Test

--- tests/analyzer/Test/Structures/OneLineTwoInstructions.php
+++ tests/analyzer/Test/Structures/OneLineTwoInstructions.php
@@ -8,11 +8,12 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_OneLineTwoInstructions extends Analyzer {
-    /* 4 methods */
+    /* 5 methods */
 
     public function testStructures_OneLineTwoInstructions01()  { $this->generic_test('Structures_OneLineTwoInstructions.01'); }
     public function testStructures_OneLineTwoInstructions02()  { $this->generic_test('Structures_OneLineTwoInstructions.02'); }
     public function testStructures_OneLineTwoInstructions03()  { $this->generic_test('Structures_OneLineTwoInstructions.03'); }
     public function testStructures_OneLineTwoInstructions04()  { $this->generic_test('Structures_OneLineTwoInstructions.04'); }
+    public function testStructures_OneLineTwoInstructions05()  { $this->generic_test('Structures/OneLineTwoInstructions.05'); }
 }
 ?>
---
+++ tests/analyzer/exp/Structures/OneLineTwoInstructions.05.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('$d = "e" . $f . ""',
+                      '<?= $K', 
+                      'echo $j'
+);
+
+$expected_not = array('echo $g',
+                      'echo $h', 
+                      'echo $hi', 
+);
+
+?>
