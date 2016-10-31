43041dbaa28ac54a646f0d51d4cd7273d703b3fc
Damien Seguy <damien.seguy@gmail.com>
2016-10-31 12:04:17 +0100

    New analysis : drop else after return

---
+++ tests/analyzer/exp/Structures/DropElseAfterReturn.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('if($a) { /**/ } else { /**/ } ',
+                      'if($b) { /**/ } else { /**/ } ');
+
+$expected_not = array('if($a2) { /**/ } else { /**/ } ',
+                      'if($a3) { /**/ } else { /**/ } ');
+
+?>
