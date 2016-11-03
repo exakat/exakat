701b5f4eb7ff8a269c889ff0525835ee7337bccb
Damien Seguy <damien.seguy@gmail.com>
2016-11-03 18:39:20 +0100

    Removed useless property

--- library/Exakat/Reports/Ambassador.php
+++ library/Exakat/Reports/Ambassador.php
@@ -31,7 +31,6 @@ use Exakat\Reports\Reports;
 
 class Ambassador extends Reports {
 
-    protected $dump            = null; // Dump.sqlite
     protected $analyzers       = array(); // cache for analyzers [Title] = object
     protected $projectPath     = null;
     protected $finalName       = null;
