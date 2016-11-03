d3570f82ebd7c0dcff68737a374e0203c9e993da
Damien Seguy <damien.seguy@gmail.com>
2016-11-03 10:03:04 +0100

    New analyzer : prefere ::class when possible.

---
+++ tests/analyzer/exp/Classes/UseClassOperator.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('\x',
+                      '\X',
+                      'x',
+);
+
+$expected_not = array('"x$a"',
+                      '\\a');
+
+?>
---
+++ tests/analyzer/exp/Classes/UseClassOperator.02.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array("'a\b\c\x'",
+                      "'\a\b\c\x'",
+);
+
+$expected_not = array('"x$a"',
+                      '\\a',
+                      '\x',
+                      '\X',
+                      'x',);
+
+?>
