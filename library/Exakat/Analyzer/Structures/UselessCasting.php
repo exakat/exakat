02efe9613de02b4851122b5094e1780fcde49d89
Damien Seguy <damien.seguy@gmail.com>
2016-11-07 12:59:13 +0100

    New analyzer to spot useless casting

---
+++ library/Exakat/Analyzer/Structures/UselessCasting.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright 2012-2016 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Analyzer\Structures;
+
+use Exakat\Analyzer\Analyzer;
+use Exakat\Data\Methods;
+
+class UselessCasting extends Analyzer {
+    public function analyze() {
+        $casts = ['T_STRING_CAST'  => 'string',
+                  'T_BOOL_CAST'    => 'bool',
+                  'T_INT_CAST'     => 'int',
+                  'T_ARRAY_CAST'   => 'array',
+                  'T_DOUBLE_CAST'  => 'real'
+                  ];
+        
+        $data = new Methods();
+        $returnTypes = $data->getFunctionsByReturn();
+        
+        foreach($casts as $token => $type) {
+            $this->atomIs('Cast')
+                 ->tokenIs($token)
+                 ->outIs('CAST')
+                 ->atomIs('Functioncall')
+                 ->fullnspathIs($returnTypes[$type])
+                 ->back('first');
+            $this->prepareQuery();
+        }
+    }
+}
+
+?>
