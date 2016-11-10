13af311cf59feae9d4a3d82dc897a6fe086795b0
Damien Seguy <damien.seguy@gmail.com>
2016-11-10 10:50:02 +0100

    Spot useless Checks

---
+++ library/Exakat/Analyzer/Structures/UselessCheck.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Copyright 2012-2016 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Analyzer\Structures;
+
+use Exakat\Analyzer\Analyzer;
+
+class UselessCheck extends Analyzer {
+    public function analyze() {
+        //    if (count($anArray) > 0){    foreach ($anArray as $el){
+        $this->atomIs('Ifthen')
+             ->hasNoOut('ELSE')
+             ->outIs('CONDITION')
+             ->atomInside('Functioncall')
+             // count($a) > 0, sizeof($a) != 0, !empty($a)
+             ->fullnspathIs(array('\\count', '\\sizeof', '\\empty'))
+             ->outIs('ARGUMENTS')
+             ->outWithRank('ARGUMENT', 0)
+             ->savePropertyAs('fullcode', 'var')
+             ->back('first')
+             ->outIs('THEN')
+             ->is('count', 1)
+             ->outWithRank('ELEMENT', 0)
+             ->atomIs('Foreach')
+             ->outIs('SOURCE')
+             ->samePropertyAs('fullcode', 'var')
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/source/Structures/UselessCheck.02.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!empty($a)){    
+    foreach ($a as $c){
+    }
+}
+
+if (sizeof($a->b) != 0){    
+    $c++;
+    foreach ($a->b as $c){
+    }
+}
+
+if (count(B::$a) > 0)   
+    foreach (B::$a as $c){
+    }
+
+// Nope, not only foreach
+if (sizeof($a->b) > 0) {
+    $c++;
+    foreach ($a->b as $c){
+    }
+}
+
+// Nope, has ELSE
+if (count($a[3]) > 0){    
+    foreach ($a[3] as $c){
+    }
+} else {
+    $d++;
+}
+
+
+?>
