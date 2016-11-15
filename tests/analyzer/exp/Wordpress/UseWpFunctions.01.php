07eccd92ef8c5a066fe567c81859a529134b3dba
Damien Seguy <damien.seguy@gmail.com>
2016-11-15 14:42:36 +0100

    New analysis for Wordpress

---
+++ tests/analyzer/exp/Wordpress/UseWpFunctions.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array("mail('a', 'b', 'c')",
+                      "die('d')",
+                      "exit('e')",
+                      "header('f')");
+
+$expected_not = array('$a->rand( )',
+                      "A::{'b' . 'c'}( )");
+);
+
+?>
---
+++ tests/analyzer/source/Wordpress/UseWpFunctions.01.php
@@ -0,0 +1,10 @@
+<?php
+
+mail('a', 'b', 'c');
+die('d');
+exit('e');
+header('f');
+$a->rand();
+A::{'b'.'c'}();
+
+?>
