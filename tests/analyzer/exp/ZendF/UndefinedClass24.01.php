6010d82978bdc8f4f7300f8ac8f399c025659069
Damien Seguy <damien.seguy@gmail.com>
2016-11-25 17:39:07 +0100

    Spot undefined ZF 2.4 classes

---
+++ tests/analyzer/exp/ZendF/UndefinedClass24.01.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array('Zend\Cache\Service\StorageAdapterPluginManagerFactory');
+
+$expected_not = array('Zend\Authentication\Adapter\DbTable\CredentialTreatmentAdapter');
+
+?>
---
+++ tests/analyzer/source/ZendF/UndefinedClass24.01.php
@@ -0,0 +1,8 @@
+<?php
+// 2.3 class
+$a = new Zend\Authentication\Adapter\DbTable\AbstractAdapter();
+
+// Not a 2.3 class
+$b = $d instanceof Zend\Cache\Service\StorageAdapterPluginManagerFactory;
+
+?>
