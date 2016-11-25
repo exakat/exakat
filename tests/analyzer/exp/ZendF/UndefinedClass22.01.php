caeb863cb93d993beba2b29b6306afd56cb25cc6
Damien Seguy <damien.seguy@gmail.com>
2016-11-25 17:34:04 +0100

    Undefined classes ZF 2.2

---
+++ tests/analyzer/exp/ZendF/UndefinedClass22.01.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('Zend\Authentication\Adapter\Callback');
+
+$expected_not = array('Zend\Authentication\Adapter\DbTable\AbstractAdapter',
+                      'Not\Zend\Authentication\Adapter\DbTable\AbstractAdapter',
+                      'Not\Zend\Authentication\Adapter\Callback');
+?>
