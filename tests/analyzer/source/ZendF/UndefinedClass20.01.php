ddfc8da8d58138e0e0b273281513b7a2fa15f297
Damien Seguy <damien.seguy@gmail.com>
2016-11-25 17:05:24 +0100

    Spot classes that are not in ZF2.0

---
+++ tests/analyzer/source/ZendF/UndefinedClass20.01.php
@@ -0,0 +1,12 @@
+<?php
+
+// 2.0 only class
+$a = new Zend\Authentication\Adapter\Digest();
+
+// Not a 2.0 class (2.1+)
+$b = $d instanceof Zend\Authentication\Adapter\Callback;
+
+$c = new Not\Zend\Authentication\Adapter\Callback();
+$c = new Not\Zend\Code\Scanner\VariableScanner;
+
+?>
