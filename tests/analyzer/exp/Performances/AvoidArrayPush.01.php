d862e8b091f27b722a72a89454c358f6aebd4bea
Damien Seguy <damien.seguy@gmail.com>
2016-11-29 09:23:58 +0100

    New analysis : avoid array_push

---
+++ tests/analyzer/exp/Performances/AvoidArrayPush.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('array_push($a, 4)',
+                      'array_push($a, 5,6,7)'
+);
+
+$expected_not = array('$a[] = 4');
+
+?>
---
+++ tests/analyzer/source/Performances/AvoidArrayPush.01.php
@@ -0,0 +1,12 @@
+$a = [1,2,3];
+// Fast version
+$a[] = 4;
+
+$a[] = 5;
+$a[] = 6;
+$a[] = 7;
+$count = count($a);
+
+// Slow version
+array_push($a, 4);
+$count = array_push($a, 5,6,7);
