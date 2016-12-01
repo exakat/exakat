577ea72ebed1264ad82096c03b7257af29409391
Damien Seguy <damien.seguy@gmail.com>
2016-12-01 20:37:33 +0100

    Report usage of $GLOBALS or global

---
+++ tests/analyzer/source/Php/GlobalsVsGlobal.03.php
@@ -0,0 +1,21 @@
+<?php
+
+global $a;
+
+$GLOBALS['a'] = 2;
+$GLOBALS['ab'] = 2;
+$GLOBALS['ac'] = 2;
+$GLOBALS['ad'] = 2;
+$GLOBALS['abd'] = 2;
+$GLOBALS['acd'] = 2;
+$GLOBALS['ae'] = 2;
+$GLOBALS['aeb'] = 2;
+$GLOBALS['ace'] = 2;
+$GLOBALS['aeb'] = 2;
+$GLOBALS['ace'] = 2;
+$GLOBALS['aeb'] = 2;
+$GLOBALS['ace'] = 2;
+
+$c = $_POST[1];
+
+?>
