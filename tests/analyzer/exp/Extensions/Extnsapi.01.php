9e1e1447bd1c4849ca63c0c5e507838140e45ad7
Damien Seguy <damien.seguy@gmail.com>
2016-12-05 10:18:03 +0100

    Fixed UT for Nsapi

--- tests/analyzer/exp/Extensions/Extnsapi.01.php
+++ tests/analyzer/exp/Extensions/Extnsapi.01.php
@@ -1,7 +1,7 @@
 <?php
 
 $expected     = array('nsapi_request_headers( )',
-                      'nsapi.read_timeout');
+                      '\'nsapi.read_timeout\'');
 
 $expected_not = array();
 
