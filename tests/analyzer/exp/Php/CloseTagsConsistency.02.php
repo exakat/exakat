26f81a9288696194962db759c80a79cb7270eaa7
Damien Seguy <damien.seguy@gmail.com>
2016-12-12 12:44:33 +0100

    New analyser for preferences : closing tag consistency

---
+++ tests/analyzer/exp/Php/CloseTagsConsistency.02.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array('./tests/analyzer/source/Php/CloseTagsConsistency.02.php/b20.php');
+
+$expected_not = array('./tests/analyzer/source/Php/CloseTagsConsistency.02.php/a1.php');
+
+?>
---
+++ tests/analyzer/exp/Php/CloseTagsConsistency.04.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array('./tests/analyzer/source/Php/CloseTagsConsistency.04.php/b20.php');
+
+$expected_not = array('./tests/analyzer/source/Php/CloseTagsConsistency.04.php/a1.php');
+
+?>
