9558c1836045cc9ef5f76adf537eb4b4fd9bc148
Damien Seguy <damien.seguy@gmail.com>
2016-12-19 12:00:08 +0100

    Added log mark for literals

--- library/Exakat/Tasks/LoadFinal.php
+++ library/Exakat/Tasks/LoadFinal.php
@@ -286,7 +286,7 @@ GREMLIN;
             display('Link constant definitions : skipping.');
         }
         
-        //display('Mark constants expressions');
+        display('Mark literal expressions as constants');
         $query = <<<GREMLIN
 g.V().hasLabel("Integer", "Boolean", "Real", "Null", "Void", "InlineHtml", "Magicconstant", "Staticconstant", "Void")
      .sideEffect{ it.get().property("constant", true); }
