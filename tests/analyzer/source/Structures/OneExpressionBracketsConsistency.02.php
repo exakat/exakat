2e0110734637bc105b9c478f1424203e05c72b22
Damien Seguy <damien.seguy@gmail.com>
2017-01-04 19:23:08 +0100

    Added new preference {} or not around one expressions

---
+++ tests/analyzer/source/Structures/OneExpressionBracketsConsistency.02.php
@@ -0,0 +1,15 @@
+<?php
+
+foreach($a2 as $b)  $c++; 
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+
+?>
---
+++ tests/analyzer/source/Structures/OneExpressionBracketsConsistency.03.php
@@ -0,0 +1,24 @@
+<?php
+
+foreach($a2 as $b)  $c++; 
+foreach($a2 as $b)  $c++; 
+foreach($a2 as $b)  $c++; 
+foreach($a2 as $b)  $c++; 
+foreach($a2 as $b)  $c++; 
+foreach($a2 as $b)  $c++; 
+foreach($a2 as $b)  $c++; 
+foreach($a2 as $b)  $c++; 
+foreach($a2 as $b)  $c++; 
+foreach($a2 as $b)  $c++; 
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+foreach($a1 as $b) { $c++; }
+
+?>
