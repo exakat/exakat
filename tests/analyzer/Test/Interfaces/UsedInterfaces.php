a327cabf1bfda08b1d18676657e043f2a256b843
Damien Seguy <damien.seguy@gmail.com>
2017-01-23 11:02:41 +0100

    Added support for interface i extends j

--- tests/analyzer/Test/Interfaces/UsedInterfaces.php
+++ tests/analyzer/Test/Interfaces/UsedInterfaces.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Interfaces_UsedInterfaces extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testInterfaces_UsedInterfaces01()  { $this->generic_test('Interfaces_UsedInterfaces.01'); }
+    public function testInterfaces_UsedInterfaces02()  { $this->generic_test('Interfaces/UsedInterfaces.02'); }
 }
 ?>
