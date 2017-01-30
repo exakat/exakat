714a838eec2338f6f148c45e2e7e4827f33e8eb4
Damien Seguy <damien.seguy@gmail.com>
2017-01-30 16:26:28 +0100

    Added first PHP 7.2 deprecated function

---
+++ tests/analyzer/exp/Php/Php72RemovedFunctions.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('png2wbmp($image)',
+                      'jpeg2wbmp($image)',
+);
+
+$expected_not = array('strlen($a)',
+                      '$a->png2wbmp($method)',
+                      'A::jpeg2wbmp($static)'
+);
+
+?>
