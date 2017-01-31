fd155b032171813edf2ffb8a3b957f524e73fc69
Damien Seguy <damien.seguy@gmail.com>
2017-01-31 21:20:56 +0100

    Recommend wpdb prepare for any query with variable

---
+++ tests/analyzer/Test/Wordpress/WpdbPrepareForVariables.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test;
+
+include_once(dirname(dirname(dirname(dirname(__DIR__)))).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_test');
+spl_autoload_register('Autoload::autoload_phpunit');
+spl_autoload_register('Autoload::autoload_library');
+
+class Wordpress_WpdbPrepareForVariables extends Analyzer {
+    /* 2 methods */
+
+    public function testWordpress_WpdbPrepareForVariables01()  { $this->generic_test('Wordpress/WpdbPrepareForVariables.01'); }
+    public function testWordpress_WpdbPrepareForVariables02()  { $this->generic_test('Wordpress/WpdbPrepareForVariables.02'); }
+}
+?>
