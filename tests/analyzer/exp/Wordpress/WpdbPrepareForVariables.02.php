8a1347fc4bd6c9f47bc6f74f521a4db0e2feaf8f
Damien Seguy <damien.seguy@gmail.com>
2017-02-01 17:40:54 +0100

    Fixed UT

--- tests/analyzer/exp/Wordpress/WpdbPrepareForVariables.02.php
+++ tests/analyzer/exp/Wordpress/WpdbPrepareForVariables.02.php
@@ -1,12 +1,12 @@
 <?php
 
 $expected     = array('$wpdb->get_col(\'SELECT * FROM table WHERE id = "\' . $x1 . \'"\')', 
-                      '$wpdb->get_row(<<<SQL
+ '$wpdb->get_results("SELECT * FROM {$wpdb->prefix}table WHERE id = \'$x\'")', 
+ '$wpdb->get_row("SELECT * FROM table WHERE id = \'$x2\'")', 
+ '$wpdb->get_var(<<<SQL
 SELECT * FROM table WHERE id = \'$x3\'
 SQL
-)', 
-                       '$wpdb->get_var("SELECT * FROM {$wpdb->prefix}table WHERE id = \'$x\'")', 
-                       '$wpdb->get_results("SELECT * FROM table WHERE id = \'$x2\'")');
+)');
 
 $expected_not = array('$wpdb->get_results("SELECT * FROM {$wpdb->prefix}table WHERE id = \'$x\'")',
                       '$wpdb->get_results("SELECT * FROM {$wpdb->prefix}table WHERE id = 1")',
