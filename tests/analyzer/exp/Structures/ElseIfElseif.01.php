bdedeb87642109f25fbbcba0f9348015a5b8d05a
Damien Seguy <damien.seguy@gmail.com>
2017-02-07 10:56:12 +0100

    Upgraded values in test

--- tests/analyzer/exp/Structures/ElseIfElseif.01.php
+++ tests/analyzer/exp/Structures/ElseIfElseif.01.php
@@ -4,6 +4,9 @@ $expected     = array( 'if(21) { /**/ } elseif(31) { /**/ } else { /**/ } ',
                        'if(20) { /**/ } elseif(30) { /**/ } else { /**/ } '
 );
 
-$expected_not = array();
+$expected_not = array('if(10) { /**/ } else { /**/ } ', 
+                      'if(11) { /**/ } else { /**/ } ', 
+                      'if(12) { /**/ } else { /**/ } ', 
+                       );
 
 ?>
