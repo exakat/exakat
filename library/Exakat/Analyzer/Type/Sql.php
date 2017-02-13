cfb15b38adfce9283d8d0504e8ee27085b4b74f0
Damien Seguy <damien.seguy@gmail.com>
2017-02-13 18:58:16 +0100

    New inventory for SQL queries

---
+++ library/Exakat/Analyzer/Type/Sql.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright 2012-2017 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Analyzer\Type;
+
+use Exakat\Analyzer\Analyzer;
+
+class Sql extends Analyzer {
+    public function analyze() {
+        $sqlKeywords = $this->loadIni('sqlKeywords.ini', 'keywords');
+        $delimiter = '[\\"'."\\\'".']';
+        $regex = '\\\\s*('.implode('|', $sqlKeywords).')';
+        
+        // SQL in a literal 'SELECT col FROM table';
+        $this->atomIs('String')
+             ->regexIs('fullcode', $delimiter.$regex)
+             ->inIsIE('CONCAT');
+        $this->prepareQuery();
+
+        // SQL in a literal 'SELECT col FROM '.$table;
+        $this->atomIs('Concatenation')
+             ->regexIs('fullcode', $delimiter.$regex);
+        $this->prepareQuery();
+
+        // SQL in a literal 'SELECT col FROM '.$table;
+        $this->atomIs('Concatenation')
+             ->outWithRank('CONCAT', 0)
+             ->regexIs('noDelimiter', $regex);
+        $this->prepareQuery();
+    }
+}
+
+?>
