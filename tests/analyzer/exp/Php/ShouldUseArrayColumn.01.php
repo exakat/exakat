58306e802c0a370d298dd09932d65cddae08b18b
Damien Seguy <damien.seguy@gmail.com>
2017-02-21 11:09:25 +0100

    New analyzer : suggest usage for array_column

---
+++ tests/analyzer/exp/Php/ShouldUseArrayColumn.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('foreach($a as $k => $v) { /**/ } ', 
+                      'foreach($f->g as $h1) { /**/ } ', 
+                      'foreach($f->g as $h0) { /**/ } '
+);
+
+$expected_not = array('foreach($f->g as $h2) { /**/ } ', );
+
+?>
