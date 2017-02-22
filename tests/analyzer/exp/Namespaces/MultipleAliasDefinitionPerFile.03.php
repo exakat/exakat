6fb90c023a8c9e4440418c7292f5041106e5edcf
Damien Seguy <damien.seguy@gmail.com>
2017-02-22 12:48:19 +0100

    New analyzer : multiple Aliases per class

---
+++ tests/analyzer/exp/Namespaces/MultipleAliasDefinitionPerFile.03.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('EB as B2', 
+                      'EB as C2',
+                      'EA as C2', 
+                      'EA as B2', 
+);
+
+$expected_not = array('DA',
+                      'DB'
+);
+
+?>
---
+++ tests/analyzer/exp/Namespaces/MultipleAliasDefinitionPerFile.05.php
@@ -0,0 +1,16 @@
+<?php
+
+$expected     = array('E as B2', 
+                      'E as C2', 
+                      'A as B3', 
+                      'A as C3', 
+                      'A', 
+                      'F as B4', 
+                      'F as C4', 
+                      'F as W4', 
+                      'F');
+
+$expected_not = array('D',
+                      'D2 as D3');
+
+?>
