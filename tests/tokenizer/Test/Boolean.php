bf81ae117fe5593d30c84fb4dd5d78b5d3845ead
Damien Seguy <damien.seguy@gmail.com>
2017-02-27 11:38:31 +0100

    Added test for Function named after boolean.

--- tests/tokenizer/Test/Boolean.php
+++ tests/tokenizer/Test/Boolean.php
@@ -7,11 +7,12 @@ spl_autoload_register('Autoload::autoload_test');
 spl_autoload_register('Autoload::autoload_phpunit');
 
 class Boolean extends Tokenizer {
-    /* 5 methods */
+    /* 6 methods */
     public function testBoolean01()  { $this->generic_test('Boolean.01'); }
     public function testBoolean02()  { $this->generic_test('Boolean.02'); }
     public function testBoolean03()  { $this->generic_test('Boolean.03'); }
     public function testBoolean04()  { $this->generic_test('Boolean.04'); }
     public function testBoolean05()  { $this->generic_test('Boolean.05'); }
+    public function testBoolean06()  { $this->generic_test('Boolean.06'); }
 }
 ?>
