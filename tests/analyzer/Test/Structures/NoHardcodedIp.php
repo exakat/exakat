618d250060affed0fb1d0a2db1283d7483a371b5
Damien Seguy <damien.seguy@gmail.com>
2017-02-27 17:46:48 +0100

    Extra UT for hardcoded IP v6

--- tests/analyzer/Test/Structures/NoHardcodedIp.php
+++ tests/analyzer/Test/Structures/NoHardcodedIp.php
@@ -8,9 +8,10 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_NoHardcodedIp extends Analyzer {
-    /* 2 methods */
+    /* 3 methods */
 
     public function testStructures_NoHardcodedIp01()  { $this->generic_test('Structures_NoHardcodedIp.01'); }
     public function testStructures_NoHardcodedIp02()  { $this->generic_test('Structures/NoHardcodedIp.02'); }
+    public function testStructures_NoHardcodedIp03()  { $this->generic_test('Structures/NoHardcodedIp.03'); }
 }
 ?>
