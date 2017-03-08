a147363b4c866514330a436f2af286407db584ce
Damien Seguy <damien.seguy@gmail.com>
2017-03-08 11:40:27 +0100

    Spot strange Constant names

---
+++ tests/analyzer/source/Constants/StrangeName.01.php
@@ -0,0 +1,9 @@
+<?php
+
+// This code looks OK : DIRECTORY_SEPARATOR is a native PHP constant
+$path = $path . DIRECTORY_SEPARATOR . $file;
+
+// Strange name DIRECOTRY_SEPARATOR
+$path = $path . DIRECOTRY_SEPARATOR . $file;
+
+?>
