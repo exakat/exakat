5caeeb421207cc9c20e9d01480d96a4b1e55ee45
Damien Seguy <damien.seguy@gmail.com>
2017-03-13 16:12:17 +0100

    New analyzer : too many findBy*

---
+++ tests/analyzer/source/Classes/TooManyFinds.02.php
@@ -0,0 +1,28 @@
+<?php
+
+// quite a fishy class
+abstract class User {
+    public abstract function findByEmail($email);
+    public abstract function findByUsername($username);
+    public abstract function findByFirstName($firstname);
+    public abstract function findByLastName($lastname);
+    public abstract function findByName($name);
+    public abstract function findById($id);
+
+    public abstract function insert($user);
+    public abstract function update($user);
+}
+
+abstract class UserOK {
+    public abstract function findByEmail($email);
+    public abstract function findByUsername($username);
+    public abstract function findByFirstName($firstname);
+    public abstract function findByLastName($lastname);
+    public abstract function findByName($name);
+    public abstract function findById($id);
+
+    public abstract function insert($user);
+    public abstract function update($user);
+}
+
+?>
---
+++ tests/analyzer/source/Classes/TooManyFinds.03.php
@@ -0,0 +1,81 @@
+<?php
+
+class cfind1 {
+    public function find1() {}
+}
+
+class cfind2 {
+    public function find1() {}
+    public function find2() {}
+}
+
+class cfind3 {
+    public function find1() {}
+    public function find2() {}
+    public function find3() {}
+}
+
+class cfind4 {
+    public function find1() {}
+    public function find2() {}
+    public function find3() {}
+    public function find4() {}
+}
+
+class cfind5 {
+    public function find1() {}
+    public function find2() {}
+    public function find3() {}
+    public function find4() {}
+    public function find5() {}
+}
+
+class cfind6 {
+    public function find1() {}
+    public function find2() {}
+    public function find3() {}
+    public function find4() {}
+    public function find5() {}
+    public function find6() {}
+}
+
+class cfind7 {
+    public function find1() {}
+    public function find2() {}
+    public function find3() {}
+    public function find4() {}
+    public function find5() {}
+    public function find6() {}
+    public function find7() {}
+}
+
+class cfind4G {
+    public function find1() {}
+    public function find2() {}
+    public function find3() {}
+    public function find4() {}
+    public function get5() {}
+    public function get6() {}
+    public function get7() {}
+}
+
+class cfind5G {
+    public function find1() {}
+    public function find2() {}
+    public function find3() {}
+    public function find4() {}
+    public function find5() {}
+    public function get6() {}
+    public function get7() {}
+}
+
+class cfind6G {
+    public function find1() {}
+    public function find2() {}
+    public function find3() {}
+    public function find4() {}
+    public function find5() {}
+    public function find6() {}
+    public function get7() {}
+}
+?>
