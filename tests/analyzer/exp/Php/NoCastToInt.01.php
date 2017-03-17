2d139974b961255b49c49e6f2ae7827d9ecf266c
Damien Seguy <damien.seguy@gmail.com>
2017-03-17 17:46:04 +0100

    New analyzer, from the doc.

---
+++ tests/analyzer/exp/Php/NoCastToInt.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('(int) (0.14 + $c)', 
+                      '(int) (0.13 + 0.7)', 
+                      '(int) (0.12 + 0.3)');
+
+$expected_not = array('(int) 0.1',
+                      '(int) 0.11 + 0.3',
+                      '(int) $o->p');
+
+?>
