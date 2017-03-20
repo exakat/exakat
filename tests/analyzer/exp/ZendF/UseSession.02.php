39b588203c1a90b72f5337b632dec77fd78dacb4
Damien Seguy <damien.seguy@gmail.com>
2017-03-20 17:30:32 +0100

    Fixed UT

---
+++ tests/analyzer/exp/ZendF/UseSession.02.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array('$e->getApplication( )->getServiceManager( )->get(\'Zend\Session\SessionManager\')');
+
+$expected_not = array('$serviceManager->get(\'Zend\Session\NotSessionManager\')');
+
+?>
