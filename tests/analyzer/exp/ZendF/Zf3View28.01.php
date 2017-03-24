e35730ccadec3337e90ad9787559c0d9531614c8
Damien Seguy <damien.seguy@gmail.com>
2017-03-24 11:37:11 +0100

    Added support for Zend-view

---
+++ tests/analyzer/exp/ZendF/Zf3View28.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('Zend\View\Helper\Asset( )',
+                      );
+
+$expected_not = array('Zend\View\NotView',
+                      'Zend\View\Helper\PartialLoop()',
+                      'Zend\View\View',
+                      'Zend\View\Helper\PaginationControl as PaginationControl');
+
+?>
---
+++ tests/analyzer/source/ZendF/Zf3View26.01.php
@@ -0,0 +1,15 @@
+<?php
+
+use Zend\View\Helper\PaginationControl as PaginationControl;
+
+new Zend\View\Helper\Asset();
+
+$ok = new Zend\View\Helper\PartialLoop();
+
+$ok = new Zend\View\View;
+
+$ko = new Zend\View\NotView;
+
+echo PaginationControl::Constante;
+
+?>
