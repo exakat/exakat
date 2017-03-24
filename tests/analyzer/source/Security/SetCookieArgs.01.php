25d2c4adb31ad814445a724b330c0c82d982102f
Damien Seguy <damien.seguy@gmail.com>
2017-03-24 17:12:51 +0100

    More tests

--- tests/analyzer/source/Security/SetCookieArgs.01.php
+++ tests/analyzer/source/Security/SetCookieArgs.01.php
@@ -7,7 +7,7 @@ setcookie($a3, null); // ignored, as unsetting cookie
 
 setcookie($a4, $b4); 
 setcookie($a5, $b5, time() + 3600); 
-setcookie($a6, $b6, time() + 3600, '/',); 
+setcookie($a6, $b6, time() + 3600, '/'); 
 setcookie($a7, $b7, time() + 3600, '/', 'domain.com'); 
 setcookie($a8, $b8, time() + 3600, '/', 'domain.com', $_SERVER['HTTPS']); 
 
