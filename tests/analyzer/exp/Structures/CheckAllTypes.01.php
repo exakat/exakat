430c4903e712bf23d607dc060fe8bd7ed7779269
Damien Seguy <damien.seguy@gmail.com>
2017-03-24 17:10:47 +0100

    New analyzer : spot missing type tests

---
+++ tests/analyzer/exp/Structures/CheckAllTypes.01.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('if(is_array($a)) { /**/ } else { /**/ } ');
+
+$expected_not = array('if(is_callable($a)) { /**/ } else { /**/ } ',
+                      'if(is_string($a)) { /**/ } else { /**/ } ');
+
+?>
