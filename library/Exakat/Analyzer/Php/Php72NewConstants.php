3c05138f898adaa4a340d940a28c7db8a10490e3
Damien Seguy <damien.seguy@gmail.com>
2017-04-10 12:07:43 +0200

    Reports possible collision with constants

---
+++ library/Exakat/Analyzer/Php/Php72NewConstants.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Copyright 2012-2017 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+
+namespace Exakat\Analyzer\Php;
+
+use Exakat\Analyzer\Analyzer;
+use Exakat\Analyzer\Common\ConstantDefinition;
+
+class Php72NewConstants extends ConstantDefinition {
+    protected $phpVersion = '7.2-';
+    
+    public function analyze() {
+        $this->constants = array(
+'PHP_OS_FAMILY',
+'PHP_FLOAT_DIG',
+'PHP_FLOAT_EPSILON',
+'PHP_FLOAT_MAX',
+'PHP_FLOAT_MIN',
+'SQLITE3_DETERMINISTIC',
+'CURLSSLOPT_NO_REVOKE',
+'CURLOPT_DEFAULT_PROTOCOL',
+'CURLOPT_STREAM_WEIGHT',
+'CURLMOPT_PUSHFUNCTION',
+'CURL_PUSH_OK',
+'CURL_PUSH_DENY',
+'CURL_HTTP_VERSION_2TLS',
+'CURLOPT_TFTP_NO_OPTIONS',
+'CURL_HTTP_VERSION_2_PRIOR_KNOWLEDGE',
+'CURLOPT_CONNECT_TO',
+'CURLOPT_TCP_FASTOPEN',
+'DNS_CAA',
+        );
+        parent::analyze();
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Php/Php72NewConstants.01.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('PHP_OS_FAMILY = 2');
+
+$expected_not = array('PHP_FLOAT_DIG = 3',
+                      'PHP_FLOAT_EPSILON = 4');
+
+?>
