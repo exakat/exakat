fa0ef998a8842df57051aee2726ba14a44826cfb
Damien Seguy <damien.seguy@gmail.com>
2017-04-10 12:16:27 +0200

    Added support for new PHP 7.2 functions

---
+++ tests/analyzer/exp/Php/Php72NewFunctions.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array( 'function mb_ord( ) { /**/ } ', 
+                       'function stream_isatty( ) { /**/ } '                          
+);
+
+$expected_not = array( 'function stream_some_function( ) { /**/ } ', 
+                       'function mb_chr( ) { /**/ } ', 
+                       'function mb_scrub( ) { /**/ } ',
+);
+
+?>
