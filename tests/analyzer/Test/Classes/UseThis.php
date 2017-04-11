5575aacc0f350b09c2cd83c23fbd89804c434a3e
Damien Seguy <damien.seguy@gmail.com>
2017-04-11 11:53:25 +0200

    One more test.

--- tests/analyzer/Test/Classes/UseThis.php
+++ tests/analyzer/Test/Classes/UseThis.php
@@ -8,10 +8,11 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Classes_UseThis extends Analyzer {
-    /* 3 methods */
+    /* 4 methods */
 
     public function testClasses_UseThis01()  { $this->generic_test('Classes/UseThis.01'); }
     public function testClasses_UseThis02()  { $this->generic_test('Classes/UseThis.02'); }
     public function testClasses_UseThis03()  { $this->generic_test('Classes/UseThis.03'); }
+    public function testClasses_UseThis04()  { $this->generic_test('Classes/UseThis.04'); }
 }
 ?>
