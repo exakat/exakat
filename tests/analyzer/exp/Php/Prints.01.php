4415d3dcd86fd163be25f240a8f1a9a89e237f8f
Damien Seguy <damien.seguy@gmail.com>
2017-04-12 11:29:48 +0200

    Mark code that output data to stdout

---
+++ tests/analyzer/exp/Php/Prints.01.php
@@ -0,0 +1,14 @@
+<?php
+
+$expected     = array('echo 1',
+                      'print 2',
+                      'print_r(3)',
+                      'debug_print_backtrace( )',
+                      'A'
+):
+
+$expected_not = array('var_export($a, true)',
+                      '$x = strtolower($a)');
+);
+
+?>
---
+++ tests/analyzer/source/Php/Prints.01.php
@@ -0,0 +1,13 @@
+<?php
+
+echo 1;
+print 2;
+
+print_r(3);
+debug_print_backtrace();
+?>A<?php
+// 
+var_export($a, true);
+
+$x = strtolower($a);
+?>
