5f93f7a244f2d9a98edebd734810751ff3f6043a
Damien Seguy <damien.seguy@gmail.com>
2017-04-14 11:52:54 +0200

    Adding the test

--- tests/analyzer/exp/Structures/RepeatedRegex.01.php
+++ tests/analyzer/exp/Structures/RepeatedRegex.01.php
@@ -1,7 +1,12 @@
 <?php
 
-$expected     = array();
+$expected     = array('preg_match(\'/three times/i\', $_GET[\'x\'])', 
+                      'preg_match(\'/three times/i\', $GLOBALS[\'x\'])', 
+                      'preg_match(\'/three times/i\', $_GET[\'x\'])', 
+                      'preg_match(\'/twice/i\', $row[\'name\'])', 
+                      'preg_match(\'/twice/i\', $_GET[\'x\'])');
 
-$expected_not = array();
+$expected_not = array('preg_match(\'/^circle|^\'.$x.\'$/i\', $string)',
+                      'preg_match(\'/^circle|^square$/i\', $string)');
 
 ?>
