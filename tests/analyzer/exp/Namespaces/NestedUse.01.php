54e28dfb082c028c5d7c59e71e589d607492db48
Damien Seguy <damien.seguy@gmail.com>
2017-04-14 19:50:17 +0200

    Report nested use

---
+++ tests/analyzer/exp/Namespaces/NestedUse.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('C\D\E as F', 
+                      'F\G as I');
+
+$expected_not = array('Z\Z\Y as A',
+                      'A\B as C');
+
+?>
---
+++ tests/analyzer/source/Namespaces/NestedUse.01.php
@@ -0,0 +1,10 @@
+<?php
+
+use A\B as C;
+use C\D\E as F;
+use F\G as I;
+
+use Z\Z\Y as A;
+
+
+?>
