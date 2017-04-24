387aafd83a138e1006c06186adde617f75e7edab
Damien Seguy <damien.seguy@gmail.com>
2017-04-24 12:14:07 +0200

    Upgraded analysis with more SQL statement to ignore

--- tests/analyzer/exp/Wordpress/WpdbBestUsage.01.php
+++ tests/analyzer/exp/Wordpress/WpdbBestUsage.01.php
@@ -2,11 +2,11 @@
 
 $expected     = array('"SELECT * FROM " . $table_prefix . " ORDER BY " . $wpdb->table_prefix', 
                       '\'SELECT * FROM \' . $wpdb->table_prefix . \' ORDER BY name\'', 
-                      '"DROP TABLE IF EXISTS $table_prefix"',
                       '"SELECT * FROM $table_prefix ORDER BY name"'
 );
 
 $expected_not = array('"SELECT * FROM $group_table ORDER BY name"',
+                      '"DROP TABLE IF EXISTS $table_prefix"',
                       "'SELECT * FROM \$group_table ORDER BY name'",
                       '"SELECT * FROM $wpdb->table_prefix ORDER BY name"',
                       '"SHOW TABLES LIKE $like"',
