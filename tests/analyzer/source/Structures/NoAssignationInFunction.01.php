0a95a3d04c4d3b48ac069b59a405c0cb9f0e7071
Damien Seguy <damien.seguy@gmail.com>
2017-04-27 08:42:22 +0200

    Fix UT

--- tests/analyzer/source/Structures/NoAssignationInFunction.01.php
+++ tests/analyzer/source/Structures/NoAssignationInFunction.01.php
@@ -9,7 +9,11 @@ function foo2() {
 }
 
 class x {
-    function foo() {
+    function foox() {
+        // assign to non local is good
+        $s = array(1,2,3,4,5,6,7,8,9,10,11);
+    }
+    function foox2() {
         // assign to non local is good
         $this->s = array(1,2,3,4,5,6,7,8,9,10,11);
     }
