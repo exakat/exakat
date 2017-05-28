cd4ebe62d50a443725341c1ab92c59e2556b6b85
Damien Seguy <damien.seguy@gmail.com>
2017-05-28 13:08:38 -0300

    Help move to Typehint and reduce code

---
+++ tests/analyzer/exp/Functions/CouldTypehint.01.php
@@ -0,0 +1,8 @@
+<?php
+
+$expected     = array('$b instanceof C', 
+                      '$a instanceof B');
+
+$expected_not = array('$e instanceof C');
+
+?>
---
+++ tests/analyzer/source/Functions/CouldTypehint.01.php
@@ -0,0 +1,9 @@
+<?php
+
+function foo($a, $b = 2, D $c, $d) {
+    if ($a instanceof B) {}
+    if ($b instanceof C) {}
+    if ($e instanceof C) {}
+}
+
+?>
