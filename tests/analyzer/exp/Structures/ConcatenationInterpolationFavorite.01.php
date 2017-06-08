bac8934fd2f973ad973ed64a33727ab055f298d7
Damien Seguy <damien.seguy@gmail.com>
2017-06-08 14:47:05 +0200

    Favorite : . or ""

---
+++ tests/analyzer/exp/Structures/ConcatenationInterpolationFavorite.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('$w.\' \'.$e');
+
+$expected_not = array('"$b $c"',
+                      '"$b $e"',
+                      '"$b $f"',
+                      '"$b $z"',
+);
+
+?>
---
+++ tests/analyzer/source/Structures/ConcatenationInterpolationFavorite.01.php
@@ -0,0 +1,19 @@
+<?php
+
+// be consistent
+$a = "$b $c";
+$d = "$b $e";
+$e = "$b $e";
+$d = "$b $f";
+$f = "$b $z";
+$h = "$b $e";
+$y = strtolower("$b $e");
+$d = "$b $x";
+$j = "$b $c";
+$d = "$b $g";
+$d = "$b $h";
+
+// Be consistent, always use the same. 
+$z = $w.' '.$e;
+
+?>
