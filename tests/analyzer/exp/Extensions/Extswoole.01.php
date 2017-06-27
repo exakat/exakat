29f9c94b5c90b01070795fe508f650955516b38a
Damien Seguy <damien.seguy@gmail.com>
2017-06-27 12:21:28 +0200

    New analysis Swoole

---
+++ tests/analyzer/exp/Extensions/Extswoole.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('Swoole\Coroutine', 
+                      'Swoole\Coroutine', 
+                      'Swoole\Coroutine\Redis( )', 
+                      'Swoole\Coroutine\Redis( )');
+
+$expected_not = array('Swoole\NoRoutine');
+
+?>
