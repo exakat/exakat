c852add6e5db9be30333ac8e04d2230d3674e236
Damien Seguy <damien.seguy@gmail.com>
2017-07-18 11:29:56 +0200

    New favortie : and or &&

---
+++ tests/analyzer/source/Php/LetterCharsLogicalFavorite.01.php
@@ -0,0 +1,14 @@
+<?php 
+$a1 = $b and $c;
+$a2 = $b And $c;
+$a3 = $b and $c;
+$a4 = $b or $c;
+$a5 = $b OR $c;
+$a6 = $b aND $c;
+$a7 = $b and $c;
+$a8 = $b xor $c;
+$a9 = $b or $c;
+$a10 = $b OR $c;
+$a = $b ^ $c;
+
+?>
