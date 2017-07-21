959669d6ff29143dfd41f761c2758d1e435e5c08
Damien Seguy <damien.seguy@gmail.com>
2017-07-21 11:21:14 +0200

    Return early

--- library/Exakat/Analyzer/Structures/RegexDelimiter.php
+++ library/Exakat/Analyzer/Structures/RegexDelimiter.php
@@ -61,6 +61,9 @@ class RegexDelimiter extends Analyzer {
 
         $types = array_filter($types, function ($x) use ($total) { return $x > 0 && $x / $total < 0.1; });
         $types =  array_keys($types);
+        if (empty($types)) {
+            return;
+        }
 
         $this->atomFunctionIs($pregFunctions)
              ->outIs('ARGUMENTS')
