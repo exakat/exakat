2f8ed9d21934aa91c8f77b65625d26c052086e51
Damien Seguy <damien.seguy@gmail.com>
2017-07-22 13:19:07 +0200

    Extended the delimiter fetching to all queries

--- tests/analyzer/Test/Structures/pregOptionE.php
+++ tests/analyzer/Test/Structures/pregOptionE.php
@@ -8,7 +8,7 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_pregOptionE extends Analyzer {
-    /* 8 methods */
+    /* 9 methods */
 
     public function testStructures_pregOptionE01()  { $this->generic_test('Structures_pregOptionE.01'); }
     public function testStructures_pregOptionE02()  { $this->generic_test('Structures/pregOptionE.02'); }
@@ -18,5 +18,6 @@ class Structures_pregOptionE extends Analyzer {
     public function testStructures_pregOptionE06()  { $this->generic_test('Structures/pregOptionE.06'); }
     public function testStructures_pregOptionE07()  { $this->generic_test('Structures/pregOptionE.07'); }
     public function testStructures_pregOptionE08()  { $this->generic_test('Structures/pregOptionE.08'); }
+    public function testStructures_pregOptionE09()  { $this->generic_test('Structures/pregOptionE.09'); }
 }
 ?>
---
+++ tests/analyzer/exp/Structures/pregOptionE.09.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array('\'/(a)(\'.$kw.\')(b)/sie\'');
+
+$expected_not = array('\'/(a)(\'.$kw.\')(b)/si\'');
+
+?>
