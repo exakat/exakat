13a0420ce94f5d9b77a7e3efd709743a2a88fdec
Damien Seguy <damien.seguy@gmail.com>
2017-07-24 11:05:53 +0200

    New analysis : xattr

---
+++ tests/analyzer/exp/Extensions/Extxattr.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('xattr_get($file, \'Listen count\')', 
+                      'xattr_set($file, \'My ranking\', \'Good\')', 
+                      'xattr_set($file, \'Listen count\', \'34\')', 
+                      'xattr_set($file, \'Artist\', \'Someone\')',
+                      );
+
+$expected_not = array('xattr_get($file, \'typo in functionname\')', 
+                      );
+
+?>
