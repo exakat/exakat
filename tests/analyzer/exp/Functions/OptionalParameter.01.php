fb78afadf131f595d3f63750bd496def4af54ac4
Damien Seguy <damien.seguy@gmail.com>
2017-07-31 22:48:51 +0200

    New analysis for Optional Arguments

---
+++ tests/analyzer/exp/Functions/OptionalParameter.01.php
@@ -0,0 +1,7 @@
+<?php
+
+$expected     = array('function methodWithOptionalArgument(bar $x = null) { /**/ } ');
+
+$expected_not = array('function methodWithCompulsoryArgument(bar $x) { /**/ } ');
+
+?>
