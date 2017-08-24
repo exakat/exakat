63e5345b3a11e7a05dfba348ac1729aa6ef7ae67
Damien Seguy <damien.seguy@gmail.com>
2017-08-24 10:11:48 +0200

    Removal of 'ARGUMENTS'

--- tests/analyzer/exp/Functions/WrongNumberOfArguments.01.php
+++ tests/analyzer/exp/Functions/WrongNumberOfArguments.01.php
@@ -4,10 +4,12 @@ $expected     = array('\\crypt( )',
                       'crypt( )',
                       'CRYpt(4, 5, 6, 9)',
                       '\\crypt(4, 5, 6, 8)',
-                      'crypt(4, 5, 6, 7)');
+                      'crypt(4, 5, 6, 7)',
+                      );
 
 $expected_not = array('unknown_function(1,2,3,4,5)',
                       'phpinfo(1,2,3)',
-                      'assert_options(6,7,8,9)');
+                      'assert_options(6,7,8,9)',
+                      );
 
 ?>
