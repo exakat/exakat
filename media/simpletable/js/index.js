79b3026a31bab78c3972a949c8d0493900f04f7d
Damien Seguy <damien.seguy@gmail.com>
2017-08-28 13:27:19 +0200

    Hide results initially

--- media/simpletable/js/index.js
+++ media/simpletable/js/index.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
 	$('[data-toggle="toggle"]').change(function(){
 		$(this).parents().next('.hide').toggle();
 	});
+	$('[data-toggle="toggle"]').parents().next('.hide').toggle();
 });
