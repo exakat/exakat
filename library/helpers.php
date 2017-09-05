bc98414f6e12f2f35ddbad2f52a682a755b9abf9
Damien Seguy <damien.seguy@gmail.com>
2017-09-05 15:17:11 +0200

    Fixed handling of password in URL

--- library/helpers.php
+++ library/helpers.php
@@ -143,7 +143,7 @@ function unparse_url($parsed_url) {
     $port     = isset($parsed_url['port'])     ? ':'.$parsed_url['port']     : '';
     $user     = isset($parsed_url['user'])     ? $parsed_url['user']           : '';
     $pass     = isset($parsed_url['pass'])     ? ':'.$parsed_url['pass']     : '';
-    $pass     = ($user || $pass)               ? '$pass@'                      : '';
+    $pass     = ($user || $pass)               ? $pass.'@'                      : '';
     $path     = isset($parsed_url['path'])     ? $parsed_url['path']           : '';
     $query    = isset($parsed_url['query'])    ? '?'.$parsed_url['query']    : '';
     $fragment = isset($parsed_url['fragment']) ? '#'.$parsed_url['fragment'] : '';
