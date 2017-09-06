e42530ae9cf3665f729555fc9103bd0b0b6539e9
Damien Seguy <damien.seguy@gmail.com>
2017-09-06 20:59:22 +0200

    Report class constants that may be made private

---
+++ tests/analyzer/exp/Classes/CouldBePrivateConstante.02.php
@@ -0,0 +1,14 @@
+<?php
+
+$expected     = array('APUBLICBUTSBPRIVATE = 1',
+                      'APROTECTESBPRIVATE = 8',
+                      'APROTECTED = 4',
+                     );
+
+$expected_not = array('APUBLICBUTREALLY = 2', 
+                      'APUBLICBUTREALLY2 = 3', 
+                      'APUBLICBUTREALLY3 = 6', 
+                      'APUBLICBUTREALLY4 = 7',
+                     );
+
+?>
