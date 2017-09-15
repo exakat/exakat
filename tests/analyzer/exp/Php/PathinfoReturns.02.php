5e6d5fdf1d8837b8104d5aff5f6be660e1fcfb61
Damien Seguy <damien.seguy@gmail.com>
2017-09-15 19:08:16 +0200

    New analyzer : catch array_values usage with pathinfo

---
+++ tests/analyzer/exp/Php/PathinfoReturns.02.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('parse_url($filename2)',
+                      '\parse_url($filename3)',
+                      );
+
+$expected_not = array('parse_url($filename4)',
+                      'parse_url($filename5)',
+                      'parse_url($filename6)',
+                      'parse_url($filename7)',
+                      );
+
+?>
---
+++ tests/analyzer/source/Php/PathinfoReturns.02.php
@@ -0,0 +1,11 @@
+<?php
+
+list(,, $extension, $filename) = array_values(parse_url($filename2));
+[,, $extension, $filename] = array_values(\parse_url($filename3));
+
+[,, $extension, $filename] = array_keys(parse_url($filename4));
+$a = array_values(parse_url($filename5));
+list($a) = array_values(parse_urlrmation($filename6));
+list($a) = myclass::array_values(parse_url($filename7));
+
+?>
