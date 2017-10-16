925e1afadc9a4c4071814df1451477e0e4deae64
Damien Seguy <damien.seguy@gmail.com>
2017-10-16 13:33:26 +0200

    New zf3 version : mail modulemanager session

---
+++ tests/analyzer/Test/ZendF/Zf3Session28.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Test;
+
+include_once(dirname(dirname(dirname(dirname(__DIR__)))).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_test');
+spl_autoload_register('Autoload::autoload_phpunit');
+spl_autoload_register('Autoload::autoload_library');
+
+class ZendF_Zf3Session28 extends Analyzer {
+    /* 1 methods */
+
+    public function testZendF_Zf3Session2801()  { $this->generic_test('ZendF/Zf3Session28.01'); }
+}
+?>
