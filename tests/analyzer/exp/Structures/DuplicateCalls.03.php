56872ccd9d31c432f9240062d9891502657cd23d
Damien Seguy <damien.seguy@gmail.com>
2017-10-16 13:33:38 +0200

    More tests for DUplicate calls

---
+++ tests/analyzer/exp/Structures/DuplicateCalls.03.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('strtolower($name)', 
+                      'ucfirst(strtolower($name))', 
+                      'strtolower($name)', 
+                      'ucfirst(strtolower($name))',
+                     );
+
+$expected_not = array('strtolower($last)', 
+                      'ucfirst(strtolower($last))',
+                     );
+
+?>
