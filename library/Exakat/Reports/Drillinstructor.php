6acf3b8ef45859de7806295870f4877dcbc2b59a
Damien Seguy <damien.seguy@gmail.com>
2017-10-16 13:34:22 +0200

    New report for the drill instructor

--- library/Exakat/Reports/Drillinstructor.php
+++ library/Exakat/Reports/Drillinstructor.php
@@ -44,9 +44,12 @@ class Drillinstructor extends Ambassador {
 
         $this->initFolder();
         $this->generateDashboard();
+        $this->generateLevels();
         $this->generateLevel1();
         $this->generateLevel2();
         $this->generateLevel3();
+        $this->generateLevel4();
+//        $this->generateLevel5();
 
         // Annex
         $this->generateAnalyzerSettings();
@@ -80,9 +83,11 @@ class Drillinstructor extends Ambassador {
           <li class="header">&nbsp;</li>
           <!-- Optionally, you can add icons to the links -->
           <li class="active"><a href="index.html"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+          <li><a href="levels.html"><i class="fa fa-dashboard"></i>Overview of levels</a></li>
           <li><a href="level1.html"><i class="fa fa-circle-o"></i>Level 1</a></li>
           <li><a href="level2.html"><i class="fa fa-circle-o"></i>Level 2</a></li>
           <li><a href="level3.html"><i class="fa fa-circle-o"></i>Level 3</a></li>
+          <li><a href="level3.html"><i class="fa fa-circle-o"></i>Level 4</a></li>
           <li class="treeview">
             <a href="#"><i class="fa fa-sticky-note-o"></i> <span>Annexes</span><i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
@@ -119,6 +124,11 @@ MENU;
     private function generateLevel3() {
         $this->generateIssuesEngine('level3',
                                     $this->getIssuesFaceted('Level 3'));
+    }
+
+    private function generateLevel4() {
+        $this->generateIssuesEngine('level4',
+                                    $this->getIssuesFaceted('Level 4'));
     }
 
     private function generateDocumentation(){
@@ -167,6 +177,364 @@ MENU;
 
         $this->putBasedPage('analyzers_doc', $finalHTML);
     }
+
+    protected function generateDashboard() {
+        $baseHTML = $this->getBasedPage('index');
+
+        $tags = array();
+        $code = array();
+
+        // Bloc top left
+        $hashData = $this->getHashData();
+        $finalHTML = $this->injectBloc($baseHTML, 'BLOCHASHDATA', $hashData);
+
+        // bloc Issues
+        $issues = $this->getIssuesBreakdown();
+        $finalHTML = $this->injectBloc($finalHTML, 'BLOCISSUES', $issues['html']);
+        $tags[] = 'SCRIPTISSUES';
+        $code[] = $issues['script'];
+
+        // bloc severity
+        $severity = $this->getSeverityBreakdown();
+        $finalHTML = $this->injectBloc($finalHTML, 'BLOCSEVERITY', $severity['html']);
+        $tags[] = 'SCRIPTSEVERITY';
+        $code[] = $severity['script'];
+
+        // Marking the audit date
+        $audit_date = 'Audit date : '.date('d-m-Y h:i:s', time());
+        $audit_name = $this->datastore->getHash('audit_name');
+        if (!empty($audit_name)) {
+            $audit_date .= ' - &quot;'.$audit_name.'&quot;';
+        }
+        $finalHTML = $this->injectBloc($finalHTML, 'AUDIT_DATE', $audit_date);
+
+        // top 10
+        $fileHTML = $this->getTopFile();
+        $finalHTML = $this->injectBloc($finalHTML, 'TOPFILE', $fileHTML);
+        $analyzerHTML = $this->getTopAnalyzers();
+        $finalHTML = $this->injectBloc($finalHTML, 'TOPANALYZER', $analyzerHTML);
+
+        $blocjs = <<<JAVASCRIPT
+  <script>
+    $(document).ready(function() {
+      Morris.Donut({
+        element: 'donut-chart_issues',
+        resize: true,
+        colors: ["#3c8dbc", "#f56954", "#00a65a", "#1424b8"],
+        data: [SCRIPTISSUES]
+      });
+      Morris.Donut({
+        element: 'donut-chart_severity',
+        resize: true,
+        colors: ["#3c8dbc", "#f56954", "#00a65a", "#1424b8"],
+        data: [SCRIPTSEVERITY]
+      });
+      Highcharts.theme = {
+         colors: ["#F56954", "#f7a35c", "#ffea6f", "#D2D6DE"],
+         chart: {
+            backgroundColor: null,
+            style: {
+               fontFamily: "Dosis, sans-serif"
+            }
+         },
+         title: {
+            style: {
+               fontSize: '16px',
+               fontWeight: 'bold',
+               textTransform: 'uppercase'
+            }
+         },
+         tooltip: {
+            borderWidth: 0,
+            backgroundColor: 'rgba(219,219,216,0.8)',
+            shadow: false
+         },
+         legend: {
+            itemStyle: {
+               fontWeight: 'bold',
+               fontSize: '13px'
+            }
+         },
+         xAxis: {
+            gridLineWidth: 1,
+            labels: {
+               style: {
+                  fontSize: '12px'
+               }
+            }
+         },
+         yAxis: {
+            minorTickInterval: 'auto',
+            title: {
+               style: {
+                  textTransform: 'uppercase'
+               }
+            },
+            labels: {
+               style: {
+                  fontSize: '12px'
+               }
+            }
+         },
+         plotOptions: {
+            candlestick: {
+               lineColor: '#404048'
+            }
+         },
+
+
+         // General
+         background2: '#F0F0EA'
+      };
+
+      // Apply the theme
+      Highcharts.setOptions(Highcharts.theme);
+
+      $('#filename').highcharts({
+          credits: {
+            enabled: false
+          },
+
+          exporting: {
+            enabled: false
+          },
+
+          chart: {
+              type: 'column'
+          },
+          title: {
+              text: ''
+          },
+          xAxis: {
+              categories: [SCRIPTDATAFILES]
+          },
+          yAxis: {
+              min: 0,
+              title: {
+                  text: ''
+              },
+              stackLabels: {
+                  enabled: false,
+                  style: {
+                      fontWeight: 'bold',
+                      color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                  }
+              }
+          },
+          legend: {
+              align: 'right',
+              x: 0,
+              verticalAlign: 'top',
+              y: -10,
+              floating: false,
+              backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+              borderColor: '#CCC',
+              borderWidth: 1,
+              shadow: false
+          },
+          tooltip: {
+              headerFormat: '<b>{point.x}</b><br/>',
+              pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+          },
+          plotOptions: {
+              column: {
+                  stacking: 'normal',
+                  dataLabels: {
+                      enabled: false,
+                      color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                      style: {
+                          textShadow: '0 0 3px black'
+                      }
+                  }
+              }
+          },
+          series: [{
+              name: 'Critical',
+              data: [SCRIPTDATACRITICAL]
+          }, {
+              name: 'Major',
+              data: [SCRIPTDATAMAJOR]
+          }, {
+              name: 'Minor',
+              data: [SCRIPTDATAMINOR]
+          }, {
+              name: 'None',
+              data: [SCRIPTDATANONE]
+          }]
+      });
+
+      $('#container').highcharts({
+          credits: {
+            enabled: false
+          },
+
+          exporting: {
+            enabled: false
+          },
+
+          chart: {
+              type: 'column'
+          },
+          title: {
+              text: ''
+          },
+          xAxis: {
+              categories: [SCRIPTDATAANALYZERLIST]
+          },
+          yAxis: {
+              min: 0,
+              title: {
+                  text: ''
+              },
+              stackLabels: {
+                  enabled: false,
+                  style: {
+                      fontWeight: 'bold',
+                      color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                  }
+              }
+          },
+          legend: {
+              align: 'right',
+              x: 0,
+              verticalAlign: 'top',
+              y: -10,
+              floating: false,
+              backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+              borderColor: '#CCC',
+              borderWidth: 1,
+              shadow: false
+          },
+          tooltip: {
+              headerFormat: '<b>{point.x}</b><br/>',
+              pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+          },
+          plotOptions: {
+              column: {
+                  stacking: 'normal',
+                  dataLabels: {
+                      enabled: false,
+                      color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                      style: {
+                          textShadow: '0 0 3px black'
+                      }
+                  }
+              }
+          },
+          series: [{
+              name: 'Critical',
+              data: [SCRIPTDATAANALYZERCRITICAL]
+          }, {
+              name: 'Major',
+              data: [SCRIPTDATAANALYZERMAJOR]
+          }, {
+              name: 'Minor',
+              data: [SCRIPTDATAANALYZERMINOR]
+          }, {
+              name: 'None',
+              data: [SCRIPTDATAANALYZERNONE]
+          }]
+      });
+    });
+  </script>
+JAVASCRIPT;
+
+        // Filename Overview
+        $fileOverview = $this->getFileOverview();
+        $tags[] = 'SCRIPTDATAFILES';
+        $code[] = $fileOverview['scriptDataFiles'];
+        $tags[] = 'SCRIPTDATAMAJOR';
+        $code[] = $fileOverview['scriptDataMajor'];
+        $tags[] = 'SCRIPTDATACRITICAL';
+        $code[] = $fileOverview['scriptDataCritical'];
+        $tags[] = 'SCRIPTDATANONE';
+        $code[] = $fileOverview['scriptDataNone'];
+        $tags[] = 'SCRIPTDATAMINOR';
+        $code[] = $fileOverview['scriptDataMinor'];
+
+        // Analyzer Overview
+        $analyzerOverview = $this->getAnalyzerOverview();
+        $tags[] = 'SCRIPTDATAANALYZERLIST';
+        $code[] = $analyzerOverview['scriptDataAnalyzer'];
+        $tags[] = 'SCRIPTDATAANALYZERMAJOR';
+        $code[] = $analyzerOverview['scriptDataAnalyzerMajor'];
+        $tags[] = 'SCRIPTDATAANALYZERCRITICAL';
+        $code[] = $analyzerOverview['scriptDataAnalyzerCritical'];
+        $tags[] = 'SCRIPTDATAANALYZERNONE';
+        $code[] = $analyzerOverview['scriptDataAnalyzerNone'];
+        $tags[] = 'SCRIPTDATAANALYZERMINOR';
+        $code[] = $analyzerOverview['scriptDataAnalyzerMinor'];
+
+        $blocjs = str_replace($tags, $code, $blocjs);
+        $finalHTML = $this->injectBloc($finalHTML, 'BLOC-JS',  $blocjs);
+        $finalHTML = $this->injectBloc($finalHTML, 'TITLE', 'Issues\' dashboard');
+        $this->putBasedPage('index', $finalHTML);
+    }
+
+    protected function generateLevels() {
+        $levels = '';
+        
+        for($level = 1; $level < 6; ++$level) {
+            
+            $levelRows = '';
+            $total = 0;
+            $analyzers = Analyzer::getThemeAnalyzers('Level '.$level);
+            if (empty($analyzers)) {
+                continue;
+            }
+            $analyzersList = makeList($analyzers);
+        
+            $res = $this->sqlite->query(<<<SQL
+SELECT analyzer AS name, count FROM resultsCounts WHERE analyzer in ($analyzersList) ORDER BY count
+SQL
+);
+            $colors = array('A' => '#00FF00',
+                            'B' => '#32CC00',
+                            'C' => '#669900',
+                            'D' => '#996600',
+                            'E' => '#CC3300',
+                            'F' => '#FF0000',
+                            );
+            $count = 0;
+            while($row = $res->fetchArray(\SQLITE3_ASSOC)) {
+                $ini = parse_ini_file($this->config->dir_root.'/human/en/'.$row['name'].'.ini');
+
+#FF0000	Bad
+#FFFF00	Bad-Average
+#FFFF00	Average
+#7FFF00	Average-Good
+#00FF00	Good
+
+                if ($row['count'] == 0) {
+                    $row['grade'] = 'A';
+                } else {
+                    $grade = min(ceil(log($row['count']) / log(count($colors))), count($colors) - 1);
+                    $row['grade'] = chr(66 + $grade); // B to F
+                }
+                $row['color'] = $colors[$row['grade']];
+                
+                $total += $row['count'];
+                $count += (int) $row['count'] === 0;
+    
+                $levelRows .= '<tr><td>'.$ini['name']."</td><td>$row[count]</td><td style=\"background-color: $row[color]\">$row[grade]</td></tr>\n";
+            }
+
+            print $count / (count($analyzers) - 1)."\n";
+           $grade = floor($count / (count($analyzers) - 1) * (count($colors) - 1));
+           $grade = chr(65 + $grade); // B to F
+           $color = $colors[$grade];
+            
+            $levels .= '<tr><td style="background-color: #bbbbbb">Level '.$level.'</td>
+                            <td style="background-color: #bbbbbb">'.$total.'</td></td>
+                            <td style="background-color: '.$color.'">'.$grade.'</td></tr>'.PHP_EOL.
+                       $levelRows;
+        }
+
+        $html = $this->getBasedPage('levels');
+        $html = $this->injectBloc($html, 'LEVELS', $levels);
+        $this->putBasedPage('levels', $html);
+    }
+
 
 }
 
