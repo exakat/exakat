78a9e66c783ecd866f57ef456df9a33b4ec8b3cd
Damien Seguy <damien.seguy@gmail.com>
2017-10-19 19:48:56 +0200

    Mark properties as being a magic call in that class

---
+++ library/Exakat/Analyzer/Classes/IsaMagicProperty.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Copyright 2012-2017 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Analyzer\Classes;
+
+use Exakat\Analyzer\Analyzer;
+
+class IsaMagicProperty extends Analyzer {
+    public function dependsOn() {
+        return array('Classes/DefinedProperty',
+                     'Classes/IsRead',
+                     'Classes/IsModified');
+    }
+    
+    public function analyze() {
+        // echo $this->a;
+        $this->atomIs('Member')
+             ->analyzerIsNot('Classes/DefinedProperty')
+             ->analyzerIs('Classes/IsRead')
+             ->hasClass()
+             ->goToClass()
+             ->raw('where( __.out("METHOD").out("NAME").has("code", "__get") )')
+             ->back('first');
+        $this->prepareQuery();
+
+        // $this->a = 1;
+        $this->atomIs('Member')
+             ->analyzerIsNot('Classes/DefinedProperty')
+             ->analyzerIs('Classes/IsModified')
+             ->hasClass()
+             ->goToClass()
+             ->raw('where( __.out("METHOD").out("NAME").has("code", "__set") )')
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Classes/IsaMagicProperty.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('$this->aw',
+                      '$this->ar',
+                     );
+
+$expected_not = array('$this->bw',
+                      '$this->br',
+                      '$this->au',
+                     );
+
+?>
