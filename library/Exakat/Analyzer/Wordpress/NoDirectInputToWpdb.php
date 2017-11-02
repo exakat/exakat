474f40b812a4ad7e8c58be95ce732ffc4ad41361
Damien Seguy <damien.seguy@gmail.com>
2017-11-02 11:31:55 -0400

    New analysis : prepare() with GPC (Wordpress)

---
+++ library/Exakat/Analyzer/Wordpress/NoDirectInputToWpdb.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Copyright 2012-2017 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Analyzer\Wordpress;
+
+use Exakat\Analyzer\Analyzer;
+
+class NoDirectInputToWpdb extends Analyzer {
+    public function analyze() {
+        // $wpdb->prepare('SQL', $_GET['dada']);
+        $this->atomIs('Methodcall')
+             ->outIs('OBJECT')
+             ->atomIs('Variableobject')
+             ->codeIs('$wpdb')
+             ->back('first')
+             ->outIs('METHOD')
+             ->codeIs('prepare')
+             ->outIs('ARGUMENT')
+             ->atomInside(array('Variable', 'Array'))
+             ->outIsIE('VARIABLE')
+             ->codeIs(array('$_GET', '$_POST', '$_REQUEST', '$_FILE', '$_COOKIE'))
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
