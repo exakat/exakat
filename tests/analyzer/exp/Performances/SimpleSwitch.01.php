1147549026a0f6f20fbd66d7ac84c4018acff55e
Damien Seguy <damien.seguy@gmail.com>
2017-11-02 10:44:44 -0400

    Performance recommendation for switch and PHP 7.2

---
+++ tests/analyzer/exp/Performances/SimpleSwitch.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('switch ($c) { /**/ } ',
+                     );
+
+$expected_not = array('switch ($a) { /**/ } ',
+                      'switch ($b) { /**/ } ',
+                     );
+
+?>
