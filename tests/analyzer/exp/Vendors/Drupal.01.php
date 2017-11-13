ccc7460c71157f8374fdbf3d4774673d180f7617
Damien Seguy <damien.seguy@gmail.com>
2017-11-13 10:21:12 +0100

    Added detection for drupal

---
+++ tests/analyzer/exp/Vendors/Drupal.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('ControllerBase',
+                      'Drupal\Core\Controller\ControllerBase',
+                     );
+
+$expected_not = array('Drupal\Core\Controller\ControllerBase',
+                     );
+
+?>
