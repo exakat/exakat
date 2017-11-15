cea46aa2f2ae7286e924b186149abbf22c0b371f
Damien Seguy <damien.seguy@gmail.com>
2017-11-15 11:23:49 +0100

    Report usage of integers as property names

---
+++ tests/analyzer/exp/Classes/IntegerAsProperty.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('$obj->{\'0\'}',
+                      '$obj->{0}',
+                     );
+
+$expected_not = array('$obj->{\'b\'}',
+                     );
+
+?>
