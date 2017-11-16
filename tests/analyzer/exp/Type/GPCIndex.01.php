7ac4ecb1334fee073ed96530153abf8944ce0069
Damien Seguy <damien.seguy@gmail.com>
2017-11-16 19:31:08 +0100

    Added new inventory : GPC index.

---
+++ tests/analyzer/exp/Type/GPCIndex.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('\'triple\'',
+                      '\'index\'',
+                      '\'double\'',
+                     );
+
+$expected_not = array('\'arraystring\'', 
+                      '\'b\'',
+                     );
+
+?>
---
+++ tests/analyzer/source/Type/GPCIndex.01.php
@@ -0,0 +1,15 @@
+<?php
+
+$a['index'] = 'notIndex';
+$o->c['triple'][]['with append'][foo()][$v] = $b['double']["dimension"];
+$x = ['arraystring', 'b'];
+$d["$interpolated string"];
+
+
+$_GET['index'] = 'notIndex';
+$_POST['triple'][]['with append'][foo()][$v] = $_post['double']["dimension"];
+$o->_POST = ['arraystring', 'b'];
+$d["$interpolated string"];
+
+
+?>
