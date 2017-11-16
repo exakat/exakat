cfa88227747069e8f4adb6d8bb3e216e07d48ae8
Damien Seguy <damien.seguy@gmail.com>
2017-11-16 11:05:23 +0100

    Missing new with a functioncall.

---
+++ tests/analyzer/exp/Structures/MissingNew.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('foo2( )',
+                     );
+
+$expected_not = array('foo1( )',
+                      'foo3( )',
+                      'foo4( )',
+                     );
+
+?>
---
+++ tests/analyzer/source/Structures/MissingNew.01.php
@@ -0,0 +1,15 @@
+<?php
+
+function foo() {}
+$a = foo();
+
+$a = foo1();
+
+class foo2 {}
+$a = foo2();
+
+class foo4 {}
+$a = \foo4();
+
+
+?>
