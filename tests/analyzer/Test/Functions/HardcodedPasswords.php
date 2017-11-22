695be81eed11235a460ad9eaf48c1e1d6fc3c0a5
Damien Seguy <damien.seguy@gmail.com>
2017-11-22 17:07:18 +0100

    Avoid reporting 'required' as a password.

--- tests/analyzer/Test/Functions/HardcodedPasswords.php
+++ tests/analyzer/Test/Functions/HardcodedPasswords.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Functions_HardcodedPasswords extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testFunctions_HardcodedPasswords01()  { $this->generic_test('Functions_HardcodedPasswords.01'); }
+    public function testFunctions_HardcodedPasswords02()  { $this->generic_test('Functions/HardcodedPasswords.02'); }
 }
 ?>
