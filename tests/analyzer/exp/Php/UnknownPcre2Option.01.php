811b729d4ca489e6b36233b355bcdaded32f88ea
Damien Seguy <damien.seguy@gmail.com>
2017-11-26 22:02:43 +0100

    Report Invalid regex.

--- tests/analyzer/exp/Php/UnknownPcre2Option.01.php
+++ tests/analyzer/exp/Php/UnknownPcre2Option.01.php
@@ -1,13 +1,13 @@
 <?php
 
-$expected     = array('preg_match("/ye\\y/X", $string)',
+$expected     = array('preg_match("/ye\\\\y/X", $string)', 
+                      'preg_match("/ye\\\\J/X", $string)',
                       'preg_match(\'/ye\y/\', $string)',
-                      'preg_match("/ye\\J/X", $string)',
                       'preg_match(\'/ye\J/\', $string)',
+                      'preg_match(\'/ye\s/S\', $string)',
                      );
 
-$expected_not = array('preg_match(\'/ye\s/S\', $string)',
-                      'preg_match(\'/ye\y/\', $string)',
+$expected_not = array('preg_match(\'/ye\y/\', $string)',
                       'preg_match(\'/ye\s/X\', $string)',
                      );
 
