c927094d335fd1a676e69c4f542187eedb92ec3b
Damien Seguy <damien.seguy@gmail.com>
2017-11-27 11:26:03 +0100

    Fixed database problem (missing ')

--- tests/analyzer/exp/Php/DirectivesUsage.01.php
+++ tests/analyzer/exp/Php/DirectivesUsage.01.php
@@ -3,9 +3,11 @@
 $expected     = array('ini_restore(\'user_dir\')', 
                       '\ini_get(\'asp_tags\')', 
                       'ini_alter(\'open_basedir\', 3)', 
-                      'ini_set(\'always_populate_raw_post_data\', 1)'
-);
+                      'ini_set(\'always_populate_raw_post_data\', 1)',
+                      );
 
-$expected_not = array('\ini_get(\'jsp_tags\')', );
+$expected_not = array('ini_restore(\'user_ini_dir\')', 
+                      '\ini_get(\'jsp_tags\')', 
+                      );
 
 ?>
--- tests/analyzer/source/Php/DirectivesUsage.01.php
+++ tests/analyzer/source/Php/DirectivesUsage.01.php
@@ -3,7 +3,8 @@
 ini_set('always_populate_raw_post_data',1);
 \ini_get('asp_tags');
 ini_alter('open_basedir', 3);
-ini_restore('user_dir');
+ini_restore('user_dir');  // OK, actual directive
+ini_restore('user_ini_dir'); // does not exists
 \ini_get('jsp_tags');
 
 $a->ini_restore('error_prepend_string');
