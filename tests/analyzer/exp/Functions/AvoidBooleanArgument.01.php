3fdc5c67688f27db6f54aab5db525b94729499cd
Damien Seguy <damien.seguy@gmail.com>
2017-12-01 12:25:31 +0100

    New analyser : use named boolean arguments

---
+++ tests/analyzer/exp/Functions/AvoidBooleanArgument.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('function foo3($a, $b = \true) { /**/ } ', 
+                      'function foo2($a = false, $b = 1) { /**/ } ', 
+                      'function foo1($a = true, $b = 1) { /**/ } ',
+                     );
+
+$expected_not = array('function foo4($a = true, boolean $b) { /**/ } ',
+                     );
+
+?>
