9a4f98fed164757b8074e33f1e83906087603a2a
Damien Seguy <damien.seguy@gmail.com>
2017-12-11 18:17:37 +0100

    Fixed syntax error

--- tests/analyzer/exp/Structures/CouldUseShortAssignation.04.php
+++ tests/analyzer/exp/Structures/CouldUseShortAssignation.04.php
@@ -3,7 +3,7 @@
 $expected     = array('$options = 44 + $options',
                      );
 
-$expected_not = array('array('' => t(\'No context selected\')) + $options',
+$expected_not = array('array(\'\' => t(\'No context selected\')) + $options',
                      );
 
 ?>
