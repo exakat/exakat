766133c90a1692c226bc36c7ad7bce7820c00fd2
Damien Seguy <damien.seguy@gmail.com>
2017-12-11 18:21:33 +0100

    Removed false positive, when string is create after the append.

--- tests/analyzer/Test/Php/NoStringWithAppend.php
+++ tests/analyzer/Test/Php/NoStringWithAppend.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Php_NoStringWithAppend extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testPhp_NoStringWithAppend01()  { $this->generic_test('Php/NoStringWithAppend.01'); }
+    public function testPhp_NoStringWithAppend02()  { $this->generic_test('Php/NoStringWithAppend.02'); }
 }
 ?>
