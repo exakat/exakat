9b80802c6d38dc5db9b6f20cada5347a81cc0d2b
Damien Seguy <damien.seguy@gmail.com>
2017-12-13 17:38:58 +0100

    Always use ./bin when starting gremlin

--- library/Exakat/Graph/GSNeo4j.php
+++ library/Exakat/Graph/GSNeo4j.php
@@ -154,9 +154,9 @@ class GSNeo4j extends Graph {
         $version = version_compare('3.3.0', $gremlinVersion) ? '.3.2' : '.3.3';
 
         if ($version === '.3.3') {
-            exec('cd '.$this->config->gsneo4j_folder.'; rm -rf db/neo4j; bin/gremlin-server.exakat.sh start conf/gsneo4j.yaml  &');
+            exec('cd '.$this->config->gsneo4j_folder.'; rm -rf db/neo4j; ./bin/gremlin-server.exakat.sh start conf/gsneo4j.yaml  &');
         } elseif ($version === '.3.2') {
-            exec('cd '.$this->config->gsneo4j_folder.'; rm -rf db/neo4j; bin/gremlin-server.sh conf/gsneo4j.yaml  > gremlin.log 2>&1 & echo $! > db/gsneo4j.pid ');
+            exec('cd '.$this->config->gsneo4j_folder.'; rm -rf db/neo4j; ./bin/gremlin-server.sh conf/gsneo4j.yaml  > gremlin.log 2>&1 & echo $! > db/gsneo4j.pid ');
         }
         sleep(1);
         
