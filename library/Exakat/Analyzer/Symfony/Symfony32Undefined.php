648321eb0a069d96f148fa772260b723e5eb9467
Damien Seguy <damien.seguy@gmail.com>
2017-12-18 16:55:40 +0100

    Added report for Symfony

---
+++ library/Exakat/Analyzer/Symfony/Symfony32Undefined.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Copyright 2012-2017 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Analyzer\Symfony;
+
+use Exakat\Analyzer\Analyzer;
+
+class Symfony32Undefined extends SymfonyMissing {
+    public function dependsOn() {
+        return array('Symfony/SymfonyUsage');
+    }
+
+    public function analyze() {
+        $this->version = '3.2';
+        
+        parent::analyze();
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Symfony/Symfony28Undefined.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('Symfony\Component\Security\Core\Authorization\DebugAccessDecisionManager( )',
+                     );
+
+$expected_not = array('Not_Yaml( )',
+                      'Yaml( )',
+                      'Symfony\Component\Validator\Violation\LegacyConstraintViolationBuilder( )',
+                      'Symfony\Component\Yaml\Not_Yaml( )',
+                     );
+
+?>
