0b8f84ffa885e2e236f5bce50ea95b1bbb118bcd
Damien Seguy <damien.seguy@gmail.com>
2017-12-21 10:22:59 +0100

    More tests for Echo and Print

---
+++ tests/analyzer/exp/Structures/RepeatedPrint.03.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('echo 1',
+                     );
+
+$expected_not = array('echo 2',
+                      'echo 3',
+                      'echo 4',
+                      'echo 5',
+                     );
+
+?>
