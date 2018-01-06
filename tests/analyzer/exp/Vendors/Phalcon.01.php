ffbcb6c417eb1424f73698de85c7a2d8bc5cee5d
Damien Seguy <damien.seguy@gmail.com>
2018-01-06 18:04:06 +0100

    More tests

--- tests/analyzer/exp/Vendors/Phalcon.01.php
+++ tests/analyzer/exp/Vendors/Phalcon.01.php
@@ -4,6 +4,10 @@ $expected     = array('Phalcon\\Mvc\\Application',
                       'Phalcon\\Di\\FactoryDefault',
                       'Phalcon\\Loader',
                       'Phalcon\\Mvc\\View',
+                      'Loader( )',
+                      'View( )',
+                      'FactoryDefault( )',
+                      'Application($di)',
                      );
 
 $expected_not = array('registerDirs',
--- tests/analyzer/exp/Vendors/Symfony.01.php
+++ tests/analyzer/exp/Vendors/Symfony.01.php
@@ -2,6 +2,7 @@
 
 $expected     = array('Symfony\\Component\\HttpFoundation\\Response',
                       'Sensio\\Bundle\\FrameworkExtraBundle\\Configuration\\Route',
+                      'Response(\'<html><body>Lucky number: \' . $number . \'</body></html>\')',
                      );
 
 $expected_not = array('Response',
