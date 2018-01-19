e29a532461761251a94d5a5f2b2dfd0d1c181b74
Damien Seguy <damien.seguy@gmail.com>
2018-01-19 16:50:47 +0100

    Upgraded same conditions to handle more cases

--- tests/analyzer/exp/Structures/SameConditions.02.php
+++ tests/analyzer/exp/Structures/SameConditions.02.php
@@ -1,6 +1,9 @@
 <?php
 
 $expected     = array('if($b > 2) { /**/ } else { /**/ } ',
+                      'if($b > 3) { /**/ } else { /**/ } ', 
+                      'if($b > 1) { /**/ } else { /**/ } ', 
+                      'if($e != \'F\') { /**/ } else { /**/ } '
                      );
 
 $expected_not = array('$b > 1',
