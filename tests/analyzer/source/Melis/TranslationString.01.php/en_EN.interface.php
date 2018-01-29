f2ee5e83e836eee97ab041f58a511ee487232d55
Damien Seguy <damien.seguy@gmail.com>
2018-01-29 10:36:57 +0100

    Spot translation strings

---
+++ tests/analyzer/source/Melis/TranslationString.01.php/en_EN.interface.php
@@ -0,0 +1,7 @@
+<?php
+    return array(
+        'tr_xa' => 'A',
+        'tr_xb' => 'B',
+        'tr_xc' => 'C',
+        'tr_xd' => 'D',
+    );
---
+++ tests/analyzer/source/Melis/TranslationString.01.php/language/es_ES.interface.php
@@ -0,0 +1,7 @@
+<?php
+    return array(
+        'tr_a' => 'A',
+        'tr_b' => 'B',
+        'tr_b' => 'C2',
+        'e' => 'E',
+    );
