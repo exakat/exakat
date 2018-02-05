7ea9d384aeb2fab57fbec3c3dcdb95604c252f7f
Damien Seguy <damien.seguy@gmail.com>
2018-02-05 16:17:27 +0100

    Extra UT review

--- tests/analyzer/exp/Extensions/Extcyrus.01.php
+++ tests/analyzer/exp/Extensions/Extcyrus.01.php
@@ -5,7 +5,7 @@ $expected     = array('cyrus_query($res, CYRUS_CONN_NONSYNCLITERAL)',
                      );
 
 $expected_not = array('CYRUS_CONN_NONSYNCLITERALLY',
-                      'cyrus_query($res, CYRUS_CONN_NONSYNCLITERALLY)'
+                      'cyrus_query($res, CYRUS_CONN_NONSYNCLITERALLY)',
                      );
 
 ?>
--- tests/analyzer/exp/Security/DirectInjection.07.php
+++ tests/analyzer/exp/Security/DirectInjection.07.php
@@ -3,7 +3,7 @@
 $expected     = array('\'ls \' . ($_SERVER[\'UNKNOWN_INDEX\'][\'a2\'])',
                       '\'ls \' . ($_GET[\'INCOMING\'][\'a3\'])',
                       '\'ls \' . ($_POST[\'DOCUMENT_ROOT\'][\'a4\'])',
-                      'print ((($_GET[\'DOCUMENT_ROOT\'][\'a3\'])))', 
+                      'print ((($_GET[\'DOCUMENT_ROOT\'][\'a3\'])))',
                       'print ((($_GET[\'DOCUMENT_RATE\'][\'a4\'])))',
                      );
 
