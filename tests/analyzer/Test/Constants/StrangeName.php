25d12d9417c977e15f8ac5a4bdb56675f91a8c58
Damien Seguy <damien.seguy@gmail.com>
2018-02-07 19:19:48 +0100

    Check for case (Important for constantes). Added some more strange names.

--- tests/analyzer/Test/Constants/StrangeName.php
+++ tests/analyzer/Test/Constants/StrangeName.php
@@ -8,9 +8,10 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Constants_StrangeName extends Analyzer {
-    /* 2 methods */
+    /* 3 methods */
 
     public function testConstants_StrangeName01()  { $this->generic_test('Constants/StrangeName.01'); }
     public function testConstants_StrangeName02()  { $this->generic_test('Constants/StrangeName.02'); }
+    public function testConstants_StrangeName03()  { $this->generic_test('Constants/StrangeName.03'); }
 }
 ?>
