0e0f1e3e46401ef49ff28096caa96d002ce9e6d0
Damien Seguy <damien.seguy@gmail.com>
2018-02-10 17:08:51 +0100

    Fixed cased where $var is in "string"

---
+++ tests/analyzer/source/Files/MissingInclude.04.php/a/b/test.php
@@ -0,0 +1,10 @@
+<?php
+
+include '../../include.php';
+include './include_b.php';
+include '../include_a.php';
+include './nonexistant.php';
+include '../nonexistant.php';
+include '../../nonexistant.php';
+
+?>
