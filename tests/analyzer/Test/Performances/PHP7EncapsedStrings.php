43a3457366cf3f0f49397a3f8b3368d05f5f1348
Damien Seguy <damien.seguy@gmail.com>
2018-02-11 15:05:54 +0100

    Upgraded analysis with Static Property.

--- tests/analyzer/Test/Performances/PHP7EncapsedStrings.php
+++ tests/analyzer/Test/Performances/PHP7EncapsedStrings.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Performances_PHP7EncapsedStrings extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testPerformances_PHP7EncapsedStrings01()  { $this->generic_test('Performances/PHP7EncapsedStrings.01'); }
+    public function testPerformances_PHP7EncapsedStrings02()  { $this->generic_test('Performances/PHP7EncapsedStrings.02'); }
 }
 ?>
