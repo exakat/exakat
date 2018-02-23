1c5a1bb2bb1adab51add8eab3d914f51ad7bfb04
Damien Seguy <damien.seguy@gmail.com>
2018-02-23 19:45:17 +0100

    Updated list of tests

--- tests/cli/reports.sh
+++ tests/cli/reports.sh
@@ -1,19 +1,88 @@
 cd ../..
-php exakat init -p ut_nlptools -R https://github.com/atrilla/nlptools.git -v
-php exakat project -p ut_nlptools -v
+php exakat init -p phrozn -R https://github.com/atrilla/nlptools.git -v
+php exakat project -p phrozn -v
 
 # testing results
-php exakat report -p ut_nlptools -format Ambassador -file ambassador -v
-php exakat report -p ut_nlptools -format Devoops -file ambassador -v
-php exakat report -p ut_nlptools -format Text -file report -v
-php exakat report -p ut_nlptools -format Uml -file uml -v
-php exakat report -p ut_nlptools -format XML -file ambassador -v
-php exakat report -p ut_nlptools -format Clustergrammer -file clustergrammer -v
-php exakat report -p ut_nlptools -format RadwellCodes -file radwell -v
-php exakat report -p ut_nlptools -format PhpConfiguration -file phpconfiguration -v
+php exakat report -p phrozn -format Text -file report -v
+php exakat report -p phrozn -format Text -file stdout -v
+php exakat report -p phrozn -format Text -v
 
-php exakat report -p ut_nlptools -format UndefinedReport -v
+php exakat report -p phrozn -format Codesniffer -file report -v
+php exakat report -p phrozn -format Codesniffer -file stdout -v
+php exakat report -p phrozn -format Codesniffer -v
 
-php exakat remove -p ut_nlptools
+php exakat report -p phrozn -format Uml -file uml -v
+php exakat report -p phrozn -format Uml -file stdout -v
+php exakat report -p phrozn -format Uml -v
+
+php exakat report -p phrozn -format Diplomat -file uml -v
+php exakat report -p phrozn -format Diplomat -file stdout -v
+php exakat report -p phrozn -format Diplomat -v
+
+php exakat report -p phrozn -format Dependencywheel -file uml -v
+php exakat report -p phrozn -format Dependencywheel -file stdout -v
+php exakat report -p phrozn -format Dependencywheel -v
+
+php exakat report -p phrozn -format Ambassador -file aaaa -v
+php exakat report -p phrozn -format Ambassador -file stdout -v
+php exakat report -p phrozn -format Ambassador -v
+
+php exakat report -p phrozn -format Text -file aaaa -v
+php exakat report -p phrozn -format Text -file stdout -v
+php exakat report -p phrozn -format Text -v
+
+php exakat report -p phrozn -format XML -file aaaa -v
+php exakat report -p phrozn -format XML -file stdout -v
+php exakat report -p phrozn -format XML -v
+
+php exakat report -p phrozn -format Clustergrammer -file aaaa -v
+php exakat report -p phrozn -format Clustergrammer -file stdout -v
+php exakat report -p phrozn -format Clustergrammer -v
+
+php exakat report -p phrozn -format RadwellCode -file aaaa -v
+php exakat report -p phrozn -format RadwellCode -file stdout -v
+php exakat report -p phrozn -format RadwellCode -v
+
+php exakat report -p phrozn -format PhpCompilation -file aaaa -v
+php exakat report -p phrozn -format PhpCompilation -file stdout -v
+php exakat report -p phrozn -format PhpCompilation -v
+
+php exakat report -p phrozn -format PhpConfiguration -file aaaa -v
+php exakat report -p phrozn -format PhpConfiguration -file stdout -v
+php exakat report -p phrozn -format PhpConfiguration -v
+
+php exakat report -p phrozn -format Inventories -file aaaa -v
+php exakat report -p phrozn -format Inventories -file stdout -v
+php exakat report -p phrozn -format Inventories -v
+
+php exakat report -p phrozn -format Plantuml -file aaaa -v
+php exakat report -p phrozn -format Plantuml -file stdout -v
+php exakat report -p monolog -format Plantuml -v
+
+php exakat report -p phrozn -format SimpleHtml -file aaaa -v
+php exakat report -p phrozn -format SimpleHtml -file stdout -v
+php exakat report -p phrozn -format SimpleHtml -v
+
+php exakat report -p phrozn -format CodeFlower -file aaaa -v
+php exakat report -p phrozn -format CodeFlower -file stdout -v
+php exakat report -p phrozn -format CodeFlower -v
+
+php exakat report -p phrozn -format Owasp -file aaaa -v
+php exakat report -p phrozn -format Owasp -file stdout -v
+php exakat report -p phrozn -format Owasp -v
+
+php exakat report -p phrozn -format Marmelab -file aaaa -v
+php exakat report -p phrozn -format Marmelab -file stdout -v
+php exakat report -p phrozn -format Marmelab -v
+
+php exakat report -p phrozn -format Drillinstructor -file aaaa -v
+php exakat report -p phrozn -format Drillinstructor -file stdout -v
+php exakat report -p phrozn -format Drillinstructor -v
+
+php exakat report -p phrozn -format Composer -file aaaa -v
+php exakat report -p phrozn -format Composer -file stdout -v
+php exakat report -p phrozn -format Composer -v
+
 cd -
+
 
