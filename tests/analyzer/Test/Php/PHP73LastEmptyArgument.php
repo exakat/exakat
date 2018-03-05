3c39e7392ce1a2b594fc7c13fe2816b704d3ad34
Damien Seguy <damien.seguy@gmail.com>
2018-03-05 19:42:09 +0100

    New analyser for PHP 7.3 : last argument is not compulsory

---
+++ tests/analyzer/Test/Php/PHP73LastEmptyArgument.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Test;
+
+include_once(dirname(dirname(dirname(dirname(__DIR__)))).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_test');
+spl_autoload_register('Autoload::autoload_phpunit');
+spl_autoload_register('Autoload::autoload_library');
+
+class Php_PHP73LastEmptyArgument extends Analyzer {
+    /* 1 methods */
+
+    public function testPhp_PHP73LastEmptyArgument01()  { $this->generic_test('Php/PHP73LastEmptyArgument.01'); }
+}
+?>
