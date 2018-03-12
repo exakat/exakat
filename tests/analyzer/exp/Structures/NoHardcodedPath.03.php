db81ff12d50cebbc804f015e60eb17e13af0c7dc
Damien Seguy <damien.seguy@gmail.com>
2018-03-12 16:07:35 +0100

    Upgraded UT, following the upgrade of the engine.

--- tests/analyzer/exp/Structures/NoHardcodedPath.03.php
+++ tests/analyzer/exp/Structures/NoHardcodedPath.03.php
@@ -1,10 +1,10 @@
 <?php
 
 $expected     = array('fopen(\'./\' . SITE, \'r+\')',
+                      'fopen("./$site", \'r+\')',
                      );
 
-$expected_not = array('fopen("./$site", \'r+\')',
-                      'fopen(\'http://www.php.net/\',\'r+\')',
+$expected_not = array('fopen(\'http://www.php.net/\',\'r+\')',
                       'fopen("https://$site",\'r+\')',
                       'fopen(\'http://\'.$site2.\'/\',\'r+\')',
                      );
