62495e25e225be2410576dd02d45fedf93842f67
Damien Seguy <damien.seguy@gmail.com>
2018-03-26 10:48:55 +0200

    More real life examples

--- scripts/docs/applications.php
+++ scripts/docs/applications.php
@@ -1,14 +1,41 @@
 <?php
-
+/*
+ * Copyright 2012-2018 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
 $applications = array(
+    'Contao'                     => array('url' => 'https://contao.org/en/'),
+    'Cleverstyle'                => array('url' => 'https://cleverstyle.org/en'),
+    'Dolibarr'                   => array('url' => 'https://www.dolibarr.org/'),
+    'Edusoho'                    => array('url' => 'https://www.edusoho.com/en'),
     'ExpressionEngine'           => array('url' => 'https://expressionengine.com/'),
     'LiveZilla'                  => array('url' => 'https://www.livezilla.net/home/en/'),
+    'Mautic'                     => array('url' => 'https://www.mautic.org/'),
+    'OpenConf'                   => array('url' => 'https://www.openconf.com/'),
+    'OpenEMR'                    => array('url' => 'https://www.open-emr.org/',),
     'phpMyAdmin'                 => array('url' => 'https://www.phpmyadmin.net/'),
     'PrestaShop'                 => array('url' => 'https://prestashop.com/'),
     'SPIP'                       => array('url' => 'https://www.spip.net/'),
     'TeamPass'                   => array('url' => 'https://teampass.net/'),
     'Traq'                       => array('url' => 'https://traq.io/'),
     'WordPress'                  => array('url' => 'https://www.wordpress.com/'),
+    'Thelia'                     => array('url' => 'https://thelia.net/'),
     'XOOPS'                      => array('url' => 'https://xoops.org/'),
 );
 
