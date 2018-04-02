e54f510386b8424d609058a625adc02d6abe2b90
Damien Seguy <damien.seguy@gmail.com>
2018-04-02 17:41:43 +0200

    Report erroneous regex in Melis documentation

---
+++ tests/analyzer/exp/Melis/CheckRegex.02.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array( '\'regex\' => \'.*/id/(?<idpage>[0-9+)\'', 
+                     );
+
+$expected_not = array('\'regex\' => \'.*/id/(?<idpage>[0-9]+)\'',
+                     );
+
+?>
