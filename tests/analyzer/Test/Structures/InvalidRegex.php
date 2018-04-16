76b9473faddb358ca6767a2fbfc708e55f180ada
Damien Seguy <damien.seguy@gmail.com>
2018-04-16 19:03:08 +0200

    Better handling of constants. Removed false positives.

--- tests/analyzer/Test/Structures/InvalidRegex.php
+++ tests/analyzer/Test/Structures/InvalidRegex.php
@@ -8,8 +8,9 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_InvalidRegex extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testStructures_InvalidRegex01()  { $this->generic_test('Structures/InvalidRegex.01'); }
+    public function testStructures_InvalidRegex02()  { $this->generic_test('Structures/InvalidRegex.02'); }
 }
 ?>
