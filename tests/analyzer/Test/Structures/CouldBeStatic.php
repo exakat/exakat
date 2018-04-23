9e6afe9276f7cf92f5cd3487c7615b52db76c7c2
Damien Seguy <damien.seguy@gmail.com>
2018-04-23 15:53:42 +0200

    Upgraded code with tests on superglobals

--- tests/analyzer/Test/Structures/CouldBeStatic.php
+++ tests/analyzer/Test/Structures/CouldBeStatic.php
@@ -8,9 +8,11 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_CouldBeStatic extends Analyzer {
-    /* 2 methods */
+    /* 4 methods */
 
     public function testStructures_CouldBeStatic01()  { $this->generic_test('Structures_CouldBeStatic.01'); }
     public function testStructures_CouldBeStatic02()  { $this->generic_test('Structures/CouldBeStatic.02'); }
+    public function testStructures_CouldBeStatic03()  { $this->generic_test('Structures/CouldBeStatic.03'); }
+    public function testStructures_CouldBeStatic04()  { $this->generic_test('Structures/CouldBeStatic.04'); }
 }
 ?>
---
+++ tests/analyzer/source/Structures/CouldBeStatic.03.php
@@ -0,0 +1,9 @@
+<?php
+
+function foo() {
+    global $_GET;
+    global $CONFIG;
+    
+    
+}
+?>
