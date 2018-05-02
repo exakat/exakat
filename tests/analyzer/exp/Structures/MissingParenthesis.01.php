457190cda63963ea57ab4265b07e70ecac78af85
Damien Seguy <damien.seguy@gmail.com>
2018-05-02 20:03:46 +0200

    New analysis : missing parenthesis

---
+++ tests/analyzer/exp/Structures/MissingParenthesis.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('-$a + $b',
+                      '!$a instanceof Stdclass',
+                      '-$a + $b - c',
+                     );
+
+$expected_not = array('-($a + $b)',
+                      '+$a + $b - c',
+                     );
+
+?>
