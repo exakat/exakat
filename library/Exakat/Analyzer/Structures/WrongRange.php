8fe10cd5e1015ed9f0559cf17b7d7fe58e3722a5
Damien Seguy <damien.seguy@gmail.com>
2018-05-04 10:14:38 +0200

    report wrong interval checks

---
+++ library/Exakat/Analyzer/Structures/WrongRange.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Copyright 2012-2018 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Analyzer\Structures;
+
+use Exakat\Analyzer\Analyzer;
+
+class WrongRange extends Analyzer {
+    public function analyze() {
+        // if ($a > 1 || $a < 1000)
+        $this->atomIs('Logical')
+             ->codeIs(array('||', 'or'))
+
+             ->outIs(array('LEFT', 'RIGHT'))
+             ->atomIs('Comparison')
+             ->codeIs(array('>', '>='))
+             ->outIs(array('LEFT', 'RIGHT'))
+             ->atomIs(self::$CONTAINERS)
+             ->savePropertyAs('fullcode', 'variable')
+             ->back('first')
+
+             ->outIs(array('LEFT', 'RIGHT'))
+             ->atomIs('Comparison')
+             ->codeIs(array('<', '<='))
+             ->outIs(array('LEFT', 'RIGHT'))
+             ->atomIs(self::$CONTAINERS)
+             ->samePropertyAs('fullcode', 'variable')
+
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/Test/Structures/WrongRange.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Test;
+
+include_once(dirname(dirname(dirname(dirname(__DIR__)))).'/library/Autoload.php');
+spl_autoload_register('Autoload::autoload_test');
+spl_autoload_register('Autoload::autoload_phpunit');
+spl_autoload_register('Autoload::autoload_library');
+
+class Structures_WrongRange extends Analyzer {
+    /* 1 methods */
+
+    public function testStructures_WrongRange01()  { $this->generic_test('Structures/WrongRange.01'); }
+}
+?>
