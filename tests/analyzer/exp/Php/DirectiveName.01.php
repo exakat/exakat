d6cb3a3bde9444c098d05441ac8043cdb728a989
Damien Seguy <damien.seguy@gmail.com>
2018-05-14 11:25:17 +0200

    Updated UT

--- tests/analyzer/exp/Php/DirectiveName.01.php
+++ tests/analyzer/exp/Php/DirectiveName.01.php
@@ -2,6 +2,20 @@
 
 $expected     = array('ini_set(\'NotADirective\', 0)',
                       '\\ini_get(\'AlsoNotADirective\')',
+                      'ini_get(\'session.auto_start\')', 
+                      'ini_get(\'session.use_trans_sid\')', 
+                      'ini_get(\'session.gc_maxlifetime\')', 
+                      'ini_get(\'session.cookie_lifetime\')', 
+                      'ini_set(\'session.use_trans_sid\', $config[\'use_trans_sid\'] ? 1 : 0)', 
+                      'ini_set(\'session.use_cookies\', $config[\'use_cookies\'] ? 1 : 0)', 
+                      'ini_get(\'session.use_cookies\')', 
+                      'ini_get(\'magic_quotes_sybase\')', 
+                      'ini_set(\'session.cookie_domain\', $config[\'domain\'])', 
+                      'ini_get(\'session.cookie_domain\')', 
+                      'ini_set(\'session.cookie_lifetime\', $config[\'expire\'])', 
+                      'ini_set(\'session.gc_maxlifetime\', $config[\'expire\'])', 
+                      'ini_set(\'session.auto_start\', 0)', 
+                      'ini_set("session.cookie_httponly", 1)',
                      );
 
 $expected_not = array('ini_get($a, $b)',
