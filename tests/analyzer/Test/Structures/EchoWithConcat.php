7ab428a98e05a6553f2f03579ca0125ad1bee2fd
Damien Seguy <damien.seguy@gmail.com>
2018-05-23 17:33:15 +0200

    Avoid reporting HEREDOC that doesn't contain variables

--- tests/analyzer/Test/Structures/EchoWithConcat.php
+++ tests/analyzer/Test/Structures/EchoWithConcat.php
@@ -8,11 +8,12 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_EchoWithConcat extends Analyzer {
-    /* 4 methods */
+    /* 5 methods */
 
     public function testStructures_EchoWithConcat01()  { $this->generic_test('Structures_EchoWithConcat.01'); }
     public function testStructures_EchoWithConcat02()  { $this->generic_test('Structures_EchoWithConcat.02'); }
     public function testStructures_EchoWithConcat03()  { $this->generic_test('Structures_EchoWithConcat.03'); }
     public function testStructures_EchoWithConcat04()  { $this->generic_test('Structures/EchoWithConcat.04'); }
+    public function testStructures_EchoWithConcat05()  { $this->generic_test('Structures/EchoWithConcat.05'); }
 }
 ?>
