83006f7e4953687ba46e0fc997e33de0d2a4dc5f
Damien Seguy <damien.seguy@gmail.com>
2018-05-24 16:43:12 +0200

    Extended analysis to Magicmethod

---
+++ tests/analyzer/exp/Classes/LocallyUsedProperty.06.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('$localyUsed = 1',
+                     );
+
+$expected_not = array('$usedInChild = 2',
+                      '$unused = 3',
+                      '$usedInGrandChild = 4',
+                     );
+
+?>
