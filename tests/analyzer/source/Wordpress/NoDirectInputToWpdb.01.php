0f56d60dba166e1659ab8c9b1b594969a9542e72
Damien Seguy <damien.seguy@gmail.com>
2018-05-25 18:16:57 +0200

    Fixed UT

--- tests/analyzer/source/Wordpress/NoDirectInputToWpdb.01.php
+++ tests/analyzer/source/Wordpress/NoDirectInputToWpdb.01.php
@@ -5,7 +5,7 @@ $query = $wpdb->prepare("SELECT * FROM table $where LIMIT %d, %d", 1, 2);
 
 $query = $wpdb->prepare("SELECT * FROM table2 %s LIMIT %d, %d", 1, 2, $_POST['data']['df']);
 
-$where = "WHERE foo = '" .  . "'";
+$where = "WHERE foo = '" . $c . "'";
 $query = $wpdb->prepare("SELECT * FROM table3 %s LIMIT %d, %d", 1, 2, esc_sql($_POST['data']));
 
 $query = $wpdb->prepare("SELECT * FROM table4 %s LIMIT %d, %d", 1, 2, 3);
