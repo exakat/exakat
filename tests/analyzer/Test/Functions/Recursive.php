eedba1fafccb8e70296ae1d149bad4360458e68e
Damien Seguy <damien.seguy@gmail.com>
2018-05-25 18:02:09 +0200

    Upgraded analysis to handle methods

--- tests/analyzer/Test/Functions/Recursive.php
+++ tests/analyzer/Test/Functions/Recursive.php
@@ -8,11 +8,12 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Functions_Recursive extends Analyzer {
-    /* 4 methods */
+    /* 5 methods */
 
     public function testFunctions_Recursive01()  { $this->generic_test('Functions_Recursive.01'); }
     public function testFunctions_Recursive02()  { $this->generic_test('Functions_Recursive.02'); }
     public function testFunctions_Recursive03()  { $this->generic_test('Functions_Recursive.03'); }
     public function testFunctions_Recursive04()  { $this->generic_test('Functions/Recursive.04'); }
+    public function testFunctions_Recursive05()  { $this->generic_test('Functions/Recursive.05'); }
 }
 ?>
