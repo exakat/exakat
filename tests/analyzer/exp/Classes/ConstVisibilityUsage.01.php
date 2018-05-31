70a3c99b8b7345bada769410cd16dbe19abf6982
Damien Seguy <damien.seguy@gmail.com>
2018-05-31 16:44:56 +0200

    Report usage of visibility for constants

---
+++ tests/analyzer/exp/Classes/ConstVisibilityUsage.01.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('protected const b = 2', 
+                      'private const c = 3', 
+                      'public const a = 1', 
+                      'public const a = 1'
+                     );
+
+$expected_not = array('const d = 4', 
+                      'const d = 4', 
+                     );
+
+?>
---
+++ tests/analyzer/source/Classes/ConstVisibilityUsage.01.php
@@ -0,0 +1,14 @@
+<?php
+
+class x {
+    public const a = 1;
+    protected const b = 2;
+    private const c = 3;
+    const d = 4;
+}
+
+interface i {
+    public const a = 1;
+      const d = 4;
+}
+?>
