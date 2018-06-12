0dfb0dace26b8523a4df31beccb159fd61afb7ea
Damien Seguy <damien.seguy@gmail.com>
2018-06-12 16:06:17 +0200

    userpath is provided if one of them is available

--- library/helpers.php
+++ library/helpers.php
@@ -144,13 +144,16 @@ function unparse_url($parsed_url) {
     $scheme   = isset($parsed_url['scheme'])   ? $parsed_url['scheme'].'://'   : '';
     $host     = isset($parsed_url['host'])     ? $parsed_url['host']           : '';
     $port     = isset($parsed_url['port'])     ? ':'.$parsed_url['port']       : '';
-    $user     = isset($parsed_url['user'])     ? $parsed_url['user']           : '';
-    $pass     = isset($parsed_url['pass'])     ? ':'.$parsed_url['pass']       : '';
-    $pass     = ($user || $pass)               ? $pass.'@'                     : '';
+
+    $user     = empty($parsed_url['user'])     ? '' : $parsed_url['user'];
+    $pass     = empty($parsed_url['pass'])     ? '' : ':'.$parsed_url['pass'];
+    $userpass     = ($user || $pass)           ? $pass.'@'                     : '';
+
     $path     = isset($parsed_url['path'])     ? $parsed_url['path']           : '';
     $query    = isset($parsed_url['query'])    ? '?'.$parsed_url['query']      : '';
     $fragment = isset($parsed_url['fragment']) ? '#'.$parsed_url['fragment']   : '';
-    return $scheme.$user.$pass.$host.$port.$path.$query.$fragment;
+
+    return "$scheme$userpass$host$port$path$query$fragment";
 }
 
 // Returns a list of unique values, when all values are arrays
