16e39cc3d51d4818cf5fd582b825ef1dd6e18fae
Damien Seguy <damien.seguy@gmail.com>
2018-06-18 09:46:16 +0200

    Cleaned code

--- server/api.php
+++ server/api.php
@@ -303,16 +303,19 @@ function serverLog($message) {
 }
 
 function unparse_url($parsed_url) {
-    $scheme   = isset($parsed_url['scheme'])   ? $parsed_url['scheme'].'://' : '';
+    $scheme   = isset($parsed_url['scheme'])   ? $parsed_url['scheme'].'://'   : '';
     $host     = isset($parsed_url['host'])     ? $parsed_url['host']           : '';
-    $port     = isset($parsed_url['port'])     ? ':'.$parsed_url['port']     : '';
-    $user     = isset($parsed_url['user'])     ? $parsed_url['user']           : '';
-    $pass     = isset($parsed_url['pass'])     ? ':'.$parsed_url['pass']     : '';
-    $pass     = ($user || $pass)               ? $pass.'@'                      : '';
+    $port     = isset($parsed_url['port'])     ? ':'.$parsed_url['port']       : '';
+
+    $user     = empty($parsed_url['user'])     ? '' : $parsed_url['user'];
+    $pass     = empty($parsed_url['pass'])     ? '' : ':'.$parsed_url['pass'];
+    $userpass = ($user || $pass)               ? "$user$pass@"                 : '';
+
     $path     = isset($parsed_url['path'])     ? $parsed_url['path']           : '';
-    $query    = isset($parsed_url['query'])    ? '?'.$parsed_url['query']    : '';
-    $fragment = isset($parsed_url['fragment']) ? '#'.$parsed_url['fragment'] : '';
-    return $scheme.$user.$pass.$host.$port.$path.$query.$fragment;
+    $query    = isset($parsed_url['query'])    ? '?'.$parsed_url['query']      : '';
+    $fragment = isset($parsed_url['fragment']) ? '#'.$parsed_url['fragment']   : '';
+
+    return "$scheme$userpass$host$port$path$query$fragment";
 }
 
 /**
