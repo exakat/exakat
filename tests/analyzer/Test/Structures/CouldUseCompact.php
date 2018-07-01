ba2c069a4a3813b841f4d62183a7369c4bb0c8c7
Damien Seguy <damien.seguy@gmail.com>
2018-07-02 00:10:30 +0200

    Upraded could use compact with better handling of constants

--- tests/analyzer/Test/Structures/CouldUseCompact.php
+++ tests/analyzer/Test/Structures/CouldUseCompact.php
@@ -8,12 +8,13 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_CouldUseCompact extends Analyzer {
-    /* 5 methods */
+    /* 6 methods */
 
     public function testStructures_CouldUseCompact01()  { $this->generic_test('Structures/CouldUseCompact.01'); }
     public function testStructures_CouldUseCompact02()  { $this->generic_test('Structures/CouldUseCompact.02'); }
     public function testStructures_CouldUseCompact03()  { $this->generic_test('Structures/CouldUseCompact.03'); }
     public function testStructures_CouldUseCompact04()  { $this->generic_test('Structures/CouldUseCompact.04'); }
     public function testStructures_CouldUseCompact05()  { $this->generic_test('Structures/CouldUseCompact.05'); }
+    public function testStructures_CouldUseCompact06()  { $this->generic_test('Structures/CouldUseCompact.06'); }
 }
 ?>
