3f068b5893e1a83086e645fa1146dfbb9a6343fd
Damien Seguy <damien.seguy@gmail.com>
2018-07-10 16:50:24 +0200

    Support for new extension  lzf

---
+++ tests/analyzer/exp/Extensions/Extlzf.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('lzf_compress("This is test of LZF extension")',
+                     );
+
+$expected_not = array('lzf_uncompress($compressed)',
+                     );
+
+?>
