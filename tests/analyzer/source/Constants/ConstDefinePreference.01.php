fba28bd4b62cfc1adb01a1fb8a925ee757044e71
Damien Seguy <damien.seguy@gmail.com>
2018-08-01 08:23:30 -0400

    New analyser : define or const

---
+++ tests/analyzer/source/Constants/ConstDefinePreference.01.php
@@ -0,0 +1,16 @@
+<?php
+
+const A1 = 1;
+const A2 = 1;
+const A3 = 1;
+const A4 = 1;
+const A5 = 1;
+const A6 = 1;
+const A7 = 1;
+const A8 = 1;
+const A9 = 1;
+const A10 = 1;
+
+define('B', 3);
+
+?>
