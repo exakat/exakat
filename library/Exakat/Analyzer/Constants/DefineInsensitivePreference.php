46af80860d471bb5dc616e102c8cc599e914091c
Damien Seguy <damien.seguy@gmail.com>
2018-08-01 08:36:00 -0400

    Report case sensitivity preference

---
+++ library/Exakat/Analyzer/Constants/DefineInsensitivePreference.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * Copyright 2012-2018 Damien Seguy – Exakat Ltd <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+
+namespace Exakat\Analyzer\Constants;
+
+use Exakat\Analyzer\Analyzer;
+
+class DefineInsensitivePreference extends Analyzer {
+
+    public function analyze() {
+        $mapping = <<<GREMLIN
+x2 = it.get().vertices(OUT, "ARGUMENT").size();
+GREMLIN;
+        $storage = array('sensitive'   => 2,
+                         'insensitive' => 3);
+
+        $this->atomIs('Defineconstant')
+             ->raw("map{ $mapping }")
+             ->raw('groupCount("gf").cap("gf").sideEffect{ s = it.get().values().sum(); }');
+        $types = $this->rawQuery()->toArray()[0];
+
+        $store = array();
+        $total = 0;
+        foreach($storage as $key => $v) {
+            $c = empty($types[$v]) ? 0 : $types[$v];
+            $store[] = array('key'   => $key,
+                             'value' => $c);
+            $total += $c;
+        }
+        Analyzer::$datastore->addRowAnalyzer($this->analyzerQuoted, $store);
+        if ($total === 0) {
+            return;
+        }
+
+        $types = array_filter($types, function ($x) use ($total) { return $x > 0 && $x / $total < 0.1; });
+        if (empty($types)) {
+            return;
+        }
+        $types = array_keys($types);
+
+        $this->atomIs('Defineconstant')
+             ->raw("sideEffect{ $mapping }")
+             ->raw('filter{ x2 in ***}', $types)
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
