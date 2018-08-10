ceee36e32acd5a4e574b202d111e3492eda313be
Damien Seguy <damien.seguy@gmail.com>
2018-08-10 06:53:05 -0400

    New analyzer : report Relaxed Heredoc

---
+++ tests/analyzer/exp/Php/RelaxedHeredoc.01.php
@@ -0,0 +1,27 @@
+<?php
+
+$expected     = array('<<<RELAXEDT
+	
+	
+	RELAXEDT',
+                      '<<<\'RELAXED4\'
+    b
+    c
+    RELAXED4',
+                      '<<<RELAXED1
+  
+  
+ RELAXED1',
+                      '<<<RELAXED2
+  
+  
+  RELAXED2',
+                     );
+
+$expected_not = array('<<<NORMAL
+  
+  
+RELAXED2',
+                     );
+
+?>
---
+++ tests/analyzer/source/Php/RelaxedHeredoc.01.php
@@ -0,0 +1,27 @@
+<?php
+
+$a = <<<NORMAL
+
+NORMAL;
+
+$b = <<<RELAXED1
+  
+  
+ RELAXED1;
+
+$b = <<<RELAXED2
+  
+  
+  RELAXED2;
+
+$b = <<<RELAXEDT
+	
+	
+	RELAXEDT;
+
+$b = <<<'RELAXED4'
+    b
+    c
+    RELAXED4;
+
+?>
