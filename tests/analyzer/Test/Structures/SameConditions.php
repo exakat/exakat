84ffefb4c95b66fba83c00de64743c286fe1d3fa
Damien Seguy <damien.seguy@gmail.com>
2018-08-13 07:26:49 -0400

    Extra UT

--- tests/analyzer/Test/Structures/SameConditions.php
+++ tests/analyzer/Test/Structures/SameConditions.php
@@ -8,10 +8,11 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class Structures_SameConditions extends Analyzer {
-    /* 3 methods */
+    /* 4 methods */
 
     public function testStructures_SameConditions01()  { $this->generic_test('Structures/SameConditions.01'); }
     public function testStructures_SameConditions02()  { $this->generic_test('Structures/SameConditions.02'); }
     public function testStructures_SameConditions03()  { $this->generic_test('Structures/SameConditions.03'); }
+    public function testStructures_SameConditions04()  { $this->generic_test('Structures/SameConditions.04'); }
 }
 ?>
---
+++ tests/analyzer/exp/Structures/SameConditions.04.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('if($a1 == 1) { /**/ } else { /**/ } ', 
+                      'if($a2 == 1) { /**/ } else { /**/ } ',
+                     );
+
+$expected_not = array('if($a3 == 1) { /**/ } else { /**/ } ',
+                     );
+
+?>
