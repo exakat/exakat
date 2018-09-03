5f4c1db7a8980b5e0faa86f49fef0e11daab2404
Damien Seguy <damien.seguy@gmail.com>
2018-09-03 14:58:11 +0200

    Extended tests to constants.

--- tests/analyzer/exp/Structures/ExitUsage.03.php
+++ tests/analyzer/exp/Structures/ExitUsage.03.php
@@ -5,6 +5,8 @@ $expected     = array(
 
 $expected_not = array('exit( )',
                       'exit(2)',
+                      'exit(3)',
+                      'eXit'
                      );
 
 ?>
--- tests/analyzer/source/Structures/ExitUsage.03.php
+++ tests/analyzer/source/Structures/ExitUsage.03.php
@@ -1,6 +1,13 @@
 #!/usr/bin/php
 <?php
 
+class x {
+    const eXit = 2;
+}
+
+echo x::eXit;
+
 A::exit(2);
+$a->exit(3);
 exit();
 ?>
