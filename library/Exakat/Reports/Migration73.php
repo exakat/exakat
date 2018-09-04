208415bf1ddf32498897f9253dfe63e1c0bd70c5
Damien Seguy <damien.seguy@gmail.com>
2018-09-04 19:08:36 +0200

    Finished PHP 7.3 migration audit

--- library/Exakat/Reports/Migration73.php
+++ library/Exakat/Reports/Migration73.php
@@ -31,10 +31,23 @@ use Exakat\Reports\Reports;
 class Migration73 extends Ambassador {
     const FILE_FILENAME  = 'migration73';
     const FILE_EXTENSION = '';
+    
+    private $analyzerList = array();
+    private $theme = 'CompatibilityPHP73';
 
     public function dependsOnAnalysis() {
         return array('CompatibilityPHP73',
                      );
+    }
+
+    public function __construct($config) {
+        parent::__construct($config);
+
+        $this->frequences        = $this->themes->getFrequences();
+        $this->timesToFix        = $this->themes->getTimesToFix();
+        $this->themesForAnalyzer = $this->themes->getThemesForAnalyzer();
+        $this->severities        = $this->themes->getSeverities();
+        $this->analyzerList      = $this->themes->getThemeAnalyzers($this->theme);
     }
 
     protected function getBasedPage($file) {
@@ -86,15 +99,11 @@ class Migration73 extends Ambassador {
 
         // Compatibility
         $this->generateCompilations();
-        $res = $this->sqlite->query('SELECT DISTINCT SUBSTR(thema, -2) AS version FROM themas WHERE thema LIKE "CompatibilityPHP73"');
-        $list = array();
-        while($row = $res->fetchArray(\SQLITE3_ASSOC)) {
-            $list[] = "CompatibilityPHP$row[version]";
-            $this->generateCompatibility($row['version']);
-        }
         $this->generateCompatibilityEstimate();
-        $this->generateIssuesEngine('compatibility_issues',
-                                    $this->getIssuesFaceted($list));
+        $this->generateCompatibility('73');
+        
+        $this->generateIssuesEngine('issues',
+                                    $this->getIssuesFaceted('CompatibilityPHP73'));
 
         // Annex
         $this->generateAnalyzerSettings();
@@ -155,6 +164,397 @@ class Migration73 extends Ambassador {
         }
 
         return $data;
+    }
+
+    protected function generateCompilations() {
+        $compilations = '';
+
+        $total = $this->sqlite->querySingle('SELECT value FROM hash WHERE key = "files"');
+        $info = array();
+        
+        $suffixes = array_unique(array_merge(array('73', $this->config->phpversion[0].$this->config->phpversion[2]), $this->config->other_php_versions));
+        foreach($suffixes as $suffix) {
+            $version = "$suffix[0].$suffix[1]";
+            $res = $this->sqlite->querySingle("SELECT name FROM sqlite_master WHERE type='table' AND name='compilation$suffix'");
+            if (!$res) {
+                $compilations .= "<tr><td>$version</td><td>N/A</td><td>N/A</td><td>Compilation not tested</td><td>N/A</td></tr>\n";
+                continue; // Table was not created
+            }
+
+            $res = $this->sqlite->query("SELECT file FROM compilation$suffix");
+            $files = array();
+            while($row = $res->fetchArray(\SQLITE3_ASSOC)) {
+                $files[] = $row['file'];
+            }
+            if (empty($files)) {
+                $files       = 'No compilation error found.';
+                $errors      = 'N/A';
+                $total_error = 'N/A';
+            } else {
+                $res = $this->sqlite->query('SELECT error FROM compilation'.$suffix);
+                $readErrors = array();
+                while($row = $res->fetchArray(\SQLITE3_ASSOC)) {
+                    $readErrors[] = $row['error'];
+                }
+                $errors      = array_count_values($readErrors);
+                $errors      = array_keys($errors);
+                $errors      = array_keys(array_count_values($errors));
+                $errors       = '<ul><li>'.implode("</li>\n<li>", $errors).'</li></ul>';
+
+                $total_error = count($files).' ('.number_format(count($files) / $total * 100, 0).'%)';
+                $files       = array_keys(array_count_values($files));
+                $files       = '<ul><li>'.implode("</li>\n<li>", $files).'</li></ul>';
+            }
+
+            $compilations .= "<tr><td>$version</td><td>$total</td><td>$total_error</td><td>$files</td><td>$errors</td></tr>\n";
+        }
+
+        $html = $this->getBasedPage('compatibility_compilations');
+        $html = $this->injectBloc($html, 'COMPILATIONS', $compilations);
+        $html = $this->injectBloc($html, 'TITLE', 'Compilations overview');
+        $this->putBasedPage('compatibility_compilations', $html);
+    }
+
+    protected function generateDashboard() {
+        $baseHTML = $this->getBasedPage('index_migration');
+        $this->themesToShow = array($this->theme);
+
+        $tags = array();
+        $code = array();
+
+        // Bloc top left
+        $hashData = $this->getHashData();
+        $finalHTML = $this->injectBloc($baseHTML, 'BLOCHASHDATA', $hashData);
+
+        // bloc severity
+        $severity = $this->getSeverityBreakdown();
+        $finalHTML = $this->injectBloc($finalHTML, 'BLOCSEVERITY', $severity['html']);
+        $tags[] = 'SCRIPTSEVERITY';
+        $code[] = $severity['script'];
+
+        // Marking the audit date
+        $this->makeAuditDate($finalHTML);
+
+        // top 10
+        $fileHTML = $this->getTopFile($this->analyzerList);
+        $finalHTML = $this->injectBloc($finalHTML, 'TOPFILE', $fileHTML);
+        $analyzerHTML = $this->getTopAnalyzers($this->analyzerList);
+        $finalHTML = $this->injectBloc($finalHTML, 'TOPANALYZER', $analyzerHTML);
+
+        $blocjs = <<<JAVASCRIPT
+  <script>
+    $(document).ready(function() {
+      Morris.Donut({
+        element: 'donut-chart_severity',
+        resize: true,
+        colors: ["#3c8dbc", "#f56954", "#00a65a", "#1424b8"],
+        data: [SCRIPTSEVERITY]
+      });
+      Highcharts.theme = {
+         colors: ["#F56954", "#f7a35c", "#ffea6f", "#D2D6DE"],
+         chart: {
+            backgroundColor: null,
+            style: {
+               fontFamily: "Dosis, sans-serif"
+            }
+         },
+         title: {
+            style: {
+               fontSize: '16px',
+               fontWeight: 'bold',
+               textTransform: 'uppercase'
+            }
+         },
+         tooltip: {
+            borderWidth: 0,
+            backgroundColor: 'rgba(219,219,216,0.8)',
+            shadow: false
+         },
+         legend: {
+            itemStyle: {
+               fontWeight: 'bold',
+               fontSize: '13px'
+            }
+         },
+         xAxis: {
+            gridLineWidth: 1,
+            labels: {
+               style: {
+                  fontSize: '12px'
+               }
+            }
+         },
+         yAxis: {
+            minorTickInterval: 'auto',
+            title: {
+               style: {
+                  textTransform: 'uppercase'
+               }
+            },
+            labels: {
+               style: {
+                  fontSize: '12px'
+               }
+            }
+         },
+         plotOptions: {
+            candlestick: {
+               lineColor: '#404048'
+            }
+         },
+
+
+         // General
+         background2: '#F0F0EA'
+      };
+
+      // Apply the theme
+      Highcharts.setOptions(Highcharts.theme);
+
+      $('#filename').highcharts({
+          credits: {
+            enabled: false
+          },
+
+          exporting: {
+            enabled: false
+          },
+
+          chart: {
+              type: 'column'
+          },
+          title: {
+              text: ''
+          },
+          xAxis: {
+              categories: [SCRIPTDATAFILES]
+          },
+          yAxis: {
+              min: 0,
+              title: {
+                  text: ''
+              },
+              stackLabels: {
+                  enabled: false,
+                  style: {
+                      fontWeight: 'bold',
+                      color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                  }
+              }
+          },
+          legend: {
+              align: 'right',
+              x: 0,
+              verticalAlign: 'top',
+              y: -10,
+              floating: false,
+              backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+              borderColor: '#CCC',
+              borderWidth: 1,
+              shadow: false
+          },
+          tooltip: {
+              headerFormat: '<b>{point.x}</b><br/>',
+              pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+          },
+          plotOptions: {
+              column: {
+                  stacking: 'normal',
+                  dataLabels: {
+                      enabled: false,
+                      color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                      style: {
+                          textShadow: '0 0 3px black'
+                      }
+                  }
+              }
+          },
+          series: [{
+              name: 'Critical',
+              data: [SCRIPTDATACRITICAL]
+          }, {
+              name: 'Major',
+              data: [SCRIPTDATAMAJOR]
+          }, {
+              name: 'Minor',
+              data: [SCRIPTDATAMINOR]
+          }, {
+              name: 'None',
+              data: [SCRIPTDATANONE]
+          }]
+      });
+
+      $('#container').highcharts({
+          credits: {
+            enabled: false
+          },
+
+          exporting: {
+            enabled: false
+          },
+
+          chart: {
+              type: 'column'
+          },
+          title: {
+              text: ''
+          },
+          xAxis: {
+              categories: [SCRIPTDATAANALYZERLIST]
+          },
+          yAxis: {
+              min: 0,
+              title: {
+                  text: ''
+              },
+              stackLabels: {
+                  enabled: false,
+                  style: {
+                      fontWeight: 'bold',
+                      color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                  }
+              }
+          },
+          legend: {
+              align: 'right',
+              x: 0,
+              verticalAlign: 'top',
+              y: -10,
+              floating: false,
+              backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+              borderColor: '#CCC',
+              borderWidth: 1,
+              shadow: false
+          },
+          tooltip: {
+              headerFormat: '<b>{point.x}</b><br/>',
+              pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+          },
+          plotOptions: {
+              column: {
+                  stacking: 'normal',
+                  dataLabels: {
+                      enabled: false,
+                      color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                      style: {
+                          textShadow: '0 0 3px black'
+                      }
+                  }
+              }
+          },
+          series: [{
+              name: 'Critical',
+              data: [SCRIPTDATAANALYZERCRITICAL]
+          }, {
+              name: 'Major',
+              data: [SCRIPTDATAANALYZERMAJOR]
+          }, {
+              name: 'Minor',
+              data: [SCRIPTDATAANALYZERMINOR]
+          }, {
+              name: 'None',
+              data: [SCRIPTDATAANALYZERNONE]
+          }]
+      });
+    });
+  </script>
+JAVASCRIPT;
+
+        // Filename Overview
+        $fileOverview = $this->getFileOverview();
+        $tags[] = 'SCRIPTDATAFILES';
+        $code[] = $fileOverview['scriptDataFiles'];
+        $tags[] = 'SCRIPTDATAMAJOR';
+        $code[] = $fileOverview['scriptDataMajor'];
+        $tags[] = 'SCRIPTDATACRITICAL';
+        $code[] = $fileOverview['scriptDataCritical'];
+        $tags[] = 'SCRIPTDATANONE';
+        $code[] = $fileOverview['scriptDataNone'];
+        $tags[] = 'SCRIPTDATAMINOR';
+        $code[] = $fileOverview['scriptDataMinor'];
+
+        // Analyzer Overview
+        $analyzerOverview = $this->getAnalyzerOverview();
+        $tags[] = 'SCRIPTDATAANALYZERLIST';
+        $code[] = $analyzerOverview['scriptDataAnalyzer'];
+        $tags[] = 'SCRIPTDATAANALYZERMAJOR';
+        $code[] = $analyzerOverview['scriptDataAnalyzerMajor'];
+        $tags[] = 'SCRIPTDATAANALYZERCRITICAL';
+        $code[] = $analyzerOverview['scriptDataAnalyzerCritical'];
+        $tags[] = 'SCRIPTDATAANALYZERNONE';
+        $code[] = $analyzerOverview['scriptDataAnalyzerNone'];
+        $tags[] = 'SCRIPTDATAANALYZERMINOR';
+        $code[] = $analyzerOverview['scriptDataAnalyzerMinor'];
+
+        $blocjs = str_replace($tags, $code, $blocjs);
+        $finalHTML = $this->injectBloc($finalHTML, 'BLOC-JS',  $blocjs);
+        $finalHTML = $this->injectBloc($finalHTML, 'TITLE', 'Issues\' dashboard');
+        $this->putBasedPage('index', $finalHTML);
+    }
+
+    protected function getSeveritiesNumberBy($type = 'file') {
+        $listSQL = makeList($this->analyzerList);
+        $list = $this->analyzerList;
+        
+        $query = <<<SQL
+SELECT $type, severity, count(*) AS count
+    FROM results
+    WHERE analyzer IN ($listSQL)
+    GROUP BY $type, severity
+SQL;
+
+        $stmt = $this->sqlite->query($query);
+
+        $return = array();
+        while ($row = $stmt->fetchArray(\SQLITE3_ASSOC) ) {
+            if ( isset($return[$row[$type]]) ) {
+                $return[$row[$type]][$row['severity']] = $row['count'];
+            } else {
+                $return[$row[$type]] = array($row['severity'] => $row['count']);
+            }
+        }
+
+        return $return;
+    }
+
+    protected function getTotalAnalysedFile() {
+        $query = "SELECT COUNT(DISTINCT file) FROM results WHERE file LIKE '/%' AND analyzer in (".makeList($this->analyzerList).")";
+        $result = $this->sqlite->query($query);
+
+        $result = $result->fetchArray(\SQLITE3_NUM);
+        return $result[0];
+    }
+
+    protected function getTotalAnalyzer($issues = false) {
+        $query = "SELECT count(*) AS total, COUNT(CASE WHEN rc.count != 0 THEN 1 ELSE null END) AS yielding 
+            FROM resultsCounts AS rc
+            WHERE rc.count >= 0 AND
+                  analyzer in (".makeList($this->analyzerList).")";
+
+        $stmt = $this->sqlite->prepare($query);
+        $result = $stmt->execute();
+
+        return $result->fetchArray(\SQLITE3_NUM);
+    }
+
+    protected function cleanFolder() {
+        if (file_exists("{$this->tmpName}/datas/base.html")) {
+            unlink("{$this->tmpName}/datas/base.html");
+            unlink("{$this->tmpName}/datas/menu.html");
+            unlink("{$this->tmpName}/datas/index_migration.html");
+        }
+
+        // Clean final destination
+        if ($this->finalName !== '/') {
+            rmdirRecursive($this->finalName);
+        }
+
+        if (file_exists($this->finalName)) {
+            display($this->finalName." folder was not cleaned. Please, remove it before producing the report. Aborting report\n");
+            return;
+        }
+
+        rename($this->tmpName, $this->finalName);
     }
 }
 
