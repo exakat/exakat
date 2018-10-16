faef9680472bf3f977b19defb1f2acf724f8b3e3
Damien Seguy <damien.seguy@gmail.com>
2018-10-16 17:56:10 +0200

    Refactored analysis without MAX_LOOPING

--- library/Exakat/Analyzer/Classes/PropertyUsedBelow.php
+++ library/Exakat/Analyzer/Classes/PropertyUsedBelow.php
@@ -26,51 +26,39 @@ use Exakat\Analyzer\Analyzer;
 
 class PropertyUsedBelow extends Analyzer {
     public function analyze() {
-        $MAX_LOOPING = self::MAX_LOOPING;
-        
         //////////////////////////////////////////////////////////////////
         // property + $this->property
         //////////////////////////////////////////////////////////////////
-        $this->atomIs('Ppp')
+        $this->atomIs('Class')
+             ->savePropertyAs('fullnspath', 'fnp')
+             ->outIs('PPP')
              ->isNot('static', true)
              ->outIs('PPP')
              ->_as('ppp')
-             ->savePropertyAs('propertyname', 'propertyname')
+             ->outIs('DEFINITION')
+             ->atomIs('Member')
              ->goToClass()
-             ->raw(<<<GREMLIN
-where( __.repeat( out("DEFINITION").in("EXTENDS") ).emit().times($MAX_LOOPING)
-         .where( __.out("METHOD").out("BLOCK")
-                   .repeat( __.out({$this->linksDown})).emit(hasLabel("Member")).times($MAX_LOOPING)
-                   .where( __.out("OBJECT").hasLabel("This"))
-                   .out("MEMBER").has("token", "T_STRING").filter{ it.get().value("code") == propertyname}
-          )
-)
-GREMLIN
-)
+             ->goToAllParents(self::INCLUDE_SELF)
+             ->samePropertyAs('fullnspath', 'fnp')
              ->back('ppp');
         $this->prepareQuery();
 
         //////////////////////////////////////////////////////////////////
         // static property : inside the self class
         //////////////////////////////////////////////////////////////////
-        $this->atomIs('Ppp')
+        $this->atomIs('Class')
+             ->savePropertyAs('fullnspath', 'fnp')
+             ->outIs('PPP')
              ->is('static', true)
              ->outIs('PPP')
              ->_as('ppp')
-             ->savePropertyAs('code', 'property')
+             ->outIs('DEFINITION')
+             ->atomIs('Staticproperty')
              ->goToClass()
-             ->raw(<<<GREMLIN
-where( __.repeat( out("DEFINITION").in("EXTENDS") ).emit().times($MAX_LOOPING)
-      .where( __.out("METHOD").out("BLOCK")
-                .repeat( __.out({$this->linksDown})).emit(hasLabel("Staticproperty")).times($MAX_LOOPING)
-                .out("MEMBER").has("token", "T_VARIABLE").filter{ it.get().value("code") == property}
-      )
-)
-GREMLIN
-)
+             ->goToAllParents(self::INCLUDE_SELF)
+             ->samePropertyAs('fullnspath', 'fnp')
              ->back('ppp');
         $this->prepareQuery();
-        
         // This could be also checking for fnp : it needs to be a 'family' class check.
     }
 }
