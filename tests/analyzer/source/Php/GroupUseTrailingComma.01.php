4512e467c456993f06ff297e6f698cd7302ec952
Damien Seguy <damien.seguy@gmail.com>
2018-10-30 08:44:47 +0100

    Update UT

--- tests/analyzer/source/Php/GroupUseTrailingComma.01.php
+++ tests/analyzer/source/Php/GroupUseTrailingComma.01.php
@@ -5,8 +5,9 @@ use a\b\{c, d, e, f, };
 use a1\b1\{c1, d1, e1, f1};
 
 trait t {
-use a3\b3\{c3, d3};
+    use HelloWorld { sayHello as protected; 
+                     B::smallTalk insteadof A;}
 
-use a4\b4\{c4, d4,};
+    use dd\d { c4 as protected; }
 }
 ?>
