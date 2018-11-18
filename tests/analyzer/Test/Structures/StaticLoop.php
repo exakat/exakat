1570e06655a5e95bacf52704552ed2b3c8bda5e8
Damien Seguy <damien.seguy@gmail.com>
2018-11-18 10:08:33 +0100

    Extra check on calling function (may be a static methodcall)

--- tests/analyzer/Test/Structures/StaticLoop.php
+++ tests/analyzer/Test/Structures/StaticLoop.php
@@ -10,7 +10,7 @@ spl_autoload_register('Autoload::autoload_phpunit');
 spl_autoload_register('Autoload::autoload_library');
 
 class StaticLoop extends Analyzer {
-    /* 7 methods */
+    /* 8 methods */
 
     public function testStructures_StaticLoop01()  { $this->generic_test('Structures_StaticLoop.01'); }
     public function testStructures_StaticLoop02()  { $this->generic_test('Structures_StaticLoop.02'); }
@@ -19,5 +19,6 @@ class StaticLoop extends Analyzer {
     public function testStructures_StaticLoop05()  { $this->generic_test('Structures/StaticLoop.05'); }
     public function testStructures_StaticLoop06()  { $this->generic_test('Structures/StaticLoop.06'); }
     public function testStructures_StaticLoop07()  { $this->generic_test('Structures/StaticLoop.07'); }
+    public function testStructures_StaticLoop08()  { $this->generic_test('Structures/StaticLoop.08'); }
 }
 ?>
---
+++ tests/analyzer/source/Structures/StaticLoop.08.php
@@ -0,0 +1,16 @@
+<?php
+
+foreach($code as $k => &$v) {
+    $v = strtolower($v);
+}
+
+foreach($code as $k => $v2) {
+    $v2 = strtolower($v2);
+}
+
+foreach($code as $k => $v3) {
+    $v = strtolower($v);
+    $a->b['c']($d, false, $e - 1);
+}
+
+?>
