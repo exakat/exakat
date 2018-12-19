251eb5f8eb7c7e4e03135a44031abc04a2b22cb5
Damien Seguy <damien.seguy@gmail.com>
2018-12-19 10:32:15 +0100

    Extra check when building 'new issues' list

--- library/Exakat/Reports/Ambassador.php
+++ library/Exakat/Reports/Ambassador.php
@@ -2058,7 +2058,7 @@ JAVASCRIPTCODE;
         $sqlite = new \Sqlite3($path);
         $res = $sqlite->query('SELECT count(*) FROM sqlite_master WHERE type = "table" AND name != "sqlite_sequence";');
         
-        if ($res->fetchArray(\SQLITE3_NUM) < 10) {
+        if ($res->fetchArray(\SQLITE3_NUM)[0] < 10) {
             return array();
         }
 
