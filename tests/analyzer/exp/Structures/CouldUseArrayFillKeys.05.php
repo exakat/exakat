d0231f143917ea0d6228312c564668a9b2998e62
Damien Seguy <damien.seguy@gmail.com>
2018-12-31 17:14:47 +0100

    Fixed UT

--- tests/analyzer/exp/Structures/CouldUseArrayFillKeys.05.php
+++ tests/analyzer/exp/Structures/CouldUseArrayFillKeys.05.php
@@ -2,7 +2,7 @@
 
 $expected     = array('array_map(\'foo\', $a)', 
                       'array_map(\'x::foo\', $a)', 
-                      'array_map(array(\'\\x\', \'foo\'), $a)',
+                      'array_map(array(\'\\\\x\', \'foo\'), $a)',
                      );
 
 $expected_not = array('array_map(\'foo2\', $a)', 
