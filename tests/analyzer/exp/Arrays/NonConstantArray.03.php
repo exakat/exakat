84a0dd64f25118867a23080f507b7ea542fcd799
Damien Seguy <damien.seguy@gmail.com>
2018-12-31 17:00:47 +0100

    Upgraded UT for Heredoc

--- tests/analyzer/exp/Arrays/NonConstantArray.03.php
+++ tests/analyzer/exp/Arrays/NonConstantArray.03.php
@@ -1,10 +1,10 @@
 <?php
 
 $expected     = array('D1',
-                      'D3',
                      );
 
 $expected_not = array('D2',
+                      'D3',
                       'E_ALL',
                      );
 
--- tests/analyzer/source/Arrays/NonConstantArray.03.php
+++ tests/analyzer/source/Arrays/NonConstantArray.03.php
@@ -1,7 +1,9 @@
 <?php
- $b = "B$cC$d[D2]E";
- $b2 = 'B'.$c.'C'.$d[D1].'E';
- $b3 = <<<DD
+   $b = "B$cC$d[D2]E";
+   $b2 = 'B'.$c.'C'.$d[D1].'E';
+   $b3 = <<<DD
 B$cC$d[D3]E
 DD;
- $b4 = "B$cC$d[E_ALL]E";
+   $b4 = "B$cC$d[E_ALL]E";
+   
+?>
