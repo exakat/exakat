4b3731fa1f0def89f73a089cbe02456dab122f33
Damien Seguy <damien.seguy@gmail.com>
2018-12-31 16:00:33 +0100

    Fixed UT

--- tests/analyzer/exp/Php/PHP73LastEmptyArgument.03.php
+++ tests/analyzer/exp/Php/PHP73LastEmptyArgument.03.php
@@ -1,12 +1,12 @@
 <?php
 
 $expected     = array('class(1, 2,  )',
-                      'too(1,2,3,  )',
+                      'too(1, 2, 3,  )',
                      );
 
 $expected_not = array('class(1, 2, 3)',
                       'class',
-                      'too(1,2,3,4)',
+                      'too(1, 2, 3, 4)',
                      );
 
 ?>
