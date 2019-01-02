c7ae6e0489dbf5ba21332067943ecfc5d6553bca
Damien Seguy <damien.seguy@gmail.com>
2019-01-02 17:44:58 +0100

    added extra protection for \ in regex

--- library/Exakat/Analyzer/Classes/AvoidUsing.php
+++ library/Exakat/Analyzer/Classes/AvoidUsing.php
@@ -80,7 +80,7 @@ class AvoidUsing extends Analyzer {
         // class_alias is covered by string test just below
         // mentions in strings
         $this->atomIs('String')
-             ->regexIs('noDelimiter', implode('|', $classes));
+             ->regexIs('noDelimiter', addslashes(implode('|', $classes)));
         $this->prepareQuery();
     }
 }
