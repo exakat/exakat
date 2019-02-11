bb819bde16c1b9e046639bda12fe4c1cfa5d48d3
Damien Seguy <damien.seguy@gmail.com>
2019-02-11 20:53:36 +0100

    Omit array() and list() and their usage as argument

--- tests/analyzer/Test/Php/PHP73LastEmptyArgument.php
+++ tests/analyzer/Test/Php/PHP73LastEmptyArgument.php
@@ -7,10 +7,11 @@ use Test\Analyzer;
 include_once './Test/Analyzer.php';
 
 class PHP73LastEmptyArgument extends Analyzer {
-    /* 3 methods */
+    /* 4 methods */
 
     public function testPhp_PHP73LastEmptyArgument01()  { $this->generic_test('Php/PHP73LastEmptyArgument.01'); }
     public function testPhp_PHP73LastEmptyArgument02()  { $this->generic_test('Php/PHP73LastEmptyArgument.02'); }
     public function testPhp_PHP73LastEmptyArgument03()  { $this->generic_test('Php/PHP73LastEmptyArgument.03'); }
+    public function testPhp_PHP73LastEmptyArgument04()  { $this->generic_test('Php/PHP73LastEmptyArgument.04'); }
 }
 ?>
---
+++ tests/analyzer/exp/Php/PHP73LastEmptyArgument.04.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('list($a, $b, $d,  )', 
+                      'array(\'b\', $d,  )',
+                     );
+
+$expected_not = array('list($a, $b, $c,  )', 
+                      'array(\'b\', $c,  )',
+                      '[\'b\', $c,  ]',
+                     );
+
+?>
