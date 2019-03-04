6d361a14d02371ef0d5cee57d390c31449b22e0e
Damien Seguy <damien.seguy@gmail.com>
2019-03-04 16:01:04 +0100

    Fixed special cases with closures

--- tests/analyzer/exp/Variables/VariableUsedOnce.15.php
+++ tests/analyzer/exp/Variables/VariableUsedOnce.15.php
@@ -2,11 +2,11 @@
 
 $expected     = array('$a',
                       '$useUnique',
-                      '$useArg',
                       '$fUnique',
                      );
 
 $expected_not = array('$fArg',
+                      '$useArg',
                      );
 
 ?>
