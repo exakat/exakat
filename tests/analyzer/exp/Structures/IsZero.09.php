3e28f89230a50adc1ea3c57ab1794872969fa536
Damien Seguy <damien.seguy@gmail.com>
2019-03-05 05:39:11 -0500

    Reworked the analysis.

---
+++ tests/analyzer/exp/Structures/IsZero.09.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('$c7 - ($a7 = $c7)', 
+                      '$c5 - $a5 = $c5', 
+                      '$c - ($a + $c)',
+                     );
+
+$expected_not = array('$c8 - ($b8 - $a8 = $c8)',
+                     );
+
+?>
