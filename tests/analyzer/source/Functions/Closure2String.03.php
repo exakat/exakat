e407f4e4ed814912036c361f6509909ee24814d5
Damien Seguy <damien.seguy@gmail.com>
2019-03-27 17:55:27 +0100

    Added usage of the argument

--- tests/analyzer/source/Functions/Closure2String.03.php
+++ tests/analyzer/source/Functions/Closure2String.03.php
@@ -8,7 +8,7 @@ $c = array_map(function ($fileKO) { return basename($fileKO);}, $projects);
 print_r($c);
 $d = array_map('basename', $projects, array_fill(0, 2, '.json'));
 print_r($d);
-$c = array_map(function ($fileOK2) { return basename($fileOK[2]);}, $projects);
+$c = array_map(function ($fileOK2) { return basename($fileOK2[2]);}, $projects);
 print_r($c);
 
 ?>
