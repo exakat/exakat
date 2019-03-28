4c86fe2f3135f7228b341d67e45a4eca20c8b27f
Damien Seguy <damien.seguy@gmail.com>
2019-03-28 10:57:20 +0100

    Added new command 'show' to recreate a project

--- library/Exakat/Exakat.php
+++ library/Exakat/Exakat.php
@@ -259,6 +259,11 @@ class Exakat {
                 $task->run();
                 break;
 
+            case 'show' :
+                $task = new Tasks\Show($this->gremlin, $this->config);
+                $task->run();
+                break;
+
             case 'version' :
             default :
                 $version = self::VERSION;
@@ -273,7 +278,7 @@ class Exakat {
 |________|[__]`\_]\'-;__/[__|  \_]\'-;__/\__/  
                                                
 
-Exakat : @ 2014-2018 Damien Seguy. 
+Exakat : @ 2014-2019 Damien Seguy. 
 Version : ", $version, ' - Build ', $build, ' - ', $date, "\n";
 
                 break;
---
+++ library/Exakat/Tasks/Show.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Copyright 2012-2019 Damien Seguy – Exakat SAS <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+
+namespace Exakat\Tasks;
+
+use Exakat\Config;
+use Exakat\Project as ProjectName;
+use Exakat\Exceptions\InvalidProjectName;
+use Exakat\Exceptions\NoSuchProject;
+use Exakat\Exceptions\ProjectNeeded;
+
+class Show extends Tasks {
+    const CONCURENCE = self::ANYTIME;
+
+    public function run() {
+        $project = new ProjectName($this->config->project);
+
+        if (!$project->validate()) {
+            throw new InvalidProjectName($project->getError());
+        }
+
+        if ($this->config->project === 'default') {
+            throw new ProjectNeeded();
+        }
+        
+        print "Create project '$project' with : 
+    {$this->config->php} {$this->config->executable} init -p $project -R {$this->config->project_url} -{$this->config->project_vcs} -v\n";
+    }
+}
+
+?>
