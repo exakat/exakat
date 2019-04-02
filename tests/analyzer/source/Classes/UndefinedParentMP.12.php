767e24454b19ca88eb5fa6a0515be7fcd734232b
Damien Seguy <damien.seguy@gmail.com>
2019-04-02 16:42:46 +0200

    Missing Module classes

--- tests/analyzer/source/Classes/UndefinedParentMP.12.php
+++ tests/analyzer/source/Classes/UndefinedParentMP.12.php
@@ -4,25 +4,31 @@ use Exception as X;
 
 class MyException extends \Exception {
     function __construct() {
-        parent::__construct();
+        parent::__construct($myexception);
     }
 }
 
 class MyException2 extends Exception {
     function __construct() {
-        parent::__construct();
+        parent::__construct($myexception2);
     }
 }
 
 class MyException3 extends Exxeption {
     function __construct() {
-        parent::__construct();
+        parent::__construct($myexception3);
     }
 }
 
 class MyException4 extends X {
     function __construct() {
-        parent::__construct();
+        parent::__construct($myexception4);
+    }
+}
+
+class MyException5 extends MyException4 {
+    function __construct() {
+        parent::__construct($myexception5);
     }
 }
 
