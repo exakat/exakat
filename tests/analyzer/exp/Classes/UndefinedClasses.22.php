33e9b2b58859f47bae015adc3a86b9eb501e8c98
Damien Seguy <damien.seguy@gmail.com>
2019-04-04 11:18:44 +0200

    Extended list of unwanted feedback

--- tests/analyzer/exp/Classes/UndefinedClasses.22.php
+++ tests/analyzer/exp/Classes/UndefinedClasses.22.php
@@ -3,7 +3,7 @@
 $expected     = array('function foo(parent $x) { /**/ } ',
                      );
 
-$expected_not = array('function foo(parent $y) { /**/ } '
+$expected_not = array('function foo(parent $y) { /**/ } ',
                       'function foo(parent $z) { /**/ } '
                      );
 
--- tests/analyzer/exp/Classes/UndefinedClasses.23.php
+++ tests/analyzer/exp/Classes/UndefinedClasses.23.php
@@ -3,7 +3,7 @@
 $expected     = array('$x instanceof parent'
                      );
 
-$expected_not = array('$y instanceof parent'
+$expected_not = array('$y instanceof parent',
                       '$z instanceof parent'
                      );
 
