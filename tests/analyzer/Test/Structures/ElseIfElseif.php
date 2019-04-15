75e0e993c9c3f5cdb383be831dffc5f2413f46e3
Damien Seguy <damien.seguy@gmail.com>
2019-04-15 11:04:59 +0200

    Extended analysis to one-liner in the else block. Docs

--- tests/analyzer/Test/Structures/ElseIfElseif.php
+++ tests/analyzer/Test/Structures/ElseIfElseif.php
@@ -7,8 +7,9 @@ use Test\Analyzer;
 include_once './Test/Analyzer.php';
 
 class ElseIfElseif extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testStructures_ElseIfElseif01()  { $this->generic_test('Structures_ElseIfElseif.01'); }
+    public function testStructures_ElseIfElseif02()  { $this->generic_test('Structures/ElseIfElseif.02'); }
 }
 ?>
