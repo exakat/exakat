78c7bf339caa66be34332170ba9b1c5edf65ab14
Damien Seguy <damien.seguy@gmail.com>
2019-04-17 17:02:56 +0200

    Extended to pi(); reinstated in Analyze

--- library/Exakat/Analyzer/Structures/UseConstant.php
+++ library/Exakat/Analyzer/Structures/UseConstant.php
@@ -27,7 +27,7 @@ use Exakat\Analyzer\Analyzer;
 
 class UseConstant extends Analyzer {
     public function analyze() {
-        $this->atomFunctionIs(array('\\php_version', '\\php_sapi'))
+        $this->atomFunctionIs(array('\\php_version', '\\php_sapi_name', '\\pi'))
              ->back('first');
         $this->prepareQuery();
 
