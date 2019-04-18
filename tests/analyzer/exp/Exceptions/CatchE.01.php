2e2af9aa1354d4d1c6762a5d997678e5c0b1eeeb
Damien Seguy <damien.seguy@gmail.com>
2019-04-18 14:48:58 +0200

    UT is on the catch, not the variable

--- tests/analyzer/exp/Exceptions/CatchE.01.php
+++ tests/analyzer/exp/Exceptions/CatchE.01.php
@@ -1,10 +1,10 @@
 <?php
 
-$expected     = array('$e2',
-                      '$e3',
+$expected     = array('catch (E $e3) { /**/ } ', 
+                      'catch (E $e2) { /**/ } ',
                      );
 
-$expected_not = array('$e',
+$expected_not = array( 'catch (E $e) { /**/ } ', 
                      );
 
 ?>
