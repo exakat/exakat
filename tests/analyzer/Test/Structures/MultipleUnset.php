1bfbb378c7545cb3cbd025a6143bc217baee841f
Damien Seguy <damien.seguy@gmail.com>
2019-04-22 12:27:25 +0200

    New analysis : merge multiple unset into one call

---
+++ tests/analyzer/Test/Structures/MultipleUnset.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test\Structures;
+
+use Test\Analyzer;
+
+include_once './Test/Analyzer.php';
+
+class MultipleUnset extends Analyzer {
+    /* 1 methods */
+
+    public function testStructures_MultipleUnset01()  { $this->generic_test('Structures/MultipleUnset.01'); }
+}
+?>
---
+++ tests/analyzer/exp/Structures/MultipleUnset.01.php
@@ -0,0 +1,14 @@
+<?php
+
+$expected     = array('unset($a2)', 
+                      'unset($a)',
+                     );
+
+$expected_not = array('unset($b)', 
+                      'unset($c)', 
+                      'unset($d)', 
+                      'unset($a4)', 
+                      'unset($b4)', 
+                     );
+
+?>
