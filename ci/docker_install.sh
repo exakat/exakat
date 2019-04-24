98b39c2ea32f829eb072b480a161b91467ff5623
Damien Seguy <damien.seguy@gmail.com>
2019-04-24 10:55:22 +0200

    Gitlab integration

---
+++ ci/docker_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+# Install git (the php image doesn't have it) which is required by composer
+apt-get update -yqq
+apt-get install git -yqq
+
+# Install phpunit, the tool that we will use for testing
+curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+chmod +x /usr/local/bin/phpunit
+
+# Install mysql driver
+# Here you can install any other extension that you need
+docker-php-ext-install sqlite3
