2ec643bbe7a0de53f8c5e004aee7eae29d46388e
Damien Seguy <damien.seguy@gmail.com>
2019-04-24 14:10:23 +0200

    Checking composer install

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -10,7 +10,8 @@ mkdir -p /usr/share/man/man1mkdir -p /usr/share/man/man1
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install default-jre apt-utils git lsof unzip -yqq
+#apt-get install default-jre apt-utils git lsof unzip -yqq
+apt-get install apt-utils git lsof unzip -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
@@ -18,36 +19,35 @@ chmod +x /usr/local/bin/phpunit
 
 #    apt-get install -y --no-install-recommends git subversion mercurial bzr lsof unzip zip && \
 
-#    \
-#    echo "===> Composer"  && \
-#    curl -sS https://getcomposer.org/installer -o composer-setup.php && \
-#    HASH="$(curl --silent -o - https://composer.github.io/installer.sig)" && \
-#    php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" && \
-#    php composer-setup.php --install-dir=/usr/local/bin --filename=composer && \
-#    \
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+HASH="$(curl --silent -o - https://composer.github.io/installer.sig)"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
-curl --silent -o apache-tinkerpop-gremlin-server-3.3.6-bin.zip http://dist.exakat.io/apache-tinkerpop-gremlin-server-3.3.6-bin.zip
-unzip apache-tinkerpop-gremlin-server-3.3.6-bin.zip
-mv apache-tinkerpop-gremlin-server-3.3.6 tinkergraph
-rm -rf apache-tinkerpop-gremlin-server-3.3.6-bin.zip
-cd tinkergraph
-bin/gremlin-server.sh install org.apache.tinkerpop neo4j-gremlin 3.3.6
-cd .. 
+composer install
 
-echo "graphdb = 'gsneo4j';
-
-; where is neo4j inside a gremlin server host
-gsneo4j_host     = '127.0.0.1';
-gsneo4j_port     = '8182';
-gsneo4j_folder   = 'tinkergraph';
-
-phpversion = 7.3
-
-php73 = /usr/local/bin/php
-
-" >> config/exakat.ini
-
-php exakat doctor
+#curl --silent -o apache-tinkerpop-gremlin-server-3.3.6-bin.zip http://dist.exakat.io/apache-tinkerpop-gremlin-server-3.3.6-bin.zip
+#unzip apache-tinkerpop-gremlin-server-3.3.6-bin.zip
+#mv apache-tinkerpop-gremlin-server-3.3.6 tinkergraph
+#rm -rf apache-tinkerpop-gremlin-server-3.3.6-bin.zip
+#cd tinkergraph
+#bin/gremlin-server.sh install org.apache.tinkerpop neo4j-gremlin 3.3.6
+#cd .. 
+#
+#echo "graphdb = 'gsneo4j';
+#
+#; where is neo4j inside a gremlin server host
+#gsneo4j_host     = '127.0.0.1';
+#gsneo4j_port     = '8182';
+#gsneo4j_folder   = 'tinkergraph';
+#
+#phpversion = 7.3
+#
+#php73 = /usr/local/bin/php
+#
+#" >> config/exakat.ini
+#
+#php exakat doctor
 
 # Install mysql driver
 # Here you can install any other extension that you need
