a210fc409237bdd39eaa82a819a1cccfb58981a3
Damien Seguy <damien.seguy@gmail.com>
2019-04-24 11:30:07 +0200

    adding unzip, testing java

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,7 +7,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git default-jre -yqq
+apt-get install git default-jre unzip -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
@@ -23,13 +23,13 @@ chmod +x /usr/local/bin/phpunit
 #    php composer-setup.php --install-dir=/usr/local/bin --filename=composer && \
 #    \
 
-curl --silent -o apache-tinkerpop-gremlin-server-3.3.6-bin.zip http://dist.exakat.io/apache-tinkerpop-gremlin-server-3.3.6-bin.zip
-unzip apache-tinkerpop-gremlin-server-3.3.6-bin.zip
-mv apache-tinkerpop-gremlin-server-3.3.6 tinkergraph
-rm -rf apache-tinkerpop-gremlin-server-3.3.6-bin.zip
-cd tinkergraph
-bin/gremlin-server.sh install org.apache.tinkerpop neo4j-gremlin 3.3.6
-cd .. 
+#curl --silent -o apache-tinkerpop-gremlin-server-3.3.6-bin.zip http://dist.exakat.io/apache-tinkerpop-gremlin-server-3.3.6-bin.zip
+#unzip apache-tinkerpop-gremlin-server-3.3.6-bin.zip
+#mv apache-tinkerpop-gremlin-server-3.3.6 tinkergraph
+#rm -rf apache-tinkerpop-gremlin-server-3.3.6-bin.zip
+#cd tinkergraph
+#bin/gremlin-server.sh install org.apache.tinkerpop neo4j-gremlin 3.3.6
+#cd .. 
 
 php exakat doctor
 
