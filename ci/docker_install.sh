32b9bc46642d11b9baec53ae5d77fea2178501aa
Damien Seguy <damien.seguy@gmail.com>
2019-04-24 11:32:27 +0200

    All But Java

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,7 +7,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git default-jre unzip -yqq
+apt-get install apt-utils git lsof unzip -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
