a5f0ee13bfbdd23ee53af40248da71385d867cde
Damien Seguy <damien.seguy@gmail.com>
2019-04-25 12:07:50 +0200

    Renamed test to OneTest

--- tests/analyzer/one.php
+++ tests/analyzer/one.php
@@ -4,8 +4,7 @@ use PHPUnit\Framework\TestSuite;
 
 include_once __DIR__.'/Test/Analyzer.php';
 
-class Framework_Onetest extends TestSuite {
-
+class Onetest extends TestSuite {
     public static function suite() {
         $tests = glob(__DIR__.'/Test/Type/Path.php');
         return Test\testSuiteBuilder($tests);
