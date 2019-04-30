839d747f217fc6cf13e42ec079346b23ef4c6f9b
Damien Seguy <damien.seguy@gmail.com>
2019-04-30 07:44:58 +0200

    Fixed UT

--- tests/analyzer/exp/Classes/UnresolvedClasses.13.php
+++ tests/analyzer/exp/Classes/UnresolvedClasses.13.php
@@ -1,7 +1,7 @@
 <?php
 
 $expected     = array('sqliTE4(\':memory:\')', ,
-                      'a\sqliTE3(\':memory:\')', ,
+                      'a\sqliTE3(\':memory:\')',
                      );
 
 $expected_not = array('\Sqlite3(\':memory:\')',
