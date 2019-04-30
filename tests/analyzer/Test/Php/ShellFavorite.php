9ac7e732598024d7c6c953a5d21bca9f948f01cb
Damien Seguy <damien.seguy@gmail.com>
2019-04-30 09:33:39 +0200

    Added new UT

--- tests/analyzer/Test/Php/ShellFavorite.php
+++ tests/analyzer/Test/Php/ShellFavorite.php
@@ -7,8 +7,9 @@ use Test\Analyzer;
 include_once dirname(__DIR__, 2).'/Test/Analyzer.php';
 
 class ShellFavorite extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testPhp_ShellFavorite01()  { $this->generic_test('Php/ShellFavorite.01'); }
+    public function testPhp_ShellFavorite02()  { $this->generic_test('Php/ShellFavorite.02'); }
 }
 ?>
