1d80878b48d97c80d2a1cc8d009af31303b355c4
Damien Seguy <damien.seguy@gmail.com>
2019-04-30 07:42:30 +0200

    Added check on PHP classes

---
+++ tests/analyzer/exp/Classes/UnresolvedClasses.13.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('sqliTE4(\':memory:\')', ,
+                      'a\sqliTE3(\':memory:\')', ,
+                     );
+
+$expected_not = array('\Sqlite3(\':memory:\')',
+                      'sqliTE3(\':memory:\')',
+                     );
+
+?>
