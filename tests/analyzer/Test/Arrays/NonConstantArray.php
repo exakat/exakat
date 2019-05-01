8670e31670cffa72538db19a099aee7627c1dead
Damien Seguy <damien.seguy@gmail.com>
2019-05-01 11:10:04 +0200

    Refactored analysis with extension's constants

--- tests/analyzer/Test/Arrays/NonConstantArray.php
+++ tests/analyzer/Test/Arrays/NonConstantArray.php
@@ -7,12 +7,13 @@ use Test\Analyzer;
 include_once dirname(__DIR__, 2).'/Test/Analyzer.php';
 
 class NonConstantArray extends Analyzer {
-    /* 5 methods */
+    /* 6 methods */
 
     public function testArrays_NonConstantArray01()  { $this->generic_test('Arrays/NonConstantArray.01'); }
     public function testArrays_NonConstantArray02()  { $this->generic_test('Arrays/NonConstantArray.02'); }
     public function testArrays_NonConstantArray03()  { $this->generic_test('Arrays/NonConstantArray.03'); }
     public function testArrays_NonConstantArray04()  { $this->generic_test('Arrays/NonConstantArray.04'); }
     public function testArrays_NonConstantArray05()  { $this->generic_test('Arrays/NonConstantArray.05'); }
+    public function testArrays_NonConstantArray06()  { $this->generic_test('Arrays/NonConstantArray.06'); }
 }
 ?>
---
+++ tests/analyzer/exp/Arrays/NonConstantArray.06.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('state',
+                      'state3',
+                     );
+
+$expected_not = array('CURLOPT_CAINFO',
+                      'state2',
+                      'state2',
+                      'CURLOPT_COOKIEJAR',
+                     );
+
+?>
