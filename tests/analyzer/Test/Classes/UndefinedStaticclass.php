709163e646f2ed1cf874405c47955d0f199dde1a
Damien Seguy <damien.seguy@gmail.com>
2019-05-06 15:45:51 +0200

    Extended support for PHP native classes

--- tests/analyzer/Test/Classes/UndefinedStaticclass.php
+++ tests/analyzer/Test/Classes/UndefinedStaticclass.php
@@ -7,8 +7,9 @@ use Test\Analyzer;
 include_once dirname(__DIR__, 2).'/Test/Analyzer.php';
 
 class UndefinedStaticclass extends Analyzer {
-    /* 1 methods */
+    /* 2 methods */
 
     public function testClasses_UndefinedStaticclass01()  { $this->generic_test('Classes/UndefinedStaticclass.01'); }
+    public function testClasses_UndefinedStaticclass02()  { $this->generic_test('Classes/UndefinedStaticclass.02'); }
 }
 ?>
