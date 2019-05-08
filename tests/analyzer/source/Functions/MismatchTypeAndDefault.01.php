26881f22aa1f4b08a8831c6d4c182bbc441b5d5d
Damien Seguy <damien.seguy@gmail.com>
2019-05-08 10:29:50 +0200

    Reviewed possible default by type

--- tests/analyzer/source/Functions/MismatchTypeAndDefault.01.php
+++ tests/analyzer/source/Functions/MismatchTypeAndDefault.01.php
@@ -1,5 +1,9 @@
 <?php
 
+
+const STRING = 'a', STRING2 = 'b';
+const INTEGER = 1;
+
 //declare(strict_types=1);
 
 //function foo(string $s = 1) {}
@@ -20,14 +24,6 @@ function foo7(string $s = Null) {}
 //function foo8(string $s = True) {}
 //function foo9(string $s = False) {}
 
-interface i {
-    const STRING = 'a';
-    const INTEGER = 1;
-    const ARRAY = [3,4];
-}
-const STRING = 'a', STRING2 = 'b';
-const INTEGER = 1;
-
 function foo10(string $s = STRING) {}
 function foo11(string $s = \STRING) {}
 function foo12(string $s = \INTEGER) {}
@@ -35,5 +31,11 @@ function foo13(string $s = INTEGER) {echo $s;}
 function foo14(string $s = i::INTEGER) { echo $s;}
 function foo15(string $s = i::STRING) {}
 function foo16(string $s = i::ARRAY) {echo $s;}
+
+interface i {
+    const STRING = 'a';
+    const INTEGER = 1;
+    const ARRAY = [3,4];
+}
 
 ?>
