603bbca52c88ba572f222b648ffa2e83efa32df9
Damien Seguy <damien.seguy@gmail.com>
2019-05-10 11:08:09 +0200

    Extended analysis to a members and arrays and references

---
+++ tests/analyzer/exp/Structures/StaticLoop.11.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('foreach($i as [$j]) { /**/ } ', 
+                      'foreach($c as [$d]) { /**/ } ',
+                     );
+
+$expected_not = array('foreach($a as [&$b]) { /**/ } ',
+                      'foreach($f as [&$g]) { /**/ } ',
+                     );
+
+?>
---
+++ tests/analyzer/source/Structures/StaticLoop.12.php
@@ -0,0 +1,15 @@
+<?php
+
+foreach($a as $b->x) {
+    $c += $b->x[6];
+}
+
+foreach($c as $d->x) {
+    $c += $e->x[6];
+}
+
+foreach($f as $g->x) {
+    $c += $g->x->h;
+}
+
+?>
