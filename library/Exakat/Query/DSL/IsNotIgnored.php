0c9515790bad99c9d62f736aa9e4b42a7c88168c
Damien Seguy <damien.seguy@gmail.com>
2019-05-12 21:43:22 +0200

    Simplified DSL with new property

--- library/Exakat/Query/DSL/IsNotIgnored.php
+++ library/Exakat/Query/DSL/IsNotIgnored.php
@@ -27,37 +27,8 @@ use Exakat\Query\Query;
 use Exakat\Analyzer\Analyzer;
 
 class IsNotIgnored extends DSL {
-    const IGNORED_CLASSES = 1;
-    const IGNORED_FUNCTIONS = 2;
-    const IGNORED_CONSTANTS = 3;
-    
     public function run() : Command {
-        list($type, ) = func_get_args();
-        
-        switch($type) {
-            case IsNotIgnored::IGNORED_CLASSES :
-                $fullnspath = $this->ignoredcit;
-                break;
-            case IsNotIgnored::IGNORED_CONSTANTS :
-                $fullnspath = $this->ignoredconstants;
-                break;
-            case IsNotIgnored::IGNORED_FUNCTIONS :
-                $fullnspath = $this->ignoredfunctions;
-                break;
-            default :
-                throw new \Exception('Unknown type of ignored structure');
-        }
-
-        if (empty($fullnspath)) {
-            return new Command(Query::NO_QUERY);
-        }
-
-        $has = $this->dslfactory->factory('has');
-        $return = $has->run('fullnspath');
-
-        $propertyIsNot = $this->dslfactory->factory('propertyIsNot');
-        
-        return $return->add($propertyIsNot->run('fullnspath', $fullnspath, Analyzer::CASE_SENSITIVE));
+        return new Command('not(has("ignored_dir", true))', array() );
     }
 }
 ?>
