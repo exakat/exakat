53adf46845621e55b6979641ca37e3071bc1d62e
Damien Seguy <damien.seguy@gmail.com>
2019-05-13 17:18:22 +0200

    Upgraded tests with 2 new assertions

--- tests/analyzer/exp/Php/ForeachObject.01.php
+++ tests/analyzer/exp/Php/ForeachObject.01.php
@@ -1,11 +1,13 @@
 <?php
 
 $expected     = array('foreach($array1 as $o->$b) { /**/ } ',
+                      'foreach($array2 as $o->$b[1]) { /**/ } ',
+                      'foreach($array3 as $o->b) { /**/ } ',
+                      'foreach($array6 as $o->$b->$c) { /**/ } ',
                      );
 
-$expected_not = array('foreach($array2 as $o->$b) { /**/ } ',
-                      'foreach($array3 as $o->$b) { /**/ } ',
-                      'foreach($array4 as $o->$b) { /**/ } ',
+$expected_not = array('foreach($array4 as $o->$b) { /**/ } ',
+                      'foreach($array5 as [$o->b] ) { /**/ } ',
                      );
 
 ?>
