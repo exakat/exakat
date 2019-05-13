95f6045d28ff743ddae82c280769d6ff40c26db2
Damien Seguy <damien.seguy@gmail.com>
2019-05-13 17:55:08 +0200

    Extended coverage

--- tests/analyzer/exp/Structures/ObjectReferences.01.php
+++ tests/analyzer/exp/Structures/ObjectReferences.01.php
@@ -4,12 +4,14 @@ $expected     = array('function x(a &$b, c $d, &$e, &$f) { /**/ } ',
                       '$g',
                       '$k',
                       'function x(a &$b, c $d, &$e, &$f) { /**/ } ',
+                      '$v2',
                      );
 
 $expected_not = array('$d',
                       '$j',
                       '$h',
                       '$i',
+                      '$i2',
                      );
 
 ?>
--- tests/analyzer/source/Structures/ObjectReferences.01.php
+++ tests/analyzer/source/Structures/ObjectReferences.01.php
@@ -13,8 +13,16 @@ foreach($j as &$k) {
     $k->go;
 }
 
+foreach($j2 as $k => &$v2) {
+    $v2->go;
+}
+
 foreach($h as &$i) {
     $i++;
+}
+
+foreach($h2 as $k => &$i2) {
+    $i2++;
 }
 
 ?>
