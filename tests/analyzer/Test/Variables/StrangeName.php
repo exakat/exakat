5551f1d09bdd22cd83361805af22d46d81e9a994
Damien Seguy <damien.seguy@gmail.com>
2019-05-14 17:15:10 +0200

    Extended strange names to strange type in names

--- tests/analyzer/Test/Variables/StrangeName.php
+++ tests/analyzer/Test/Variables/StrangeName.php
@@ -7,7 +7,7 @@ use Test\Analyzer;
 include_once dirname(__DIR__, 2).'/Test/Analyzer.php';
 
 class StrangeName extends Analyzer {
-    /* 6 methods */
+    /* 7 methods */
 
     public function testVariables_StrangeName01()  { $this->generic_test('Variables/StrangeName.01'); }
     public function testVariables_StrangeName02()  { $this->generic_test('Variables/StrangeName.02'); }
@@ -15,5 +15,6 @@ class StrangeName extends Analyzer {
     public function testVariables_StrangeName04()  { $this->generic_test('Variables/StrangeName.04'); }
     public function testVariables_StrangeName05()  { $this->generic_test('Variables/StrangeName.05'); }
     public function testVariables_StrangeName06()  { $this->generic_test('Variables/StrangeName.06'); }
+    public function testVariables_StrangeName07()  { $this->generic_test('Variables/StrangeName.07'); }
 }
 ?>
---
+++ tests/analyzer/exp/Variables/StrangeName.07.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('${42}', 
+                      '${[2]}', 
+                      '${rand( ) % 2 == 0}', 
+                      '${true}', 
+                      '${43}',
+                     );
+
+$expected_not = array('$value',
+                     );
+
+?>
