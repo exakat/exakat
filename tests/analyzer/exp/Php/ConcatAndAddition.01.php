bc6d6d1f8ca2afad9e06645c8b88a56a2acc1cb8
Damien Seguy <damien.seguy@gmail.com>
2019-05-17 14:54:19 +0200

    Prepared for PHP 7.4 : new analyzer : addition and concatenations

---
+++ tests/analyzer/exp/Php/ConcatAndAddition.01.php
@@ -0,0 +1,13 @@
+<?php
+
+$expected     = array('$a2 - $b2 . "sum: "',
+                      '"sum: " . $a2 - $b2',
+                      '$a1 + $b1 . "sum: "',
+                      '"sum: " . $a + $b',
+                     );
+
+$expected_not = array('("sum: " . $a) + $b',
+                      '"sum :" . ($a + $b)',
+                     );
+
+?>
