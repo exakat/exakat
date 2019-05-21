6a28326c0739b3bbecc178aa3e45469a78f9b835
Damien Seguy <damien.seguy@gmail.com>
2019-05-21 18:17:23 +0200

    Fixed compilation

--- tests/analyzer/exp/Security/MinusOneOnError.01.php
+++ tests/analyzer/exp/Security/MinusOneOnError.01.php
@@ -2,7 +2,7 @@
 
 $expected     = array('pg_set_client_encoding( ) ? 1 : 3', 
                        'if(openssl_verify(1, 2, 3, 4)) { /**/ } elseif(openssl_verify(1, 2, 3, 4)) { /**/ } elseif(openssl_x509_checkpurpose( ) == 1) { /**/ } ', 
-                      'elseif(openssl_verify(1, 2, 3, 4)) { /**/ } elseif(openssl_x509_checkpurpose( ) == 1) { /**/ } '
+                      'elseif(openssl_verify(1, 2, 3, 4)) { /**/ } elseif(openssl_x509_checkpurpose( ) == 1) { /**/ } ',
                       'while (pcntl_wait( )) { /**/ } ', 
                       'do { /**/ } while(pcntl_wait( ))',
                      );
