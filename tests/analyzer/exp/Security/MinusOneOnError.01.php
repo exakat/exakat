b72b620632f4c8806677f426de7aa8c7118ffd99
Damien Seguy <damien.seguy@gmail.com>
2019-05-21 15:46:10 +0200

    Fixed expected values

--- tests/analyzer/exp/Security/MinusOneOnError.01.php
+++ tests/analyzer/exp/Security/MinusOneOnError.01.php
@@ -1,8 +1,8 @@
 <?php
 
 $expected     = array('pg_set_client_encoding( ) ? 1 : 3', 
-                      'elseif(openssl_verify(1, 2, 3, 4)) { /**/ } ', 
-                      'if(openssl_verify(1, 2, 3, 4)) { /**/ } elseif(openssl_verify(1, 2, 3, 4)) { /**/ } ', 
+                       'if(openssl_verify(1, 2, 3, 4)) { /**/ } elseif(openssl_verify(1, 2, 3, 4)) { /**/ } elseif(openssl_x509_checkpurpose( ) == 1) { /**/ } ', 
+                      'elseif(openssl_verify(1, 2, 3, 4)) { /**/ } elseif(openssl_x509_checkpurpose( ) == 1) { /**/ } '
                       'while (pcntl_wait( )) { /**/ } ', 
                       'do { /**/ } while(pcntl_wait( ))',
                      );
