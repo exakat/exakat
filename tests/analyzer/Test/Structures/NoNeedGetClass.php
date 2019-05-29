1b5268add4464bf495cf24af879ec0d64bad1379
Damien Seguy <damien.seguy@gmail.com>
2019-05-29 10:27:19 +0200

    New analyser : no need for get_class()

---
+++ tests/analyzer/Test/Structures/NoNeedGetClass.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test\Structures;
+
+use Test\Analyzer;
+
+include_once dirname(__DIR__, 2).'/Test/Analyzer.php';
+
+class NoNeedGetClass extends Analyzer {
+    /* 1 methods */
+
+    public function testStructures_NoNeedGetClass01()  { $this->generic_test('Structures/NoNeedGetClass.01'); }
+}
+?>
