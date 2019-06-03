3c38d99613c7952300d5fa00f899172062f5de61
Damien Seguy <damien.seguy@gmail.com>
2019-06-03 15:19:29 +0200

    Refactored UT

--- tests/analyzer/exp/Structures/UseSystemTmp.01.php
+++ tests/analyzer/exp/Structures/UseSystemTmp.01.php
@@ -6,6 +6,7 @@ $expected     = array('rmdir("C:\\WINDOWS\\\\" . A . ".txt", $a)',
                       'file_put_contents(\'/tmp/a.txt\', $a)',
                       'mkdir(\'C:\\WINDOWS\\b.txt\', $a)',
                       'rmdir("C:\\WINDOWS\\\\TEMP\\\\" . B . ".txt", $a)',
+                      'file_put_contents("/tmp/$a.txt", $a)',
                      );
 
 $expected_not = array('file_put_contents(tmpFolder.$a.\'.txt\', $a)',
