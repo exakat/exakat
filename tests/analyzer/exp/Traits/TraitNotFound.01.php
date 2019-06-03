80d1ad37e2c14fe5e2c3e3a7b2cba6fa1d79c03d
Damien Seguy <damien.seguy@gmail.com>
2019-06-03 15:19:21 +0200

    Added UT

--- tests/analyzer/exp/Traits/TraitNotFound.01.php
+++ tests/analyzer/exp/Traits/TraitNotFound.01.php
@@ -1,11 +1,11 @@
 <?php
 
-$expected     = array('',
-                      '',
+$expected     = array('class x1 { /**/ } ',
+                      'class x3 { /**/ } ', 
+                      'class x4 { /**/ } ', 
                      );
 
-$expected_not = array('',
-                      '',
+$expected_not = array('class x2 { /**/ } ',
                      );
 
 ?>
--- tests/analyzer/source/Traits/TraitNotFound.01.php
+++ tests/analyzer/source/Traits/TraitNotFound.01.php
@@ -1,3 +1,28 @@
 <?php
 
+class x1 {
+    use t {
+        t2::a as b;
+    }
+}
+
+class x2 {
+    use t, t2 {
+        t::a as a;
+        t2::a as b;
+    }
+}
+
+class x3 {
+    use t {
+        t::a insteadof c;
+    }
+}
+
+class x4 {
+    use t {
+        c::a insteadof t;
+    }
+}
+
 ?>
