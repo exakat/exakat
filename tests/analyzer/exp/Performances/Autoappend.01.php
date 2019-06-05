57ba5199f95b10fd1e93045b7a64b1e7c313f09c
Damien Seguy <damien.seguy@gmail.com>
2019-06-05 18:35:32 +0200

    New analyzer : no array append

---
+++ tests/analyzer/exp/Performances/Autoappend.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('C::$D[] = C::$D', 
+                      '$c[] = $c',
+                     );
+
+$expected_not = array('$a->b[] = $a->c',
+                     );
+
+?>
