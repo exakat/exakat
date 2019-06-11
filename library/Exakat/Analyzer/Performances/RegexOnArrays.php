45b0429f3adfd56395d93f82de1ae5cdfb5d98ec
Damien Seguy <damien.seguy@gmail.com>
2019-06-11 18:47:04 +0200

    New analyzer : preg_match may work on arrays

---
+++ library/Exakat/Analyzer/Performances/RegexOnArrays.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Copyright 2012-2019 Damien Seguy – Exakat SAS <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Analyzer\Performances;
+
+use Exakat\Analyzer\Analyzer;
+
+class RegexOnArrays extends Analyzer {
+    public function analyze() {
+        // foreach($a as $b) { preg_match()}
+        $this->atomIs('Foreach')
+             ->outIs('VALUE')
+             ->savePropertyAs('fullcode', 'blinds')
+             ->back('first')
+             ->outIs('BLOCK')
+             ->atomInsideNoDefinition('Functioncall')
+             ->functioncallIs('\\preg_match')
+             ->outWithRank('ARGUMENT', 1)
+             ->samePropertyAs('fullcode', 'blinds')
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
---
+++ tests/analyzer/exp/Performances/RegexOnArrays.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('foreach($A as $B) { /**/ } ', 
+                      'foreach($a as $b) { /**/ } ',
+                     );
+
+$expected_not = array('foreach($a1 as $b1) { /**/ } ',
+                      'foreach($a2 as $b2) { /**/ } ',
+                     );
+
+?>
