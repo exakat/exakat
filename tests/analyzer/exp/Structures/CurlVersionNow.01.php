9af1a29d2778e6ee12249fce82a04e28d6f2a753
Damien Seguy <damien.seguy@gmail.com>
2019-06-14 12:10:01 +0200

    New analyzer : report curl_version argument usage

---
+++ tests/analyzer/exp/Structures/CurlVersionNow.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('curl_version(CURLVERSION_TOMORROW)', 
+                     );
+
+$expected_not = array('curl_version(\CURLVERSION_NOW)', 
+                      'curl_version(CURLVERSION_NOW)',
+                     );
+
+?>
---
+++ tests/analyzer/source/Structures/CurlVersionNow.01.php
@@ -0,0 +1,6 @@
+<?php
+curl_version(CURLVERSION_NOW);
+curl_version(\CURLVERSION_NOW);
+curl_version(CURLVERSION_TOMORROW);
+
+?>
