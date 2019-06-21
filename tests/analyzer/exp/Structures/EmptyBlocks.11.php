b3880752dff0755f83de70d7727d6094efc3b028
Damien Seguy <damien.seguy@gmail.com>
2019-06-21 15:42:18 +0200

    Fixed exp from UT

--- tests/analyzer/exp/Structures/EmptyBlocks.11.php
+++ tests/analyzer/exp/Structures/EmptyBlocks.11.php
@@ -1,9 +1,9 @@
 <?php
 
-$expected     = array 'declare (ticks = 1)  { /**/ } ',
+$expected     = array('declare (ticks = 1)  { /**/ } ',
                      );
 
-$expected_not = array( 'declare (strict_types = 1)  { /**/ } ',
+$expected_not = array('declare (strict_types = 1)  { /**/ } ',
                      );
 
 ?>
