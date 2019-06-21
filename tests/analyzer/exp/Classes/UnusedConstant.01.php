5fd16c3386e7352a6ba4c41e781da974c52c0cda
Damien Seguy <damien.seguy@gmail.com>
2019-06-21 15:42:08 +0200

    New analyser : report unused class constants

---
+++ tests/analyzer/exp/Classes/UnusedConstant.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array( 'UNUSED = 1',
+                     );
+
+$expected_not = array('USED1 = 1',
+                      'USED2 = 2',
+                      'USED3 = 4',
+                     );
+
+?>
