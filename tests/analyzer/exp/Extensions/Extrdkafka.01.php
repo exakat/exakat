91bb9131b40570c36f6b0372d32b4313c4cd519e
Damien Seguy <damien.seguy@gmail.com>
2019-06-21 17:25:30 +0200

    Also added test on constant

--- tests/analyzer/exp/Extensions/Extrdkafka.01.php
+++ tests/analyzer/exp/Extensions/Extrdkafka.01.php
@@ -2,6 +2,7 @@
 
 $expected     = array('rd_kafka_errno2err( )',
                       'RdKafka\\Producer( )',
+                      'RD_KAFKA_PARTITION_UA',
                      );
 
 $expected_not = array('rd_kafka_not_function( )',
