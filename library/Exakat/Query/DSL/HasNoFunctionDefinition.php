63240c00eda362b2e6f011d0e6ab0f2b71260d42
Damien Seguy <damien.seguy@gmail.com>
2019-06-25 19:49:45 +0200

    Added Arrowfunction to Function definition check

--- library/Exakat/Query/DSL/HasNoFunctionDefinition.php
+++ library/Exakat/Query/DSL/HasNoFunctionDefinition.php
@@ -25,7 +25,7 @@ namespace Exakat\Query\DSL;
 
 class HasNoFunctionDefinition extends DSL {
     public function run() : Command {
-        return new Command('not( where( __.in("DEFINITION").hasLabel("Function", "Method", "Magicmethod", "Closure") ) )');
+        return new Command('not( where( __.in("DEFINITION").hasLabel("Function", "Method", "Magicmethod", "Closure", "Arrowfunction") ) )');
     }
 }
 ?>
