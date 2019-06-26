bf7f1e5a51e590ba3d4c23f97c84435e926fb2bd
Damien Seguy <damien.seguy@gmail.com>
2019-06-26 16:41:19 +0200

    Refactored Recursive analysis

--- library/Exakat/Analyzer/Analyzer.php
+++ library/Exakat/Analyzer/Analyzer.php
@@ -1762,6 +1762,12 @@ GREMLIN;
         
         return $this;
     }
+
+    public function collectArguments($variable = 'args') {
+        $this->query->collectArguments($variable);
+        
+        return $this;
+    }
     
     // Calculate The lenght of a string in a property, and report it in the named string
     public function getStringLength($property = 'noDelimiter', $variable = 'l') {
