1b1e24fc6d91f16b773c74c0ef19b9c3ce46eb1d
Damien Seguy <damien.seguy@gmail.com>
2019-07-02 09:03:12 +0200

    Extra UT for function in namespaces with fallback

---
+++ tests/analyzer/source/Php/Php70RemovedFunctions.03.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace A\B;
+
+use function C\D\split;
+use function E\D\split as strtolower;
+
+list(, $c) = split(trim(strtolower($d )));
+splity(ereg());
+
+function trim($a) { echo $b;}
