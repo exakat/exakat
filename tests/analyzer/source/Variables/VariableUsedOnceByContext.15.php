3b6da0b0ad5424f2125f9fcaa4290334be5371d3
Damien Seguy <damien.seguy@gmail.com>
2019-07-04 18:01:27 +0200

    Better handling of parameters in abstract methods

---
+++ tests/analyzer/source/Variables/VariableUsedOnceByContext.15.php
@@ -0,0 +1,13 @@
+<?php
+abstract class a {
+    abstract public function a1($a1, $a2 = array(), $a3 = 4);
+    public function a2($a12, $a22 = array(), $a32 = 4) {}
+}
+
+interface c {
+    public function c0($c1, $c2 = array(), $c3 = array());
+}
+
+class b {
+    public function d($d1, $d2 = array(), $d3 = array(1)) {}
+}
