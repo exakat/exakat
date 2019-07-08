1c9079526c39ffdf4a5d0daf26dfc5aead5b0693
Damien Seguy <damien.seguy@gmail.com>
2019-07-08 12:45:57 +0200

    Characterized methods that only emit an error (and their cousins)

---
+++ library/Exakat/Analyzer/Functions/CantUse.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * Copyright 2012-2019 Damien Seguy – Exakat SAS <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Analyzer\Functions;
+
+use Exakat\Analyzer\Analyzer;
+
+class CantUse extends Analyzer {
+    public function analyze() {
+        // Function foo() { throw new exception(); }
+        $this->atomIs(self::$FUNCTIONS_ALL)
+             ->outIs('BLOCK')
+             ->is('count', 1)
+             ->outIs('EXPRESSION')
+             ->atomIs('Throw')
+             ->back('first');
+        $this->prepareQuery();
+
+        // Function foo() { trigger_error(); }
+        $this->atomIs(self::$FUNCTIONS_ALL)
+             ->outIs('BLOCK')
+             ->is('count', 1)
+             ->outIs('EXPRESSION')
+             ->functioncallIs(array('\\trigger_error', '\\user_error'))
+             ->back('first');
+        $this->prepareQuery();
+
+        // propagation to custom functions
+        // Function foo() { trigger_error(); }
+        // Function bar() { bar(); }
+        $this->atomIs(self::$FUNCTIONS_ALL)
+             ->outIs('BLOCK')
+             ->is('count', 1)
+             ->outIs('EXPRESSION')
+             ->atomIs(self::$FUNCTIONS_CALLS)
+             ->inIs('DEFINITION')
+             ->analyzerIs('self')
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
