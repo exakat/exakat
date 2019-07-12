f8bcb14e9625c7a795c217d161b8c90efdbf8f3b
Damien Seguy <damien.seguy@gmail.com>
2019-07-12 15:28:45 +0200

    Better error feedback

--- library/Exakat/Query/Query.php
+++ library/Exakat/Query/Query.php
@@ -60,7 +60,7 @@ class Query {
             $command = $this->queryFactory->factory($name);
             $this->commands[] = $command->run(...$args);
         } catch (UnknownDsl $e) {
-            die('This is an unknown DSL');
+            die('This is an unknown DSL : '.$name);
         }
         
         return $this;
