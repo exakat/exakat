c104ee479f71a243c16690bd1b69d8e515164e17
Damien Seguy <damien.seguy@gmail.com>
2019-07-16 17:40:49 +0200

    Modernized code

--- library/Exakat/Analyzer/Structures/Htmlentitiescall.php
+++ library/Exakat/Analyzer/Structures/Htmlentitiescall.php
@@ -27,51 +27,52 @@ use Exakat\Analyzer\Analyzer;
 
 class Htmlentitiescall extends Analyzer {
     public function analyze() {
+        $html_functions = array('\\htmlentities', '\\htmlspecialchars');
+
         // Case with no 2nd argument (using default)
-        $this->atomFunctionIs(array('\\htmlentities', '\\htmlspecialchars'))
+        $this->atomFunctionIs($html_functions)
              ->noChildWithRank('ARGUMENT', 1)
              ->back('first');
         $this->prepareQuery();
 
         // Case with no 3rd argument (using default)
-        $this->atomFunctionIs(array('\\htmlentities', '\\htmlspecialchars'))
+        $this->atomFunctionIs($html_functions)
              ->hasChildWithRank('ARGUMENT', 1)
              ->noChildWithRank('ARGUMENT', 2)
              ->back('first');
         $this->prepareQuery();
 
         $constants = array('ENT_COMPAT', 'ENT_QUOTES', 'ENT_NOQUOTES', 'ENT_IGNORE', 'ENT_SUBSTITUTE', 'ENT_DISALLOWED', 'ENT_HTML401', 'ENT_XML1', 'ENT_XHTML', 'ENT_HTML5');
-        $constantsRegex = '(' . implode('|', $constants) . ')\$';
+        $constants = makeFullNsPath($constants, \FNP_CONSTANT);
 
         // Case 2nd argument is a constant
-        $this->atomFunctionIs(array('\\htmlentities', '\\htmlspecialchars'))
-             ->hasChildWithRank('ARGUMENT', 2)
+        $this->atomFunctionIs($html_functions)
              ->outWithRank('ARGUMENT', 1)
              ->atomIs(array('Identifier', 'Nsname'))
-             ->regexIsNot('fullnspath', $constantsRegex)
+             ->fullnspathIsNot($constants)
              ->back('first');
         $this->prepareQuery();
 
         // Case 2nd argument is a combinaison
-        $this->atomFunctionIs(array('\\htmlentities', '\\htmlspecialchars'))
+        $this->atomFunctionIs($html_functions)
              ->hasChildWithRank('ARGUMENT', 2)
              ->outWithRank('ARGUMENT', 1)
              ->atomIs('Logical')
-             ->outIsIE(array('LEFT', 'RIGHT'))
+             ->outIsIE(array('LEFT', 'RIGHT', 'CODE'))
              ->atomIs(array('Identifier', 'Nsname'))
-             ->regexIsNot('fullnspath', $constantsRegex)
+             ->fullnspathIsNot($constants)
              ->back('first');
         $this->prepareQuery();
 
         // Case 3rd argument is one of the following value
-        $this->atomFunctionIs(array('\\htmlentities', '\\htmlspecialchars'))
+        $this->atomFunctionIs($html_functions)
              ->hasChildWithRank('ARGUMENT', 2)
              ->outWithRank('ARGUMENT', 2)
              ->atomIs('String')
              ->noDelimiterIsNot(array('ISO-8859-1', 'ISO8859-1', 'ISO-8859-5', 'ISO8859-5', 'ISO-8859-15', 'ISO8859-15', 'UTF-8', 'cp866',
                                       'ibm866', '866', 'cp1251', 'Windows-1251', 'win-1251', '1251', 'cp1252', 'Windows-1252', '1252', 'KOI8-R',
                                       'koi8-ru', 'koi8r', 'BIG5', '950', 'GB2312', '936', 'BIG5-HKSCS', 'Shift_JIS', 'SJIS', 'SJIS-win', 'cp932',
-                                      '932', 'EUC-JP', 'EUCJP', 'eucJP-win', 'MacRoman', ''), true)
+                                      '932', 'EUC-JP', 'EUCJP', 'eucJP-win', 'MacRoman', ''), self::CASE_INSENSITIVE)
              ->back('first');
         $this->prepareQuery();
     }
--- tests/analyzer/exp/Structures/Htmlentitiescall.03.php
+++ tests/analyzer/exp/Structures/Htmlentitiescall.03.php
@@ -8,10 +8,10 @@ $expected     = array('htmlentities($miss_2)',
 
 $expected_not = array('htmlentities($miss_11, ENT_QUOTES, \'UTF-8\')',
                       'htmlentities($miss_12, \\ENT_QUOTES, \'UTF-8\')',
-                      'htmlentities($miss_14, \\ENT_QUOTES | \\ENT_COMPAT, \'UTF-8\')',
-                      'htmlentities($miss_15, ENT_QUOTES | \\ENT_COMPAT, \'UTF-8\')',
+                      'htmlentities($miss_14, \ENT_QUOTES | \ENT_COMPAT, \'UTF-8\')',
+                      'htmlentities($miss_15, ENT_QUOTES | \ENT_COMPAT, \'UTF-8\')',
                       'htmlentities($miss_13, ENT_QUOTES | ENT_COMPAT, \'UTF-8\')',
-                      'htmlentities($miss_16, ENT_QUOTES | \\ENT_COMPAT, \'UTF-8\')',
+                      'htmlentities($miss_16, ENT_QUOTES | \ENT_COMPAT, \'UTF-8\')',
                      );
 
 ?>
