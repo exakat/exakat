c14369b1810c901f30c3f5458132187d76a775dc
Damien Seguy <damien.seguy@gmail.com>
2019-07-17 15:38:50 +0200

    version 1.8.8

--- library/Exakat/Exakat.php
+++ library/Exakat/Exakat.php
@@ -26,8 +26,8 @@ use Exakat\Tasks;
 use Exakat\Config;
 
 class Exakat {
-    const VERSION = '1.8.7';
-    const BUILD = 961;
+    const VERSION = '1.8.8';
+    const BUILD = 967;
 
     private $gremlin = null;
     private $config = null;
