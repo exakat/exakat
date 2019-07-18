fe960f61dcfd13df6b5f917d4d2e52ce8218c660
Damien Seguy <damien.seguy@gmail.com>
2019-07-18 19:05:07 +0200

    Differentiated with ThrowUsage

--- tests/analyzer/exp/Exceptions/ThrownExceptions.01.php
+++ tests/analyzer/exp/Exceptions/ThrownExceptions.01.php
@@ -1,12 +1,13 @@
 <?php
 
-$expected     = array('$x = new Exception( )',
-                      'new Exception2( )',
-                      'new Exception( )',
-                      '$x',
+$expected     = array('class x1 extends Exception { /**/ } ', 
+                      'class x2 extends Exception { /**/ } ', 
+                      'class x3 extends Exception { /**/ } ', 
+                      'class x4 extends Exception { /**/ } '
+                      'class x5 extends Exception { /**/ } ', 
                      );
 
-$expected_not = array('Exception3( )',
+$expected_not = array('class x5 extends Exception { /**/ } ', 
                       '$b',
                       'throw($b)',
                      );
--- tests/analyzer/source/Exceptions/ThrownExceptions.01.php
+++ tests/analyzer/source/Exceptions/ThrownExceptions.01.php
@@ -1,10 +1,30 @@
 <?php
-throw new Exception();
-throw new Exception2();
-throw $x;
-throw $x = new Exception();
 
-new Exception3();
+class x1 extends Exception {}
+class x2 extends Exception {}
+class x3 extends Exception {}
+class x4 extends Exception {}
+class x5 extends Exception {}
+class x6 extends Exception {}
+class x7 extends Exception {}
+class x8 extends Exception {}
+class x9 extends Exception {}
+class x10 extends Exception {}
+
+throw new x1();
+throw new X2;
+
+throw $y;
+
+throw $x = new x3();
+throw ($x = new \X4());
+
+$w = new x5;
+throw $w;
+
+new X6('a');
 
 $a->throw($b);
+
+
 ?>  
