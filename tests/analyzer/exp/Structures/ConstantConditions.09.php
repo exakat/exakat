e0ad9d5c373b7eeecf60bd68d9c20a9fcdcc2cdb
Damien Seguy <damien.seguy@gmail.com>
2019-07-20 12:26:42 +0200

    Extra UT with assignations

---
+++ tests/analyzer/exp/Structures/ConstantConditions.09.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('if(!@$equal = 1) { /**/ } ', 
+                      'if(($equal = \'00\' . 0)) { /**/ } ',
+                      'if($a = $b = $c = 1) { /**/ } ',
+                     );
+
+$expected_not = array('if(!@$plus += 1) { /**/ } ',
+                     );
+
+?>
---
+++ tests/analyzer/source/Structures/ConstantConditions.09.php
@@ -0,0 +1,20 @@
+<?php
+
+//$a = new x;
+$mail = '100' . 1;
+
+if(!@$equal = 1) {
+    print "Then = : ".$mail;
+} 
+
+if(($equal = '00'. 0)) {
+    print "Then = : ".$mail;
+} 
+
+if(!@$plus += 1) {
+    print "Then = : ".$mail;
+} 
+
+if($a = $b = $c = 1) {
+    print "Then = : ".$mail;
+} 
