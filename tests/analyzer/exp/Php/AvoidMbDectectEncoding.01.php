92e3be83382a4dc21093412ef82380afc38988af
Damien Seguy <damien.seguy@gmail.com>
2019-07-22 13:58:22 +0200

    New analysis : avoid detect_encoding

---
+++ tests/analyzer/exp/Php/AvoidMbDectectEncoding.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('mb_detect_encoding($string)',
+                     );
+
+$expected_not = array('mb_detect_encoding($methodcall)',
+                     );
+
+?>
---
+++ tests/analyzer/source/Php/AvoidMbDectectEncoding.01.php
@@ -0,0 +1,7 @@
+<?php
+
+$encoded_string = mb_detect_encoding($string);
+
+$encoded_string = A::mb_detect_encoding($methodcall);
+
+?>
