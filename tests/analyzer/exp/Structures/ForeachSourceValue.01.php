08d71ba3fb44566f08b9183cf059152c24ec9846
Damien Seguy <damien.seguy@gmail.com>
2019-07-22 11:15:35 +0200

    New analyser : check that source and blind values are distinct

---
+++ tests/analyzer/exp/Structures/ForeachSourceValue.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('foreach($c as $b => $c) { /**/ } ', 
+                      'foreach($d as $d => $d) { /**/ } ', 
+                      'foreach($b as $b => $c) { /**/ } ',
+                      'foreach($a as $a) { /**/ } ',
+                     );
+
+$expected_not = array('foreach($a1 as $a) { /**/ } ',
+                     );
+
+?>
---
+++ tests/analyzer/source/Structures/ForeachSourceValue.01.php
@@ -0,0 +1,16 @@
+<?php
+
+foreach($a as $a) {}
+foreach($a1 as $a) {}
+
+foreach($b as $b => $c) {}
+
+foreach($c as $b => $c) {}
+
+foreach($d as $d => $d) {}
+
+foreach($e as $d => [$e]) {}
+
+foreach($f as $d => [[$f]]) {}
+
+?>
