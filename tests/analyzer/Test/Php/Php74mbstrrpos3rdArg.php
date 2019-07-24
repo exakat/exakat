183ae99c5a7a0f1cea387190550cc6cb88f39256
Damien Seguy <damien.seguy@gmail.com>
2019-07-24 17:25:41 +0200

    Report usage of mb_strrpos 4th argument

---
+++ tests/analyzer/Test/Php/Php74mbstrrpos3rdArg.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test\Php;
+
+use Test\Analyzer;
+
+include_once dirname(__DIR__, 2).'/Test/Analyzer.php';
+
+class Php74mbstrrpos3rdArg extends Analyzer {
+    /* 1 methods */
+
+    public function testPhp_Php74mbstrrpos3rdArg01()  { $this->generic_test('Php/Php74mbstrrpos3rdArg.01'); }
+}
+?>
---
+++ tests/analyzer/exp/Php/Php74mbstrrpos3rdArg.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('mb_strrpos($test, \'é\', 0, \'UTF9\')',
+                     );
+
+$expected_not = array('mb_strrpos($test, \'é\', 0)',
+                     );
+
+?>
