1c783c59ef839fde7c7368cc612e4ef067aaf089
Damien Seguy <damien.seguy@gmail.com>
2019-07-24 17:33:05 +0200

    Report deprecated functions in 7.4

---
+++ library/Exakat/Analyzer/Php/Php74RemovedFunctions.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Copyright 2012-2019 Damien Seguy – Exakat SAS <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+
+namespace Exakat\Analyzer\Php;
+
+use Exakat\Analyzer\Analyzer;
+use Exakat\Analyzer\Common\PhpFunctionUsage;
+
+class Php74RemovedFunctions extends PhpFunctionUsage {
+    public function analyze() {
+        $this->functions = array('hebrevc',
+                                 'convert_cyr_string',
+                                 'ezmlm_hash',
+                                 'money_format',
+                                 'restore_include_path',
+                                );
+        parent::analyze();
+    }
+}
+
+?>
