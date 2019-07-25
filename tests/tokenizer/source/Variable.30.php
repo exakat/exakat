e7c77bc14145b15e58957ac59c7b69de3289b601
Damien Seguy <damien.seguy@gmail.com>
2019-07-25 10:11:03 +0200

    Added test for Emoji

---
+++ tests/tokenizer/source/Variable.30.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = "😀 😁 😂 ";
+echo $a;
+
+class 😀 {
+    function foo() { 
+        echo parent::$x;
+    }
+    
+    function 😫 () {
+        echo "😫 ";
+    }
+}
+
+$x = new 😀😫;
+$x->😫 ();
