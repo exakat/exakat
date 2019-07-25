3f08ca9342f3868d8d438d44ee1d57b7885809ae
Damien Seguy <damien.seguy@gmail.com>
2019-07-25 15:00:33 +0200

    Report classes that use parent keyword without having one

---
+++ tests/analyzer/exp/Classes/NoParent.01.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('parent::foo( )',
+                     );
+
+$expected_not = array('parent::foo2( )',
+                      'parent::foo3( )',
+                      'parent::foo4( )',
+                      'parent::foo5( )',
+                     );
+
+?>
