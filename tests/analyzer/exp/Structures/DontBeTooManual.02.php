6b815a293e5c1c64fbbaf12c227ac674a82b3bba
Damien Seguy <damien.seguy@gmail.com>
2019-07-25 15:26:06 +0200

    Extended to catch and for

---
+++ tests/analyzer/exp/Structures/DontBeTooManual.02.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('for($i = 0 ; $i < 10 ; ++$i) { /**/ } ', 
+                      'catch (Exception $e) { /**/ } ',
+                     );
+
+$expected_not = array('for($i = 0 ; $i < 10 ; ++$i) { /**/ } ', 
+                      'catch (Exception $e) { /**/ } ',
+                     );
+
+?>
