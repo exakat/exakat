05820bd015e31a46eced6e72a9d3819e9121de7b
Damien Seguy <damien.seguy@gmail.com>
2019-08-29 12:14:37 +0200

    Openssl_random_pseudo_byte second argument

---
+++ tests/analyzer/exp/Structures/OpensslRandomPseudoByteSecondArg.01.php
@@ -0,0 +1,9 @@
+<?php
+
+$expected     = array('openssl_random_pseudo_bytes($i, $cstrong)',
+                     );
+
+$expected_not = array('openssl_random_pseudo_bytes($i)',
+                     );
+
+?>
