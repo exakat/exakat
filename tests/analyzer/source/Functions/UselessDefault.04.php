1b3bf92c36f087deee35400ca52a56d1d89394db
Damien Seguy <damien.seguy@gmail.com>
2019-09-03 11:45:11 +0200

    Moving Complete to prepareQuery, not rawQuery

--- tests/analyzer/source/Functions/UselessDefault.04.php
+++ tests/analyzer/source/Functions/UselessDefault.04.php
@@ -1,17 +1,21 @@
 <?php
 
+// insufficient calls
 $x1 = function ($a1, $b = 1) {};
 $x1(1, 2);
 
+// insufficient calls
 $x2 = function ($a2, $b = 1) {};
 $x2(1, 2);
 $x2(1, 2);
 
+// issue
 $x3 = function ($a3, $b = 1) {};
 $x3(1, 2);
 $x3(1, 2);
 $x3(1, 2);
 
+// One actual use of the default
 $x3a = function ($a3a, $b = 1) {};
 $x3a(1, 2);
 $x3a(1, 2);
