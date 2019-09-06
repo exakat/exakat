cb0df74a762a2dadaee51ef42b5e320f1e407138
Damien Seguy <damien.seguy@gmail.com>
2019-09-06 14:58:08 +0200

    New analyse : spread operator for arrays

---
+++ tests/analyzer/exp/Php/SpreadOperatorForArray.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('array(...$b)',
+                      '[1,2,3, ...foo( )]',
+                     );
+
+$expected_not = array('[$a, $b]',
+                      '$a->array(...C)',
+                     );
+
+?>
