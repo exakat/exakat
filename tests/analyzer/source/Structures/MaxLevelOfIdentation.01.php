b81e32c4f496116c1f5889a768cd090117fd163b
Damien Seguy <damien.seguy@gmail.com>
2019-09-06 11:32:35 +0200

    New analyzer : Maximum level of indentation

---
+++ tests/analyzer/source/Structures/MaxLevelOfIdentation.01.php
@@ -0,0 +1,26 @@
+<?php
+
+function foo() {
+    if (1) {
+        if (2) {
+            if (3) {
+                if (4) {
+                    if (5) {
+                        51;
+                    } else {
+                        5;
+                    }
+                } else {
+                    4;
+                }
+            } else {
+                3;
+            }
+        } else {
+            2;
+        }
+    } else {
+        1;
+    }
+}
+?>
