dee4136e4eea2ab2dc054a42dde0680b7671d2f2
Damien Seguy <damien.seguy@gmail.com>
2019-09-06 14:40:49 +0200

    Added TU for use function expression

--- tests/analyzer/Test/Structures/ShouldUseForeach.php
+++ tests/analyzer/Test/Structures/ShouldUseForeach.php
@@ -7,10 +7,11 @@ use Test\Analyzer;
 include_once dirname(__DIR__, 2).'/Test/Analyzer.php';
 
 class ShouldUseForeach extends Analyzer {
-    /* 3 methods */
+    /* 4 methods */
 
     public function testStructures_ShouldUseForeach01()  { $this->generic_test('Structures/ShouldUseForeach.01'); }
     public function testStructures_ShouldUseForeach02()  { $this->generic_test('Structures/ShouldUseForeach.02'); }
     public function testStructures_ShouldUseForeach03()  { $this->generic_test('Structures/ShouldUseForeach.03'); }
+    public function testStructures_ShouldUseForeach04()  { $this->generic_test('Structures/ShouldUseForeach.04'); }
 }
 ?>
---
+++ tests/analyzer/exp/Structures/ShouldUseForeach.04.php
@@ -0,0 +1,12 @@
+<?php
+
+$expected     = array('while ($v = array_pop($a)) { /**/ } ', 
+                      'while ($v = b($a)) { /**/ } ', 
+                      'do { /**/ } while($v = array_pop($a))', 
+                      'do { /**/ } while($v = b($a))',
+                     );
+
+$expected_not = array('while($v = array_unshift($a)) { /**/ }',
+                     );
+
+?>
