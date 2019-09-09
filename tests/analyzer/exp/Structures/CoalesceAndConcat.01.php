6e3f57c466889e340eec2f93b71398ce2e3d85fe
Damien Seguy <damien.seguy@gmail.com>
2019-09-09 23:00:15 +0200

    New analysers : too many dimensions for arrays, and conflict between . and ??

---
+++ tests/analyzer/exp/Structures/CoalesceAndConcat.01.php
@@ -0,0 +1,10 @@
+<?php
+
+$expected     = array('\'a\' . $b ?? $c', 
+                      '$b ?? \'d\' . \'e\'',
+                     );
+
+$expected_not = array('$b ?? \'d\'',
+                     );
+
+?>
