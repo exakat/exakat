dad9cc155e60863488bba6de24e10fb506df16fa
Damien Seguy <damien.seguy@gmail.com>
2019-09-09 10:49:59 +0200

    Adding UT for Complete

--- tests/analyzer/source/Complete/CreateDefaultValues.01.php
+++ tests/analyzer/source/Complete/CreateDefaultValues.01.php
@@ -1,3 +1,20 @@
 <?php
 
+class x {
+    private $p = 2;
+    private $pp;
+    
+    function foo($a) {
+        static $s = 1;
+        global $g;
+        
+        $a = 1;
+        $b = 2;
+        $this->pp = 3;
+        $this->p = 3;
+        
+        $g = 2;
+        $s = 4;
+    }
+}
 ?>
