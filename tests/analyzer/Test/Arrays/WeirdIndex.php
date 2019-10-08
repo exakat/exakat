12318eda0dc6d1a38c384931587e5695a2645b6a
Damien Seguy <damien.seguy@gmail.com>
2019-10-08 19:09:02 +0200

    New analyzer : report weird index in arrays

---
+++ tests/analyzer/Test/Arrays/WeirdIndex.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test\Arrays;
+
+use Test\Analyzer;
+
+include_once dirname(__DIR__, 2).'/Test/Analyzer.php';
+
+class WeirdIndex extends Analyzer {
+    /* 1 methods */
+
+    public function testArrays_WeirdIndex01()  { $this->generic_test('Arrays/WeirdIndex.01'); }
+}
+?>
---
+++ tests/analyzer/source/Arrays/WeirdIndex.01.php
@@ -0,0 +1,22 @@
+<?php
+
+$a[' a'] = 1;
+$a['a '] = 2;
+$a[' a '] = 3;
+$a['a a'] = 3;
+
+switch ($c) {
+    case ' b' :
+    case 'b ' :
+    case ' b ' : 
+    case 'b b' : 
+}
+
+array(' c' => 2,
+      'c ' => 3,
+      ' c ' => 4,
+      'c c' => 5,
+
+      );
+
+?>
