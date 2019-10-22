210529f7c699a09d2a9da92f7a722441d7323c59
Damien Seguy <damien.seguy@gmail.com>
2019-10-22 17:03:44 +0200

    Report possible iterable typehint

---
+++ tests/analyzer/Test/Functions/CouldTypeWithIterable.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test\Functions;
+
+use Test\Analyzer;
+
+include_once dirname(__DIR__, 2).'/Test/Analyzer.php';
+
+class CouldTypeWithIterable extends Analyzer {
+    /* 1 methods */
+
+    public function testFunctions_CouldTypeWithIterable01()  { $this->generic_test('Functions/CouldTypeWithIterable.01'); }
+}
+?>
