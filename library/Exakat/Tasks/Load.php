054a1a056138266b611f6dcebfea5f0b402b9abd
Damien Seguy <damien.seguy@gmail.com>
2019-11-03 21:41:30 +0100

    Fixed initialisation for single files

--- library/Exakat/Tasks/Load.php
+++ library/Exakat/Tasks/Load.php
@@ -488,6 +488,10 @@ class Load extends Tasks {
             }
             
             try {
+                $this->callsDatabase = new \Sqlite3($this->sqliteLocation);
+                $this->loader = new $clientClass($this->gremlin, $this->config, $this->callsDatabase, $this->id0);
+                $this->calls = new Calls($this->config->projects_root, $this->callsDatabase);
+
                 $clientClass = "\\Exakat\\Loader\\{$this->config->loader}";
                 display("Loading with $clientClass\n");
                 if (!class_exists($clientClass)) {
