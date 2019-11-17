9b00fe75a392872ade2ac85fbb5517f9f8bdd5ae
Damien Seguy <damien.seguy@gmail.com>
2019-11-17 11:23:59 +0100

    Extended analysis to more expressions

--- library/Exakat/Analyzer/Structures/DontMixPlusPlus.php
+++ library/Exakat/Analyzer/Structures/DontMixPlusPlus.php
@@ -29,7 +29,7 @@ class DontMixPlusPlus extends Analyzer {
         // $a[$i++] = --$o + $b--;
         $this->atomIs(array('Postplusplus', 'Preplusplus'))
              ->inIs()
-             ->atomIs(array('Addition', 'Multiplication', 'Power', 'Array'));
+             ->atomIsNot(array('Array', 'Sequence'));
         $this->prepareQuery();
     }
 }
