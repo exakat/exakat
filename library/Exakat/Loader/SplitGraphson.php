3e05bc51cb12ec26e3d5bffec96f15714538a1e3
Damien Seguy <damien.seguy@gmail.com>
2020-04-04 12:05:44 +0200

    Fix coding standards

--- library/Exakat/Loader/SplitGraphson.php
+++ library/Exakat/Loader/SplitGraphson.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 1);
 /*
  * Copyright 2012-2019 Damien Seguy – Exakat SAS <contact(at)exakat.io>
  * This file is part of Exakat.
@@ -119,7 +119,7 @@ class SplitGraphson extends Loader {
             $chunk = 0;
         }
 
-        $definitionSQL = <<<SQL
+        $definitionSQL = <<<'SQL'
 SELECT DISTINCT CASE WHEN definitions.id IS NULL THEN definitions2.id ELSE definitions.id END AS definition, GROUP_CONCAT(DISTINCT calls.id) AS call, count(calls.id) AS id
 FROM calls
 LEFT JOIN definitions 
