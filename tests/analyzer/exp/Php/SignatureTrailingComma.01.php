e09ce1b0c2fd0e185d7b788eccfecf05ea3bb04f
Damien Seguy <damien.seguy@gmail.com>
2020-05-05 16:27:59 +0200

    New analyzer : trailing comma in PHP 8.0

---
+++ tests/analyzer/exp/Php/SignatureTrailingComma.01.php
@@ -0,0 +1,17 @@
+<?php
+
+$expected     = array('function foof($a,  ) { /**/ } ', 
+                      'function ($ac, $b = 1,  ) { /**/ } ', 
+                      'function foo($a, A $b = null,  ) { /**/ } ', 
+                      'function __construct($a, A $b = null,  ) { /**/ } ', 
+                      'function foot($a, A $b = null,  ) { /**/ } ',
+                      'fn ($afn, &$b,  ) => rand( )',
+                     );
+
+$expected_not = array('bar($c,  )',
+                      'A\bar($c,  )',
+                      'list($a, $b,  )',
+                      'list($a, , $b,  )',
+                     );
+
+?>
---
+++ tests/analyzer/source/Php/SignatureTrailingComma.01.php
@@ -0,0 +1,22 @@
+<?php
+
+function foof($a, ) {}
+function ($ac, $b = 1, ) {};
+fn ($afn, &$b, ) => rand();
+
+class x {
+    function foo($a,A $b = null, ) {}
+    function __construct($a,A $b = null, ) {}
+
+}
+
+interface i {
+    function fooi($a,A $b = null, );
+}
+
+trait t {
+    function foot($a,A $b = null, ) {}
+}
+
+list($a, $b, ) = bar($c, );
+list($a, , $b, ) = A\bar($c, );
