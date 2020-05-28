9444b1b3d80561c0ef786d4da636172dcef0511f
Damien Seguy <damien.seguy@gmail.com>
2020-05-28 11:16:56 +0200

    Refined UT for Closure 2 string

--- tests/analyzer/exp/Functions/Closure2String.04.php
+++ tests/analyzer/exp/Functions/Closure2String.04.php
@@ -4,9 +4,11 @@ $expected     = array('function ($o1) { /**/ } ',
                       'function ($o3) { /**/ } ',
                       'function ($O1) { /**/ } ',
                       'function ($O2) { /**/ } ',
+                      'function ($o2) use ($a) { /**/ } ',
+                      'function ($o2a) { /**/ } ',
                      );
 
-$expected_not = array('function ($o2) use ($a) { /**/ } ',
+$expected_not = array('function ($o2b) { /**/ } ',
                      );
 
 ?>
