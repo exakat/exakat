f1a3737d298472ecf2c2dbcccaff1dc2c50b024c
Damien Seguy <damien.seguy@gmail.com>
2020-07-07 19:16:36 +0200

    First report that suggest Typehints

---
+++ library/Exakat/Reports/TypeSuggestion.php
@@ -0,0 +1,196 @@
+<?php declare(strict_types = 1);
+/*
+ * Copyright 2012-2019 Damien Seguy – Exakat SAS <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Reports;
+
+
+class TypeSuggestion extends Reports {
+    const FILE_EXTENSION = 'html';
+    const FILE_FILENAME  = 'typehint.suggestion';
+    
+    const NO_SUGGESTION = '&nbsp;';
+
+    protected $finalName     = null;
+    private $tmpName         = '';
+    private $classes         = array();
+
+    public function generate(string $folder, string $name = self::FILE_FILENAME): string {
+        if ($name === self::STDOUT) {
+            print "Can't produce SimpleHtml format to stdout\n";
+            return '';
+        }
+
+        $colorClass     = '#fdb863';
+        $colorMethod    = '#fee0b6';
+        $colorProperty  = '#d0d1e6';
+        $colorParameter = '#a6bddb';
+        $colorReturn    = '#74a9cf';
+
+        $list = $this->rulesets->getRulesetsAnalyzers(array('Typehints'));
+        $results = $this->dump->fetchAnalysers($list);
+        $results->load();
+
+        $suggestions = array();
+        foreach($results->toArray() as $row) {
+
+            list(, $type) = explode('/', $row['analyzer']);
+            if (preg_match('/function (\S+)\\(/', $row['fullcode'], $r)) {
+                $suggestions[$row['file']][$row['line']][$r[1]][] = $type;
+            } elseif (preg_match('/(\$\\S+)/', $row['fullcode'], $r)) {
+                $suggestions[$row['file']][$row['line']][$r[1]][] = $type;
+            } else {
+                print 'Error : ' . $row['fullcode'] . "\n";
+            }
+        }
+
+        $html = array();
+
+        $res = $this->dump->fetchTableProperties();
+        foreach($res->toArray() as $row) {
+            if (!empty($row['typehint'])) {
+                $list = self::NO_SUGGESTION;
+            } elseif (isset($suggestions[$row['file']][$row['line']][$row['property']])) {
+                $s = array_filter($suggestions[$row['file']][$row['line']][$row['property']], function($x) : bool { return $x !== 'CouldNotType'; });
+                $list = $this->toHtmlList($s);
+            } else {
+                $list = self::NO_SUGGESTION;
+            }
+            
+            $fullnspath = explode('::', $row['fullnspath'])[0];
+            $className = $this->makeClassName($row['type'], $row['class'], $fullnspath);
+            $classId = $this->getClassId($className);
+
+            $html[$classId]['Properties'][] = <<<HTML
+<td style="background-color: $colorProperty">$row[property]</td>
+<td style="background-color: $colorProperty">$row[typehint]</td>
+<td style="background-color: $colorProperty">$list</td>
+HTML;
+        }
+
+        $res = $this->dump->fetchTableMethodsByArgument();
+        foreach($res->toArray() as $row) {
+            if (!empty($row['typehint'])) {
+                $list = self::NO_SUGGESTION;
+            } elseif (isset($suggestions[$row['file']][$row['line']][$row['argument']])) {
+                $s = array_filter($suggestions[$row['file']][$row['line']][$row['argument']], function($x) : bool { return $x !== 'CouldNotType'; });
+                $list = $this->toHtmlList($s);
+            } else {
+                $list = self::NO_SUGGESTION;
+            }
+
+            $fullnspath = explode('::', $row['fullnspath'])[0];
+            $className = $this->makeClassName($row['citType'], $row['citName'], $fullnspath);
+            $classId = $this->getClassId($className);
+
+            $html[$classId][$row['method']][(int) $row['rank']] = <<<HTML
+<td style="background-color: $colorParameter; vertical-align: top;">$row[argument]</td>
+<td style="background-color: $colorParameter; border-right-style: none; border-left-style: none;vertical-align: top;">$row[typehint]</td>
+<td style="background-color: $colorParameter; border-left-style: none; vertical-align: top;">$list</td>
+HTML;
+        }
+
+        $res = $this->dump->fetchTableMethods();
+        foreach($res->toArray() as $row) {
+            if (in_array(mb_strtolower($row['method']), array('__construct', '__destruct'))) {
+                continue;
+            }
+            if (!empty($row['returntype'])) {
+                $list = self::NO_SUGGESTION;
+            } elseif (isset($suggestions[$row['file']][$row['line']][$row['method']])) {
+                $s = array_filter($suggestions[$row['file']][$row['line']][$row['method']], function($x) : bool { return $x !== 'CouldNotType'; });
+                $list = $this->toHtmlList($s);
+            } else {
+                $list = self::NO_SUGGESTION;
+            }
+
+            $fullnspath = explode('::', $row['fullnspath'])[0];
+            $className = $this->makeClassName($row['type'], $row['class'], $fullnspath);
+            $classId = $this->getClassId($className);
+
+            $html[$classId][$row['method']][-1] = <<<HTML
+<td style="background-color: $colorReturn; vertical-align: top;">: return</td>
+<td style="background-color: $colorReturn; vertical-align: top;">$row[returntype]</td>
+<td  style="background-color: $colorReturn; vertical-align: top;">$list</td>
+HTML;
+        }
+        
+        foreach($html as $className => &$methods) {
+            $classCount = 0;
+            foreach($methods as $methodName => &$returnAndArgs) {
+                ksort($returnAndArgs);
+                $classCount += count($returnAndArgs);
+
+                $first = array_shift($returnAndArgs);
+                $returnAndArgs = implode(PHP_EOL, 
+                                         array_merge(array('<td rowspan="'.(count($returnAndArgs) + 1).'" style="background-color: '.$colorMethod.'; border: black 1px solid; vertical-align: top">'.( $methodName === 'Properties' ?  $methodName : ' function '.$methodName.'()').'</td>'.$first), 
+                                             array_map(function($x) { return '<tr>'.$x.'</tr>';}, $returnAndArgs))
+                                        );
+            }
+
+            $first = array_shift($methods);
+            $methods = '<tr >'.implode(PHP_EOL, 
+                               array_merge(array('<td style="background-color: '.$colorClass.'; vertical-align: top; border: border:black 1px solid;" rowspan="'.$classCount.'">'.$this->classes[$className].'</td>'.$first), 
+                                          array_map(function($x) { return '<tr>'.$x.'</tr>';}, $methods))
+                               ).'</tr>';
+
+        }
+
+        $html = '<table style="border:black 1px solid; border-collaspe: collapse">' . 
+                '<tr>
+                    <th>Class</th>
+                    <th>Method</th>
+                    <th>Parameter, Returntype, Property</th>
+                    <th>In code</th>
+                    <th>Suggestions</th>
+                </tr>' . 
+        
+                implode(PHP_EOL, $html) . '</table>';
+
+        $this->tmpName = "{$this->config->project_dir}/$name.html";
+
+        file_put_contents("{$this->tmpName}", $html);
+
+        return 'oui';
+    }
+
+    protected function toHtmlList(array $array): string {
+        if (empty($array)) {
+            return '&nbsp;';
+        }
+        return '<ul><li>' . implode("</li>\n<li>", $array) . '</li></ul>';
+    }
+    
+    private function getClassId(string $description) : int {
+        if (($c = array_search($description, $this->classes)) === false) {
+            $c = count($this->classes);
+            $this->classes[$c] = $description;
+        }
+
+        return $c;
+    }
+    
+    private function makeClassName(string $type, string $name, string $fullnspath) : string {
+        return $type.' '.$name.'<br /><div style="color: gray">'.$fullnspath.'</div>';
+    }
+}
+
+?>
