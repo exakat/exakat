35eec115cf9b5f9d75c4b3bcadda55798f725abc
Damien Seguy <damien.seguy@gmail.com>
2020-07-17 20:02:44 +0200

    Extended test

--- tests/analyzer/exp/Typehints/CouldBeVoid.01.php
+++ tests/analyzer/exp/Typehints/CouldBeVoid.01.php
@@ -1,11 +1,14 @@
 <?php
 
 $expected     = array('function barx2($g) { /**/ } ',
+                      'function barx3($g) { /**/ } ', 
+                      'function barx4($g) { /**/ } ',
                      );
 
 $expected_not = array('function barx($g) { /**/ } ',
                       'abstract function barx($g) { /**/ } ',
                       'function bari($g) { /**/ } ',
+                      'function barx5($g) { /**/ } ',
                      );
 
 ?>
--- tests/analyzer/source/Typehints/CouldBeVoid.01.php
+++ tests/analyzer/source/Typehints/CouldBeVoid.01.php
@@ -3,6 +3,9 @@
 abstract class x {
     abstract function barx($g);
     function barx2($g) {}
+    function barx3($g) { return; }
+    function barx4($g) { if (rand(1,2)) {return;} else { return; } }
+    function barx5($g) { if (rand(1,2)) {return;} else { return null; } }
 }
 
 interface i {
