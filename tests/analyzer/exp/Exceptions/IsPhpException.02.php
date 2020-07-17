4fa9b19d04794430ef4bf56004372a72e5877a48
Damien Seguy <damien.seguy@gmail.com>
2020-07-17 19:03:13 +0200

    New UT with namespaces and fallback

---
+++ tests/analyzer/exp/Exceptions/IsPhpException.02.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('\LengthException("D")',
+                      'Exception("A")',
+                     );
+
+$expected_not = array('NotException("B")',
+                      'DomainException("C")',
+                     );
+
+?>
