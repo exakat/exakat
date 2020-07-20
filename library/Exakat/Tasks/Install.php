bbda3afc7c066e665800918aff7bd7b5b8e4fe69
Damien Seguy <damien.seguy@gmail.com>
2020-07-20 16:53:27 +0200

    Upgraded calls to doctor with actual configuration.

--- library/Exakat/Tasks/Install.php
+++ library/Exakat/Tasks/Install.php
@@ -69,7 +69,7 @@ class Install extends Tasks {
             print "Neo4j for Tinkergraph installed\n";
         }
 
-        print shell_exec('php exakat.phar doctor');
+        print shell_exec(PHP_BINARY.' '.$this->config->executable.' doctor');
     }
 }
 
