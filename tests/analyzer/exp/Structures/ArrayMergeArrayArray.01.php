a965a79864d56e776b6df72c783b0101070692c6
Damien Seguy <damien.seguy@gmail.com>
2020-07-27 22:34:36 +0200

    New analysis : array_merge() must be initialisation with [[]]

---
+++ tests/analyzer/exp/Structures/ArrayMergeArrayArray.01.php
@@ -0,0 +1,11 @@
+<?php
+
+$expected     = array('array_merge($a)',
+                      'array_merge($b)',
+                      'array_merge($c)',
+                     );
+
+$expected_not = array('array_merge($d)',
+                     );
+
+?>
---
+++ tests/analyzer/source/Structures/ArrayMergeArrayArray.01.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = array();
+array_merge($a);
+
+$b = array(1);
+array_merge($b);
+
+$c = array(3 => 1);
+array_merge($c);
+
+$d = array(3 => array());
+array_merge($d);
+
+?>
