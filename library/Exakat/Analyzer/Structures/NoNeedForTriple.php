19b3ceb70cceeee86511b541de7c872d63bddd7f
Damien Seguy <damien.seguy@gmail.com>
2020-07-28 09:43:34 +0200

    New analysis : report useless === comparison

---
+++ library/Exakat/Analyzer/Structures/NoNeedForTriple.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Copyright 2012-2019 Damien Seguy – Exakat SAS <contact(at)exakat.io>
+ * This file is part of Exakat.
+ *
+ * Exakat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Exakat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Exakat.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://exakat.io/>.
+ *
+*/
+
+namespace Exakat\Analyzer\Structures;
+
+use Exakat\Analyzer\Analyzer;
+
+class NoNeedForTriple extends Analyzer {
+    public function analyze() : void {
+        // function foo() : bool {}
+        // if (foo() === true)
+        // todo : case for other types, 
+        // todo : which dependencies? 
+        $this->atomIs('Comparison')
+             ->codeIs(array('===', '!=='))
+             ->outIs(array('LEFT', 'RIGHT'))
+             ->savePropertyAs('label', 'type')
+             ->back('first')
+
+             ->outIs(array('LEFT', 'RIGHT'))
+             ->atomIs(self::CALLS)
+             ->inIs('DEFINITION')
+             ->compatibleWithType('type')
+
+             ->back('first');
+        $this->prepareQuery();
+    }
+}
+
+?>
--- library/Exakat/Query/DSL/CompatibleWithType.php
+++ library/Exakat/Query/DSL/CompatibleWithType.php
@@ -25,26 +25,23 @@ namespace Exakat\Query\DSL;
 
 use Exakat\Query\Query;
 
-class NotCompatibleWithType extends DSL {
-    public const DISALLOW_NULL = false;
-    public const ALLOW_NULL = true;
-
+class CompatibleWithType extends DSL {
     public function run(): Command {
         switch(func_num_args()) {
             case 2 : 
                 list($types, $withNull) = func_get_args();
-                $withNull = in_array($withNull, array(self::ALLOW_NULL, self::DISALLOW_NULL), STRICT_COMPARISON) ? $withNull : self::DISALLOW_NULL;
+                $withNull = in_array($withNull, array(NotCompatibleWithType::ALLOW_NULL, NotCompatibleWithType::DISALLOW_NULL), STRICT_COMPARISON) ? $withNull : NotCompatibleWithType::DISALLOW_NULL;
                 break;
 
             case 1: 
                 list($types) = func_get_args();
-                $withNull = self::DISALLOW_NULL;
+                $withNull = NotCompatibleWithType::DISALLOW_NULL;
 
             default:
                 assert(func_num_args() <= 2, 'Wrong number of argument for ' . __METHOD__ . '. 2 are expected, ' . func_num_args() . ' provided');
         }
         
-        if ($withNull === self::ALLOW_NULL) {
+        if ($withNull === NotCompatibleWithType::ALLOW_NULL) {
             $withNullGremlin = '.not(hasLabel("Null"))';
         } else {
             $withNullGremlin = '';
@@ -59,45 +56,45 @@ __.sideEffect{ typehints = []; }
   .fold() 
 )
 .filter{
-    results = true;
+    results = false;
     for(typehint in typehints) {
         switch(typehint) {
             case "\\\\string":
-                results = results && !($types in ["Magicconstant", "Heredoc", "String", "Concatenation", "Classconstant", "Shell"]);
+                results = results || ($types in ["Magicconstant", "Heredoc", "String", "Concatenation", "Classconstant", "Shell"]);
                 break;
                 
             case "\\\\int":
-                results = results && !($types in ["Integer", "Addition", "Multiplication", "Bitshift", "Logical", "Bitoperation", "Power", "Postplusplus", "Preplusplus", "Not"]);
+                results = results || ($types in ["Integer", "Addition", "Multiplication", "Bitshift", "Logical", "Bitoperation", "Power", "Postplusplus", "Preplusplus", "Not"]);
                 break;
     
             case "\\\\numeric":
-                results = results && !($types in ["Integer", "Addition", "Multiplication", "Bitshift", "Logical", "Bitoperation", "Power", "Float", "Postplusplus", "Preplusplus"]);
+                results = results || ($types in ["Integer", "Addition", "Multiplication", "Bitshift", "Logical", "Bitoperation", "Power", "Float", "Postplusplus", "Preplusplus"]);
                 break;
     
             case "\\\\float":
-                results = results && !($types in ["Float", "Addition", "Multiplication", "Bitshift", "Power"]);
+                results = results || ($types in ["Float", "Addition", "Multiplication", "Bitshift", "Power"]);
                 break;
     
             case "\\\\bool":
-                results = results && !($types in ["Boolean", "Logical", "Not", "Comparison"]);
+                results = results || ($types in ["Boolean", "Logical", "Not", "Comparison"]);
                 break;
     
             case "\\\\array":
-                results = results && !($types in ["Arrayliteral", "Addition"]);
+                results = results || ($types in ["Arrayliteral", "Addition"]);
                 break;
     
             case "\\\\mixed":
-                results = false; // anything is mixed, so this is always false
+                results = true; // anything is mixed, so this is always false
                 break;
 
             case "\\\\null":
-                results = results && !($types in ["Null"]);
+                results = results || !($types in ["Null"]);
                 break;
     
             case "\\\\void":
             case "\\\\resource":
             default: 
-                false;
+                true;
         }
     }
     
