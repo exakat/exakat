590612012888fd892516e73b341a1dc8948e47c9
Damien Seguy <damien.seguy@gmail.com>
2020-09-14 19:09:26 +0200

    Catch up UT

--- tests/analyzer/exp/Variables/WrittenOnlyVariable.05.php
+++ tests/analyzer/exp/Variables/WrittenOnlyVariable.05.php
@@ -3,6 +3,10 @@
 $expected     = array('$d',
                       '$b2',
                       '$b2',
+                      '$b4',
+                      '$b4',
+                      '$b3',
+                      '$b3',
                      );
 
 $expected_not = array('$a',
