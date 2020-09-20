52e453c958fa86430abf38565a00615a7af99c49
Famille Seguy <famille@NeoMacBookPro.local>
2020-09-20 07:37:35 +0200

    Default to stdout

--- library/Exakat/Reports/Sarif.php
+++ library/Exakat/Reports/Sarif.php
@@ -26,7 +26,7 @@ use Exakat\Reports\Helpers\Sarif as SarifJson;
 
 class Sarif extends Reports {
     const FILE_EXTENSION = 'sarif';
-    const FILE_FILENAME  = 'exakat';
+    const FILE_FILENAME  = self::STDOUT;
 
     public function _generate(array $analyzerList): string {
         $analysisResults = $this->dump->fetchAnalysers($analyzerList);
